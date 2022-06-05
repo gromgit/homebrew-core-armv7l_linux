bf5042effe2c6a53ecb8886842e8d73e68ce53fb
Adrian Ho <aho+rpi5@03s.net>
2022-06-05 23:38:53 +0800

    maxwell: no rebuilding required

---
+++ Formula/maxwell.rb
@@ -0,0 +1,44 @@
+class Maxwell < Formula
+  desc "Reads MySQL binlogs and writes row updates as JSON to Kafka"
+  homepage "https://maxwells-daemon.io/"
+  url "https://github.com/zendesk/maxwell/releases/download/v1.37.6/maxwell-1.37.6.tar.gz"
+  sha256 "73e61df7e5fbabc3fb3efdb1762fdb15dc4ef23ed574872a2a0bd9faa1d761af"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "759865d53afa7a2d8b47f3d2d546e1ee35f43e8d9681b879432cf96a2a0aa9d0"
+  end
+
+  depends_on "openjdk@11"
+
+  def install
+    libexec.install Dir["*"]
+
+    %w[maxwell maxwell-bootstrap].each do |f|
+      bin.install libexec/"bin/#{f}"
+    end
+
+    bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env("11.0"))
+  end
+
+  test do
+    log = testpath/"maxwell.log"
+
+    fork do
+      $stdout.reopen(log)
+      $stderr.reopen(log)
+      # Tell Maxwell to connect to a bogus host name so we don't actually connect to a local instance
+      # The '.invalid' TLD is reserved as never to be installed as a valid TLD.
+      exec "#{bin}/maxwell --host not.real.invalid"
+    end
+    sleep 15
+
+    # Validate that we actually got in to Maxwell far enough to attempt to connect.
+    assert_match "CommunicationsException: Communications link failure", log.read
+  end
+end
