4965d0d0efceb8aab2b8a75c7401aa2807581608
Adrian Ho <aho+rpi5@03s.net>
2022-06-05 23:34:58 +0800

    docker-completion: no rebuilding required

---
+++ Formula/docker-completion.rb
@@ -0,0 +1,31 @@
+class DockerCompletion < Formula
+  desc "Bash, Zsh and Fish completion for Docker"
+  homepage "https://www.docker.com/"
+  url "https://github.com/docker/cli.git",
+      tag:      "v20.10.16",
+      revision: "aa7e414fdcb23a66e8fabbef0a560ef1769eace5"
+  license "Apache-2.0"
+  head "https://github.com/docker/cli.git", branch: "master"
+
+  livecheck do
+    formula "docker"
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "96e060ddd2136c21e8367c8ae80936c91c2d5e78ccd41859c8d09001bcdfd067"
+  end
+
+  conflicts_with "docker",
+    because: "docker already includes these completion scripts"
+
+  def install
+    bash_completion.install "contrib/completion/bash/docker"
+    fish_completion.install "contrib/completion/fish/docker.fish"
+    zsh_completion.install "contrib/completion/zsh/_docker"
+  end
+
+  test do
+    assert_match "-F _docker",
+      shell_output("bash -c 'source #{bash_completion}/docker && complete -p docker'")
+  end
+end
