252a32aa5a49dc4c2ca9d1c2b09a6d154ff1b923
Adrian Ho <aho+rpi5@03s.net>
2022-06-05 23:41:10 +0800

    gromacs: no rebuilding required

---
+++ Formula/gromacs.rb
@@ -0,0 +1,79 @@
+class Gromacs < Formula
+  desc "Versatile package for molecular dynamics calculations"
+  homepage "https://www.gromacs.org/"
+  url "https://ftp.gromacs.org/pub/gromacs/gromacs-2022.1.tar.gz"
+  sha256 "85ddab5197d79524a702c4959c2c43be875e0fc471df3a35224939dce8512450"
+  license "LGPL-2.1-or-later"
+
+  livecheck do
+    url "https://ftp.gromacs.org/pub/gromacs/"
+    regex(/href=.*?gromacs[._-]v?(\d+(?:\.\d+)*)\.t/i)
+  end
+
+  bottle do
+    sha256 arm64_monterey: "0f30ec35bff8c7e16f8dd60dba21cdf6f73ac29635056b794bd6c7badc2da80d"
+    sha256 arm64_big_sur:  "879afa4d9628dba24dc75e6e42613135ab1b6eb10d2edd04e4f8d60d6bd5dfe3"
+    sha256 monterey:       "928c1be7fdd62cf74aaf045742e1119f421d8ee6f2bba68c9af153a3f75fa389"
+    sha256 big_sur:        "36bd932264524413566a638926ad451bdaa7c50129d77d100bde846ee6a88c77"
+    sha256 catalina:       "0b875e5c88e58ce68f2ab1ccaabdc2e7f5d5fee36d6699ac817da2b3af516514"
+    sha256 x86_64_linux:   "1616d5ef33fc92fd289dc3693ba437e287e9d02841b9f1686468fe125659e2e3"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "fftw"
+  depends_on "gcc" # for OpenMP
+  depends_on "openblas"
+
+  fails_with :clang
+  fails_with gcc: "5"
+  fails_with gcc: "6"
+
+  def install
+    # Non-executable GMXRC files should be installed in DATADIR
+    inreplace "scripts/CMakeLists.txt", "CMAKE_INSTALL_BINDIR",
+                                        "CMAKE_INSTALL_DATADIR"
+
+    # Avoid superenv shim reference
+    gcc = Formula["gcc"]
+    cc = gcc.opt_bin/"gcc-#{gcc.any_installed_version.major}"
+    cxx = gcc.opt_bin/"g++-#{gcc.any_installed_version.major}"
+    inreplace "src/gromacs/gromacs-hints.in.cmake" do |s|
+      s.gsub! "@CMAKE_LINKER@", "/usr/bin/ld"
+      s.gsub! "@CMAKE_C_COMPILER@", cc
+      s.gsub! "@CMAKE_CXX_COMPILER@", cxx
+    end
+
+    inreplace "src/buildinfo.h.cmakein" do |s|
+      s.gsub! "@BUILD_C_COMPILER@", cc
+      s.gsub! "@BUILD_CXX_COMPILER@", cxx
+    end
+
+    inreplace "src/gromacs/gromacs-config.cmake.cmakein", "@GROMACS_CXX_COMPILER@", cxx
+
+    args = %W[
+      -DGROMACS_CXX_COMPILER=#{cxx}
+      -DGMX_VERSION_STRING_OF_FORK=#{tap.user}
+    ]
+    # Force SSE2/SSE4.1 for compatibility when building Intel bottles
+    args << "-DGMX_SIMD=#{MacOS.version.requires_sse41? ? "SSE4.1" : "SSE2"}" if Hardware::CPU.intel? && build.bottle?
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args, *args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+
+    bash_completion.install "build/scripts/GMXRC" => "gromacs-completion.bash"
+    bash_completion.install bin/"gmx-completion-gmx.bash" => "gmx-completion-gmx.bash"
+    bash_completion.install bin/"gmx-completion.bash" => "gmx-completion.bash"
+    zsh_completion.install "build/scripts/GMXRC.zsh" => "_gromacs"
+  end
+
+  def caveats
+    <<~EOS
+      GMXRC and other scripts installed to:
+        #{HOMEBREW_PREFIX}/share/gromacs
+    EOS
+  end
+
+  test do
+    system "#{bin}/gmx", "help"
+  end
+end
