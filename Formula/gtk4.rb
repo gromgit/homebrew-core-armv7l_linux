266017974887913a20be76b8e465569df3718da3
Adrian Ho <aho+rpi5@03s.net>
2022-06-05 23:41:55 +0800

    gtk4:

---
+++ Formula/gtk4.rb
@@ -0,0 +1,98 @@
+class Gtk4 < Formula
+  desc "Toolkit for creating graphical user interfaces"
+  homepage "https://gtk.org/"
+  url "https://download.gnome.org/sources/gtk/4.6/gtk-4.6.4.tar.xz"
+  sha256 "a79a2bbcc7931b703fc4fa1fcbe1b906e229a1522d854d522801766a79bcac9f"
+  license "LGPL-2.1-or-later"
+
+  livecheck do
+    url :stable
+    regex(/gtk[._-](4\.([0-8]\d*?)?[02468](?:\.\d+)*?)\.t/i)
+  end
+
+  bottle do
+    sha256 arm64_monterey: "f9cd78f0c32bce330039dd4d684cd1412751bcef8af57f1756d53b256da19632"
+    sha256 arm64_big_sur:  "ec4fa919bdb969d463e40b79443a09cc81aac181454338275130024b07e7ee00"
+    sha256 monterey:       "12d2689cadc98a18f772deee9061318fd7705447e8006c2b6651ac593cc166ce"
+    sha256 big_sur:        "2bcede3886fd9bcabab2ea31c2181d7881c5361dfb288f5772e01c3a3036f4da"
+    sha256 catalina:       "e26f1805d43375a0505aa0f6ecfe64194f6ede184217348a5f1ad81564d208a7"
+    sha256 x86_64_linux:   "0fbdd52f365c118fba8a4e5a3676b5916a201a0208e6dd50fc79682f97cd3b54"
+  end
+
+  depends_on "docbook" => :build
+  depends_on "docbook-xsl" => :build
+  depends_on "docutils" => :build
+  depends_on "gobject-introspection" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => [:build, :test]
+  depends_on "sassc" => :build
+  depends_on "gdk-pixbuf"
+  depends_on "glib"
+  depends_on "graphene"
+  depends_on "hicolor-icon-theme"
+  depends_on "libepoxy"
+  depends_on "pango"
+
+  uses_from_macos "libxslt" => :build # for xsltproc
+  uses_from_macos "cups"
+
+  on_linux do
+    depends_on "libxkbcommon"
+    depends_on "libxcursor"
+  end
+
+  def install
+    args = std_meson_args + %w[
+      -Dgtk_doc=false
+      -Dman-pages=true
+      -Dintrospection=enabled
+      -Dbuild-examples=false
+      -Dbuild-tests=false
+      -Dmedia-gstreamer=disabled
+    ]
+
+    if OS.mac?
+      args << "-Dx11-backend=false"
+      args << "-Dmacos-backend=true"
+      args << "-Dprint-cups=disabled" if MacOS.version <= :mojave
+    end
+
+    # ensure that we don't run the meson post install script
+    ENV["DESTDIR"] = "/"
+
+    # Find our docbook catalog
+    ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
+
+    # Disable asserts and cast checks explicitly
+    ENV.append "CPPFLAGS", "-DG_DISABLE_ASSERT -DG_DISABLE_CAST_CHECKS"
+
+    mkdir "build" do
+      system "meson", *args, ".."
+      system "ninja", "-v"
+      system "ninja", "install", "-v"
+    end
+  end
+
+  def post_install
+    system "#{Formula["glib"].opt_bin}/glib-compile-schemas", "#{HOMEBREW_PREFIX}/share/glib-2.0/schemas"
+    system bin/"gtk4-update-icon-cache", "-f", "-t", "#{HOMEBREW_PREFIX}/share/icons/hicolor"
+    system "#{Formula["glib"].opt_bin}/gio-querymodules", "#{HOMEBREW_PREFIX}/lib/gtk-4.0/4.0.0/printbackends"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <gtk/gtk.h>
+
+      int main(int argc, char *argv[]) {
+        gtk_disable_setlocale();
+        return 0;
+      }
+    EOS
+    flags = shell_output("#{Formula["pkg-config"].opt_bin}/pkg-config --cflags --libs gtk4").strip.split
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
+    # include a version check for the pkg-config files
+    assert_match version.to_s, shell_output("cat #{lib}/pkgconfig/gtk4.pc").strip
+  end
+end
