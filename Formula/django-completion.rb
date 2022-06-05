6ac526898e14775019e8d0825ebc430d312b3f82
Adrian Ho <aho+rpi5@03s.net>
2022-06-05 23:34:46 +0800

    django-completion: no rebuilding required

---
+++ Formula/django-completion.rb
@@ -0,0 +1,26 @@
+class DjangoCompletion < Formula
+  desc "Bash completion for Django"
+  homepage "https://www.djangoproject.com/"
+  url "https://github.com/django/django/archive/4.0.5.tar.gz"
+  sha256 "e57159ff9c21be795947ad1892b81b983ed7b21be6c5841d0ccc3b8ac1e9824b"
+  license "BSD-3-Clause"
+  head "https://github.com/django/django.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "b597e6b0347ac17ab4b7ae1bf73f433324f86585d8f435c41511762fe02f81ee"
+  end
+
+  def install
+    bash_completion.install "extras/django_bash_completion" => "django"
+  end
+
+  test do
+    assert_match "-F _django_completion",
+      shell_output("bash -c 'source #{bash_completion}/django && complete -p django-admin'")
+  end
+end
