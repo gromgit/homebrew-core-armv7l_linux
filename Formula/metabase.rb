01f68044d2ed5720cfedb24d01986b27049f4ffb
Adrian Ho <aho+rpi5@03s.net>
2022-06-05 23:40:38 +0800

    metabase: no rebuilding required

---
+++ Formula/metabase.rb
@@ -0,0 +1,50 @@
+class Metabase < Formula
+  desc "Business intelligence report server"
+  homepage "https://www.metabase.com/"
+  url "https://downloads.metabase.com/v0.43.2/metabase.jar"
+  sha256 "ec14257991817fdc6dc6891c170dcb12f727781ad381e706c870aa08d469d71c"
+  license "AGPL-3.0-only"
+
+  livecheck do
+    url "https://www.metabase.com/start/oss/jar.html"
+    regex(%r{href=.*?/v?(\d+(?:\.\d+)+)/metabase\.jar}i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "aeeae999b587d792fbbffec3d6d2a3ecae5fc4c2459609d40ba79c31451db91f"
+  end
+
+  head do
+    url "https://github.com/metabase/metabase.git"
+
+    depends_on "leiningen" => :build
+    depends_on "node" => :build
+    depends_on "yarn" => :build
+  end
+
+  depends_on "openjdk"
+
+  def install
+    if build.head?
+      system "./bin/build"
+      libexec.install "target/uberjar/metabase.jar"
+    else
+      libexec.install "metabase.jar"
+    end
+
+    bin.write_jar_script libexec/"metabase.jar", "metabase"
+  end
+
+  plist_options startup: true
+  service do
+    run opt_bin/"metabase"
+    keep_alive true
+    working_dir var/"metabase"
+    log_path var/"metabase/server.log"
+    error_log_path "/dev/null"
+  end
+
+  test do
+    system bin/"metabase", "migrate", "up"
+  end
+end
