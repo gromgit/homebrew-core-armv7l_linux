bf6e2fefaefaf717f667d35376cb03ebd98702a0
Adrian Ho <aho+rpi5@03s.net>
2022-06-05 23:39:59 +0800

    prettier: no rebuilding required

---
+++ Formula/prettier.rb
@@ -0,0 +1,27 @@
+require "language/node"
+
+class Prettier < Formula
+  desc "Code formatter for JavaScript, CSS, JSON, GraphQL, Markdown, YAML"
+  homepage "https://prettier.io/"
+  url "https://registry.npmjs.org/prettier/-/prettier-2.6.2.tgz"
+  sha256 "d10800fa3dc4e389e1f22187d5019d444b915f6239dddf5e1973186ea784e9df"
+  license "MIT"
+  head "https://github.com/prettier/prettier.git", branch: "main"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "4274d166597aabf2971028b29c028e30829b85a08f1c8b28e1405b8fccc54b1e"
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    (testpath/"test.js").write("const arr = [1,2];")
+    output = shell_output("#{bin}/prettier test.js")
+    assert_equal "const arr = [1, 2];", output.chomp
+  end
+end
