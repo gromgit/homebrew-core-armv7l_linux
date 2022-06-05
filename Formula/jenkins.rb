45b763ae350e9d74e5454e455cfacccf1c1a2324
Adrian Ho <aho+rpi5@03s.net>
2022-06-05 23:34:43 +0800

    jenkins: no rebuilding required

---
+++ Formula/jenkins.rb
@@ -0,0 +1,64 @@
+class Jenkins < Formula
+  desc "Extendable open source continuous integration server"
+  homepage "https://www.jenkins.io/"
+  url "https://get.jenkins.io/war/2.350/jenkins.war"
+  sha256 "ae7e255b0ce27a5ed01a7371321c1c4c24e8c52984c9b44f2a84d868c729be4e"
+  license "MIT"
+
+  livecheck do
+    url "https://www.jenkins.io/download/"
+    regex(%r{href=.*?/war/v?(\d+(?:\.\d+)+)/jenkins\.war}i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "15c47a0e87b0b4bb51e221019ed1c5349662ad513f4a3062ffe960972fc23dde"
+  end
+
+  head do
+    url "https://github.com/jenkinsci/jenkins.git"
+    depends_on "maven" => :build
+  end
+
+  depends_on "openjdk@11"
+
+  def install
+    if build.head?
+      system "mvn", "clean", "install", "-pl", "war", "-am", "-DskipTests"
+    else
+      system "#{Formula["openjdk@11"].opt_bin}/jar", "xvf", "jenkins.war"
+    end
+    libexec.install Dir["**/jenkins.war", "**/cli-#{version}.jar"]
+    bin.write_jar_script libexec/"jenkins.war", "jenkins", java_version: "11"
+    bin.write_jar_script libexec/"cli-#{version}.jar", "jenkins-cli", java_version: "11"
+
+    (var/"log/jenkins").mkpath
+  end
+
+  def caveats
+    <<~EOS
+      Note: When using launchctl the port will be 8080.
+    EOS
+  end
+
+  service do
+    run [Formula["openjdk@11"].opt_bin/"java", "-Dmail.smtp.starttls.enable=true", "-jar", opt_libexec/"jenkins.war",
+         "--httpListenAddress=127.0.0.1", "--httpPort=8080"]
+    keep_alive true
+    log_path var/"log/jenkins/output.log"
+    error_log_path var/"log/jenkins/error.log"
+  end
+
+  test do
+    ENV["JENKINS_HOME"] = testpath
+    ENV.prepend "_JAVA_OPTIONS", "-Djava.io.tmpdir=#{testpath}"
+
+    port = free_port
+    fork do
+      exec "#{bin}/jenkins --httpPort=#{port}"
+    end
+    sleep 60
+
+    output = shell_output("curl localhost:#{port}/")
+    assert_match(/Welcome to Jenkins!|Unlock Jenkins|Authentication required/, output)
+  end
+end
