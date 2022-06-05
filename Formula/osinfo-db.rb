d479cbdc6552ccbd05973617e1b308a120e10118
Adrian Ho <aho+rpi5@03s.net>
2022-06-05 23:35:02 +0800

    osinfo-db: no rebuilding required

---
+++ Formula/osinfo-db.rb
@@ -0,0 +1,26 @@
+class OsinfoDb < Formula
+  desc "Osinfo database of operating systems for virtualization provisioning tools"
+  homepage "https://libosinfo.org/"
+  url "https://releases.pagure.org/libosinfo/osinfo-db-20220516.tar.xz"
+  sha256 "d60f69d8afc9dcc53d76a2fb68d54c26d1fdc3a822b95c1801de58c3c66cda6d"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url "https://releases.pagure.org/libosinfo/?C=M&O=D"
+    regex(/href=.*?osinfo-db[._-]v?(\d+(?:\.\d+)*)\.t/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "a920c368b1a69bead8b048b9240738b754f3e50f5c2672ac357ad16fa1b78a30"
+  end
+
+  depends_on "osinfo-db-tools" => [:build, :test]
+
+  def install
+    system "osinfo-db-import", "--dir=#{share}/osinfo", cached_download
+  end
+
+  test do
+    system "osinfo-db-validate", "--system"
+  end
+end
