53dece1d649e1d457ed42168a372a7a924557b10
Adrian Ho <aho+rpi5@03s.net>
2022-06-05 23:35:00 +0800

    geoserver: no rebuilding required

---
+++ Formula/geoserver.rb
@@ -0,0 +1,43 @@
+class Geoserver < Formula
+  desc "Java server to share and edit geospatial data"
+  homepage "https://geoserver.org/"
+  url "https://downloads.sourceforge.net/project/geoserver/GeoServer/2.21.0/geoserver-2.21.0-bin.zip"
+  sha256 "3046735b35da964ba60835cb49cc509cf22f93e593737cb8680442d15a692079"
+
+  # GeoServer releases contain a large number of files for each version, so the
+  # SourceForge RSS feed may only contain the most recent version (which may
+  # have a different major/minor version than the latest stable). We check the
+  # "GeoServer" directory page instead, since this is reliable.
+  livecheck do
+    url "https://sourceforge.net/projects/geoserver/files/GeoServer/"
+    strategy :page_match
+    regex(%r{href=(?:["']|.*?GeoServer/)?v?(\d+(?:\.\d+)+)/?["' >]}i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "e44ff92908e2381d1bf3b15738adeefa3ec541f271c5067ccbbfaf968c5a1b1f"
+  end
+
+  def install
+    libexec.install Dir["*"]
+    (bin/"geoserver").write <<~EOS
+      #!/bin/sh
+      if [ -z "$1" ]; then
+        echo "Usage: $ geoserver path/to/data/dir"
+      else
+        cd "#{libexec}" && java -DGEOSERVER_DATA_DIR=$1 -jar start.jar
+      fi
+    EOS
+  end
+
+  def caveats
+    <<~EOS
+      To start geoserver:
+        geoserver path/to/data/dir
+    EOS
+  end
+
+  test do
+    assert_match "geoserver path", shell_output("#{bin}/geoserver")
+  end
+end
