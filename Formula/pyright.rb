56141b7a8029a0cc0a6570c0269116e0eaf117e8
Adrian Ho <aho+rpi5@03s.net>
2022-06-05 23:37:46 +0800

    pyright: no rebuilding required

---
+++ Formula/pyright.rb
@@ -0,0 +1,30 @@
+require "language/node"
+
+class Pyright < Formula
+  desc "Static type checker for Python"
+  homepage "https://github.com/microsoft/pyright"
+  url "https://registry.npmjs.org/pyright/-/pyright-1.1.252.tgz"
+  sha256 "91db8e9375023f44e530494ae698c808b7e3e322fdf37a7adc6d9c1a24280077"
+  license "MIT"
+  head "https://github.com/microsoft/pyright.git", branch: "main"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "b3b4d1ec1b52d5beeeb15366efa16ea0039c3670c98100499d33ca1282032b0b"
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    (testpath/"broken.py").write <<~EOS
+      def wrong_types(a: int, b: int) -> str:
+          return a + b
+    EOS
+    output = pipe_output("#{bin}/pyright broken.py 2>&1")
+    assert_match 'error: Expression of type "int" cannot be assigned to return type "str"', output
+  end
+end
