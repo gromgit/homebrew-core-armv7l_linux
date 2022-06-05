b94bceb7864880d90bb56fb5b919e91404143122
Adrian Ho <aho+rpi5@03s.net>
2022-06-05 23:38:53 +0800

    parallel: no rebuilding required

---
+++ Formula/parallel.rb
@@ -0,0 +1,48 @@
+class Parallel < Formula
+  desc "Shell command parallelization utility"
+  homepage "https://savannah.gnu.org/projects/parallel/"
+  url "https://ftp.gnu.org/gnu/parallel/parallel-20220522.tar.bz2"
+  mirror "https://ftpmirror.gnu.org/parallel/parallel-20220522.tar.bz2"
+  sha256 "bb6395f8d964e68f3bdb26a764d3c48b69bc5b759a92ac3ab2bd1895c7fa8c1f"
+  license "GPL-3.0-or-later"
+  version_scheme 1
+  head "https://git.savannah.gnu.org/git/parallel.git", branch: "master"
+
+  livecheck do
+    url :homepage
+    regex(/GNU Parallel v?(\d{6,8}).*? released/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "3d24270fd722dfa991eb0211aef7f64b585ab72c37b5ee0c0fe8c5347876b48e"
+  end
+
+  conflicts_with "moreutils", because: "both install a `parallel` executable"
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+
+    inreplace_files = [
+      bin/"parallel",
+      doc/"parallel.texi",
+      doc/"parallel_design.texi",
+      man1/"parallel.1",
+      man7/"parallel_design.7",
+    ]
+    inreplace inreplace_files, "/usr/local", HOMEBREW_PREFIX
+  end
+
+  def caveats
+    <<~EOS
+      To use the --csv option, the Perl Text::CSV module has to be installed.
+      You can install it via:
+        perl -MCPAN -e'install Text::CSV'
+    EOS
+  end
+
+  test do
+    assert_equal "test\ntest\n",
+                 shell_output("#{bin}/parallel --will-cite echo ::: test test")
+  end
+end
