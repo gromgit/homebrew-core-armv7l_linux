837019aefc3eb816be160b8e838e44dae25b529c
Adrian Ho <aho+rpi5@03s.net>
2022-06-05 23:35:22 +0800

    asciidoctorj: no rebuilding required

---
+++ Formula/asciidoctorj.rb
@@ -0,0 +1,53 @@
+class Asciidoctorj < Formula
+  desc "Java wrapper and bindings for Asciidoctor"
+  homepage "https://github.com/asciidoctor/asciidoctorj"
+  url "https://search.maven.org/remotecontent?filepath=org/asciidoctor/asciidoctorj/2.5.4/asciidoctorj-2.5.4-bin.zip"
+  sha256 "ee2a050dcfae7e03b365a6825b252fe62a28c71094c1745bf5ce1903633683dd"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://search.maven.org/remotecontent?filepath=org/asciidoctor/asciidoctorj/maven-metadata.xml"
+    regex(%r{<version>v?(\d+(?:\.\d+)+)</version>}i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "dcaf780a98eb9ef83be5b67a35487fb1f990eef036959004c635fd93fc6c2e7f"
+  end
+
+  depends_on "openjdk"
+
+  def install
+    rm_rf Dir["bin/*.bat"] # Remove Windows files.
+    libexec.install Dir["*"]
+    (bin/"asciidoctorj").write_env_script libexec/"bin/asciidoctorj", JAVA_HOME: Formula["openjdk"].opt_prefix
+  end
+
+  test do
+    (testpath/"test.adoc").write <<~EOS
+      = AsciiDoc is Writing Zen
+      Random J. Author <rjauthor@example.com>
+      :icons: font
+
+      Hello, World!
+
+      == Syntax Highlighting
+
+      Python source.
+
+      [source, python]
+      ----
+      import something
+      ----
+
+      List
+
+      - one
+      - two
+      - three
+    EOS
+    system bin/"asciidoctorj", "-b", "html5", "-o", "test.html", "test.adoc"
+    assert_match "<h1>AsciiDoc is Writing Zen</h1>", File.read("test.html")
+    system bin/"asciidoctorj", "-r", "asciidoctor-pdf", "-b", "pdf", "-o", "test.pdf", "test.adoc"
+    assert_match "/Title (AsciiDoc is Writing Zen)", File.read("test.pdf", mode: "rb")
+  end
+end
