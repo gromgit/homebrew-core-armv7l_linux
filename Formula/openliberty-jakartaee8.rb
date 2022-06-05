726bc6ed14f29e5e3a27e656d0499f8d08f7242f
Adrian Ho <aho+rpi5@03s.net>
2022-06-05 23:37:35 +0800

    openliberty-jakartaee8: no rebuilding required

---
+++ Formula/openliberty-jakartaee8.rb
@@ -0,0 +1,48 @@
+class OpenlibertyJakartaee8 < Formula
+  desc "Lightweight open framework for Java (Jakarta EE 8)"
+  homepage "https://openliberty.io"
+  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/22.0.0.5/openliberty-javaee8-22.0.0.5.zip"
+  sha256 "644b48c3de536d88b110c8d8d459b07452a6fd4796f2b950fdc937b3d3a117c3"
+  license "EPL-1.0"
+
+  livecheck do
+    url "https://openliberty.io/api/builds/data"
+    regex(/openliberty[._-]v?(\d+(?:\.\d+)+)\.zip/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "81d220657558860c7385115bce2efd192c8c0364349b48b375e98fe0bbbb6eb7"
+  end
+
+  depends_on "openjdk"
+
+  def install
+    rm_rf Dir["bin/**/*.bat"]
+
+    prefix.install_metafiles
+    libexec.install Dir["*"]
+    (bin/"openliberty-jakartaee8").write_env_script "#{libexec}/bin/server",
+                                                    Language::Java.overridable_java_home_env
+  end
+
+  def caveats
+    <<~EOS
+      The home of Open Liberty Jakarta EE 8 is:
+        #{opt_libexec}
+    EOS
+  end
+
+  test do
+    ENV["WLP_USER_DIR"] = testpath
+
+    begin
+      system bin/"openliberty-jakartaee8", "start"
+      assert_predicate testpath/"servers/.pid/defaultServer.pid", :exist?
+    ensure
+      system bin/"openliberty-jakartaee8", "stop"
+    end
+
+    refute_predicate testpath/"servers/.pid/defaultServer.pid", :exist?
+    assert_match "<feature>javaee-8.0</feature>", (testpath/"servers/defaultServer/server.xml").read
+  end
+end
