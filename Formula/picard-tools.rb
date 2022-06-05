33a5c5fb9b704b31786bc95551fc7febcb4c984a
Adrian Ho <aho+rpi5@03s.net>
2022-06-05 23:37:24 +0800

    picard-tools: no rebuilding required

---
+++ Formula/picard-tools.rb
@@ -0,0 +1,36 @@
+class PicardTools < Formula
+  desc "Tools for manipulating HTS data and formats"
+  homepage "https://broadinstitute.github.io/picard/"
+  url "https://github.com/broadinstitute/picard/releases/download/2.27.2/picard.jar"
+  sha256 "f7b623a93a56efd54f8d93220650ec31f9159c7ec20e431be834cfb221c61a2e"
+  license "MIT"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "91603420700d2a3f9f9dd474dff2e9c593e15f6147efa7aa6acf40bd8200a16b"
+  end
+
+  depends_on "openjdk"
+
+  def install
+    libexec.install "picard.jar"
+    (bin/"picard").write <<~EOS
+      #!/bin/bash
+      exec "#{Formula["openjdk"].opt_bin}/java" $JAVA_OPTS -jar "#{libexec}/picard.jar" "$@"
+    EOS
+  end
+
+  test do
+    (testpath/"test.fasta").write <<~EOS
+      >U00096.2:1-70
+      AGCTTTTCATTCTGACTGCAACGGGCAATATGTCT
+      CTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC
+    EOS
+    cmd = "#{bin}/picard NormalizeFasta I=test.fasta O=/dev/stdout"
+    assert_match "TCTCTG", shell_output(cmd)
+  end
+end
