6f0ef0d24be8dbf225ecbdb90d512b75b0038979
Adrian Ho <aho+rpi5@03s.net>
2022-06-05 23:41:00 +0800

    kotlin:

---
+++ Formula/kotlin.rb
@@ -0,0 +1,39 @@
+class Kotlin < Formula
+  desc "Statically typed programming language for the JVM"
+  homepage "https://kotlinlang.org/"
+  url "https://github.com/JetBrains/kotlin/releases/download/v1.6.21/kotlin-compiler-1.6.21.zip"
+  sha256 "632166fed89f3f430482f5aa07f2e20b923b72ef688c8f5a7df3aa1502c6d8ba"
+  license "Apache-2.0"
+
+  # This repository has thousands of development tags, so the `GithubLatest`
+  # strategy is used to minimize data transfer in this extreme case.
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "4c53c499a664e15a04789fcb915d1d01b7548d64427086fce3f625621977a558"
+  end
+
+  depends_on "openjdk"
+
+  def install
+    libexec.install "bin", "build.txt", "lib"
+    rm Dir[libexec/"bin/*.bat"]
+    bin.install Dir[libexec/"bin/*"]
+    bin.env_script_all_files libexec/"bin", Language::Java.overridable_java_home_env
+    prefix.install "license"
+  end
+
+  test do
+    (testpath/"test.kt").write <<~EOS
+      fun main(args: Array<String>) {
+        println("Hello World!")
+      }
+    EOS
+    system bin/"kotlinc", "test.kt", "-include-runtime", "-d", "test.jar"
+    system bin/"kotlinc-js", "test.kt", "-output", "test.js"
+    system bin/"kotlinc-jvm", "test.kt", "-include-runtime", "-d", "test.jar"
+  end
+end
