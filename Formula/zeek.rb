dc536aa2307952e5614ef419047388924f2292e7
Adrian Ho <aho+rpi5@03s.net>
2022-06-05 23:36:25 +0800

    zeek:

---
+++ Formula/zeek.rb
@@ -0,0 +1,80 @@
+class Zeek < Formula
+  desc "Network security monitor"
+  homepage "https://www.zeek.org"
+  url "https://github.com/zeek/zeek.git",
+      tag:      "v4.2.1",
+      revision: "45491fc4e19ef15428db3ccdc5e1c55130f79131"
+  license "BSD-3-Clause"
+  revision 1
+  head "https://github.com/zeek/zeek.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    sha256 arm64_monterey: "fbd937cfa11d2dca393b778da7e99cfaba86cb84ef3e4d8fb2436bbef0a78af1"
+    sha256 arm64_big_sur:  "0c6a41eea0a22834978e2a6621c74009f33095ea3916d144ce4cbe9b303d8ef7"
+    sha256 monterey:       "2711c827e76e9eae9e5c0f1fbdf05a19e7d42dae4f9cf4b6f6d14e03237a6a3a"
+    sha256 big_sur:        "eb4176e1e4c479b08c4366bf4dbed88f95fd8504de584a230fcb6ba0ad6aba2a"
+    sha256 catalina:       "b191c5915200f6e6abe83e4dd5d6d552a92e1f5ae85c9018ab55dc12ebd8684a"
+    sha256 x86_64_linux:   "7c4b98ce61cc2c407c21059ae2918fdf21475c3dd4bc57caceeb87d44fbecdea"
+  end
+
+  depends_on "bison" => :build
+  depends_on "cmake" => :build
+  depends_on "swig" => :build
+  depends_on "caf"
+  depends_on "geoip"
+  depends_on "libmaxminddb"
+  depends_on macos: :mojave
+  depends_on "openssl@1.1"
+  depends_on "python@3.10"
+
+  uses_from_macos "flex"
+  uses_from_macos "libpcap"
+  uses_from_macos "libxcrypt"
+  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "gcc" # For C++17
+  end
+
+  fails_with gcc: "5"
+
+  def install
+    # Remove SDK paths from zeek-config. This breaks usage with other SDKs.
+    # https://github.com/corelight/zeek-community-id/issues/15
+    inreplace "zeek-config.in" do |s|
+      s.gsub! "@ZEEK_CONFIG_PCAP_INCLUDE_DIR@", ""
+      s.gsub! "@ZEEK_CONFIG_ZLIB_INCLUDE_DIR@", ""
+    end
+
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args,
+                      "-DBROKER_DISABLE_TESTS=on",
+                      "-DBUILD_SHARED_LIBS=on",
+                      "-DINSTALL_AUX_TOOLS=on",
+                      "-DINSTALL_ZEEKCTL=on",
+                      "-DUSE_GEOIP=on",
+                      "-DCAF_ROOT=#{Formula["caf"].opt_prefix}",
+                      "-DOPENSSL_ROOT_DIR=#{Formula["openssl@1.1"].opt_prefix}",
+                      "-DZEEK_ETC_INSTALL_DIR=#{etc}",
+                      "-DZEEK_LOCAL_STATE_DIR=#{var}"
+      system "make", "install"
+    end
+  end
+
+  test do
+    assert_match "version #{version}", shell_output("#{bin}/zeek --version")
+    assert_match "ARP packet analyzer", shell_output("#{bin}/zeek --print-plugins")
+    system bin/"zeek", "-C", "-r", test_fixtures("test.pcap")
+    assert_predicate testpath/"conn.log", :exist?
+    refute_predicate testpath/"conn.log", :empty?
+    assert_predicate testpath/"http.log", :exist?
+    refute_predicate testpath/"http.log", :empty?
+    # For bottling MacOS SDK paths must not be part of the public include directories, see zeek/zeek#1468.
+    refute_includes shell_output("#{bin}/zeek-config --include_dir").chomp, "MacOSX"
+  end
+end
