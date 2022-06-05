ca5e8fc3f6e14fc3002eabd4628e928e22dd9fc9
Adrian Ho <aho+rpi5@03s.net>
2022-06-05 23:39:34 +0800

    node-build: no rebuilding required

---
+++ Formula/node-build.rb
@@ -0,0 +1,30 @@
+class NodeBuild < Formula
+  desc "Install NodeJS versions"
+  homepage "https://github.com/nodenv/node-build"
+  url "https://github.com/nodenv/node-build/archive/v4.9.82.tar.gz"
+  sha256 "d242d661bda6f1ffb2fc6e7e4af80481499f693cdfd2f1e13a66437e7734ba70"
+  license "MIT"
+  head "https://github.com/nodenv/node-build.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "230efd9662f10de0928ea1320820f287dc068037b3280055792f697ac37a5886"
+  end
+
+  depends_on "autoconf"
+  depends_on "openssl@1.1"
+  depends_on "pkg-config"
+
+  def install
+    ENV["PREFIX"] = prefix
+    system "./install.sh"
+  end
+
+  test do
+    system "#{bin}/node-build", "--definitions"
+  end
+end
