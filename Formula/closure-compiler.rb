1c0c742fbf55909f601e38169f680df52f2fb242
Adrian Ho <aho+rpi5@03s.net>
2022-06-05 23:37:30 +0800

    closure-compiler: no rebuilding required

---
+++ Formula/closure-compiler.rb
@@ -0,0 +1,36 @@
+class ClosureCompiler < Formula
+  desc "JavaScript optimizing compiler"
+  homepage "https://developers.google.com/closure/compiler"
+  url "https://search.maven.org/remotecontent?filepath=com/google/javascript/closure-compiler/v20220601/closure-compiler-v20220601.jar"
+  sha256 "4b4eb54d072f2d13f5b70fa2d1719ca54d5ca40ea68484a70a300557f63e94b8"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://search.maven.org/remotecontent?filepath=com/google/javascript/closure-compiler/"
+    regex(/href=.*?v?(\d{8})/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "2240def7f5db732558af42483c3bea90e5f2e3ea17d415685097b5e475b30c2d"
+  end
+
+  depends_on "openjdk"
+
+  def install
+    libexec.install Dir["*"]
+    bin.write_jar_script libexec/"closure-compiler-v#{version}.jar", "closure-compiler"
+  end
+
+  test do
+    (testpath/"test.js").write <<~EOS
+      (function(){
+        var t = true;
+        return t;
+      })();
+    EOS
+    system bin/"closure-compiler",
+           "--js", testpath/"test.js",
+           "--js_output_file", testpath/"out.js"
+    assert_equal (testpath/"out.js").read.chomp, "(function(){return!0})();"
+  end
+end
