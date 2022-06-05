9dcbe89e446e07fe8ce6246e0e91f8671743a687
Adrian Ho <aho+rpi5@03s.net>
2022-06-05 23:36:47 +0800

    openliberty-webprofile8: no rebuilding required

---
+++ Formula/openliberty-webprofile8.rb
@@ -0,0 +1,48 @@
+class OpenlibertyWebprofile8 < Formula
+  desc "Lightweight open framework for Java (Jakarta EE Web Profile 8)"
+  homepage "https://openliberty.io"
+  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/22.0.0.5/openliberty-webProfile8-22.0.0.5.zip"
+  sha256 "8f6716343162d8657917b4d188e0affd3c0c169c6c7a2cc771b14bad7353f266"
+  license "EPL-1.0"
+
+  livecheck do
+    url "https://openliberty.io/api/builds/data"
+    regex(/openliberty[._-]v?(\d+(?:\.\d+)+)\.zip/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "0b44eb487abab46a7bfff0877d2d38dec62560c62faf69d0c2adeb784883d774"
+  end
+
+  depends_on "openjdk"
+
+  def install
+    rm_rf Dir["bin/**/*.bat"]
+
+    prefix.install_metafiles
+    libexec.install Dir["*"]
+    (bin/"openliberty-webprofile8").write_env_script "#{libexec}/bin/server",
+                                                     Language::Java.overridable_java_home_env
+  end
+
+  def caveats
+    <<~EOS
+      The home of Open Liberty Jakarta EE Web Profile 8 is:
+        #{opt_libexec}
+    EOS
+  end
+
+  test do
+    ENV["WLP_USER_DIR"] = testpath
+
+    begin
+      system bin/"openliberty-webprofile8", "start"
+      assert_predicate testpath/"servers/.pid/defaultServer.pid", :exist?
+    ensure
+      system bin/"openliberty-webprofile8", "stop"
+    end
+
+    refute_predicate testpath/"servers/.pid/defaultServer.pid", :exist?
+    assert_match "<feature>webProfile-8.0</feature>", (testpath/"servers/defaultServer/server.xml").read
+  end
+end
