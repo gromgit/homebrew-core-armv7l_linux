b56efd85a3d6e0fcb2cac4a0e93a17c476b272e5
Adrian Ho <aho+rpi5@03s.net>
2022-06-05 23:43:36 +0800

    clojure:

---
+++ Formula/clojure.rb
@@ -0,0 +1,35 @@
+class Clojure < Formula
+  desc "Dynamic, general-purpose programming language"
+  homepage "https://clojure.org"
+  url "https://download.clojure.org/install/clojure-tools-1.11.1.1113.tar.gz"
+  sha256 "0c954a56a071f33b9e039f8ab905f8372a5a601a0d14a32e0ccf230ea7606a22"
+  license "EPL-1.0"
+  version_scheme 1
+
+  livecheck do
+    url "https://raw.githubusercontent.com/clojure/homebrew-tools/master/Formula/clojure.rb"
+    regex(/url ".*?clojure-tools-v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "b35d5fb6e987b547c9759df8d0d86c60a024253dbcd3c8fcb988539617eaafc1"
+  end
+
+  depends_on "openjdk"
+  depends_on "rlwrap"
+
+  uses_from_macos "ruby" => :build
+
+  def install
+    system "./install.sh", prefix
+    bin.env_script_all_files libexec/"bin", Language::Java.overridable_java_home_env
+  end
+
+  test do
+    ENV["TERM"] = "xterm"
+    system("#{bin}/clj", "-e", "nil")
+    %w[clojure clj].each do |clj|
+      assert_equal "2", shell_output("#{bin}/#{clj} -e \"(+ 1 1)\"").strip
+    end
+  end
+end
