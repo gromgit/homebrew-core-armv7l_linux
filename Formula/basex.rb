9c5c198ebf0322547aec08c3d5e1ff4052f08868
Adrian Ho <aho+rpi5@03s.net>
2022-06-05 23:34:59 +0800

    basex: no rebuilding required

---
+++ Formula/basex.rb
@@ -0,0 +1,34 @@
+class Basex < Formula
+  desc "Light-weight XML database and XPath/XQuery processor"
+  homepage "https://basex.org"
+  url "https://files.basex.org/releases/9.7.2/BaseX972.zip"
+  version "9.7.2"
+  sha256 "ef7aa90d856fa3aa8b41730d74429ff2f98bd88e802acbb5b244a4f5d8e15ec0"
+  license "BSD-3-Clause"
+
+  livecheck do
+    url "https://files.basex.org/releases/"
+    regex(%r{href=.*?v?(\d+(?:\.\d+)+)/?["' >]}i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "c642512410ac393ee7392df6a4de1a7d8e1776b7835b80065b705968771b56a2"
+  end
+
+  depends_on "openjdk"
+
+  def install
+    rm Dir["bin/*.bat"]
+    rm_rf "repo"
+    rm_rf "data"
+    rm_rf "etc"
+    prefix.install_metafiles
+    libexec.install Dir["*"]
+    bin.install Dir["#{libexec}/bin/*"]
+    bin.env_script_all_files libexec/"bin", JAVA_HOME: Formula["openjdk"].opt_prefix
+  end
+
+  test do
+    assert_equal "1\n2\n3\n4\n5\n6\n7\n8\n9\n10", shell_output("#{bin}/basex '1 to 10'")
+  end
+end
