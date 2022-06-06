2745401d052402c05272102dfeb179c81981dee5
Adrian Ho <aho+rpi5@03s.net>
2022-06-06 21:38:07 +0800

    anycable-go: update 1.2.0 bottle.

---
+++ Formula/anycable-go.rb
@@ -0,0 +1,46 @@
+class AnycableGo < Formula
+  desc "WebSocket server with action cable protocol"
+  homepage "https://github.com/anycable/anycable-go"
+  url "https://github.com/anycable/anycable-go/archive/v1.2.0.tar.gz"
+  sha256 "d8d411e078f0e3de754623fe4d68025d60c8a9fcd31a29972d8c2f72d2a8d541"
+  license "MIT"
+  head "https://github.com/anycable/anycable-go.git", branch: "master"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/anycable-go"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "7c82f918a859fe38c6b618bb136e628ce6c2bfc9dd1e695d2bbac608a761303f"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %w[
+      -s -w
+    ]
+    ldflags << if build.head?
+      "-X github.com/anycable/anycable-go/utils.sha=#{version.commit}"
+    else
+      "-X github.com/anycable/anycable-go/utils.version=#{version}"
+    end
+
+    system "go", "build", "-mod=vendor", *std_go_args(ldflags: ldflags),
+                          "-v", "github.com/anycable/anycable-go/cmd/anycable-go"
+  end
+
+  test do
+    port = free_port
+    pid = fork do
+      exec "#{bin}/anycable-go --port=#{port}"
+    end
+    sleep 1
+    output = shell_output("curl -sI http://localhost:#{port}/health")
+    assert_match(/200 OK/m, output)
+  ensure
+    Process.kill("HUP", pid)
+  end
+end
