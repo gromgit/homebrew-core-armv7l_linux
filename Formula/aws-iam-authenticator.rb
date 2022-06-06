69c9f3bf25cc6355ca9580cb0515841783f6dbb1
Adrian Ho <aho+rpi5@03s.net>
2022-06-07 00:58:50 +0800

    aws-iam-authenticator: update 0.5.7 bottle.

---
+++ Formula/aws-iam-authenticator.rb
@@ -0,0 +1,41 @@
+class AwsIamAuthenticator < Formula
+  desc "Use AWS IAM credentials to authenticate to Kubernetes"
+  homepage "https://github.com/kubernetes-sigs/aws-iam-authenticator"
+  url "https://github.com/kubernetes-sigs/aws-iam-authenticator.git",
+      tag:      "v0.5.7",
+      revision: "2a9ee95fecab59fab41a0b646a63227d66113434"
+  license "Apache-2.0"
+  head "https://github.com/kubernetes-sigs/aws-iam-authenticator.git", branch: "master"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/aws-iam-authenticator"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "b2149e6faa0e944b30f036a08c0f46dc0b324fe0af216c8515a137b694274b82"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = ["-s", "-w",
+               "-X sigs.k8s.io/aws-iam-authenticator/pkg.Version=#{version}",
+               "-X sigs.k8s.io/aws-iam-authenticator/pkg.CommitID=#{Utils.git_head}",
+               "-buildid=''"]
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/aws-iam-authenticator"
+    prefix.install_metafiles
+  end
+
+  test do
+    output = shell_output("#{bin}/aws-iam-authenticator version")
+    assert_match %Q("Version":"#{version}"), output
+
+    system "#{bin}/aws-iam-authenticator", "init", "-i", "test"
+    contents = Dir.entries(".")
+    ["cert.pem", "key.pem", "aws-iam-authenticator.kubeconfig"].each do |created|
+      assert_includes contents, created
+    end
+  end
+end
