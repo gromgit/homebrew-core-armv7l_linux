0c1b1e71ed246efb78077a8a11e14aa620d12358
Adrian Ho <aho+rpi5@03s.net>
2022-06-06 20:15:23 +0800

    afl-fuzz: update 2.57b_1 bottle.

---
+++ Formula/afl-fuzz.rb
@@ -0,0 +1,46 @@
+class AflFuzz < Formula
+  desc "American fuzzy lop: Security-oriented fuzzer"
+  homepage "https://github.com/google/AFL"
+  url "https://github.com/google/AFL/archive/v2.57b.tar.gz"
+  version "2.57b"
+  sha256 "6f05a6515c07abe49f6f292bd13c96004cc1e016bda0c3cc9c2769dd43f163ee"
+  license "Apache-2.0"
+  revision 1
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+b?)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/afl-fuzz"
+    sha256 armv7l_linux: "50a58ed218a8982ae3dc9444f099ec384a921645de3132f22eb33075f020aadb"
+  end
+
+  def install
+    system "make", "PREFIX=#{prefix}", "AFL_NO_X86=1"
+    system "make", "install", "PREFIX=#{prefix}", "AFL_NO_X86=1"
+
+    # Delete incompatible elf32-i386 testcase file
+    rm Dir[share/"afl/**/elf/small_exec.elf"]
+  end
+
+  test do
+    cpp_file = testpath/"main.cpp"
+    cpp_file.write <<~EOS
+      #include <iostream>
+
+      int main() {
+        std::cout << "Hello, world!";
+      }
+    EOS
+
+    cmd = if OS.mac?
+      "afl-clang++"
+    else
+      "afl-g++"
+    end
+    system bin/cmd, "-g", cpp_file, "-o", "test"
+    assert_equal "Hello, world!", shell_output("./test")
+  end
+end
