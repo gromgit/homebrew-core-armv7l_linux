6fa6d9da76fa1c19df3aebb1c8dee0cc60693de1
Adrian Ho <aho+rpi5@03s.net>
2022-06-07 05:05:17 +0800

    boost-build: update 1.78.0 bottle.

---
+++ Formula/boost-build.rb
@@ -0,0 +1,43 @@
+class BoostBuild < Formula
+  desc "C++ build system"
+  homepage "https://www.boost.org/build/"
+  url "https://github.com/boostorg/build/archive/boost-1.78.0.tar.gz"
+  sha256 "78ba74587c8ba137d2f5f5ee1472e49d6291842ac944e2c24012c9e8a3d2f664"
+  license "BSL-1.0"
+  version_scheme 1
+  head "https://github.com/boostorg/build.git", branch: "develop"
+
+  livecheck do
+    url :stable
+    regex(/^boost[._-]v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/boost-build"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "139580ed347638b22b823e3869d89fcf10a1221833d106453b31fcfb22259a35"
+  end
+
+  conflicts_with "b2-tools", because: "both install `b2` binaries"
+
+  def install
+    system "./bootstrap.sh"
+    system "./b2", "--prefix=#{prefix}", "install"
+    pkgshare.install "boost-build.jam"
+  end
+
+  test do
+    (testpath/"hello.cpp").write <<~EOS
+      #include <iostream>
+      int main (void) { std::cout << "Hello world"; }
+    EOS
+    (testpath/"Jamroot.jam").write("exe hello : hello.cpp ;")
+
+    system bin/"b2", "release"
+
+    compiler = File.basename(ENV.cc)
+    out = Dir["bin/#{compiler}*/release/hello"]
+    assert out.length == 1
+    assert_predicate testpath/out[0], :exist?
+    assert_equal "Hello world", shell_output(out[0])
+  end
+end
