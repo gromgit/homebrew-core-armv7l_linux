814ae8e74f9d6dced1a2d9b4cc9406a9d569ba97
Adrian Ho <aho+rpi5@03s.net>
2022-06-06 19:57:41 +0800

    act: update 0.2.26 bottle.

---
+++ Formula/act.rb
@@ -0,0 +1,42 @@
+class Act < Formula
+  desc "Run your GitHub Actions locally 🚀"
+  homepage "https://github.com/nektos/act"
+  url "https://github.com/nektos/act/archive/v0.2.26.tar.gz"
+  sha256 "73d75205293bd18e4f529ef1aa47da79fcd7eda2413b790816f4ca63b7aa02ce"
+  license "MIT"
+  head "https://github.com/nektos/act.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/act"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "05adcbbf8a5ca425fb04fd1ee0e1d34eab8b4cfc98de9cb86e1cceeb198d1b2a"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "make", "build", "VERSION=#{version}"
+    bin.install "dist/local/act"
+  end
+
+  test do
+    (testpath/".actrc").write <<~EOS
+      -P ubuntu-latest=node:12.6-buster-slim
+      -P ubuntu-12.04=node:12.6-buster-slim
+      -P ubuntu-18.04=node:12.6-buster-slim
+      -P ubuntu-16.04=node:12.6-stretch-slim
+    EOS
+
+    system "git", "clone", "https://github.com/stefanzweifel/laravel-github-actions-demo.git"
+
+    cd "laravel-github-actions-demo" do
+      system "git", "checkout", "v2.0"
+
+      pull_request_jobs = shell_output("#{bin}/act pull_request --list")
+      assert_match "php-cs-fixer", pull_request_jobs
+
+      push_jobs = shell_output("#{bin}/act push --list")
+      assert_match "phpinsights", push_jobs
+      assert_match "phpunit", push_jobs
+    end
+  end
+end
