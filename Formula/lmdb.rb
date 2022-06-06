ec99edd5664e81c561be5f3d3bc7ce7c61288b45
Adrian Ho <aho+rpi5@03s.net>
2022-06-07 04:31:55 +0800

    lmdb: update 0.9.29 bottle.

---
+++ Formula/lmdb.rb
@@ -0,0 +1,32 @@
+class Lmdb < Formula
+  desc "Lightning memory-mapped database: key-value data store"
+  homepage "https://www.symas.com/symas-embedded-database-lmdb"
+  url "https://git.openldap.org/openldap/openldap/-/archive/LMDB_0.9.29/openldap-LMDB_0.9.29.tar.bz2"
+  sha256 "182e69af99788b445585b8075bbca89ae8101069fbeee25b2756fb9590e833f8"
+  license "OLDAP-2.8"
+  version_scheme 1
+  head "https://git.openldap.org/openldap/openldap.git", branch: "mdb.master"
+
+  livecheck do
+    url :stable
+    regex(/^LMDB[._-]v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/lmdb"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "23e05d9f8bf1aefc89122104b791ccf3d246a687b7c0f90ba2cff9478ede7813"
+  end
+
+  def install
+    cd "libraries/liblmdb" do
+      args = []
+      args << "SOEXT=.dylib" if OS.mac?
+      system "make", *args
+      system "make", "install", *args, "prefix=#{prefix}"
+    end
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/mdb_dump -V")
+  end
+end
