a4c55be7168e22541ddb0c78c91ef863ffaf3014
Adrian Ho <aho+rpi5@03s.net>
2022-06-06 09:53:47 +0800

    zstd: update 1.5.2 bottle.

---
+++ Formula/zstd.rb
@@ -0,0 +1,39 @@
+class Zstd < Formula
+  desc "Zstandard is a real-time compression algorithm"
+  homepage "https://facebook.github.io/zstd/"
+  url "https://github.com/facebook/zstd/archive/v1.5.2.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/zstd-1.5.2.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/legacy/zstd-1.5.2.tar.gz"
+  sha256 "f7de13462f7a82c29ab865820149e778cbfe01087b3a55b5332707abf9db4a6e"
+  license "BSD-3-Clause"
+  head "https://github.com/facebook/zstd.git", branch: "dev"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/zstd"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "873840f962f4d3b821273f6f72d4252f0a9e5f6f9da1f47382bcd7102ecaa919"
+  end
+
+  depends_on "cmake" => :build
+
+  def install
+    # Legacy support is the default after
+    # https://github.com/facebook/zstd/commit/db104f6e839cbef94df4df8268b5fecb58471274
+    # Set it to `ON` to be explicit about the configuration.
+    system "cmake", "-S", "build/cmake", "-B", "builddir",
+                    "-DZSTD_PROGRAMS_LINK_SHARED=ON", # link `zstd` to `libzstd`
+                    "-DZSTD_BUILD_CONTRIB=ON",
+                    "-DCMAKE_INSTALL_RPATH=#{rpath}",
+                    "-DZSTD_LEGACY_SUPPORT=ON",
+                    *std_cmake_args
+    system "cmake", "--build", "builddir"
+    system "cmake", "--install", "builddir"
+  end
+
+  test do
+    assert_equal "hello\n",
+      pipe_output("#{bin}/zstd | #{bin}/zstd -d", "hello\n", 0)
+
+    assert_equal "hello\n",
+      pipe_output("#{bin}/pzstd | #{bin}/pzstd -d", "hello\n", 0)
+  end
+end
