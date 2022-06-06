0f3d09c3a09c0e02b129b448765f784b7fd76826
Adrian Ho <aho+rpi5@03s.net>
2022-06-07 05:37:13 +0800

    brigade-cli: update 2.5.0 bottle.

---
+++ Formula/brigade-cli.rb
@@ -0,0 +1,45 @@
+class BrigadeCli < Formula
+  desc "Brigade command-line interface"
+  homepage "https://brigade.sh"
+  url "https://github.com/brigadecore/brigade.git",
+      tag:      "v2.5.0",
+      revision: "838e8fd2357e3ace0b2080544a9faf3229be8b3d"
+  license "Apache-2.0"
+  head "https://github.com/brigadecore/brigade.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/brigade-cli"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "ad53f20283e925b6445b9621d509107d8d198c201901682fd4fad96205111d84"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ENV["SKIP_DOCKER"] = "true"
+    ENV["VERSION"] = "v#{version}"
+
+    system "make", "hack-build-cli"
+
+    os = Utils.safe_popen_read("go", "env", "GOOS").strip
+    arch = Utils.safe_popen_read("go", "env", "GOARCH").strip
+    bin.install "bin/brig-#{os}-#{arch}" => "brig"
+  end
+
+  test do
+    system bin/"brig", "init", "--id", "foo"
+    assert_predicate testpath/".brigade", :directory?
+
+    version_output = shell_output(bin/"brig version 2>&1")
+    assert_match "Brigade client:", version_output
+
+    return unless build.stable?
+
+    commit = stable.specs[:revision][0..6]
+    assert_match "Brigade client: version v#{version} -- commit #{commit}", version_output
+  end
+end
