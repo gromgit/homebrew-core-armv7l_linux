3269212cef0ffd0aa06025780f3d7b340a3d9fbc
Adrian Ho <aho+rpi5@03s.net>
2022-06-07 06:10:38 +0800

    buildpulse-test-reporter: update 0.24.0 bottle.

---
+++ Formula/buildpulse-test-reporter.rb
@@ -0,0 +1,37 @@
+class BuildpulseTestReporter < Formula
+  desc "Connect your CI to BuildPulse to detect, track, and rank flaky tests"
+  homepage "https://buildpulse.io"
+  url "https://github.com/buildpulse/test-reporter/archive/refs/tags/v0.24.0.tar.gz"
+  sha256 "ddf899694f17c072822191a1294c6968ab0992c1ce7bbd1318a148fbad497704"
+  license "MIT"
+  head "https://github.com/buildpulse/test-reporter.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/buildpulse-test-reporter"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "4a1ec09519a2179a0098e4507d85d84a4975c0062ecd1f4ce0d4b3738ee3ef2d"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    goldflags = %W[
+      -s -w
+      -X main.Version=#{version}
+      -X main.Commit=#{tap.user}
+    ]
+    system "go", "build", *std_go_args(ldflags: goldflags), "./cmd/test-reporter"
+  end
+
+  test do
+    binary = bin/"buildpulse-test-reporter"
+    assert_match version.to_s, shell_output("#{binary} --version")
+
+    fake_dir = "im-not-real"
+    assert_match "Received args: #{fake_dir}", shell_output("#{binary} submit #{fake_dir}", 1)
+  end
+end
