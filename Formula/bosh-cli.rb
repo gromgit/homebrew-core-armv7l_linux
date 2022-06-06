d747b9b57a168e5ca8f6960f645a1477196bc81b
Adrian Ho <aho+rpi5@03s.net>
2022-06-07 05:11:04 +0800

    bosh-cli: update 7.0.1 bottle.

---
+++ Formula/bosh-cli.rb
@@ -0,0 +1,29 @@
+class BoshCli < Formula
+  desc "Cloud Foundry BOSH CLI v2"
+  homepage "https://bosh.io/docs/cli-v2/"
+  url "https://github.com/cloudfoundry/bosh-cli/archive/v7.0.1.tar.gz"
+  sha256 "28eef4aebcf522c3816d21872f25b8f51891a1dba9fa32a0ae22be48ae94d35b"
+  license "Apache-2.0"
+  head "https://github.com/cloudfoundry/bosh-cli.git", branch: "main"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/bosh-cli"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "79ab20fc56cd360942cc2d924015023a996a19ce52000d732b237ab236923b55"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    # https://github.com/cloudfoundry/bosh-cli/blob/master/ci/tasks/build.sh#L23-L24
+    inreplace "cmd/version.go", "[DEV BUILD]", "#{version}-#{tap.user}-#{time.iso8601}"
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    system bin/"bosh-cli", "generate-job", "brew-test"
+    assert_equal 0, $CHILD_STATUS.exitstatus
+    assert_predicate testpath/"jobs/brew-test", :exist?
+
+    assert_match version.to_s, shell_output("#{bin}/bosh-cli --version")
+  end
+end
