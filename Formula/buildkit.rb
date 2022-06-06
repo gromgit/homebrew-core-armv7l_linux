b55d417b675501685c956508d6da2d0ce0ce8edb
Adrian Ho <aho+rpi5@03s.net>
2022-06-07 06:04:52 +0800

    buildkit: update 0.10.3 bottle.

---
+++ Formula/buildkit.rb
@@ -0,0 +1,40 @@
+class Buildkit < Formula
+  desc "Сoncurrent, cache-efficient, and Dockerfile-agnostic builder toolkit"
+  homepage "https://github.com/moby/buildkit"
+  url "https://github.com/moby/buildkit.git",
+      tag:      "v0.10.3",
+      revision: "c8d25d9a103b70dc300a4fd55e7e576472284e31"
+  license "Apache-2.0"
+  head "https://github.com/moby/buildkit.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/buildkit"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "cd7c8b8ba616f953a5b3ac5bdbe8909cd9b8a54b287cf9b43efbdbef9d740e1f"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    revision = Utils.git_head
+    ldflags = %W[
+      -s -w
+      -X github.com/moby/buildkit/version.Version=#{version}
+      -X github.com/moby/buildkit/version.Revision=#{revision}
+      -X github.com/moby/buildkit/version.Package=github.com/moby/buildkit
+    ]
+
+    system "go", "build", "-mod=vendor", "-trimpath",
+      "-ldflags", ldflags.join(" "), "-o", bin/"buildctl", "./cmd/buildctl"
+
+    doc.install Dir["docs/*.md"]
+  end
+
+  test do
+    shell_output("#{bin}/buildctl --addr unix://dev/null --timeout 0 du 2>&1", 1)
+  end
+end
