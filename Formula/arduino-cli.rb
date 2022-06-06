699b2366c5a165654956bb74e7f1a314f0c648d0
Adrian Ho <aho+rpi5@03s.net>
2022-06-06 22:37:04 +0800

    arduino-cli: update 0.22.0 bottle.

---
+++ Formula/arduino-cli.rb
@@ -0,0 +1,52 @@
+class ArduinoCli < Formula
+  desc "Arduino command-line interface"
+  homepage "https://github.com/arduino/arduino-cli"
+  url "https://github.com/arduino/arduino-cli.git",
+      tag:      "0.22.0",
+      revision: "65f662a782780f9e410a3c327b213e55163f4de9"
+  license "GPL-3.0-only"
+  head "https://github.com/arduino/arduino-cli.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/arduino-cli"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "459305952bfb90e9646464e45ed0861ef51b157a682ba803f11e83dc23466a83"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/arduino/arduino-cli/version.versionString=#{version}
+      -X github.com/arduino/arduino-cli/version.commit=#{Utils.git_head(length: 8)}
+      -X github.com/arduino/arduino-cli/version.date=#{time.iso8601}
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags)
+
+    output = Utils.safe_popen_read(bin/"arduino-cli", "completion", "bash")
+    (bash_completion/"arduino-cli").write output
+
+    output = Utils.safe_popen_read(bin/"arduino-cli", "completion", "zsh")
+    (zsh_completion/"_arduino-cli").write output
+
+    output = Utils.safe_popen_read(bin/"arduino-cli", "completion", "fish")
+    (fish_completion/"arduino-cli.fish").write output
+  end
+
+  test do
+    system "#{bin}/arduino-cli", "sketch", "new", "test_sketch"
+    assert File.directory?("#{testpath}/test_sketch")
+
+    version_output = shell_output("#{bin}/arduino-cli version 2>&1")
+    assert_match("arduino-cli  Version: #{version}", version_output)
+    assert_match("Commit:", version_output)
+    assert_match(/[a-f0-9]{8}/, version_output)
+    assert_match("Date: ", version_output)
+    assert_match(/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z/, version_output)
+  end
+end
