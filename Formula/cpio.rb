dd3f28557788fcb9b592f5d1efda06d374655b5d
Adrian Ho <aho+rpi5@03s.net>
2022-06-07 04:37:35 +0800

    cpio: update 2.13_3 bottle.

---
+++ Formula/cpio.rb
@@ -0,0 +1,36 @@
+class Cpio < Formula
+  desc "Copies files into or out of a cpio or tar archive"
+  homepage "https://www.gnu.org/software/cpio/"
+  url "https://ftp.gnu.org/gnu/cpio/cpio-2.13.tar.bz2"
+  mirror "https://ftpmirror.gnu.org/cpio/cpio-2.13.tar.bz2"
+  sha256 "eab5bdc5ae1df285c59f2a4f140a98fc33678a0bf61bdba67d9436ae26b46f6d"
+  license "GPL-3.0-or-later"
+  revision 3
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/cpio"
+    sha256 armv7l_linux: "10f061b8fab8faadb79219408c716a22ee12adfaea71fbde7165f47b4252397c"
+  end
+
+  keg_only :shadowed_by_macos, "macOS provides cpio"
+
+  def install
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    system "make", "install"
+
+    return if OS.mac?
+
+    # Delete rmt, which causes conflict with `gnu-tar`
+    (libexec/"rmt").unlink
+    (man8/"rmt.8").unlink
+  end
+
+  test do
+    (testpath/"test.cc").write <<~EOS
+      #include <iostream>
+      #include <string>
+    EOS
+    system "ls #{testpath} | #{bin}/cpio -ov > #{testpath}/directory.cpio"
+    assert_path_exists "#{testpath}/directory.cpio"
+  end
+end
