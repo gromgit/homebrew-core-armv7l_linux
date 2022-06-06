e61ec5a3d3dbd84df5eaeb8e8f117795f479af92
Adrian Ho <aho+rpi5@03s.net>
2022-06-06 19:51:47 +0800

    ace: update 7.0.7 bottle.

---
+++ Formula/ace.rb
@@ -0,0 +1,51 @@
+class Ace < Formula
+  desc "ADAPTIVE Communication Environment: OO network programming in C++"
+  homepage "https://www.dre.vanderbilt.edu/~schmidt/ACE.html"
+  url "https://github.com/DOCGroup/ACE_TAO/releases/download/ACE%2BTAO-7_0_7/ACE+TAO-7.0.7.tar.bz2"
+  sha256 "13f010e2de296a9d1f956c7f36795fec08991be500456bdb2cbc93a5503992e0"
+  license "DOC"
+
+  livecheck do
+    url :stable
+    regex(/^ACE(?:\+[A-Z]+)*?[._-]v?(\d+(?:[._]\d+)+)$/i)
+    strategy :git do |tags, regex|
+      tags.map { |tag| tag[regex, 1]&.tr("_", ".") }
+    end
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/ace"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "af5be524ab44be0acc3e6e4b48fb256a14be5aaf4fbdea42093b70b7d6d14ca8"
+  end
+
+  def install
+    os = OS.mac? ? "macosx" : "linux"
+    ln_sf "config-#{os}.h", "ace/config.h"
+    ln_sf "platform_#{os}.GNU", "include/makeinclude/platform_macros.GNU"
+
+    # Set up the environment the way ACE expects during build.
+    ENV.cxx11
+    ENV["ACE_ROOT"] = buildpath
+    ENV["DYLD_LIBRARY_PATH"] = "#{buildpath}/lib"
+
+    # Done! We go ahead and build.
+    system "make", "-C", "ace", "-f", "GNUmakefile.ACE",
+                   "INSTALL_PREFIX=#{prefix}",
+                   "LDFLAGS=",
+                   "DESTDIR=",
+                   "INST_DIR=/ace",
+                   "debug=0",
+                   "shared_libs=1",
+                   "static_libs=0",
+                   "install"
+
+    ENV.append "LDFLAGS", "-Wl,-rpath,#{lib}" if OS.mac?
+    system "make", "-C", "examples/Log_Msg"
+    pkgshare.install "examples"
+  end
+
+  test do
+    cp_r "#{pkgshare}/examples/Log_Msg/.", testpath
+    system "./test_callback"
+  end
+end
