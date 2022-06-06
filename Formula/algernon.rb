3f17abd8b5a0742d7f92416952bfc46cf294a800
Adrian Ho <aho+rpi5@03s.net>
2022-06-06 20:55:09 +0800

    algernon: update 1.13.0 bottle.

---
+++ Formula/algernon.rb
@@ -0,0 +1,40 @@
+class Algernon < Formula
+  desc "Pure Go web server with Lua, Markdown, HTTP/2 and template support"
+  homepage "https://github.com/xyproto/algernon"
+  url "https://github.com/xyproto/algernon/archive/refs/tags/v1.13.0.tar.gz"
+  sha256 "8327c1687990386eb5a48d15fd46fc69e17d400c29d726b34f6087c9c4887b9c"
+  license "BSD-3-Clause"
+  version_scheme 1
+  head "https://github.com/xyproto/algernon.git", branch: "main"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/algernon"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "1db27bc5f2e21780f7de249a7379b8cd224a0c99ed6774297879f32fd0d3e3fc"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "-mod=vendor"
+
+    bin.install "desktop/mdview"
+  end
+
+  test do
+    port = free_port
+    pid = fork do
+      exec "#{bin}/algernon", "-s", "-q", "--httponly", "--boltdb", "tmp.db",
+                              "--addr", ":#{port}"
+    end
+    sleep 20
+    output = shell_output("curl -sIm3 -o- http://localhost:#{port}")
+    assert_match(/200 OK.*Server: Algernon/m, output)
+  ensure
+    Process.kill("HUP", pid)
+  end
+end
