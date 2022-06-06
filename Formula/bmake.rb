77973ee33fdbf8a4c933f74f821cef1281fe87f2
Adrian Ho <aho+rpi5@03s.net>
2022-06-07 04:52:37 +0800

    bmake: update 20220418 bottle.

---
+++ Formula/bmake.rb
@@ -0,0 +1,43 @@
+class Bmake < Formula
+  desc "Portable version of NetBSD make(1)"
+  homepage "https://www.crufty.net/help/sjg/bmake.html"
+  url "https://www.crufty.net/ftp/pub/sjg/bmake-20220418.tar.gz"
+  sha256 "6ea7de515cba74f173dc14ee17b1488ae032582028d2e86ea12f70369cc896f5"
+  license "BSD-3-Clause"
+
+  livecheck do
+    url "https://www.crufty.net/ftp/pub/sjg/"
+    regex(/href=.*?bmake[._-]v?(\d{6,8})\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/bmake"
+    sha256 armv7l_linux: "9279ca51e8946ec65f8acb50ac8d1021fb86c33fa4ab9bd142e02006ad2f8fef"
+  end
+
+  def install
+    # Don't pre-roff cat pages.
+    inreplace "mk/man.mk", "MANTARGET?", "MANTARGET"
+
+    # -DWITHOUT_PROG_LINK means "don't symlink as bmake-VERSION."
+    # shell-ksh test segfaults since macOS 11.
+    args = ["--prefix=#{prefix}", "-DWITHOUT_PROG_LINK", "--install", "BROKEN_TESTS=shell-ksh"]
+    system "sh", "boot-strap", *args
+
+    man1.install "bmake.1"
+  end
+
+  test do
+    (testpath/"Makefile").write <<~EOS
+      all: hello
+
+      hello:
+      \t@echo 'Test successful.'
+
+      clean:
+      \trm -rf Makefile
+    EOS
+    system bin/"bmake"
+    system bin/"bmake", "clean"
+  end
+end
