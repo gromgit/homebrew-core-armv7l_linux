8e2e96397f29f61cc507471f41c56fd131b435e8
Adrian Ho <aho+rpi5@03s.net>
2022-06-07 02:06:28 +0800

    bazelisk: update 1.11.0 bottle.

---
+++ Formula/bazelisk.rb
@@ -0,0 +1,45 @@
+class Bazelisk < Formula
+  desc "User-friendly launcher for Bazel"
+  homepage "https://github.com/bazelbuild/bazelisk/"
+  url "https://github.com/bazelbuild/bazelisk.git",
+      tag:      "v1.11.0",
+      revision: "618382246fae452d0d92b6086b29903c3bc71361"
+  license "Apache-2.0"
+  head "https://github.com/bazelbuild/bazelisk.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/bazelisk"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "7296683c9f9c7fd662e62ccefcc2e500227eb40a96432f416e038e70eca4b5d0"
+  end
+
+  depends_on "go" => :build
+
+  conflicts_with "bazel", because: "Bazelisk replaces the bazel binary"
+
+  resource "bazel_zsh_completion" do
+    url "https://raw.githubusercontent.com/bazelbuild/bazel/036e533/scripts/zsh_completion/_bazel"
+    sha256 "4094dc84add2f23823bc341186adf6b8487fbd5d4164bd52d98891c41511eba4"
+  end
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w -X main.BazeliskVersion=#{version}")
+
+    bin.install_symlink "bazelisk" => "bazel"
+
+    resource("bazel_zsh_completion").stage do
+      zsh_completion.install "_bazel"
+    end
+  end
+
+  test do
+    ENV["USE_BAZEL_VERSION"] = Formula["bazel"].version
+    assert_match "Build label: #{Formula["bazel"].version}", shell_output("#{bin}/bazelisk version")
+
+    # This is an older than current version, so that we can test that bazelisk
+    # will target an explicit version we specify. This version shouldn't need to
+    # be bumped.
+    bazel_version = Hardware::CPU.arm? ? "4.1.0" : "4.0.0"
+    ENV["USE_BAZEL_VERSION"] = bazel_version
+    assert_match "Build label: #{bazel_version}", shell_output("#{bin}/bazelisk version")
+  end
+end
