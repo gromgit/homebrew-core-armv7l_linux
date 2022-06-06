30480f79058a2ab200b91cb2068ad8c1c7e95af1
Adrian Ho <aho+rpi5@03s.net>
2022-06-06 20:02:16 +0800

    adns: update 1.6.0 bottle.

---
+++ Formula/adns.rb
@@ -0,0 +1,30 @@
+class Adns < Formula
+  desc "C/C++ resolver library and DNS resolver utilities"
+  homepage "https://www.chiark.greenend.org.uk/~ian/adns/"
+  url "https://www.chiark.greenend.org.uk/~ian/adns/ftp/adns-1.6.0.tar.gz"
+  sha256 "fb427265a981e033d1548f2b117cc021073dc8be2eaf2c45fd64ab7b00ed20de"
+  license all_of: ["GPL-3.0-or-later", "LGPL-2.0-or-later"]
+  head "https://www.chiark.greenend.org.uk/ucgi/~ianmdlvl/githttp/adns.git", branch: "master"
+
+  livecheck do
+    url "https://www.chiark.greenend.org.uk/~ian/adns/ftp/"
+    regex(/href=.*?adns[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/adns"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "94aff8db5390784c92fcbe2735405e52c3bd45271439c115a497a23516f1e200"
+  end
+
+  uses_from_macos "m4" => :build
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-dynamic"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/adnsheloex", "--version"
+  end
+end
