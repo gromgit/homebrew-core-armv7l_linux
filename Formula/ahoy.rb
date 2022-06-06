d00e52dd871abec14909886eeaac0ff3cbe832ef
Adrian Ho <aho+rpi5@03s.net>
2022-06-06 20:34:22 +0800

    ahoy: update 2.0.0 bottle.

---
+++ Formula/ahoy.rb
@@ -0,0 +1,49 @@
+class Ahoy < Formula
+  desc "Creates self documenting CLI programs from commands in YAML files"
+  homepage "https://ahoy-cli.readthedocs.io/"
+  url "https://github.com/ahoy-cli/ahoy/archive/2.0.0.tar.gz"
+  sha256 "cc3e426083bf7b7309e484fa69ed53b33c9b00adf9be879cbe74c19bdaef027c"
+  license "MIT"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/ahoy"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "218c43d18d0e1232ee7f05e8ead3bde71239b884c50a650e0143b80b02205fbf"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    ENV["GO111MODULE"] = "auto"
+    bin_path = buildpath/"src/github.com/ahoy-cli/ahoy"
+    bin_path.install Dir["*"]
+    cd bin_path do
+      system "go", "build", "-o", bin/"ahoy", "-ldflags", "-X main.version=#{version}", "."
+    end
+  end
+
+  def caveats
+    <<~EOS
+      ===== UPGRADING FROM 1.x TO 2.x =====
+
+      If you are upgrading from ahoy 1.x, note that you'll
+      need to upgrade your ahoyapi settings in your .ahoy.yml
+      files to 'v2' instead of 'v1'.
+
+      See other changes at:
+
+      https://github.com/ahoy-cli/ahoy
+
+    EOS
+  end
+
+  test do
+    (testpath/".ahoy.yml").write <<~EOS
+      ahoyapi: v2
+      commands:
+        hello:
+          cmd: echo "Hello Homebrew!"
+    EOS
+    assert_equal "Hello Homebrew!\n", `#{bin}/ahoy hello`
+  end
+end
