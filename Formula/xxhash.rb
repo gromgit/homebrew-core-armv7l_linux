918932f5bcb7b2eb2b3a424ae927b33dd3428c7f
Adrian Ho <aho+rpi5@03s.net>
2022-06-07 04:23:06 +0800

    xxhash: update 0.8.1 bottle.

---
+++ Formula/xxhash.rb
@@ -0,0 +1,27 @@
+class Xxhash < Formula
+  desc "Extremely fast non-cryptographic hash algorithm"
+  homepage "https://github.com/Cyan4973/xxHash"
+  url "https://github.com/Cyan4973/xxHash/archive/v0.8.1.tar.gz"
+  sha256 "3bb6b7d6f30c591dd65aaaff1c8b7a5b94d81687998ca9400082c739a690436c"
+  license "BSD-2-Clause"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/xxhash"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "46a44e27de5c823cf28c974497b0801bcbb58cf34da0524af84f6caa8ae6a3ed"
+  end
+
+  def install
+    system "make"
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    (testpath/"leaflet.txt").write "No computer should be without one!"
+    assert_match(/^67bc7cc242ebc50a/, shell_output("#{bin}/xxhsum leaflet.txt"))
+  end
+end
