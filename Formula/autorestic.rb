ed14d7c61e9091b849a8021ee51237a485b70e4f
Adrian Ho <aho+rpi5@03s.net>
2022-06-07 00:35:17 +0800

    autorestic: update 1.7.1 bottle.

---
+++ Formula/autorestic.rb
@@ -0,0 +1,38 @@
+class Autorestic < Formula
+  desc "High level CLI utility for restic"
+  homepage "https://autorestic.vercel.app/"
+  url "https://github.com/cupcakearmy/autorestic/archive/v1.7.1.tar.gz"
+  sha256 "89ffb11c14eb02bcc66427517a43a42a7e73ea359b579b8c2047c95ce5f9a8d8"
+  license "Apache-2.0"
+  head "https://github.com/cupcakearmy/autorestic.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/autorestic"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "07ddba79256cabccb37ce296b2041d55046c6369b31e67a4467adefed870a1ec"
+  end
+
+  depends_on "go" => :build
+  depends_on "restic"
+
+  def install
+    system "go", "build", *std_go_args, "./main.go"
+    (bash_completion/"autorestic").write Utils.safe_popen_read("#{bin}/autorestic", "completion", "bash")
+    (zsh_completion/"_autorestic").write Utils.safe_popen_read("#{bin}/autorestic", "completion", "zsh")
+    (fish_completion/"autorestic.fish").write Utils.safe_popen_read("#{bin}/autorestic", "completion", "fish")
+  end
+
+  test do
+    require "yaml"
+    config = {
+      "locations" => { "foo" => { "from" => "repo", "to" => ["bar"] } },
+      "backends"  => { "bar" => { "type" => "local", "key" => "secret", "path" => "data" } },
+    }
+    config["version"] = 2
+    File.write(testpath/".autorestic.yml", config.to_yaml)
+    (testpath/"repo"/"test.txt").write("This is a testfile")
+    system "#{bin}/autorestic", "check"
+    system "#{bin}/autorestic", "backup", "-a"
+    system "#{bin}/autorestic", "restore", "-l", "foo", "--to", "restore"
+    assert compare_file testpath/"repo"/"test.txt", testpath/"restore"/testpath/"repo"/"test.txt"
+  end
+end
