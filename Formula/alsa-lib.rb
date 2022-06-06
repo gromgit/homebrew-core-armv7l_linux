6b82fa2dc477fd9ae58fccc7911e04182ad4cd06
Adrian Ho <aho+rpi5@03s.net>
2022-06-06 21:08:16 +0800

    alsa-lib: update 1.2.7 bottle.

---
+++ Formula/alsa-lib.rb
@@ -0,0 +1,41 @@
+class AlsaLib < Formula
+  desc "Provides audio and MIDI functionality to the Linux operating system"
+  homepage "https://www.alsa-project.org/"
+  url "https://www.alsa-project.org/files/pub/lib/alsa-lib-1.2.7.tar.bz2"
+  sha256 "8814e61f7ec6812c76e23a85cab00e0b0d3bba40816af36b726beb1bc04c74a7"
+  license "LGPL-2.1-or-later"
+
+  livecheck do
+    url "https://www.alsa-project.org/files/pub/lib/"
+    regex(/href=.*?alsa-lib[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/alsa-lib"
+    sha256 armv7l_linux: "a095e767a215cc682cc6dc117d85707e2757fe642aec0a06d33fdc5ce19980b3"
+  end
+
+  depends_on :linux
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <alsa/asoundlib.h>
+      int main(void)
+      {
+          snd_ctl_card_info_t *info;
+          snd_ctl_card_info_alloca(&info);
+          return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-lasound", "-o", "test"
+    system "./test"
+  end
+end
