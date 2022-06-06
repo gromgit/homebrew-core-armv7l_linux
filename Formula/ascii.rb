ec033245b54062aa35e0a7748fae9521bc0561bb
Adrian Ho <aho+rpi5@03s.net>
2022-06-06 23:15:58 +0800

    ascii: update 3.18 bottle.

---
+++ Formula/ascii.rb
@@ -0,0 +1,34 @@
+class Ascii < Formula
+  desc "List ASCII idiomatic names and octal/decimal code-point forms"
+  homepage "http://www.catb.org/~esr/ascii/"
+  url "http://www.catb.org/~esr/ascii/ascii-3.18.tar.gz"
+  sha256 "728422d5f4da61a37a17b4364d06708e543297de0a5f70305243236d80df072d"
+  license "BSD-2-Clause"
+
+  livecheck do
+    url :homepage
+    regex(/ascii[._-]v?(\d+(?:\.\d+)+)/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/ascii"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "70f98f479303d2e88cbe25d089b26239a94d5942ab1c89d1265b6730c924ff73"
+  end
+
+  head do
+    url "https://gitlab.com/esr/ascii.git"
+    depends_on "xmlto" => :build
+  end
+
+  def install
+    ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog" if build.head?
+    bin.mkpath
+    man1.mkpath
+    system "make"
+    system "make", "PREFIX=#{prefix}", "install"
+  end
+
+  test do
+    assert_match "Official name: Line Feed", shell_output(bin/"ascii 0x0a")
+  end
+end
