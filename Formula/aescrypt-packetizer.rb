dcc1b26ab4c1b18b22fca9e223df95e523fdca39
Adrian Ho <aho+rpi5@03s.net>
2022-06-06 20:06:54 +0800

    aescrypt-packetizer: update 3.16 bottle.

---
+++ Formula/aescrypt-packetizer.rb
@@ -0,0 +1,73 @@
+class AescryptPacketizer < Formula
+  desc "Encrypt and decrypt using 256-bit AES encryption"
+  homepage "https://www.aescrypt.com"
+  url "https://www.aescrypt.com/download/v3/linux/aescrypt-3.16.tgz"
+  sha256 "e2e192d0b45eab9748efe59e97b656cc55f1faeb595a2f77ab84d44b0ec084d2"
+
+  livecheck do
+    url "https://www.aescrypt.com/download/"
+    regex(%r{href=.*?/linux/aescrypt[._-]v?(\d+(?:\.\d+)+)\.t}i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/aescrypt-packetizer"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "20488ed21af63908081ceffa4823f5e262cb2a4862477f7dcb62b61f8dbda42f"
+  end
+
+  head do
+    url "https://github.com/paulej/AESCrypt.git", branch: "master"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on xcode: :build
+
+  def install
+    if build.head?
+      cd "linux"
+      system "autoreconf", "-ivf"
+
+      args = %W[
+        prefix=#{prefix}
+        --disable-gui
+      ]
+      args << "--enable-iconv" if OS.mac?
+
+      system "./configure", *args
+      system "make", "install"
+    else
+      cd "src" do
+        system "make"
+
+        bin.install "aescrypt"
+        bin.install "aescrypt_keygen"
+      end
+      man1.install "man/aescrypt.1"
+    end
+
+    # To prevent conflict with our other aescrypt, rename the binaries.
+    mv "#{bin}/aescrypt", "#{bin}/paescrypt"
+    mv "#{bin}/aescrypt_keygen", "#{bin}/paescrypt_keygen"
+  end
+
+  def caveats
+    <<~EOS
+      To avoid conflicting with our other AESCrypt package the binaries
+      have been renamed paescrypt and paescrypt_keygen.
+    EOS
+  end
+
+  test do
+    path = testpath/"secret.txt"
+    original_contents = "What grows when it eats, but dies when it drinks?"
+    path.write original_contents
+
+    system bin/"paescrypt", "-e", "-p", "fire", path
+    assert_predicate testpath/"#{path}.aes", :exist?
+
+    system bin/"paescrypt", "-d", "-p", "fire", "#{path}.aes"
+    assert_equal original_contents, path.read
+  end
+end
