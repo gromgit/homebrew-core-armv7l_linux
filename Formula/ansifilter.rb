a4e7b0bfaa8c03a54e7c6db0ffc792aa553e93f5
Adrian Ho <aho+rpi5@03s.net>
2022-06-06 21:25:19 +0800

    ansifilter: update 2.18 bottle.

---
+++ Formula/ansifilter.rb
@@ -0,0 +1,29 @@
+class Ansifilter < Formula
+  desc "Strip or convert ANSI codes into HTML, (La)Tex, RTF, or BBCode"
+  homepage "http://www.andre-simon.de/doku/ansifilter/ansifilter.html"
+  url "http://www.andre-simon.de/zip/ansifilter-2.18.tar.bz2"
+  sha256 "66cf017d36a43d5f6ae20609ce3b58647494ee6c0e41fc682c598bffce7d7d39"
+  license "GPL-3.0-or-later"
+
+  livecheck do
+    url "http://www.andre-simon.de/zip/download.php"
+    regex(/href=.*?ansifilter[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/ansifilter"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "dddf14f332bad098d9e5a0098220f889586b2b103db2ee6c149a206c8358abec"
+  end
+
+  def install
+    system "make", "PREFIX=#{prefix}"
+    system "make", "PREFIX=#{prefix}", "install"
+  end
+
+  test do
+    path = testpath/"ansi.txt"
+    path.write "f\x1b[31moo"
+
+    assert_equal "foo", shell_output("#{bin}/ansifilter #{path}").strip
+  end
+end
