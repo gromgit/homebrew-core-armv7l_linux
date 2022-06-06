6e1852bb286c41cefaca31dc50a2d4542251b35d
Adrian Ho <aho+rpi5@03s.net>
2022-06-06 09:51:28 +0800

    gpatch: update 2.7.6 bottle.

---
+++ Formula/gpatch.rb
@@ -0,0 +1,31 @@
+class Gpatch < Formula
+  desc "Apply a diff file to an original"
+  homepage "https://savannah.gnu.org/projects/patch/"
+  url "https://ftp.gnu.org/gnu/patch/patch-2.7.6.tar.xz"
+  mirror "https://ftpmirror.gnu.org/patch/patch-2.7.6.tar.xz"
+  sha256 "ac610bda97abe0d9f6b7c963255a11dcb196c25e337c61f94e4778d632f1d8fd"
+  license "GPL-3.0"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/gpatch"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "b0f2fb09903726065d8e842547dda0bdee402286b2fe55a465df22a702729977"
+  end
+
+  def install
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    testfile = testpath/"test"
+    testfile.write "homebrew\n"
+    patch = <<~EOS
+      1c1
+      < homebrew
+      ---
+      > hello
+    EOS
+    pipe_output("#{bin}/patch #{testfile}", patch)
+    assert_equal "hello", testfile.read.chomp
+  end
+end
