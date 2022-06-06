d8880ceccab7107a994a380c2e723cda2a91f224
Adrian Ho <aho+rpi5@03s.net>
2022-06-06 21:59:01 +0800

    libev: update 4.33 bottle.

---
+++ Formula/libev.rb
@@ -0,0 +1,66 @@
+class Libev < Formula
+  desc "Asynchronous event library"
+  homepage "http://software.schmorp.de/pkg/libev.html"
+  url "http://dist.schmorp.de/libev/Attic/libev-4.33.tar.gz"
+  mirror "https://fossies.org/linux/misc/libev-4.33.tar.gz"
+  sha256 "507eb7b8d1015fbec5b935f34ebed15bf346bed04a11ab82b8eee848c4205aea"
+
+  livecheck do
+    url "http://dist.schmorp.de/libev/"
+    regex(/href=.*?libev[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/libev"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "d0a27c3dfa5302a66c9170f5fd43f63dff7a831846628cdad3eab359ce922479"
+  end
+
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
+    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
+  end
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--mandir=#{man}"
+    system "make", "install"
+
+    # Remove compatibility header to prevent conflict with libevent
+    (include/"event.h").unlink
+  end
+
+  test do
+    (testpath/"test.c").write <<~'EOS'
+      /* Wait for stdin to become readable, then read and echo the first line. */
+
+      #include <stdio.h>
+      #include <stdlib.h>
+      #include <unistd.h>
+      #include <ev.h>
+
+      ev_io stdin_watcher;
+
+      static void stdin_cb (EV_P_ ev_io *watcher, int revents) {
+        char *buf;
+        size_t nbytes = 255;
+        buf = (char *)malloc(nbytes + 1);
+        getline(&buf, &nbytes, stdin);
+        printf("%s", buf);
+        ev_io_stop(EV_A_ watcher);
+        ev_break(EV_A_ EVBREAK_ALL);
+      }
+
+      int main() {
+        ev_io_init(&stdin_watcher, stdin_cb, STDIN_FILENO, EV_READ);
+        ev_io_start(EV_DEFAULT, &stdin_watcher);
+        ev_run(EV_DEFAULT, 0);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lev", "-o", "test"
+    input = "hello, world\n"
+    assert_equal input, pipe_output("./test", input, 0)
+  end
+end
