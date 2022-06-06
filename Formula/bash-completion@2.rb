28cd4b6ee1bff1557d033e60c54d20dd950631e6
Adrian Ho <aho+rpi5@03s.net>
2022-06-07 01:52:26 +0800

    bash-completion@2: update 2.11 bottle.

---
+++ Formula/bash-completion@2.rb
@@ -0,0 +1,53 @@
+class BashCompletionAT2 < Formula
+  desc "Programmable completion for Bash 4.2+"
+  homepage "https://github.com/scop/bash-completion"
+  url "https://github.com/scop/bash-completion/releases/download/2.11/bash-completion-2.11.tar.xz"
+  sha256 "73a8894bad94dee83ab468fa09f628daffd567e8bef1a24277f1e9a0daf911ac"
+  license "GPL-2.0"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/bash-completion@2"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "c4551dd2b4efcc64fe37febc7471365cba49648a46437972aeb57bb3ca0a3b08"
+  end
+
+  head do
+    url "https://github.com/scop/bash-completion.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
+  depends_on "bash"
+
+  conflicts_with "bash-completion",
+    because: "each are different versions of the same formula"
+
+  def install
+    inreplace "bash_completion" do |s|
+      s.gsub! "readlink -f", "readlink"
+      # Automatically read Homebrew's existing v1 completions
+      s.gsub! ":-/etc/bash_completion.d", ":-#{etc}/bash_completion.d"
+    end
+
+    system "autoreconf", "-i" if build.head?
+    system "./configure", "--prefix=#{prefix}"
+    ENV.deparallelize
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      Add the following line to your ~/.bash_profile:
+        [[ -r "#{etc}/profile.d/bash_completion.sh" ]] && . "#{etc}/profile.d/bash_completion.sh"
+    EOS
+  end
+
+  test do
+    system "test", "-f", "#{share}/bash-completion/bash_completion"
+  end
+end
