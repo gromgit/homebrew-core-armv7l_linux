65e219a966050a74cf5e0e8355c158808ff7d850
Adrian Ho <aho+rpi5@03s.net>
2022-06-07 04:43:18 +0800

    blitz: update 1.0.2 bottle.

---
+++ Formula/blitz.rb
@@ -0,0 +1,40 @@
+class Blitz < Formula
+  desc "Multi-dimensional array library for C++"
+  homepage "https://github.com/blitzpp/blitz/wiki"
+  url "https://github.com/blitzpp/blitz/archive/1.0.2.tar.gz"
+  sha256 "500db9c3b2617e1f03d0e548977aec10d36811ba1c43bb5ef250c0e3853ae1c2"
+  license "Artistic-2.0"
+  head "https://github.com/blitzpp/blitz.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/blitz"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "cd0c1d0b1eed3aceedffe3871cb79c24655f93e0155536c8c77a5b1d23407838"
+  end
+
+  depends_on "cmake" => :build
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "lib"
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"testfile.cpp").write <<~EOS
+      #include <blitz/array.h>
+      #include <cstdlib>
+      using namespace blitz;
+      int main(){
+        Array<float,2> A(3,1);
+        A = 17, 2, 97;
+        cout << "A = " << A << endl;
+        return 0;}
+    EOS
+    system ENV.cxx, "testfile.cpp", "-o", "testfile"
+    output = shell_output("./testfile")
+    var = "/A\ =\ \(0,2\)\ x\ \(0,0\)\n\[\ 17\ \n\ \ 2\ \n\ \ 97\ \]\n\n/"
+    assert_match output, var
+  end
+end
