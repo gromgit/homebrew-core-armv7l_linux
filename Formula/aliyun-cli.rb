c1b732051c558a7201962d5a73b9c02a763a14a5
Adrian Ho <aho+rpi5@03s.net>
2022-06-06 21:02:32 +0800

    aliyun-cli: update 3.0.121 bottle.

---
+++ Formula/aliyun-cli.rb
@@ -0,0 +1,35 @@
+class AliyunCli < Formula
+  desc "Universal Command-Line Interface for Alibaba Cloud"
+  homepage "https://github.com/aliyun/aliyun-cli"
+  url "https://github.com/aliyun/aliyun-cli.git",
+      tag:      "v3.0.121",
+      revision: "624846497c415f00e0a652e0a386c916068f21c6"
+  license "Apache-2.0"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/aliyun-cli"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "7f2593981c2a475b28e5d6e404034dc4a61a8f7091cfcb816cee7d7ff685ae36"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X github.com/aliyun/aliyun-cli/cli.Version=#{version}"
+    system "go", "build", *std_go_args(output: bin/"aliyun", ldflags: ldflags), "main/main.go"
+  end
+
+  test do
+    version_out = shell_output("#{bin}/aliyun version")
+    assert_match version.to_s, version_out
+
+    help_out = shell_output("#{bin}/aliyun --help")
+    assert_match "Alibaba Cloud Command Line Interface Version #{version}", help_out
+    assert_match "", help_out
+    assert_match "Usage:", help_out
+    assert_match "aliyun <product> <operation> [--parameter1 value1 --parameter2 value2 ...]", help_out
+
+    oss_out = shell_output("#{bin}/aliyun oss")
+    assert_match "Object Storage Service", oss_out
+    assert_match "aliyun oss [command] [args...] [options...]", oss_out
+  end
+end
