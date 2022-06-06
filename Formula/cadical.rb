bc2bd24362c7c46528b9926fdf00d3a32c1a2c83
Adrian Ho <aho+rpi5@03s.net>
2022-06-07 07:17:00 +0800

    cadical: update 1.5.2_1 bottle.

--- Formula/cadical.rb
+++ Formula/cadical.rb
@@ -12,12 +12,9 @@ class Cadical < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "56c14010ece80c5889af1c9f4f87b0e7890dad48a686c8f5c8430299812eaf48"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b2ede739d74122cd47e687aab34bcaae1ed69e0c7ff41e2b8390befd0c959d31"
-    sha256 cellar: :any_skip_relocation, monterey:       "bec982b9cd3bd2ffe04b0226dd34cdde9278f1fe54184fde851d9648b0932cee"
-    sha256 cellar: :any_skip_relocation, big_sur:        "abf51a356e0c2567a306981f4178a76279169e7040fdb0cbed914c5ef0f67c66"
-    sha256 cellar: :any_skip_relocation, catalina:       "b31d7cb63660964c6382e78f49065b67a106ba19d7ba0b0e108e7438413bb058"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1a508084a9b72046fda4f6cf6543b4402626aa14705b12813f73bd343edaf57f"
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/cadical"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "39fdd7d9d5f9f8c6601ebeebf255f0621a59594c900678da4ec987eb5e061c7d"
   end
 
   def install
