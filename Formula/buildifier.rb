57e63d3bf51c08d54537c584c3deb31d7b870f6a
Adrian Ho <aho+rpi5@03s.net>
2022-06-07 06:01:30 +0800

    buildifier: update 5.1.0 bottle.

---
+++ Formula/buildifier.rb
@@ -0,0 +1,24 @@
+class Buildifier < Formula
+  desc "Format bazel BUILD files with a standard convention"
+  homepage "https://github.com/bazelbuild/buildtools"
+  url "https://github.com/bazelbuild/buildtools/archive/5.1.0.tar.gz"
+  sha256 "e3bb0dc8b0274ea1aca75f1f8c0c835adbe589708ea89bf698069d0790701ea3"
+  license "Apache-2.0"
+  head "https://github.com/bazelbuild/buildtools.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/buildifier"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "608149659f299fae9567a3e2cdeadde6beba4dc84be4c9c63ceb80440ddc5cfd"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "./buildifier"
+  end
+
+  test do
+    touch testpath/"BUILD"
+    system "#{bin}/buildifier", "-mode=check", "BUILD"
+  end
+end
