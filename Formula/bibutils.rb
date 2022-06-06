8ae3e8cd215306919933a68db0e79020a3a05669
Adrian Ho <aho+rpi5@03s.net>
2022-06-07 04:03:26 +0800

    bibutils: update 7.2 bottle.

---
+++ Formula/bibutils.rb
@@ -0,0 +1,38 @@
+class Bibutils < Formula
+  desc "Bibliography conversion utilities"
+  homepage "https://sourceforge.net/p/bibutils/home/Bibutils/"
+  url "https://downloads.sourceforge.net/project/bibutils/bibutils_7.2_src.tgz"
+  sha256 "6e028aef1e8a6b3e5acef098584a7bb68708f35cfe74011b341c11fea5e4b5c3"
+  license "GPL-2.0-only"
+
+  livecheck do
+    url :stable
+    regex(%r{url=.*?/bibutils[._-]v?(\d+(?:\.\d+)+)[._-]src\.t}i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/bibutils"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "b585919473c78b39f4be4407cacdd9e19209daa0863ead413a10854c3450d4f3"
+  end
+
+  def install
+    system "./configure", "--install-dir", bin,
+                          "--install-lib", lib
+    system "make", "install", "CC=#{ENV.cc}"
+  end
+
+  test do
+    (testpath/"test.bib").write <<~EOS
+      @article{Homebrew,
+          title   = {Something},
+          author  = {Someone},
+          journal = {Something},
+          volume  = {1},
+          number  = {2},
+          pages   = {3--4}
+      }
+    EOS
+
+    system "#{bin}/bib2xml", "test.bib"
+  end
+end
