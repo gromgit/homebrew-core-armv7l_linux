7385ecbc87c83e6548a853b93c77c4735f2ff153
Adrian Ho <aho+rpi5@03s.net>
2022-06-06 23:14:00 +0800

    arkade: update 0.8.26 bottle.

---
+++ Formula/arkade.rb
@@ -0,0 +1,42 @@
+class Arkade < Formula
+  desc "Open Source Kubernetes Marketplace"
+  homepage "https://blog.alexellis.io/kubernetes-marketplace-two-year-update/"
+  url "https://github.com/alexellis/arkade.git",
+      tag:      "0.8.26",
+      revision: "613830dba8a5a8a529ce9ff2390b054b20fa6929"
+  license "MIT"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/arkade"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "7183fb99696edfbc13cc373572a8821b77f7b9788fefa743ea1826387126273d"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/alexellis/arkade/cmd.Version=#{version}
+      -X github.com/alexellis/arkade/cmd.GitCommit=#{Utils.git_head}
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags)
+
+    bin.install_symlink "arkade" => "ark"
+
+    (zsh_completion/"_arkade").write Utils.safe_popen_read(bin/"arkade", "completion", "zsh")
+    (bash_completion/"arkade").write Utils.safe_popen_read(bin/"arkade", "completion", "bash")
+    (fish_completion/"arkade.fish").write Utils.safe_popen_read(bin/"arkade", "completion", "fish")
+    # make zsh completion also work for `ark` symlink
+    inreplace zsh_completion/"_arkade", "#compdef _arkade arkade", "#compdef _arkade arkade ark=arkade"
+  end
+
+  test do
+    assert_match "Version: #{version}", shell_output(bin/"arkade version")
+    assert_match "Info for app: openfaas", shell_output(bin/"arkade info openfaas")
+  end
+end
