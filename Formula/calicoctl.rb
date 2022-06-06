04b05234ea53ea12526e7e54152fd641f7d9038f
Adrian Ho <aho+rpi5@03s.net>
2022-06-07 07:23:21 +0800

    calicoctl: update 3.23.1 bottle.

---
+++ Formula/calicoctl.rb
@@ -0,0 +1,36 @@
+class Calicoctl < Formula
+  desc "Calico CLI tool"
+  homepage "https://www.projectcalico.org"
+  url "https://github.com/projectcalico/calico.git",
+      tag:      "v3.23.1",
+      revision: "967e2454314046f6568d1c571e8d97e000540f2d"
+  license "Apache-2.0"
+  head "https://github.com/projectcalico/calico.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/calicoctl"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "39783de1315a3a2bf6341508b19a1e6e5ed5eef204cf155b406d6762b14ce788"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    commands = "github.com/projectcalico/calico/calicoctl/calicoctl/commands"
+    ldflags = "-X #{commands}.VERSION=#{version} " \
+              "-X #{commands}.GIT_REVISION=#{Utils.git_short_head} " \
+              "-s -w"
+    system "go", "build", *std_go_args(ldflags: ldflags), "calicoctl/calicoctl/calicoctl.go"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/calicoctl version")
+
+    assert_match "invalid configuration: no configuration has been provided",
+      shell_output("#{bin}/calicoctl datastore migrate lock 2>&1", 1)
+  end
+end
