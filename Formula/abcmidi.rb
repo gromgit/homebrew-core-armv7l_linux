412c9a65b914bd86bedbe5d8aaf202a67c0784c8
Adrian Ho <aho+rpi5@03s.net>
2022-06-06 19:33:52 +0800

    abcmidi: update 2022.05.20 bottle.

---
+++ Formula/abcmidi.rb
@@ -0,0 +1,47 @@
+class Abcmidi < Formula
+  desc "Converts abc music notation files to MIDI files"
+  homepage "https://ifdo.ca/~seymour/runabc/top.html"
+  url "https://ifdo.ca/~seymour/runabc/abcMIDI-2022.05.20.zip"
+  sha256 "2842a47e4ce5fc7d2f1d9e2c1c7cecf342ac01a2cb55e02174c643a738779480"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?abcMIDI[._-]v?(\d{4}(?:\.\d+)+)\.zip/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/abcmidi"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "451904a2405013a9dd967767aed973937cc1f5edffd984d67ddf74fabd1f80d8"
+  end
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--prefix=#{prefix}",
+                          "--mandir=#{man}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"balk.abc").write <<~EOS
+      X: 1
+      T: Abdala
+      F: https://www.youtube.com/watch?v=YMf8yXaQDiQ
+      L: 1/8
+      M: 2/4
+      K:Cm
+      Q:1/4=180
+      %%MIDI bassprog 32 % 32 Acoustic Bass
+      %%MIDI program 23 % 23 Tango Accordian
+      %%MIDI bassvol 69
+      %%MIDI gchord fzfz
+      |:"G"FDEC|D2C=B,|C2=B,2 |C2D2   |\
+        FDEC   |D2C=B,|C2=B,2 |A,2G,2 :|
+      |:=B,CDE |D2C=B,|C2=B,2 |C2D2   |\
+        =B,CDE |D2C=B,|C2=B,2 |A,2G,2 :|
+      |:C2=B,2 |A,2G,2| C2=B,2|A,2G,2 :|
+    EOS
+
+    system "#{bin}/abc2midi", (testpath/"balk.abc")
+  end
+end
