807f8a127956da6a11f43043fd49e0b8df98a0aa
Adrian Ho <aho+rpi5@03s.net>
2022-06-07 06:29:57 +0800

    byacc: update 20220128 bottle.

---
+++ Formula/byacc.rb
@@ -0,0 +1,27 @@
+class Byacc < Formula
+  desc "(Arguably) the best yacc variant"
+  homepage "https://invisible-island.net/byacc/"
+  url "https://invisible-mirror.net/archives/byacc/byacc-20220128.tgz"
+  sha256 "42c1805cc529314e6a76326fe1b33e80c70862a44b01474da362e2f7db2d749c"
+  license :public_domain
+
+  livecheck do
+    url "https://invisible-mirror.net/archives/byacc/"
+    regex(/href=.*?byacc[._-]v?(\d{6,8})\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/byacc"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "61153e7c2fb73029a1392cbb36226c475c997ea7254cfc10a8192483839c2595"
+  end
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--program-prefix=b", "--prefix=#{prefix}", "--man=#{man}"
+    system "make", "install"
+  end
+
+  test do
+    system bin/"byacc", "-V"
+  end
+end
