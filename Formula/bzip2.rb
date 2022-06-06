a6e59c36101d23a32743b1dc1cba183c89b61a42
Adrian Ho <aho+rpi5@03s.net>
2022-06-06 22:14:49 +0800

    bzip2: update 1.0.8 bottle.

---
+++ Formula/bzip2.rb
@@ -0,0 +1,46 @@
+class Bzip2 < Formula
+  desc "Freely available high-quality data compressor"
+  homepage "https://sourceware.org/bzip2/"
+  url "https://sourceware.org/pub/bzip2/bzip2-1.0.8.tar.gz"
+  sha256 "ab5a03176ee106d3f0fa90e381da478ddae405918153cca248e682cd0c4a2269"
+  license "bzip2-1.0.6"
+
+  livecheck do
+    url "https://sourceware.org/pub/bzip2/"
+    regex(/href=.*?bzip2[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/bzip2"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "434c7b77c5efda8482b1096329aac86e56a8d660fda7c7b062b65f457ecffd15"
+  end
+
+  keg_only :provided_by_macos
+
+  def install
+    inreplace "Makefile", "$(PREFIX)/man", "$(PREFIX)/share/man"
+
+    system "make", "install", "PREFIX=#{prefix}"
+
+    if OS.linux?
+      # Install shared libraries
+      system "make", "-f", "Makefile-libbz2_so", "clean"
+      system "make", "-f", "Makefile-libbz2_so"
+      lib.install "libbz2.so.#{version}", "libbz2.so.#{version.major_minor}"
+      lib.install_symlink "libbz2.so.#{version}" => "libbz2.so.#{version.major}"
+      lib.install_symlink "libbz2.so.#{version}" => "libbz2.so"
+    end
+  end
+
+  test do
+    testfilepath = testpath + "sample_in.txt"
+    zipfilepath = testpath + "sample_in.txt.bz2"
+
+    testfilepath.write "TEST CONTENT"
+
+    system "#{bin}/bzip2", testfilepath
+    system "#{bin}/bunzip2", zipfilepath
+
+    assert_equal "TEST CONTENT", testfilepath.read
+  end
+end
