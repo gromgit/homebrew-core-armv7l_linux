b4f1220bce50b8fdf2dcecf8cafd7689298f2217
Adrian Ho <aho+rpi5@03s.net>
2022-06-06 22:59:26 +0800

    argocd-vault-plugin: update 1.11.0 bottle.

---
+++ Formula/argocd-vault-plugin.rb
@@ -0,0 +1,37 @@
+class ArgocdVaultPlugin < Formula
+  desc "Argo CD plugin to retrieve secrets from Secret Management tools"
+  homepage "https://argocd-vault-plugin.readthedocs.io"
+  url "https://github.com/argoproj-labs/argocd-vault-plugin.git",
+      tag:      "v1.11.0",
+      revision: "4133295001e037b917a8884da84af42f12a51cae"
+  license "Apache-2.0"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/argocd-vault-plugin"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "aff8ca648ed6d160b698a28dad3d5a7dbe38790072f46d325bd9587f075dd5d2"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+
+    ldflags = %W[
+      -s -w
+      -X github.com/argoproj-labs/argocd-vault-plugin/version.Version=#{version}
+      -X github.com/argoproj-labs/argocd-vault-plugin/version.BuildDate=#{time.iso8601}
+      -X github.com/argoproj-labs/argocd-vault-plugin/version.CommitSHA=#{Utils.git_head}
+    ]
+
+    system "go", "build", *std_go_args(ldflags: ldflags)
+  end
+
+  test do
+    assert_match "This is a plugin to replace <placeholders> with Vault secrets",
+      shell_output("#{bin}/argocd-vault-plugin --help")
+
+    touch testpath/"empty.yaml"
+    assert_match "Error: Must provide a supported Vault Type",
+      shell_output("#{bin}/argocd-vault-plugin generate ./empty.yaml 2>&1", 1)
+  end
+end
