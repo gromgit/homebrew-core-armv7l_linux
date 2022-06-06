728b7de7c444b18c7fd5ea9e1d28967cab46de0d
Adrian Ho <aho+rpi5@03s.net>
2022-06-07 06:58:09 +0800

    caddy: update 2.5.1 bottle.

--- Formula/caddy.rb
+++ Formula/caddy.rb
@@ -7,12 +7,9 @@ class Caddy < Formula
   head "https://github.com/caddyserver/caddy.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7ab2af1cb302e0995d647d7176a159f6713a09b040b6c42108f6f8cd0ba1a8a0"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7ab2af1cb302e0995d647d7176a159f6713a09b040b6c42108f6f8cd0ba1a8a0"
-    sha256 cellar: :any_skip_relocation, monterey:       "0dead9c1b56850d827fd38af040beb27df250147dfee9d96fca2d3b7588bd2ee"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0dead9c1b56850d827fd38af040beb27df250147dfee9d96fca2d3b7588bd2ee"
-    sha256 cellar: :any_skip_relocation, catalina:       "0dead9c1b56850d827fd38af040beb27df250147dfee9d96fca2d3b7588bd2ee"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b782b13b8d77625c9c8de6276cd444fabbcf1ef687bdcb0e38f1616fa9a2f3aa"
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/caddy"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "548be0de165b9a4b1390b21386502fc5932ff1220557cf1a98287bb25a6f9a1c"
   end
 
   depends_on "go" => :build
