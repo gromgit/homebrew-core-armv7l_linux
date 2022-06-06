a387845383144da2abddaa925798fc5dc66f1ca1
Adrian Ho <aho+rpi5@03s.net>
2022-06-06 19:31:31 +0800

    abcm2ps: update 8.14.13 bottle.

---
+++ Formula/abcm2ps.rb
@@ -0,0 +1,45 @@
+class Abcm2ps < Formula
+  desc "ABC music notation software"
+  homepage "http://moinejf.free.fr"
+  url "https://github.com/leesavide/abcm2ps/archive/v8.14.13.tar.gz"
+  sha256 "8db29e33f30fce609ddffeb641c3bfe60ae5d16c3305402157ddc88bcaaf0c5b"
+  license "GPL-3.0-or-later"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/abcm2ps"
+    sha256 armv7l_linux: "01b22aebfe8b4680d61d956ec6b7f1f19e78ccefb8bd41d6572dadd0dd0f9315"
+  end
+
+  depends_on "pkg-config" => :build
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"voices.abc").write <<~EOS
+      X:7
+      T:Qui Tolis (Trio)
+      C:Andre Raison
+      M:3/4
+      L:1/4
+      Q:1/4=92
+      %%staves {(Pos1 Pos2) Trompette}
+      K:F
+      %
+      V:Pos1
+      %%MIDI program 78
+      "Positif"x3 |x3|c'>ba|Pga/g/f|:g2a |ba2 |g2c- |c2P=B  |c>de  |fga |
+      V:Pos2
+      %%MIDI program 78
+              Mf>ed|cd/c/B|PA2d |ef/e/d |:e2f |ef2 |c>BA |GA/G/F |E>FG |ABc- |
+      V:Trompette
+      %%MIDI program 56
+      "Trompette"z3|z3 |z3 |z3 |:Mc>BA|PGA/G/F|PE>EF|PEF/E/D|C>CPB,|A,G,F,-|
+    EOS
+
+    system "#{bin}/abcm2ps", testpath/"voices"
+    assert_predicate testpath/"Out.ps", :exist?
+  end
+end
