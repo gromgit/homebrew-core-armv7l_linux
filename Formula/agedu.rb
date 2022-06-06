8298cf8836b24ed812d566f1b4615ab8ea09ab5e
Adrian Ho <aho+rpi5@03s.net>
2022-06-06 20:27:53 +0800

    agedu: update 20211129 bottle.

---
+++ Formula/agedu.rb
@@ -0,0 +1,32 @@
+class Agedu < Formula
+  desc "Unix utility for tracking down wasted disk space"
+  homepage "https://www.chiark.greenend.org.uk/~sgtatham/agedu/"
+  url "https://www.chiark.greenend.org.uk/~sgtatham/agedu/agedu-20211129.8cd63c5.tar.gz"
+  version "20211129"
+  sha256 "ceaee592ef21b8cbb254aa7e9c5d22cefab24535e137618a4d0af591eba8339f"
+  license "MIT"
+  head "https://git.tartarus.org/simon/agedu.git", branch: "main"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?agedu[._-]v?(\d+(?:\.\d+)*)(?:[._-][\da-z]+)?\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/agedu"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "94e29b4aa8ec0341f04cfc9abbf7914d8508dd28ef33c1253ede622463ba2c74"
+  end
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    system bin/"agedu", "-s", "."
+    assert_predicate testpath/"agedu.dat", :exist?
+  end
+end
