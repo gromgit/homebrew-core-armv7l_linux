26b56a3d082d7fd1aaff6a5feb8cf0a734a8c546
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 17:44:44 +0800

    ctlptl: update 0.8.3 bottle.

---
+++ Formula/ctlptl.rb
@@ -0,0 +1,41 @@
+class Ctlptl < Formula
+  desc "Making local Kubernetes clusters fun and easy to set up"
+  homepage "https://github.com/tilt-dev/ctlptl"
+  url "https://github.com/tilt-dev/ctlptl/archive/v0.8.3.tar.gz"
+  sha256 "321fcefac0e1e6377e503c036b8c370348ee2df7836c5508133e8b7eeabd5d81"
+  license "Apache-2.0"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/ctlptl"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "2f2e8fbdcd9d13a8c3111b5041177e29ced632de7044da192d5f6361feb07393"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+      -X main.date=#{time.iso8601}
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/ctlptl"
+
+    # Install bash completion
+    output = Utils.safe_popen_read(bin/"ctlptl", "completion", "bash")
+    (bash_completion/"ctlptl").write output
+
+    # Install zsh completion
+    output = Utils.safe_popen_read(bin/"ctlptl", "completion", "zsh")
+    (zsh_completion/"_ctlptl").write output
+
+    # Install fish completion
+    output = Utils.safe_popen_read(bin/"ctlptl", "completion", "fish")
+    (fish_completion/"ctlptl.fish").write output
+  end
+
+  test do
+    assert_match "v#{version}", shell_output("#{bin}/ctlptl version")
+    assert_equal "", shell_output("#{bin}/ctlptl get")
+    assert_match "not found", shell_output("#{bin}/ctlptl delete cluster nonexistent 2>&1", 1)
+  end
+end
