057edd86b8be8f5ad9908d8141e6ec5d61617f59
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 07:33:14 +0800

    gitleaks: update 8.8.7 bottle.

---
+++ Formula/gitleaks.rb
@@ -0,0 +1,37 @@
+class Gitleaks < Formula
+  desc "Audit git repos for secrets"
+  homepage "https://github.com/zricethezav/gitleaks"
+  url "https://github.com/zricethezav/gitleaks/archive/v8.8.7.tar.gz"
+  sha256 "b99b8613f3482707d35e866c23ca8f39ad0fa3bda6613568c087766a050c20a2"
+  license "MIT"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/gitleaks"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "86686254e62305248cb0cd18380867452a8fd08e48cb0ead33cd1598b2f27d21"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-X github.com/zricethezav/gitleaks/v#{version.major}/cmd.Version=#{version}"
+    system "go", "build", *std_go_args(ldflags: ldflags)
+
+    bash_output = Utils.safe_popen_read(bin/"gitleaks", "completion", "bash")
+    (bash_completion/"gitleaks").write bash_output
+
+    zsh_output = Utils.safe_popen_read(bin/"gitleaks", "completion", "zsh")
+    (zsh_completion/"_gitleaks").write zsh_output
+
+    fish_output = Utils.safe_popen_read(bin/"gitleaks", "completion", "fish")
+    (fish_completion/"gitleaks.fish").write fish_output
+  end
+
+  test do
+    (testpath/"README").write "ghp_deadbeefdeadbeefdeadbeefdeadbeefdeadbeef"
+    system "git", "init"
+    system "git", "add", "README"
+    system "git", "commit", "-m", "Initial commit"
+    assert_match(/WRN\S* leaks found: [1-9]/, shell_output("#{bin}/gitleaks detect 2>&1", 1))
+    assert_equal version.to_s, shell_output("#{bin}/gitleaks version").strip
+  end
+end
