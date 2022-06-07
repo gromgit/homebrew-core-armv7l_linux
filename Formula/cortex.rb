3326173f46bf9a8bfb3cc53cbe2cf9e3c6efd0b3
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 16:56:51 +0800

    cortex: update 1.11.1 bottle.

---
+++ Formula/cortex.rb
@@ -0,0 +1,51 @@
+class Cortex < Formula
+  desc "Long term storage for Prometheus"
+  homepage "https://cortexmetrics.io/"
+  url "https://github.com/cortexproject/cortex/archive/v1.11.1.tar.gz"
+  sha256 "7219bac21a49af6b3c3dd691aa867573f8b8d89f91a4433bfa185bc775ec3d3b"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/cortex"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "766eb7b4887b92d2d120bd9437ec452523de07d88bdf16ba5b9a54de9eb09afa"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args, "./cmd/cortex"
+    cd "docs/chunks-storage" do
+      inreplace "single-process-config.yaml", "/tmp", var
+      etc.install "single-process-config.yaml" => "cortex.yaml"
+    end
+  end
+
+  service do
+    run [opt_bin/"cortex", "-config.file=#{etc}/cortex.yaml"]
+    keep_alive true
+    error_log_path var/"log/cortex.log"
+    log_path var/"log/cortex.log"
+    working_dir var
+  end
+
+  test do
+    port = free_port
+
+    cp etc/"cortex.yaml", testpath
+    inreplace "cortex.yaml" do |s|
+      s.gsub! "9009", port.to_s
+      s.gsub! var, testpath
+    end
+
+    fork { exec bin/"cortex", "-config.file=cortex.yaml", "-server.grpc-listen-port=#{free_port}" }
+    sleep 3
+
+    output = shell_output("curl -s localhost:#{port}/services")
+    assert_match "Running", output
+  end
+end
