1ad28cb12aef0f30ada573dc07a215c91174a048
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 19:02:31 +0800

    dnscontrol: update 3.16.2 bottle.

---
+++ Formula/dnscontrol.rb
@@ -0,0 +1,41 @@
+class Dnscontrol < Formula
+  desc "It is system for maintaining DNS zones"
+  homepage "https://github.com/StackExchange/dnscontrol"
+  url "https://github.com/StackExchange/dnscontrol/archive/v3.16.2.tar.gz"
+  sha256 "c2c923bde3d6bad1790664dcd72fff6ca9f135656b5e19f862d3ad706a4b54ff"
+  license "MIT"
+  version_scheme 1
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/dnscontrol"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "7ddda5cd977e3d6482c554db5f7e0e42fde973f3f7c3e18968dcb1d9106ec176"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    (testpath/"dnsconfig.js").write <<~EOS
+      var namecom = NewRegistrar("name.com", "NAMEDOTCOM");
+      var r53 = NewDnsProvider("r53", "ROUTE53")
+
+      D("example.com", namecom, DnsProvider(r53),
+        A("@", "1.2.3.4"),
+        CNAME("www","@"),
+        MX("@",5,"mail.myserver.com."),
+        A("test", "5.6.7.8")
+      )
+    EOS
+
+    output = shell_output("#{bin}/dnscontrol check #{testpath}/dnsconfig.js 2>&1").strip
+    assert_equal "No errors.", output
+  end
+end
