740063200150d677b3cd32b004b04ea74fbf9521
Adrian Ho <aho+rpi5@03s.net>
2022-06-07 08:35:38 +0800

    cern-ndiff: update 5.08.01 bottle.

---
+++ Formula/cern-ndiff.rb
@@ -0,0 +1,34 @@
+class CernNdiff < Formula
+  desc "Numerical diff tool"
+  # NOTE: ndiff is a sub-project of Mad-X at the moment..
+  homepage "https://mad.web.cern.ch/mad/"
+  url "https://github.com/MethodicalAcceleratorDesign/MAD-X/archive/5.08.01.tar.gz"
+  sha256 "89c943fcb474344a4f7d28de98e8eae0aec40f779bf908daff79043bf3520555"
+  head "https://github.com/MethodicalAcceleratorDesign/MAD-X.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/cern-ndiff"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "bb81722879ad7aebfc594e8f91364fdbdc4a1889923fc045b0e642132286b23f"
+  end
+
+  depends_on "cmake" => :build
+
+  def install
+    cd "tools/numdiff" do
+      system "cmake", ".", *std_cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"lhs.txt").write("0.0 2e-3 0.003")
+    (testpath/"rhs.txt").write("1e-7 0.002 0.003")
+    (testpath/"test.cfg").write("*   * abs=1e-6")
+    system "#{bin}/ndiff", "lhs.txt", "rhs.txt", "test.cfg"
+  end
+end
