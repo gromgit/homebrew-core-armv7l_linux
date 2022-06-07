9bb5fbf1229ec7c665e76edb36beb78f3ab667f2
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 04:53:09 +0800

    eksctl: update 0.100.0 bottle.

---
+++ Formula/eksctl.rb
@@ -0,0 +1,56 @@
+class Eksctl < Formula
+  desc "Simple command-line tool for creating clusters on Amazon EKS"
+  homepage "https://eksctl.io"
+  url "https://github.com/weaveworks/eksctl.git",
+      tag:      "0.100.0",
+      revision: "b6400b6c60844de35a7e9b173cc76d0debaac438"
+  license "Apache-2.0"
+  head "https://github.com/weaveworks/eksctl.git", branch: "main"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/eksctl"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "d07a33ba9b523297b329bd6d660ec5ee02c0723dde1860bdc7685f7681b49a7c"
+  end
+
+  depends_on "counterfeiter" => :build
+  depends_on "go" => :build
+  depends_on "go-bindata" => :build
+  depends_on "mockery" => :build
+  depends_on "aws-iam-authenticator"
+
+  # Eksctl requires newer version of ifacemaker
+  #
+  # Replace with `depends_on "ifacemaker" => :build` when ifacemaker > 1.2.0
+  # Until then get the resource version from go.mod
+  resource "ifacemaker" do
+    url "https://github.com/vburenin/ifacemaker/archive/b2018d8549dc4d51ce7e2254d6b0a743643613be.tar.gz"
+    sha256 "41888bf97133b4e7e190f2040378661b5bcab290d009e1098efbcb9db0f1d82f"
+  end
+
+  def install
+    resource("ifacemaker").stage do
+      system "go", "build", *std_go_args(ldflags: "-s -w", output: buildpath/"ifacemaker")
+    end
+    inreplace "build/scripts/generate-aws-interfaces.sh", "${GOBIN}/ifacemaker",
+                                                          buildpath/"ifacemaker"
+
+    ENV["GOBIN"] = HOMEBREW_PREFIX/"bin"
+    system "make", "build"
+    bin.install "eksctl"
+
+    bash_output = Utils.safe_popen_read(bin/"eksctl", "completion", "bash")
+    (bash_completion/"eksctl").write bash_output
+    zsh_output = Utils.safe_popen_read(bin/"eksctl", "completion", "zsh")
+    (zsh_completion/"_eksctl").write zsh_output
+    fish_output = Utils.safe_popen_read(bin/"eksctl", "completion", "fish")
+    (fish_completion/"eksctl.fish").write fish_output
+  end
+
+  test do
+    assert_match "The official CLI for Amazon EKS",
+      shell_output("#{bin}/eksctl --help")
+
+    assert_match "Error: couldn't create node group filter from command line options: --cluster must be set",
+      shell_output("#{bin}/eksctl create nodegroup 2>&1", 1)
+  end
+end
