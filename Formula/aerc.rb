3f05018269c9766efd7a89340f470ef7b57a7ff5
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 14:23:27 +0800

    aerc: update 0.10.0 bottle.

--- Formula/aerc.rb
+++ Formula/aerc.rb
@@ -7,12 +7,9 @@ class Aerc < Formula
   head "https://git.sr.ht/~rjarry/aerc", branch: "master"
 
   bottle do
-    sha256 arm64_monterey: "07b0f639519dfeebab62d97d34e2be7948d7a7eed34c2296200f2753f4e8cb1f"
-    sha256 arm64_big_sur:  "7e5b591f483879bace3dc00f675e15aa1b07bc4fdd7af0c913b5d9f884325131"
-    sha256 monterey:       "a78fa9e52072723610b91543d8e5a1cf8beccf9ed39247d6564015cdb6e2d929"
-    sha256 big_sur:        "a71ff65f4b44d97327fb92ab2cd4f3adb387fe82b546875deeb6e782abdad7d7"
-    sha256 catalina:       "9ddcc5a17bd657c13e4fc170d8d48c0c6593fabfefde103c0f31d0a8ad45f753"
-    sha256 x86_64_linux:   "57c91eb2697be7bedba392caa3b2594b33ef66167a276ac68aabcd1cc495497d"
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/aerc"
+    rebuild 1
+    sha256 armv7l_linux: "e58b898cf02816248b6ecdd53ee8d7215d3e90af9cd713b7e152504739ef3ebc"
   end
 
   depends_on "go" => :build
