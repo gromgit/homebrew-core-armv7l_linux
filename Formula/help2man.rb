954f13840701a7198a3e841bf42c0584e2a64ba9
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 12:26:12 +0800

    help2man: update 1.49.2 bottle.

---
+++ Formula/help2man.rb
@@ -0,0 +1,55 @@
+class Help2man < Formula
+  desc "Automatically generate simple man pages"
+  homepage "https://www.gnu.org/software/help2man/"
+  url "https://ftp.gnu.org/gnu/help2man/help2man-1.49.2.tar.xz"
+  mirror "https://ftpmirror.gnu.org/help2man/help2man-1.49.2.tar.xz"
+  sha256 "9e2e0e213a7e0a36244eed6204d902b6504602a578b6ecd15268b1454deadd36"
+  license "GPL-3.0-or-later"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/help2man"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "383b411c2f38bcc248ef62253135047fcdf62d9dc53f8204d8789648705750e2"
+  end
+
+  depends_on "gettext" if Hardware::CPU.intel?
+
+  uses_from_macos "perl"
+
+  resource "Locale::gettext" do
+    url "https://cpan.metacpan.org/authors/id/P/PV/PVANDRY/gettext-1.07.tar.gz"
+    sha256 "909d47954697e7c04218f972915b787bd1244d75e3bd01620bc167d5bbc49c15"
+  end
+
+  def install
+    ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
+
+    if Hardware::CPU.intel?
+      resource("Locale::gettext").stage do
+        system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}"
+        system "make", "install"
+      end
+    end
+
+    # install is not parallel safe
+    # see https://github.com/Homebrew/homebrew/issues/12609
+    ENV.deparallelize
+
+    args = []
+    args << "--enable-nls" if Hardware::CPU.intel?
+
+    system "./configure", "--prefix=#{prefix}", *args
+    system "make", "install"
+    (libexec/"bin").install "#{bin}/help2man"
+    (bin/"help2man").write_env_script("#{libexec}/bin/help2man", PERL5LIB: ENV["PERL5LIB"])
+  end
+
+  test do
+    out = if Hardware::CPU.intel?
+      shell_output("#{bin}/help2man --locale=en_US.UTF-8 #{bin}/help2man")
+    else
+      shell_output("#{bin}/help2man #{bin}/help2man")
+    end
+
+    assert_match "help2man #{version}", out
+  end
+end
