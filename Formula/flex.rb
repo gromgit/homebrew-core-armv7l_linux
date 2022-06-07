a5909bd3e557f8504a16889cefebcc889b8db0d7
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 14:12:25 +0800

    flex: update 2.6.4_2 bottle.

---
+++ Formula/flex.rb
@@ -0,0 +1,83 @@
+class Flex < Formula
+  desc "Fast Lexical Analyzer, generates Scanners (tokenizers)"
+  homepage "https://github.com/westes/flex"
+  license "BSD-2-Clause"
+  revision 2
+
+  stable do
+    url "https://github.com/westes/flex/releases/download/v2.6.4/flex-2.6.4.tar.gz"
+    sha256 "e87aae032bf07c26f85ac0ed3250998c37621d95f8bd748b31f15b33c45ee995"
+
+    # Fix -flat_namespace being used on Big Sur and later.
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
+      sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
+    end
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/flex"
+    sha256 armv7l_linux: "2f0277294c133fa823add637891218e711e54c36bfc5842f8efb5eae70b5334c"
+  end
+
+  head do
+    url "https://github.com/westes/flex.git", branch: "master"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+
+    # https://github.com/westes/flex/issues/294
+    depends_on "gnu-sed" => :build
+
+    depends_on "libtool" => :build
+    depends_on :macos # Needs a pre-existing `flex` to bootstrap.
+  end
+
+  keg_only :provided_by_macos
+
+  depends_on "help2man" => :build
+  depends_on "gettext"
+
+  uses_from_macos "bison" => :build
+  uses_from_macos "m4"
+
+  def install
+    if build.head?
+      ENV.prepend_path "PATH", Formula["gnu-sed"].opt_libexec/"gnubin"
+
+      system "./autogen.sh"
+    end
+
+    # Fix segmentation fault during install on Ubuntu 18.04 (caused by glibc 2.26+),
+    # remove with the next release
+    ENV.append "CPPFLAGS", "-D_GNU_SOURCE" if OS.linux?
+
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--enable-shared",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+    bin.install_symlink "flex" => "lex"
+  end
+
+  test do
+    (testpath/"test.flex").write <<~EOS
+      CHAR   [a-z][A-Z]
+      %%
+      {CHAR}+      printf("%s", yytext);
+      [ \\t\\n]+   printf("\\n");
+      %%
+      int main()
+      {
+        yyin = stdin;
+        yylex();
+      }
+    EOS
+    system "#{bin}/flex", "test.flex"
+    system ENV.cc, "lex.yy.c", "-L#{lib}", "-lfl", "-o", "test"
+    assert_equal shell_output("echo \"Hello World\" | ./test"), <<~EOS
+      Hello
+      World
+    EOS
+  end
+end
