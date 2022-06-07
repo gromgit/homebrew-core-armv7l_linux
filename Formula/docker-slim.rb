74433874d4f72943bcae36b8b7bde04e6590b12e
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 19:30:34 +0800

    docker-slim: update 1.37.5 bottle.

---
+++ Formula/docker-slim.rb
@@ -0,0 +1,40 @@
+class DockerSlim < Formula
+  desc "Minify and secure Docker images"
+  homepage "https://dockersl.im"
+  url "https://github.com/docker-slim/docker-slim/archive/1.37.5.tar.gz"
+  sha256 "e395a8865fb888a190032783ee0a9f1a5ac9a13c296b9bd0c503fe81937eed18"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/docker-slim"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "ef4236d6b5032df2d034f284f416ec25e4c23e6f54614c2eca9dccc9e9e7d757"
+  end
+
+  depends_on "go" => :build
+
+  skip_clean "bin/docker-slim-sensor"
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    ldflags = "-s -w -X github.com/docker-slim/docker-slim/pkg/version.appVersionTag=#{version}"
+    system "go", "build", "-trimpath", "-ldflags=#{ldflags}", "-o",
+           bin/"docker-slim", "./cmd/docker-slim"
+
+    # docker-slim-sensor is a Linux binary that is used within Docker
+    # containers rather than directly on the macOS host.
+    ENV["GOOS"] = "linux"
+    system "go", "build", "-trimpath", "-ldflags=#{ldflags}", "-o",
+           bin/"docker-slim-sensor", "./cmd/docker-slim-sensor"
+    (bin/"docker-slim-sensor").chmod 0555
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/docker-slim --version")
+    system "test", "-x", bin/"docker-slim-sensor"
+  end
+end
