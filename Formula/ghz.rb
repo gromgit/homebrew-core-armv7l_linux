29d4f9c48831575c48d7f3fd7d2ab02d666f0e9d
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 06:55:49 +0800

    ghz: update 0.109.0 bottle.

---
+++ Formula/ghz.rb
@@ -0,0 +1,41 @@
+class Ghz < Formula
+  desc "Simple gRPC benchmarking and load testing tool"
+  homepage "https://ghz.sh"
+  url "https://github.com/bojand/ghz/archive/v0.109.0.tar.gz"
+  sha256 "4b0b3c651861923a60ca3370de652eb9f3eb5b0c7510c877ec1af8d82508fd08"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/ghz"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "a8aff716f0739d50d18cfde14fecd9d1c22bc9103bed89a58725c35238bce561"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build",
+      "-ldflags", "-s -w -X main.version=#{version}",
+      *std_go_args,
+      "cmd/ghz/main.go"
+    prefix.install_metafiles
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/ghz -v 2>&1")
+    (testpath/"config.toml").write <<~EOS
+      proto = "greeter.proto"
+      call = "helloworld.Greeter.SayHello"
+      host = "0.0.0.0:50051"
+      insecure = true
+      [data]
+      name = "Bob"
+    EOS
+    assert_match "open greeter.proto: no such file or directory",
+      shell_output("#{bin}/ghz --config config.toml 2>&1", 1)
+  end
+end
