a542a3b6008d1a6b3f6f7e342b90ccd25dd4a357
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 21:23:04 +0800

    fetch: update 0.4.4 bottle.

--- Formula/fetch.rb
+++ Formula/fetch.rb
@@ -7,12 +7,9 @@ class Fetch < Formula
   head "https://github.com/gruntwork-io/fetch.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "837779f78d0700888b4f42cd0c3ae77bfb4365dcc1ffc4480082375b3de4750f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5f1a5814ad2577387d356b1a10bf897b3ec2185b5b9de9a44fdbcd11f9116258"
-    sha256 cellar: :any_skip_relocation, monterey:       "0d5dba201cabff02a8da2779a805ef4552ef54098b4eab86b110085c7d81c8b5"
-    sha256 cellar: :any_skip_relocation, big_sur:        "da985a806b15f92791d6df43b5e541df94e2fb9f6a0238a8fe3b4ae6064a52ac"
-    sha256 cellar: :any_skip_relocation, catalina:       "85ce4f6c485a19995f8e4ed2f80a7bd898b31bc28a734175268d3087fc4457d9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c415e6fb8720c825034807d77167f4ddcd382ab9c1374d912ba46398b4f41643"
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/fetch"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "3a3ae739225d0baf150222e52e1742ce57173d65c2a306cd3447128cf0470af6"
   end
 
   depends_on "go" => :build
