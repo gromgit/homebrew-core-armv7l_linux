dca4c129327882a7f7c1d1ddac97b79034261b47
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 20:35:55 +0800

    efm-langserver: update 0.0.44 bottle.

---
+++ Formula/efm-langserver.rb
@@ -0,0 +1,34 @@
+class EfmLangserver < Formula
+  desc "General purpose Language Server"
+  homepage "https://github.com/mattn/efm-langserver"
+  url "https://github.com/mattn/efm-langserver/archive/v0.0.44.tar.gz"
+  sha256 "825aef5815fb6eff656370e9f01fc31f91e5b2ab9d2b1f080881839676020dac"
+  license "MIT"
+  head "https://github.com/mattn/efm-langserver.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/efm-langserver"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "b8b29c8007ed744f5333f586ff7c0a2c207a4e13b5f02468f7fb5bed2176f83a"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args
+  end
+
+  test do
+    (testpath/"config.yml").write <<~EOS
+      version: 2
+      root-markers:
+        - ".git/"
+      languages:
+        python:
+          - lint-command: "flake8 --stdin-display-name ${INPUT} -"
+            lint-stdin: true
+    EOS
+    output = shell_output("#{bin}/efm-langserver -c #{testpath/"config.yml"} -d")
+    assert_match "version: 2", output
+    assert_match "lint-command: flake8 --stdin-display-name ${INPUT} -", output
+  end
+end
