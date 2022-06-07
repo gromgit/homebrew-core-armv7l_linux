417dcf4385427cb953e6f5a4f8f3e97f72f87923
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 20:27:28 +0800

    duf: update 0.8.1 bottle.

---
+++ Formula/duf.rb
@@ -0,0 +1,27 @@
+class Duf < Formula
+  desc "Disk Usage/Free Utility - a better 'df' alternative"
+  homepage "https://github.com/muesli/duf"
+  url "https://github.com/muesli/duf/archive/v0.8.1.tar.gz"
+  sha256 "ebc3880540b25186ace220c09af859f867251f4ecaef435525a141d98d71a27a"
+  license "MIT"
+  head "https://github.com/muesli/duf.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/duf"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "dd05ae821a511542530a4cff0d2583c60481c9ad9783eb5a64a2c9e70a06dd58"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args
+  end
+
+  test do
+    require "json"
+
+    devices = JSON.parse shell_output("#{bin}/duf --json")
+    assert root = devices.find { |d| d["mount_point"] == "/" }
+    assert_equal "local", root["device_type"]
+  end
+end
