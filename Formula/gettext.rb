fbf5653473e8d5fe5589426b9cb3ad775c87e197
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 14:08:48 +0800

    gettext: update 0.21 bottle.

---
+++ Formula/gettext.rb
@@ -0,0 +1,63 @@
+class Gettext < Formula
+  desc "GNU internationalization (i18n) and localization (l10n) library"
+  homepage "https://www.gnu.org/software/gettext/"
+  url "https://ftp.gnu.org/gnu/gettext/gettext-0.21.tar.gz"
+  mirror "https://ftpmirror.gnu.org/gettext/gettext-0.21.tar.gz"
+  mirror "http://ftp.gnu.org/gnu/gettext/gettext-0.21.tar.gz"
+  sha256 "c77d0da3102aec9c07f43671e60611ebff89a996ef159497ce8e59d075786b12"
+  license "GPL-3.0-or-later"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/gettext"
+    sha256 armv7l_linux: "4266de4aefd1bfd43322b450ecca5a4ecbc41b19d7b2beead68c078f8802e73f"
+  end
+
+  uses_from_macos "libxml2"
+  uses_from_macos "ncurses"
+
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
+    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
+    directory "gettext-runtime/libasprintf"
+  end
+
+  def install
+    args = [
+      "--disable-dependency-tracking",
+      "--disable-silent-rules",
+      "--disable-debug",
+      "--prefix=#{prefix}",
+      "--with-included-glib",
+      "--with-included-libcroco",
+      "--with-included-libunistring",
+      "--with-included-libxml",
+      "--with-emacs",
+      "--with-lispdir=#{elisp}",
+      "--disable-java",
+      "--disable-csharp",
+      # Don't use VCS systems to create these archives
+      "--without-git",
+      "--without-cvs",
+      "--without-xz",
+    ]
+    args << if OS.mac?
+      # Ship libintl.h. Disabled on linux as libintl.h is provided by glibc
+      # https://gcc-help.gcc.gnu.narkive.com/CYebbZqg/cc1-undefined-reference-to-libintl-textdomain
+      # There should never be a need to install gettext's libintl.h on
+      # GNU/Linux systems using glibc. If you have it installed you've borked
+      # your system somehow.
+      "--with-included-gettext"
+    else
+      "--with-libxml2-prefix=#{Formula["libxml2"].opt_prefix}"
+    end
+    system "./configure", *args
+    system "make"
+    ENV.deparallelize # install doesn't support multiple make jobs
+    system "make", "install"
+  end
+
+  test do
+    system bin/"gettext", "test"
+  end
+end
