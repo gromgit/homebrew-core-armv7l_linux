4e8cc43dc4b0e4122d30d543b1258129c200970a
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 19:05:31 +0800

    dnscrypt-proxy: update 2.1.1 bottle.

---
+++ Formula/dnscrypt-proxy.rb
@@ -0,0 +1,68 @@
+class DnscryptProxy < Formula
+  desc "Secure communications between a client and a DNS resolver"
+  homepage "https://dnscrypt.info"
+  url "https://github.com/DNSCrypt/dnscrypt-proxy/archive/2.1.1.tar.gz"
+  sha256 "cc4a2f274ce48c3731ff981e940e6475d912fb356a80481e91725e81d67bde14"
+  license "ISC"
+  head "https://github.com/DNSCrypt/dnscrypt-proxy.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/dnscrypt-proxy"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "8eecccee1e991885fa9fe4086faf06984758888c01ef0835c660b4b03c8835c5"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    cd "dnscrypt-proxy" do
+      system "go", "build", "-ldflags", "-X main.version=#{version}", "-o",
+             sbin/"dnscrypt-proxy"
+      pkgshare.install Dir["example*"]
+      etc.install pkgshare/"example-dnscrypt-proxy.toml" => "dnscrypt-proxy.toml"
+    end
+  end
+
+  def caveats
+    <<~EOS
+      After starting dnscrypt-proxy, you will need to point your
+      local DNS server to 127.0.0.1. You can do this by going to
+      System Preferences > "Network" and clicking the "Advanced..."
+      button for your interface. You will see a "DNS" tab where you
+      can click "+" and enter 127.0.0.1 in the "DNS Servers" section.
+
+      By default, dnscrypt-proxy runs on localhost (127.0.0.1), port 53,
+      balancing traffic across a set of resolvers. If you would like to
+      change these settings, you will have to edit the configuration file:
+        #{etc}/dnscrypt-proxy.toml
+
+      To check that dnscrypt-proxy is working correctly, open Terminal and enter the
+      following command. Replace en1 with whatever network interface you're using:
+
+        sudo tcpdump -i en1 -vvv 'port 443'
+
+      You should see a line in the result that looks like this:
+
+       resolver.dnscrypt.info
+    EOS
+  end
+
+  plist_options startup: true
+
+  service do
+    run [opt_sbin/"dnscrypt-proxy", "-config", etc/"dnscrypt-proxy.toml"]
+    keep_alive true
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{sbin}/dnscrypt-proxy --version")
+
+    config = "-config #{etc}/dnscrypt-proxy.toml"
+    output = shell_output("#{sbin}/dnscrypt-proxy #{config} -list 2>&1")
+    assert_match "Source [public-resolvers] loaded", output
+  end
+end
