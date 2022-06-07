5dde3207c2563fbef509971e77e39de2bdf9b3a0
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 05:54:23 +0800

    fluxctl: update 1.25.2 bottle.

---
+++ Formula/fluxctl.rb
@@ -0,0 +1,49 @@
+class Fluxctl < Formula
+  desc "Command-line tool to access Weave Flux, the Kubernetes GitOps operator"
+  homepage "https://github.com/fluxcd/flux"
+  url "https://github.com/fluxcd/flux.git",
+      tag:      "1.25.2",
+      revision: "710825f0303fa122d9c45bf9b80351d588b8da7c"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/fluxctl"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "519b93fb5000806d28ca0e4dbb949ab75f08e554649000a68a605608d55f2add"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "./cmd/fluxctl"
+  end
+
+  test do
+    run_output = shell_output("#{bin}/fluxctl 2>&1")
+    assert_match "fluxctl helps you deploy your code.", run_output
+
+    version_output = shell_output("#{bin}/fluxctl version 2>&1")
+    assert_match version.to_s, version_output
+
+    # As we can't bring up a Kubernetes cluster in this test, we simply
+    # run "fluxctl sync" and check that it 1) errors out, and 2) complains
+    # about a missing .kube/config file.
+    require "pty"
+    require "timeout"
+    r, _w, pid = PTY.spawn("#{bin}/fluxctl sync", err: :out)
+    begin
+      Timeout.timeout(5) do
+        assert_match "Error: Could not load kubernetes configuration file", r.gets.chomp
+        Process.wait pid
+        assert_equal 1, $CHILD_STATUS.exitstatus
+      end
+    rescue Timeout::Error
+      puts "process not finished in time, killing it"
+      Process.kill("TERM", pid)
+    end
+  end
+end
