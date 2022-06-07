dcc0d1c82b46c92c100c46a0088dbda80c30cfba
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 05:34:25 +0800

    fleetctl: update 1.0.0 bottle.

---
+++ Formula/fleetctl.rb
@@ -0,0 +1,31 @@
+class Fleetctl < Formula
+  desc "Distributed init system"
+  homepage "https://github.com/coreos/fleet"
+  url "https://github.com/coreos/fleet.git",
+      tag:      "v1.0.0",
+      revision: "b8127afc06e3e41089a7fc9c3d7d80c9925f4dab"
+  license "Apache-2.0"
+  head "https://github.com/coreos/fleet.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/fleetctl"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "5efb1a3d80c26fd5b949a5f1e7578a8f6fdff061fd678ca803a5f80ca6341a2f"
+  end
+
+  # "CoreOS recommends Kubernetes for all clustering needs":
+  # https://coreos.com/blog/migrating-from-fleet-to-kubernetes.html
+  deprecate! date: "2020-04-15", because: :repo_archived
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    ENV["GO111MODULE"] = "auto"
+    system "./build"
+    bin.install "bin/fleetctl"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/fleetctl --version")
+  end
+end
