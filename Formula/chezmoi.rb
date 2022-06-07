aa75dd97fc4782ed4bf39413832640d207b86e69
Adrian Ho <aho+rpi5@03s.net>
2022-06-07 09:17:27 +0800

    chezmoi: update 2.17.1 bottle.

---
+++ Formula/chezmoi.rb
@@ -0,0 +1,42 @@
+class Chezmoi < Formula
+  desc "Manage your dotfiles across multiple diverse machines, securely"
+  homepage "https://chezmoi.io/"
+  url "https://github.com/twpayne/chezmoi.git",
+      tag:      "v2.17.1",
+      revision: "565cbbe117746aa6bfec5f2cee20ae4cbbb5e645"
+  license "MIT"
+  head "https://github.com/twpayne/chezmoi.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/chezmoi"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "aee1fd201057bd84d163eb52b8e6a1749b372dfdefb3b4c1b74825bb00e392b2"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+      -X main.commit=#{Utils.git_head}
+      -X main.date=#{time.rfc3339}
+      -X main.builtBy=#{tap.user}
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags)
+
+    bash_completion.install "completions/chezmoi-completion.bash"
+    fish_completion.install "completions/chezmoi.fish"
+    zsh_completion.install "completions/chezmoi.zsh" => "_chezmoi"
+
+    prefix.install_metafiles
+  end
+
+  test do
+    # test version to ensure that version number is embedded in binary
+    assert_match "version v#{version}", shell_output("#{bin}/chezmoi --version")
+    assert_match "built by #{tap.user}", shell_output("#{bin}/chezmoi --version")
+
+    system "#{bin}/chezmoi", "init"
+    assert_predicate testpath/".local/share/chezmoi", :exist?
+  end
+end
