d7b04c882e8110ddee10a455a55bc0aeeea02f64
Adrian Ho <aho+rpi5@03s.net>
2022-06-07 08:30:00 +0800

    cdlabelgen: update 4.3.0 bottle.

---
+++ Formula/cdlabelgen.rb
@@ -0,0 +1,26 @@
+class Cdlabelgen < Formula
+  desc "CD/DVD inserts and envelopes"
+  homepage "https://www.aczoom.com/tools/cdinsert/"
+  url "https://www.aczoom.com/pub/tools/cdlabelgen-4.3.0.tgz"
+  sha256 "94202a33bd6b19cc3c1cbf6a8e1779d7c72d8b3b48b96267f97d61ced4e1753f"
+
+  livecheck do
+    url "https://www.aczoom.com/pub/tools/"
+    regex(/href=.*?cdlabelgen[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/cdlabelgen"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "1ba8549a259b725eb231f8ae136a290a24156dc267050796bbff796f6135c9f8"
+  end
+
+  def install
+    man1.mkpath
+    system "make", "install", "BASE_DIR=#{prefix}"
+  end
+
+  test do
+    system bin/"cdlabelgen", "-c", "TestTitle", "-t", share/"cdlabelgen/template.ps", "--output-file", "testout.eps"
+    File.file?("testout.eps")
+  end
+end
