90fcffc2250b16f6400f9e41a09a8217a2ece09d
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 14:15:56 +0800

    libpcap: update 1.10.1 bottle.

---
+++ Formula/libpcap.rb
@@ -0,0 +1,34 @@
+class Libpcap < Formula
+  desc "Portable library for network traffic capture"
+  homepage "https://www.tcpdump.org/"
+  url "https://www.tcpdump.org/release/libpcap-1.10.1.tar.gz"
+  sha256 "ed285f4accaf05344f90975757b3dbfe772ba41d1c401c2648b7fa45b711bdd4"
+  license "BSD-3-Clause"
+  head "https://github.com/the-tcpdump-group/libpcap.git", branch: "master"
+
+  livecheck do
+    url "https://www.tcpdump.org/release/"
+    regex(/href=.*?libpcap[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/libpcap"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "4618615bb1e839f4b40a43d9519eeb101f085722dbfc4186666fbf45fd5ec613"
+  end
+
+  keg_only :provided_by_macos
+
+  uses_from_macos "bison" => :build
+  uses_from_macos "flex" => :build
+
+  def install
+    system "./configure", "--prefix=#{prefix}",
+                          "--enable-ipv6",
+                          "--disable-universal"
+    system "make", "install"
+  end
+
+  test do
+    assert_match "lpcap", shell_output("#{bin}/pcap-config --libs")
+  end
+end
