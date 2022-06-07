71c2b7722bcedb0a7c4002ac5d68a32920b98546
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 17:47:42 +0800

    cue: update 0.4.3 bottle.

---
+++ Formula/cue.rb
@@ -0,0 +1,51 @@
+class Cue < Formula
+  desc "Validate and define text-based and dynamic configuration"
+  homepage "https://cuelang.org/"
+  url "https://github.com/cue-lang/cue/archive/v0.4.3.tar.gz"
+  sha256 "3d51f780f6d606a0341a5321b66e7d80bd54c294073c0d381e2ed96a3ae07c6e"
+  license "Apache-2.0"
+  head "https://github.com/cue-lang/cue.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/cue"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "6952f5feb8cd1c43e6e6af73b0cb1612557693f8797e185a71db0cc35947d3f7"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w -X cuelang.org/go/cmd/cue/cmd.version=v#{version}"), "./cmd/cue"
+
+    bash_output = Utils.safe_popen_read(bin/"cue", "completion", "bash")
+    (bash_completion/"cue").write bash_output
+    zsh_output = Utils.safe_popen_read(bin/"cue", "completion", "zsh")
+    (zsh_completion/"_cue").write zsh_output
+    fish_output = Utils.safe_popen_read(bin/"cue", "completion", "fish")
+    (fish_completion/"cue.fish").write fish_output
+  end
+
+  test do
+    (testpath/"ranges.yml").write <<~EOS
+      min: 5
+      max: 10
+      ---
+      min: 10
+      max: 5
+    EOS
+
+    (testpath/"check.cue").write <<~EOS
+      min?: *0 | number    // 0 if undefined
+      max?: number & >min  // must be strictly greater than min if defined.
+    EOS
+
+    expected = <<~EOS
+      max: invalid value 5 (out of bound >10):
+          ./check.cue:2:16
+          ./ranges.yml:5:7
+    EOS
+
+    assert_equal expected, shell_output(bin/"cue vet ranges.yml check.cue 2>&1", 1)
+
+    assert_match version.to_s, shell_output(bin/"cue version")
+  end
+end
