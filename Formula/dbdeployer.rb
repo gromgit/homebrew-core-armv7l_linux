b1aaed3ba89b7b7a114af4442712295dd728d6f2
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 18:26:09 +0800

    dbdeployer: update 1.64.0 bottle.

--- Formula/dbdeployer.rb
+++ Formula/dbdeployer.rb
@@ -7,12 +7,9 @@ class Dbdeployer < Formula
   head "https://github.com/datacharmer/dbdeployer.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5baa794aac6beb7bbd6483e0c3fab95d0f206f48fa1d787fa7432f2fccc0f759"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8cd8ed8106fafc4e33011ed80782a48a2f11adbdffefe682688f8360bbb614ab"
-    sha256 cellar: :any_skip_relocation, monterey:       "a5c34e304a761c7960db63d2822abb6574539cd52d04abdb4b475ff75bd256dc"
-    sha256 cellar: :any_skip_relocation, big_sur:        "471b15dfc7d8fde1b0d444713f36e0d4a498f13395c9d6c21469ee0173736a86"
-    sha256 cellar: :any_skip_relocation, catalina:       "42bcc1fbcb2e907492b74e4289f25843e86c581aa6fe4f7f4e7d3066c61ee1d5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a75725cf0528b9ee61cc0a9882f4b14fe12fc3bf19040e9178da5a568360a93e"
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/dbdeployer"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "18f37d788ecd455e53ac13d5f53e1c7aeb766b6777ffac9b33e00c8d0c1d960a"
   end
 
   depends_on "go" => :build
