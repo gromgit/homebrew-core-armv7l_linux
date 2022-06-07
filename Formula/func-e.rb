5da1d76aea2cc398c4a346e0fa039e8cf4f9b8f5
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 06:19:08 +0800

    func-e: update 1.1.3 bottle.

---
+++ Formula/func-e.rb
@@ -0,0 +1,41 @@
+class FuncE < Formula
+  desc "Easily run Envoy"
+  homepage "https://func-e.io"
+  url "https://github.com/tetratelabs/func-e/archive/v1.1.3.tar.gz"
+  sha256 "2dd1598efd743dae38a55f6943eaa62d17f2db9996be249edf5e52495338b5e9"
+  license "Apache-2.0"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/func-e"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "200ab1dbe8c7a18083a9c2626a12563bd5e054f40c9e3c4a1fefa13fb8fa91df"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags)
+  end
+
+  test do
+    func_e_home = testpath/".func-e"
+    ENV["FUNC_E_HOME"] = func_e_home
+
+    # While this says "--version", this is a legitimate test as the --version is interpreted by Envoy.
+    # Specifically, func-e downloads and installs Envoy. Finally, it runs `envoy --version`
+    run_output = shell_output("#{bin}/func-e run --version")
+
+    # We intentionally aren't choosing an Envoy version. The version file will have the last minor. Ex. 1.19
+    installed_envoy_minor = (func_e_home/"version").read
+    # Use a glob to resolve the full path to Envoy's binary. The dist is under the patch version. Ex. 1.19.1
+    envoy_bin = func_e_home.glob("versions/#{installed_envoy_minor}.*/bin/envoy").first
+    assert_path_exists envoy_bin
+
+    # Test output from the `envoy --version`. This uses a regex because we won't know the commit etc used. Ex.
+    # envoy  version: 98c1c9e9a40804b93b074badad1cdf284b47d58b/1.18.3/Modified/RELEASE/BoringSSL
+    assert_match %r{envoy +version: [a-f0-9]{40}/#{installed_envoy_minor}\.[0-9]+/}, run_output
+  end
+end
