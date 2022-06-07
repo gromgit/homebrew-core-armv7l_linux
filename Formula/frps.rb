85478414f465e807b52b35a2bb483ee7d3f50810
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 06:14:34 +0800

    frps: update 0.43.0 bottle.

---
+++ Formula/frps.rb
@@ -0,0 +1,46 @@
+class Frps < Formula
+  desc "Server app of fast reverse proxy to expose a local server to the internet"
+  homepage "https://github.com/fatedier/frp"
+  url "https://github.com/fatedier/frp.git",
+      tag:      "v0.43.0",
+      revision: "fe5fb0326b2aa7741d5b5d8f8c4c3ca12bb4ed91"
+  license "Apache-2.0"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/frps"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "5829768288da4f0a0fee934c7dae28a430e860cc0e2120aac403cfec7e5af807"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    (buildpath/"bin").mkpath
+    (etc/"frp").mkpath
+
+    system "make", "frps"
+    bin.install "bin/frps"
+    etc.install "conf/frps.ini" => "frp/frps.ini"
+    etc.install "conf/frps_full.ini" => "frp/frps_full.ini"
+  end
+
+  service do
+    run [opt_bin/"frps", "-c", etc/"frp/frps.ini"]
+    keep_alive true
+    error_log_path var/"log/frps.log"
+    log_path var/"log/frps.log"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/frps -v")
+    assert_match "Flags", shell_output("#{bin}/frps --help")
+
+    read, write = IO.pipe
+    fork do
+      exec bin/"frps", out: write
+    end
+    sleep 3
+
+    output = read.gets
+    assert_match "frps uses command line arguments for config", output
+  end
+end
