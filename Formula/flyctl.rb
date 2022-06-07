1e60531cdb4a73324291a7ef33bec8691d345d4d
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 05:59:57 +0800

    flyctl: update 0.0.330 bottle.

---
+++ Formula/flyctl.rb
@@ -0,0 +1,49 @@
+class Flyctl < Formula
+  desc "Command-line tools for fly.io services"
+  homepage "https://fly.io"
+  url "https://github.com/superfly/flyctl.git",
+      tag:      "v0.0.330",
+      revision: "851f5496e6c461e7f3513d06c9dea327da3274d6"
+  license "Apache-2.0"
+  head "https://github.com/superfly/flyctl.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/flyctl"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "63224c02f14f451e14a05af81e5b353a32a1079494502c2a184eb2b39349175b"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    ldflags = %W[
+      -s -w
+      -X github.com/superfly/flyctl/internal/buildinfo.environment=production
+      -X github.com/superfly/flyctl/internal/buildinfo.buildDate=#{time.iso8601}
+      -X github.com/superfly/flyctl/internal/buildinfo.version=#{version}
+      -X github.com/superfly/flyctl/internal/buildinfo.commit=#{Utils.git_short_head}
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags)
+
+    bin.install_symlink "flyctl" => "fly"
+
+    bash_output = Utils.safe_popen_read("#{bin}/flyctl", "completion", "bash")
+    (bash_completion/"flyctl").write bash_output
+    zsh_output = Utils.safe_popen_read("#{bin}/flyctl", "completion", "zsh")
+    (zsh_completion/"_flyctl").write zsh_output
+    fish_output = Utils.safe_popen_read("#{bin}/flyctl", "completion", "fish")
+    (fish_completion/"flyctl.fish").write fish_output
+  end
+
+  test do
+    assert_match "flyctl v#{version}", shell_output("#{bin}/flyctl version")
+
+    flyctl_status = shell_output("flyctl status 2>&1", 1)
+    assert_match "Error No access token available. Please login with 'flyctl auth login'", flyctl_status
+  end
+end
