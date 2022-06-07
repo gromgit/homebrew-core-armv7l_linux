27e43072131bceafaecce7acffa012d48cb82aa9
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 17:31:59 +0800

    croc: update 9.5.6 bottle.

--- Formula/croc.rb
+++ Formula/croc.rb
@@ -7,12 +7,9 @@ class Croc < Formula
   head "https://github.com/schollz/croc.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "75b525c0b2df83a55f51207e46b77e66e33afa29aa4aa811e8bdaf01fe347b8d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5047415856a7db11ce7f30b4ff106c645bccae15c6840da60696aa33afd91582"
-    sha256 cellar: :any_skip_relocation, monterey:       "535d7d7a2c68580c50e2c3d71ce0e2d769e8bee4d385e93359ccc8609f77c4fc"
-    sha256 cellar: :any_skip_relocation, big_sur:        "74902dfa2e7cc14cfe0b7b86a7757f79c1ad63b66e63cb893b77a5be068a7794"
-    sha256 cellar: :any_skip_relocation, catalina:       "0bf7c63c3d193103c7b235824872784223b66fec877dcb1d42b158de1c4330e9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f5d4ca78fa244be415f3bc40849eb021d7c2a8a79acf352c8ef67c32e0119b6b"
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/croc"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "224c2256d3a8b1cddd62bd8562c9d0367fec646a0357a439e95e5d1b4268ebc4"
   end
 
   depends_on "go" => :build
