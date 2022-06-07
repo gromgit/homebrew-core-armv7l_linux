50dd668b69f2beadc50f8971e5d6057b816c5b73
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 07:10:29 +0800

    git-bug: update 0.7.2 bottle.

---
+++ Formula/git-bug.rb
@@ -0,0 +1,45 @@
+class GitBug < Formula
+  desc "Distributed, offline-first bug tracker embedded in git, with bridges"
+  homepage "https://github.com/MichaelMure/git-bug"
+  url "https://github.com/MichaelMure/git-bug.git",
+      tag:      "v0.7.2",
+      revision: "cc4a93c8ce931b1390c61035b888ad17110b7bd6"
+  license "GPL-3.0-or-later"
+  head "https://github.com/MichaelMure/git-bug.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/git-bug"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "8d49e5013e2b04d2c61cad21822ded6d12901bf8352e7ffbf763c230e3fd11bf"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOBIN"] = bin
+    system "make", "install"
+
+    man1.install Dir["doc/man/*.1"]
+    doc.install Dir["doc/md/*.md"]
+    bash_completion.install "misc/bash_completion/git-bug"
+    zsh_completion.install "misc/zsh_completion/git-bug" => "_git-bug"
+  end
+
+  test do
+    # Version
+    assert_match version.to_s, shell_output("#{bin}/git-bug --version")
+    # Version through git
+    assert_match version.to_s, shell_output("git bug --version")
+
+    mkdir testpath/"git-repo" do
+      system "git", "init"
+      system "git", "config", "user.name", "homebrew"
+      system "git", "config", "user.email", "a@a.com"
+      system "yes 'a b http://www/www' | git bug user create"
+      system "git", "bug", "add", "-t", "Issue 1", "-m", "Issue body"
+      system "git", "bug", "add", "-t", "Issue 2", "-m", "Issue body"
+      system "git", "bug", "add", "-t", "Issue 3", "-m", "Issue body"
+
+      assert_match "Issue 2", shell_output("git bug ls")
+    end
+  end
+end
