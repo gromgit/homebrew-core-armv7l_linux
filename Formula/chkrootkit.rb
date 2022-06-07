18dbb926a1ad9dbbb229df6e53644ca7754ec315
Adrian Ho <aho+rpi5@03s.net>
2022-06-07 09:49:02 +0800

    chkrootkit: update 0.55 bottle.

---
+++ Formula/chkrootkit.rb
@@ -0,0 +1,31 @@
+class Chkrootkit < Formula
+  desc "Rootkit detector"
+  homepage "http://www.chkrootkit.org/"
+  url "ftp://ftp.pangeia.com.br/pub/seg/pac/chkrootkit-0.55.tar.gz"
+  mirror "https://fossies.org/linux/misc/chkrootkit-0.55.tar.gz"
+  sha256 "a81c0286ec449313f953701202a00e81b204fc2cf43e278585a11c12a5e0258b"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?download[^>]*>chkrootkit v?(\d+(?:\.\d+)+)/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/chkrootkit"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "3f6bfb1e945f622c668d4a3bb098486fda19ff56a38b8f41837d75329b95a527"
+  end
+
+  def install
+    system "make", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}",
+                   "STATIC=", "sense", "all"
+
+    bin.install Dir[buildpath/"*"].select { |f| File.executable? f }
+    doc.install %w[README README.chklastlog README.chkwtmp]
+  end
+
+  test do
+    assert_equal "chkrootkit version #{version}",
+                 shell_output("#{bin}/chkrootkit -V 2>&1", 1).strip
+  end
+end
