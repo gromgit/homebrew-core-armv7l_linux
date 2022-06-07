44de6090dde36ed21b63e5f7712532f5cdda641c
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 06:08:22 +0800

    fortio: update 1.32.3 bottle.

---
+++ Formula/fortio.rb
@@ -0,0 +1,41 @@
+class Fortio < Formula
+  desc "HTTP and gRPC load testing and visualization tool and server"
+  homepage "https://fortio.org/"
+  url "https://github.com/fortio/fortio.git",
+      tag:      "v1.32.3",
+      revision: "56fa5ab3bb0f93af404e124cf17f3f341450adc4"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/fortio"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "96943c6ecd10c27515201c0977415b0d601230a5c2a511179aefac291bc11058"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "make", "-j1", "official-build-clean", "official-build-version", "OFFICIAL_BIN=#{bin}/fortio",
+      "BUILD_DIR=./tmp/fortio_build"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/fortio version -s")
+
+    port = free_port
+    begin
+      pid = fork do
+        exec bin/"fortio", "server", "-http-port", port.to_s
+      end
+      sleep 2
+      output = shell_output("#{bin}/fortio load http://localhost:#{port}/ 2>&1")
+      assert_match(/^All\sdone/, output.lines.last)
+    ensure
+      Process.kill("SIGTERM", pid)
+    end
+  end
+end
