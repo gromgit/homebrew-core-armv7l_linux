1a255ccdef1d46312f3d42a6d41f7e4b95f99ef2
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 15:02:26 +0800

    cfssl: update 1.6.1 bottle.

---
+++ Formula/cfssl.rb
@@ -0,0 +1,61 @@
+class Cfssl < Formula
+  desc "CloudFlare's PKI toolkit"
+  homepage "https://cfssl.org/"
+  url "https://github.com/cloudflare/cfssl/archive/v1.6.1.tar.gz"
+  sha256 "00f5316e8f065a48eeb02b7359aa67699a5c1362b09c6e8faa19d8f35451c1d8"
+  license "BSD-2-Clause"
+  head "https://github.com/cloudflare/cfssl.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/cfssl"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "2cfe69f8fb57a64df5438adcf94bcf3b7e0c02e7f05308a626ac72370db81b62"
+  end
+
+  depends_on "go" => :build
+  depends_on "libtool"
+
+  def install
+    ldflags = "-s -w -X github.com/cloudflare/cfssl/cli/version.version=#{version}"
+
+    system "go", "build", *std_go_args(output: bin/"cfssl", ldflags: ldflags), "cmd/cfssl/cfssl.go"
+    system "go", "build", *std_go_args(output: bin/"cfssljson", ldflags: ldflags), "cmd/cfssljson/cfssljson.go"
+    system "go", "build", "-o", "#{bin}/cfsslmkbundle", "cmd/mkbundle/mkbundle.go"
+  end
+
+  def caveats
+    <<~EOS
+      `mkbundle` has been installed as `cfsslmkbundle` to avoid conflict
+      with Mono and other tools that ship the same executable.
+    EOS
+  end
+
+  test do
+    (testpath/"request.json").write <<~EOS
+      {
+        "CN" : "Your Certificate Authority",
+        "hosts" : [],
+        "key" : {
+          "algo" : "rsa",
+          "size" : 4096
+        },
+        "names" : [
+          {
+            "C" : "US",
+            "ST" : "Your State",
+            "L" : "Your City",
+            "O" : "Your Organization",
+            "OU" : "Your Certificate Authority"
+          }
+        ]
+      }
+    EOS
+    shell_output("#{bin}/cfssl genkey -initca request.json > response.json")
+    response = JSON.parse(File.read(testpath/"response.json"))
+    assert_match(/^-----BEGIN CERTIFICATE-----.*/, response["cert"])
+    assert_match(/.*-----END CERTIFICATE-----$/, response["cert"])
+    assert_match(/^-----BEGIN RSA PRIVATE KEY-----.*/, response["key"])
+    assert_match(/.*-----END RSA PRIVATE KEY-----$/, response["key"])
+
+    assert_match(/^Version:\s+#{version}/, shell_output("#{bin}/cfssl version"))
+  end
+end
