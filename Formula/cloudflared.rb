eda5c19c7fa42a122897bc8198c0aaa9e5b49b9f
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 15:37:46 +0800

    cloudflared: update 2022.5.3 bottle.

---
+++ Formula/cloudflared.rb
@@ -0,0 +1,33 @@
+class Cloudflared < Formula
+  desc "Cloudflare Tunnel client (formerly Argo Tunnel)"
+  homepage "https://developers.cloudflare.com/cloudflare-one/connections/connect-apps/install-and-setup/tunnel-guide"
+  url "https://github.com/cloudflare/cloudflared/archive/refs/tags/2022.5.3.tar.gz"
+  sha256 "d5d55a143afb918dad279490bc36e4d3537c2b862cdecfcd5dfe5bb52af63e7e"
+  license "Apache-2.0"
+  head "https://github.com/cloudflare/cloudflared.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/cloudflared"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "721b83f03d3520cf72412f14f2902480de1f62b4a911e76dafffbf0bf214234a"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "make", "install",
+      "VERSION=#{version}",
+      "DATE=#{time.iso8601}",
+      "PACKAGE_MANAGER=#{tap.user}",
+      "PREFIX=#{prefix}"
+  end
+
+  test do
+    help_output = shell_output("#{bin}/cloudflared help")
+    assert_match "cloudflared - Cloudflare's command-line tool and agent", help_output
+    assert_match version.to_s, help_output
+    assert_equal "unable to find config file\n", shell_output("#{bin}/cloudflared 2>&1", 1)
+    assert_match "Error locating origin cert", shell_output("#{bin}/cloudflared tunnel run abcd 2>&1", 1)
+    assert_match "cloudflared was installed by #{tap.user}. Please update using the same method.",
+      shell_output("#{bin}/cloudflared update 2>&1")
+  end
+end
