7c18140980bb7bd57237187144b7fcd2f3178a75
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 16:36:50 +0800

    convox: update 3.5.0 bottle.

---
+++ Formula/convox.rb
@@ -0,0 +1,41 @@
+class Convox < Formula
+  desc "Command-line interface for the Convox PaaS"
+  homepage "https://convox.com/"
+  url "https://github.com/convox/convox/archive/3.5.0.tar.gz"
+  sha256 "759d5f965a792f03353ad6dc79301c1fd8ac895606cd9e1f4e29338c140c68dc"
+  license "Apache-2.0"
+  version_scheme 1
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/convox"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "a11d9c21e26ebb749dab8fbf0bdf9ac9a4b2a437769bf1356896a21ffbe6d769"
+  end
+
+  depends_on "go" => :build
+
+  # Support go 1.17, remove when upstream patch is merged/released
+  # https://github.com/convox/convox/pull/389
+  patch do
+    url "https://github.com/convox/convox/commit/d28b01c5797cc8697820c890e469eb715b1d2e2e.patch?full_index=1"
+    sha256 "a0f94053a5549bf676c13cea877a33b3680b6116d54918d1fcfb7f3d2941f58b"
+  end
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+    ]
+
+    system "go", "build", "-mod=readonly", *std_go_args(ldflags: ldflags), "./cmd/convox"
+  end
+
+  test do
+    assert_equal "Authenticating with localhost... ERROR: invalid login\n",
+      shell_output("#{bin}/convox login -t invalid localhost 2>&1", 1)
+  end
+end
