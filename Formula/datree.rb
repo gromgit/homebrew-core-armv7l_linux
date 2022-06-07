b463c1201df6dcd328cd0bc3fdedb236e27061d6
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 18:23:40 +0800

    datree: update 1.4.37 bottle.

---
+++ Formula/datree.rb
@@ -0,0 +1,45 @@
+class Datree < Formula
+  desc "CLI tool to run policies against Kubernetes manifests YAML files or Helm charts"
+  homepage "https://www.datree.io/"
+  url "https://github.com/datreeio/datree/archive/1.4.37.tar.gz"
+  sha256 "c6cfc3a128b73dc40191c34b573981eaf506ff2a7123152d1562182b6e1d6d26"
+  license "Apache-2.0"
+  head "https://github.com/datreeio/datree.git", branch: "main"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/datree"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "4caa730f34906119d87dd1c040be89985cac59bc1add6436460b4bf5bca756cd"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/datreeio/datree/cmd.CliVersion=#{version}"), "-tags", "main"
+  end
+
+  test do
+    (testpath/"invalidK8sSchema.yaml").write <<~EOS
+      apiversion: v1
+      kind: Service
+      metadata:
+        name: my-service
+      spec:
+        selector:
+          app: MyApp
+        ports:
+          - protocol: TCP
+            port: 80
+            targetPort: 9376
+    EOS
+
+    assert_match "k8s schema validation error: For field (root): Additional property apiversion is not allowed",
+      shell_output("#{bin}/datree test #{testpath}/invalidK8sSchema.yaml --no-record 2>&1", 2)
+
+    assert_match "#{version}\n", shell_output("#{bin}/datree version")
+  end
+end
