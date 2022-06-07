82e2d3cea524ab392f890820bd0b4533813be1f1
Adrian Ho <aho+rpi5@03s.net>
2022-06-07 09:11:19 +0800

    cheat: update 4.2.3 bottle.

---
+++ Formula/cheat.rb
@@ -0,0 +1,31 @@
+class Cheat < Formula
+  desc "Create and view interactive cheat sheets for *nix commands"
+  homepage "https://github.com/cheat/cheat"
+  url "https://github.com/cheat/cheat/archive/4.2.3.tar.gz"
+  sha256 "9624160ba542fb51bbd959d8c68b76f82ea324a6186d8d6d544b0efd8c9cc8ca"
+  license "MIT"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/cheat"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "a7104b9b8a124535c7fa9e921892693ec2763620403a153fe831dc21b9b6d31d"
+  end
+
+  depends_on "go" => :build
+
+  conflicts_with "bash-snippets", because: "both install a `cheat` executable"
+
+  def install
+    system "go", "build", "-mod", "vendor", "-o", bin/"cheat", "./cmd/cheat"
+
+    bash_completion.install "scripts/cheat.bash"
+    fish_completion.install "scripts/cheat.fish"
+    zsh_completion.install "scripts/cheat.zsh"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/cheat --version")
+
+    output = shell_output("#{bin}/cheat --init 2>&1")
+    assert_match "editor: vim", output
+  end
+end
