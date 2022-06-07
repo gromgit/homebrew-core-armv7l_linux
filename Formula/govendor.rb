4bfb453ce3d73943626a81715d91e5b25f7978ed
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 17:50:11 +0800

    govendor: update 1.0.9 bottle.

---
+++ Formula/govendor.rb
@@ -0,0 +1,42 @@
+class Govendor < Formula
+  desc "Tool for vendoring Go dependencies"
+  homepage "https://github.com/kardianos/govendor"
+  url "https://github.com/kardianos/govendor/archive/v1.0.9.tar.gz"
+  sha256 "d303abf194838792234a1451c3a1e87885d1b2cd21774867b592c1f7db00551e"
+  license "BSD-3-Clause"
+  head "https://github.com/kardianos/govendor.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/govendor"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "432ef62ee37aa143cbf385f54f8de6e1805a5df6490fab0bf53e518e16562c57"
+  end
+
+  deprecate! date: "2020-03-02", because: :repo_archived
+
+  depends_on "go"
+
+  def install
+    ENV["GOPATH"] = buildpath
+    ENV["GO111MODULE"] = "auto"
+
+    (buildpath/"src/github.com/kardianos/").mkpath
+    ln_sf buildpath, buildpath/"src/github.com/kardianos/govendor"
+    system "go", "build", *std_go_args
+  end
+
+  test do
+    # Default HOMEBREW_TEMP is /tmp, which is actually a symlink to /private/tmp.
+    # `govendor` bails without `.realpath` as it expects $GOPATH to be "real" path.
+    ENV["GOPATH"] = testpath.realpath
+    commit = "89d9e62992539701a49a19c52ebb33e84cbbe80f"
+    (testpath/"src/github.com/project/testing").mkpath
+
+    cd "src/github.com/project/testing" do
+      system bin/"govendor", "init"
+      assert_predicate Pathname.pwd/"vendor", :exist?, "Failed to init!"
+      system bin/"govendor", "fetch", "-tree", "golang.org/x/crypto@#{commit}"
+      assert_match commit, File.read("vendor/vendor.json")
+      assert_match "golang.org/x/crypto/blowfish", shell_output("#{bin}/govendor list")
+    end
+  end
+end
