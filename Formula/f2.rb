aa697258f830bf99bdb9391793b965dea99fa92b
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 21:13:21 +0800

    f2: update 1.8.0 bottle.

---
+++ Formula/f2.rb
@@ -0,0 +1,29 @@
+class F2 < Formula
+  desc "Command-line batch renaming tool"
+  homepage "https://github.com/ayoisaiah/f2"
+  url "https://github.com/ayoisaiah/f2/archive/v1.8.0.tar.gz"
+  sha256 "11b127b0631ff6db979d7f8c59a6a59cc725e072489abbf676d2557ea6475dab"
+  license "MIT"
+  head "https://github.com/ayoisaiah/f2.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/f2"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "1ab412e276b18333fa851c21f1e62223ae434d461d9fe8acaeb4694aeabc6f98"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args, "./cmd..."
+  end
+
+  test do
+    touch "test1-foo.foo"
+    touch "test2-foo.foo"
+    system bin/"f2", "-s", "-f", ".foo", "-r", ".bar", "-x"
+    assert_predicate testpath/"test1-foo.bar", :exist?
+    assert_predicate testpath/"test2-foo.bar", :exist?
+    refute_predicate testpath/"test1-foo.foo", :exist?
+    refute_predicate testpath/"test2-foo.foo", :exist?
+  end
+end
