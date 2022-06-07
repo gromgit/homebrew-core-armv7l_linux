7431cbf19dfaa2d4a781b983403c0d62b83c8e1f
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 19:21:27 +0800

    docker-credential-helper-ecr: update 0.6.0 bottle.

---
+++ Formula/docker-credential-helper-ecr.rb
@@ -0,0 +1,37 @@
+class DockerCredentialHelperEcr < Formula
+  desc "Docker Credential Helper for Amazon ECR"
+  homepage "https://github.com/awslabs/amazon-ecr-credential-helper"
+  url "https://github.com/awslabs/amazon-ecr-credential-helper.git",
+      tag:      "v0.6.0",
+      revision: "69c85dc22db6511932bbf119e1a0cc5c90c69a7f"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/docker-credential-helper-ecr"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "64edc81644884a97dab4e06e2e627bb30ccc23538754a6c3f7abccf3f4283f91"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    dir = buildpath/"src/github.com/awslabs/amazon-ecr-credential-helper"
+    dir.install buildpath.children - [buildpath/".brew_home"]
+
+    cd dir do
+      system "make", "build"
+      bin.install "bin/local/docker-credential-ecr-login"
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    output = shell_output("#{bin}/docker-credential-ecr-login", 1)
+    assert_match %r{^Usage: .*/docker-credential-ecr-login.*}, output
+  end
+end
