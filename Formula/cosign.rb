f15670a41c78178a514153119829991214f7ecf5
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 17:11:55 +0800

    cosign: update 1.9.0 bottle.

---
+++ Formula/cosign.rb
@@ -0,0 +1,37 @@
+class Cosign < Formula
+  desc "Container Signing"
+  homepage "https://github.com/sigstore/cosign"
+  url "https://github.com/sigstore/cosign.git",
+      tag:      "v1.9.0",
+      revision: "a4cb262dc3d45a283a6a7513bb767a38a2d3f448"
+  license "Apache-2.0"
+  head "https://github.com/sigstore/cosign.git", branch: "main"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/cosign"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "202408379a422880c11e361105c02c67b12734a18e0cac542f6bd77a0a4679b2"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    pkg = "sigs.k8s.io/release-utils/version"
+    ldflags = %W[
+      -s -w
+      -X #{pkg}.gitVersion=#{version}
+      -X #{pkg}.gitCommit=#{Utils.git_head}
+      -X #{pkg}.gitTreeState="clean"
+      -X #{pkg}.buildDate=#{time.iso8601}
+    ]
+
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/cosign"
+  end
+
+  test do
+    assert_match "Private key written to cosign.key",
+      pipe_output("#{bin}/cosign generate-key-pair 2>&1", "foo\nfoo\n")
+    assert_predicate testpath/"cosign.pub", :exist?
+
+    assert_match version.to_s, shell_output(bin/"cosign version 2>&1")
+  end
+end
