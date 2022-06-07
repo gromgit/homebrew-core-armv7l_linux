3232867c1b704260ddbcb87dae13c48e2b248494
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 00:20:30 +0800

    ffuf: update 1.5.0 bottle.

---
+++ Formula/ffuf.rb
@@ -0,0 +1,31 @@
+class Ffuf < Formula
+  desc "Fast web fuzzer written in Go"
+  homepage "https://github.com/ffuf/ffuf"
+  url "https://github.com/ffuf/ffuf/archive/v1.5.0.tar.gz"
+  sha256 "70dc411c014c826f9c8a7b021e01d5bc50e2cba17e0dcc4df3e2e2574ad12073"
+  license "MIT"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/ffuf"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "d4d1cbe4780e9ce3ea492a9654f6662814afb86a08494efc3090700daf0d0e52"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    (testpath/"words.txt").write <<~EOS
+      dog
+      cat
+      horse
+      snake
+      ape
+    EOS
+
+    output = shell_output("#{bin}/ffuf -u https://example.org/FUZZ -w words.txt 2>&1")
+    assert_match %r{:: Progress: \[5/5\].*Errors: 0 ::$}, output
+  end
+end
