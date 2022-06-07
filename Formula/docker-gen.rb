b28cff64d5e805244be7b8a0b3ba1386444a2e09
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 19:23:54 +0800

    docker-gen: update 0.9.0 bottle.

---
+++ Formula/docker-gen.rb
@@ -0,0 +1,24 @@
+class DockerGen < Formula
+  desc "Generate files from docker container metadata"
+  homepage "https://github.com/nginx-proxy/docker-gen"
+  url "https://github.com/nginx-proxy/docker-gen/archive/0.9.0.tar.gz"
+  sha256 "9f270363d872e4d302b67b3baa3baec4d1c7b892814fd6a50e5953a2b90d745e"
+  license "MIT"
+  head "https://github.com/nginx-proxy/docker-gen.git", branch: "main"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/docker-gen"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "3e2dbbbc0b0c6002a43dc4ca0c7e2f663a4be8069dc5cc3f76538a95fa5645fb"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X main.buildVersion=#{version}"
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/docker-gen"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/docker-gen --version")
+  end
+end
