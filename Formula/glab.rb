f83b288e3ae60a6822c5406cf83bcf8d4288fbc3
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 07:41:26 +0800

    glab: update 1.22.0 bottle.

--- Formula/glab.rb
+++ Formula/glab.rb
@@ -7,12 +7,9 @@ class Glab < Formula
   head "https://github.com/profclems/glab.git", branch: "trunk"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "58a6efde41f754f75de93a2c6ddc50b888237f5a6d268cc22c375722972537a9"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4fb720719b0cd88b52688b7298ef5b07b510d0eb0fd2b3067a7f3fe47303dc9c"
-    sha256 cellar: :any_skip_relocation, monterey:       "335d3798ad846544e68dfab75abdaedaf40397db17e5de76c9a5462c231285a0"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1b384f05896c4cc28fc9e679549825792320c54bdf675293dffffb1a6c2717d1"
-    sha256 cellar: :any_skip_relocation, catalina:       "92114845aab1c15d5f52c0591d44508c28a4349a46e0f7669d5d551806155759"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3e105a8bb9694c9fd2569785a576265bf732c6a19ff8f1c42a430e8783b36d69"
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/glab"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "5bcdaeb43569d200678f87887e384d82a46392be1458a459de02774efe3ef7ec"
   end
 
   depends_on "go" => :build
