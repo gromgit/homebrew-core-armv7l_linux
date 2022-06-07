a596c7043b01093ac76c49dd7d5e8b54a7667942
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 06:12:06 +0800

    frpc: update 0.43.0 bottle.

---
+++ Formula/frpc.rb
@@ -0,0 +1,45 @@
+class Frpc < Formula
+  desc "Client app of fast reverse proxy to expose a local server to the internet"
+  homepage "https://github.com/fatedier/frp"
+  url "https://github.com/fatedier/frp.git",
+      tag:      "v0.43.0",
+      revision: "fe5fb0326b2aa7741d5b5d8f8c4c3ca12bb4ed91"
+  license "Apache-2.0"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/frpc"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "c7044f54af83567b3ec98d77197d11a2747e19ee8b329b2b3d6a6eef088bf945"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    (buildpath/"bin").mkpath
+    (etc/"frp").mkpath
+
+    system "make", "frpc"
+    bin.install "bin/frpc"
+    etc.install "conf/frpc.ini" => "frp/frpc.ini"
+    etc.install "conf/frpc_full.ini" => "frp/frpc_full.ini"
+  end
+
+  service do
+    run [opt_bin/"frpc", "-c", etc/"frp/frpc.ini"]
+    keep_alive true
+    error_log_path var/"log/frpc.log"
+    log_path var/"log/frpc.log"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/frpc -v")
+    assert_match "Commands", shell_output("#{bin}/frpc help")
+    assert_match "local_port", shell_output("#{bin}/frpc http", 1)
+    assert_match "local_port", shell_output("#{bin}/frpc https", 1)
+    assert_match "local_port", shell_output("#{bin}/frpc stcp", 1)
+    assert_match "local_port", shell_output("#{bin}/frpc tcp", 1)
+    assert_match "local_port", shell_output("#{bin}/frpc udp", 1)
+    assert_match "local_port", shell_output("#{bin}/frpc xtcp", 1)
+    assert_match "admin_port", shell_output("#{bin}/frpc status -c #{etc}/frp/frpc.ini", 1)
+    assert_match "admin_port", shell_output("#{bin}/frpc reload -c #{etc}/frp/frpc.ini", 1)
+  end
+end
