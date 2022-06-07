1caa2515d71d38fdcda1a6e10a44515e243ab342
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 06:38:37 +0800

    gdu: update 5.14.0 bottle.

---
+++ Formula/gdu.rb
@@ -0,0 +1,40 @@
+class Gdu < Formula
+  desc "Disk usage analyzer with console interface written in Go"
+  homepage "https://github.com/dundee/gdu"
+  url "https://github.com/dundee/gdu/archive/v5.14.0.tar.gz"
+  sha256 "6fb64500eb22a4e586322c065e97bed72b6f4e1aead4311dad50b75d11222cdb"
+  license "MIT"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/gdu"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "14daef4ba78f9bf8b5344ebe2b2a47d3c5445944af12925d99806c595df28c83"
+  end
+
+  depends_on "go" => :build
+
+  conflicts_with "coreutils", because: "both install `gdu` binaries"
+
+  def install
+    user = Utils.safe_popen_read("id", "-u", "-n")
+    major = version.major
+
+    ldflags = %W[
+      -s -w
+      -X "github.com/dundee/gdu/v#{major}/build.Version=v#{version}"
+      -X "github.com/dundee/gdu/v#{major}/build.Time=#{time}"
+      -X "github.com/dundee/gdu/v#{major}/build.User=#{user}"
+    ]
+
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/gdu"
+  end
+
+  test do
+    mkdir_p testpath/"test_dir"
+    (testpath/"test_dir"/"file1").write "hello"
+    (testpath/"test_dir"/"file2").write "brew"
+
+    assert_match version.to_s, shell_output("#{bin}/gdu -v")
+    assert_match "colorized", shell_output("#{bin}/gdu --help 2>&1")
+    assert_match "4.0 KiB file1", shell_output("#{bin}/gdu --non-interactive --no-progress #{testpath}/test_dir 2>&1")
+  end
+end
