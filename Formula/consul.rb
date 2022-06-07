326a9c18409b758676ae2fc830a8b7659c4806b2
Adrian Ho <the.gromgit@gmail.com>
2022-06-07 16:21:16 +0800

    consul: update 1.12.2 bottle.

---
+++ Formula/consul.rb
@@ -0,0 +1,59 @@
+class Consul < Formula
+  desc "Tool for service discovery, monitoring and configuration"
+  homepage "https://www.consul.io"
+  url "https://github.com/hashicorp/consul/archive/refs/tags/v1.12.2.tar.gz"
+  sha256 "74d627b778760d8f0a77f65742c3fd1e07bef18e8ce1710dd7791032072b4029"
+  license "MPL-2.0"
+  head "https://github.com/hashicorp/consul.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/consul"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "7bb6dbd5231d1035dc9b575fd1f7b4666248a63a9813f211acd6c377d4c7815c"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  service do
+    run [opt_bin/"consul", "agent", "-dev", "-bind", "127.0.0.1"]
+    keep_alive true
+    error_log_path var/"log/consul.log"
+    log_path var/"log/consul.log"
+    working_dir var
+  end
+
+  test do
+    http_port = free_port
+    fork do
+      # most ports must be free, but are irrelevant to this test
+      system(
+        bin/"consul",
+        "agent",
+        "-dev",
+        "-bind", "127.0.0.1",
+        "-dns-port", "-1",
+        "-grpc-port", "-1",
+        "-http-port", http_port,
+        "-serf-lan-port", free_port,
+        "-serf-wan-port", free_port,
+        "-server-port", free_port
+      )
+    end
+
+    # wait for startup
+    sleep 3
+
+    k = "brew-formula-test"
+    v = "value"
+    system bin/"consul", "kv", "put", "-http-addr", "127.0.0.1:#{http_port}", k, v
+    assert_equal v, shell_output(bin/"consul kv get -http-addr 127.0.0.1:#{http_port} #{k}").chomp
+  end
+end
