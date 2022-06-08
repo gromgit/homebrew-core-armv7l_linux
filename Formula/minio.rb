5b913392330f4a9c5e90a32290244f1e0a40a113
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 18:55:29 +0800

    minio: update 20220603014053 bottle.

---
+++ Formula/minio.rb
@@ -0,0 +1,66 @@
+class Minio < Formula
+  desc "High Performance, Kubernetes Native Object Storage"
+  homepage "https://min.io"
+  url "https://github.com/minio/minio.git",
+      tag:      "RELEASE.2022-06-03T01-40-53Z",
+      revision: "61a7434379164c0a2150db8047343e9ec769ae91"
+  version "20220603014053"
+  license "AGPL-3.0-or-later"
+  head "https://github.com/minio/minio.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(%r{href=.*?/tag/(?:RELEASE[._-]?)?([\d\-TZ]+)["' >]}i)
+    strategy :github_latest do |page, regex|
+      page.scan(regex).map { |match| match&.first&.gsub(/\D/, "") }
+    end
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/minio"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "0d170d78507217806cb06c5be8cac30661ff1ff7cc7164d4831dade7516976da"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    if build.head?
+      system "go", "build", *std_go_args
+    else
+      release = `git tag --points-at HEAD`.chomp
+      version = release.gsub(/RELEASE\./, "").chomp.gsub(/T(\d+)-(\d+)-(\d+)Z/, 'T\1:\2:\3Z')
+
+      ldflags = %W[
+        -s -w
+        -X github.com/minio/minio/cmd.Version=#{version}
+        -X github.com/minio/minio/cmd.ReleaseTag=#{release}
+        -X github.com/minio/minio/cmd.CommitID=#{Utils.git_head}
+      ]
+
+      system "go", "build", *std_go_args(ldflags: ldflags)
+    end
+  end
+
+  def post_install
+    (var/"minio").mkpath
+    (etc/"minio").mkpath
+  end
+
+  service do
+    run [opt_bin/"minio", "server", "--config-dir=#{etc}/minio", "--address=:9000", var/"minio"]
+    keep_alive true
+    working_dir HOMEBREW_PREFIX
+    log_path var/"log/minio.log"
+    error_log_path var/"log/minio.log"
+  end
+
+  test do
+    assert_match "minio server - start object storage server",
+      shell_output("#{bin}/minio server --help 2>&1")
+
+    assert_match "minio gateway - start object storage gateway",
+      shell_output("#{bin}/minio gateway 2>&1")
+    assert_match "ERROR Unable to validate credentials",
+      shell_output("#{bin}/minio gateway s3 2>&1", 1)
+  end
+end
