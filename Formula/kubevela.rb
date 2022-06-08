1cc539fa4070d0f211ae2143fd49f4004522bfde
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 15:58:00 +0800

    kubevela: update 1.4.1 bottle.

---
+++ Formula/kubevela.rb
@@ -0,0 +1,58 @@
+class Kubevela < Formula
+  desc "Application Platform based on Kubernetes and Open Application Model"
+  homepage "https://kubevela.io"
+  url "https://github.com/kubevela/kubevela.git",
+      tag:      "v1.4.1",
+      revision: "ea0508a634f7d76731f062e034feea8b75f74be1"
+  license "Apache-2.0"
+  head "https://github.com/kubevela/kubevela.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/kubevela"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "94f7ce820b686697423dd272d1d0cd24461b3264791e8e37f0fe08685e58272d"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    ldflags = %W[
+      -s -w
+      -X github.com/oam-dev/kubevela/version.VelaVersion=#{version}
+      -X github.com/oam-dev/kubevela/version.GitRevision=#{Utils.git_head}
+    ]
+
+    system "go", "build", *std_go_args(output: bin/"vela", ldflags: ldflags), "./references/cmd/cli"
+  end
+
+  test do
+    # Should error out as vela up need kubeconfig
+    status_output = shell_output("#{bin}/vela up 2>&1", 1)
+    assert_match "error: no configuration has been provided", status_output
+
+    (testpath/"kube-config").write <<~EOS
+      apiVersion: v1
+      clusters:
+      - cluster:
+          certificate-authority-data: test
+          server: http://127.0.0.1:8080
+        name: test
+      contexts:
+      - context:
+          cluster: test
+          user: test
+        name: test
+      current-context: test
+      kind: Config
+      preferences: {}
+      users:
+      - name: test
+        user:
+          token: test
+    EOS
+
+    ENV["KUBECONFIG"] = testpath/"kube-config"
+    version_output = shell_output("#{bin}/vela version 2>&1")
+    assert_match "Version: #{version}", version_output
+  end
+end
