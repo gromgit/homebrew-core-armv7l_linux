1f20d5880d90a67fb6fbd42c03e049610f051ba5
Adrian Ho <the.gromgit@gmail.com>
2022-06-09 02:37:21 +0800

    lzo: update 2.10 bottle.

---
+++ Formula/lzo.rb
@@ -0,0 +1,42 @@
+class Lzo < Formula
+  desc "Real-time data compression library"
+  homepage "https://www.oberhumer.com/opensource/lzo/"
+  url "https://www.oberhumer.com/opensource/lzo/download/lzo-2.10.tar.gz"
+  sha256 "c0f892943208266f9b6543b3ae308fab6284c5c90e627931446fb49b4221a072"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url "https://www.oberhumer.com/opensource/lzo/download/"
+    regex(/href=.*?lzo[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/lzo"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "88e62276ad41256e927bf0afecb420cb53364b4d829a9d4c89e184c28c2708e2"
+  end
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--enable-shared"
+    system "make"
+    system "make", "check"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <lzo/lzoconf.h>
+      #include <stdio.h>
+
+      int main()
+      {
+        printf("Testing LZO v%s in Homebrew.\\n",
+        LZO_VERSION_STRING);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-o", "test"
+    assert_match "Testing LZO v#{version} in Homebrew.", shell_output("./test")
+  end
+end
