67ba2d88f1fd0b224baa5be0b71cc37100430b30
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 20:27:07 +0800

    nomad: update 1.3.1 bottle.

---
+++ Formula/nomad.rb
@@ -0,0 +1,43 @@
+class Nomad < Formula
+  desc "Distributed, Highly Available, Datacenter-Aware Scheduler"
+  homepage "https://www.nomadproject.io"
+  url "https://github.com/hashicorp/nomad/archive/v1.3.1.tar.gz"
+  sha256 "55915cd6ff0433b4b6d41d1e023a96cf283c95cb4d55960329a0f77eef939485"
+  license "MPL-2.0"
+  head "https://github.com/hashicorp/nomad.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/nomad"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "f33675ff95b2fc67d9d13b6bcc60d067f2a0b462404115e5396e8ac0099df625"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "-tags", "ui"
+  end
+
+  service do
+    run [opt_bin/"nomad", "agent", "-dev"]
+    keep_alive true
+    working_dir var
+    log_path var/"log/nomad.log"
+    error_log_path var/"log/nomad.log"
+  end
+
+  test do
+    pid = fork do
+      exec "#{bin}/nomad", "agent", "-dev"
+    end
+    sleep 10
+    ENV.append "NOMAD_ADDR", "http://127.0.0.1:4646"
+    system "#{bin}/nomad", "node-status"
+  ensure
+    Process.kill("TERM", pid)
+  end
+end
