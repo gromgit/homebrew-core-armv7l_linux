3f85f4a74c8cc7d725f49eb18295487fb132ea15
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 09:40:24 +0800

    goredo: update 1.25.0 bottle.

---
+++ Formula/goredo.rb
@@ -0,0 +1,37 @@
+class Goredo < Formula
+  desc "Go implementation of djb's redo, a Makefile replacement that sucks less"
+  homepage "http://www.goredo.cypherpunks.ru/"
+  url "http://www.goredo.cypherpunks.ru/download/goredo-1.25.0.tar.zst"
+  sha256 "7044c246fcf6159d84ba7233bc25202baaa08ecccb9c30ca3c26254065eda9a4"
+  license "GPL-3.0-only"
+
+  livecheck do
+    url "http://www.goredo.cypherpunks.ru/Install.html"
+    regex(/href=.*?goredo[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/goredo"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "c36a67c8e6706c143f929baa3f7bc8b313d3628ff3cd826f7f5433b883f1e70c"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    cd "src" do
+      system "go", "build", *std_go_args, "-mod=vendor"
+    end
+
+    ENV.prepend_path "PATH", bin
+    cd bin do
+      system "goredo", "-symlinks"
+    end
+  end
+
+  test do
+    (testpath/"gore.do").write <<~EOS
+      echo YOU ARE LIKELY TO BE EATEN BY A GRUE >&2
+    EOS
+    assert_equal "YOU ARE LIKELY TO BE EATEN BY A GRUE\n", shell_output("#{bin}/redo -no-progress gore 2>&1")
+  end
+end
