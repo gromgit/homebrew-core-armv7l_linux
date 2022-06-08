31d5075c6ff685815a7dec188115acc20045ce6d
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 14:21:23 +0800

    kn: update 1.5.0 bottle.

---
+++ Formula/kn.rb
@@ -0,0 +1,45 @@
+class Kn < Formula
+  desc "Command-line interface for managing Knative Serving and Eventing resources"
+  homepage "https://github.com/knative/client"
+  url "https://github.com/knative/client.git",
+      tag:      "knative-v1.5.0",
+      revision: "0646532018621d3d08525edf6e47a125cf58438b"
+  license "Apache-2.0"
+  head "https://github.com/knative/client.git", branch: "main"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/kn"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "2974c3274af8afa1c06e8574b2ed959979e5d7e1ed99afa798a30b05cd64a611"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+
+    ldflags = %W[
+      -X knative.dev/client/pkg/kn/commands/version.Version=v#{version}
+      -X knative.dev/client/pkg/kn/commands/version.GitRevision=#{Utils.git_head(length: 8)}
+      -X knative.dev/client/pkg/kn/commands/version.BuildDate=#{time.iso8601}
+    ]
+
+    system "go", "build", "-mod=vendor", *std_go_args(ldflags: ldflags), "./cmd/..."
+  end
+
+  test do
+    system "#{bin}/kn", "service", "create", "foo",
+      "--namespace", "bar",
+      "--image", "gcr.io/cloudrun/hello",
+      "--target", "."
+
+    yaml = File.read(testpath/"bar/ksvc/foo.yaml")
+    assert_match("name: foo", yaml)
+    assert_match("namespace: bar", yaml)
+    assert_match("image: gcr.io/cloudrun/hello", yaml)
+
+    version_output = shell_output("#{bin}/kn version")
+    assert_match("Version:      v#{version}", version_output)
+    assert_match("Build Date:   ", version_output)
+    assert_match(/Git Revision: [a-f0-9]{8}/, version_output)
+  end
+end
