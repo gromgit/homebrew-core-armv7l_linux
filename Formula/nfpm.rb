07db9902ca760b89db6ca6f12f9217183dc7c9fd
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 20:08:46 +0800

    nfpm: update 2.15.1 bottle.

---
+++ Formula/nfpm.rb
@@ -0,0 +1,42 @@
+class Nfpm < Formula
+  desc "Simple deb and rpm packager"
+  homepage "https://nfpm.goreleaser.com/"
+  url "https://github.com/goreleaser/nfpm/archive/v2.15.1.tar.gz"
+  sha256 "ce43c832311f3e8ce0432d6e7a72e91789705de309cfb42883e496d57f56bcec"
+  license "MIT"
+  head "https://github.com/goreleaser/nfpm.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/nfpm"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "26a99af8dc930fc67e268ba00f4a6048de15626ab5704f04e5df6a29a7d375d0"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-X main.version=v#{version}"), "./cmd/nfpm"
+  end
+
+  test do
+    assert_match version.to_s,
+      shell_output("#{bin}/nfpm --version 2>&1")
+
+    system bin/"nfpm", "init"
+    assert_match "nfpm example config file", File.read(testpath/"nfpm.yaml")
+
+    # remove the generated default one
+    # and use stubbed one for another test
+    File.delete(testpath/"nfpm.yaml")
+    (testpath/"nfpm.yaml").write <<~EOS
+      name: "foo"
+      arch: "amd64"
+      platform: "linux"
+      version: "v1.0.0"
+      section: "default"
+      priority: "extra"
+    EOS
+
+    system bin/"nfpm", "pkg", "--packager", "deb", "--target", "."
+    assert_predicate testpath/"foo_1.0.0_amd64.deb", :exist?
+  end
+end
