ce4907764c045dbe246d7337f78d9f949be06152
Adrian Ho <the.gromgit@gmail.com>
2022-06-09 03:40:40 +0800

    ssh-vault: update 0.12.8 bottle.

---
+++ Formula/ssh-vault.rb
@@ -0,0 +1,28 @@
+class SshVault < Formula
+  desc "Encrypt/decrypt using SSH keys"
+  homepage "https://ssh-vault.com/"
+  url "https://github.com/ssh-vault/ssh-vault/archive/0.12.8.tar.gz"
+  sha256 "db20269f43ecd98064cef784ef3c7aba3e0eb25ad88ee7449ba2d3d71f13b191"
+  license "BSD-3-Clause"
+  head "https://github.com/ssh-vault/ssh-vault.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/ssh-vault"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "642eb5addef61f2d95ba06e4b2a6c395185dfe0b2788e8fdc248f7f7cd17272e"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X main.version=#{version}"
+    system "go", "build", *std_go_args(ldflags: ldflags), "cmd/ssh-vault/main.go"
+  end
+
+  test do
+    output = pipe_output("#{bin}/ssh-vault -u new create", "hi")
+    fingerprint = output.split("\n").first.split(";").last
+    cmd = "#{bin}/ssh-vault -k https://ssh-keys.online/key/#{fingerprint} view"
+    output = pipe_output(cmd, output, 0)
+    assert_equal "hi", output.chomp
+  end
+end
