9f62d5ac10fa9a7ef780b784eef0147701856920
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 20:37:46 +0800

    nuclei: update 2.7.1 bottle.

---
+++ Formula/nuclei.rb
@@ -0,0 +1,45 @@
+class Nuclei < Formula
+  desc "HTTP/DNS scanner configurable via YAML templates"
+  homepage "https://nuclei.projectdiscovery.io/"
+  url "https://github.com/projectdiscovery/nuclei/archive/v2.7.1.tar.gz"
+  sha256 "a6283e34067a6c40ff9e239ef69c53f246f7877559f99490d65ac566f56fba84"
+  license "MIT"
+  head "https://github.com/projectdiscovery/nuclei.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/nuclei"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "4198014b57bcd80be5b2f7bb01b69bb87c6836d61326d747b335aeebf72cd225"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    cd "v2/cmd/nuclei" do
+      system "go", "build", *std_go_args, "main.go"
+    end
+  end
+
+  test do
+    (testpath/"test.yaml").write <<~EOS
+      id: homebrew-test
+
+      info:
+        name: Homebrew test
+        author: bleepnetworks
+        severity: INFO
+        description: Check DNS functionality
+
+      dns:
+        - name: \"{{FQDN}}\"
+          type: A
+          class: inet
+          recursion: true
+          retries: 3
+          matchers:
+            - type: word
+              words:
+                - \"IN\tA\"
+    EOS
+    system "nuclei", "-target", "google.com", "-t", "test.yaml"
+  end
+end
