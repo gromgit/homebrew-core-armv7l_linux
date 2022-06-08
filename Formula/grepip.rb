71186ca26b21fa8ede182864674837a7c4b95340
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 10:21:11 +0800

    grepip: update 1.2.1 bottle.

---
+++ Formula/grepip.rb
@@ -0,0 +1,37 @@
+class Grepip < Formula
+  desc "Filters IPv4 & IPv6 addresses with a grep-compatible interface"
+  homepage "https://ipinfo.io"
+  url "https://github.com/ipinfo/cli/archive/grepip-1.2.1.tar.gz"
+  sha256 "e1c838ecac436e744b7808bf3a123be8ce6b72cc436dead64a0a13f409804ea8"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    regex(/^grepip[._-]v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/grepip"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "a167e831cb46a3cc3bc16ebf0d89fc1917700b4f5eb7f5003f5b492a4c3c9069"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args, "./grepip"
+  end
+
+  test do
+    assert_equal version.to_s, shell_output("#{bin}/grepip --version").chomp
+    assert_equal "1.1.1.1", pipe_output("#{bin}/grepip -o", "asdf 1.1.1.1 asdf").chomp
+
+    (testpath/"access.log").write <<~EOS
+      127.0.0.1 valid ip but reserved
+      111.119.187.44 valid ip
+      8.8.8. invalid ip
+      no ip
+    EOS
+    output = shell_output("#{bin}/grepip --exclude-reserved -h access.log")
+    assert_equal "111.119.187.44 valid ip", output.strip
+  end
+end
