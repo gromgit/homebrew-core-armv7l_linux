75e49661dae3ded7e5c9e119eb3839d9ee2a6217
Adrian Ho <the.gromgit@gmail.com>
2022-06-09 02:07:47 +0800

    seaweedfs: update 3.08 bottle.

---
+++ Formula/seaweedfs.rb
@@ -0,0 +1,48 @@
+class Seaweedfs < Formula
+  desc "Fast distributed storage system"
+  homepage "https://github.com/chrislusf/seaweedfs"
+  url "https://github.com/chrislusf/seaweedfs.git",
+      tag:      "3.08",
+      revision: "8a49240d64f5e53f119e58b923c3a6b25e85b37b"
+  license "Apache-2.0"
+  head "https://github.com/chrislusf/seaweedfs.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/seaweedfs"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "89149662104d770784db85c1fc8898c8ce13cb395e375bfedfa7eb1280e69f63"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/chrislusf/seaweedfs/weed/util.COMMIT=#{Utils.git_head}
+    ]
+    system "go", "build", *std_go_args(output: bin/"weed", ldflags: ldflags), "./weed"
+  end
+
+  test do
+    # Start SeaweedFS master server/volume server
+    master_port = free_port
+    volume_port = free_port
+    master_grpc_port = free_port
+    volume_grpc_port = free_port
+
+    fork do
+      exec bin/"weed", "server", "-dir=#{testpath}", "-ip.bind=0.0.0.0",
+           "-master.port=#{master_port}", "-volume.port=#{volume_port}",
+           "-master.port.grpc=#{master_grpc_port}", "-volume.port.grpc=#{volume_grpc_port}"
+    end
+    sleep 30
+
+    # Upload a test file
+    fid = JSON.parse(shell_output("curl http://localhost:#{master_port}/dir/assign"))["fid"]
+    system "curl", "-F", "file=@#{test_fixtures("test.png")}", "http://localhost:#{volume_port}/#{fid}"
+
+    # Download and validate uploaded test file against the original
+    expected_sum = Digest::SHA256.hexdigest(File.read(test_fixtures("test.png")))
+    actual_sum = Digest::SHA256.hexdigest(shell_output("curl http://localhost:#{volume_port}/#{fid}"))
+    assert_equal expected_sum, actual_sum
+  end
+end
