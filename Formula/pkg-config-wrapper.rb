972928a7876d2af680c56aaaceb3fd4362baae0a
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 23:33:11 +0800

    pkg-config-wrapper: update 0.2.12 bottle.

---
+++ Formula/pkg-config-wrapper.rb
@@ -0,0 +1,29 @@
+class PkgConfigWrapper < Formula
+  desc "Easier way to include C code in your Go program"
+  homepage "https://github.com/influxdata/pkg-config"
+  url "https://github.com/influxdata/pkg-config/archive/v0.2.12.tar.gz"
+  sha256 "23b2ed6a2f04d42906f5a8c28c8d681d03d47a1c32435b5df008adac5b935f1a"
+  license "MIT"
+  head "https://github.com/influxdata/pkg-config.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/pkg-config-wrapper"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "fb39aa0850eb095b1cb4bf8cc41c9b5034fa7ba96ea9041bffb244225b3778d4"
+  end
+
+  depends_on "go" => :build
+  depends_on "pkg-config"
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    assert_match "Found pkg-config executable", shell_output(bin/"pkg-config-wrapper 2>&1", 1)
+  end
+end
