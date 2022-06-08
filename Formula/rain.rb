48d4ee7d9c4eb8fbf23c562a669b981828ed3590
Adrian Ho <the.gromgit@gmail.com>
2022-06-09 00:30:14 +0800

    rain: update 1.2.0 bottle.

---
+++ Formula/rain.rb
@@ -0,0 +1,36 @@
+class Rain < Formula
+  desc "Command-line tool for working with AWS CloudFormation"
+  homepage "https://github.com/aws-cloudformation/rain"
+  url "https://github.com/aws-cloudformation/rain/archive/v1.2.0.tar.gz"
+  sha256 "064bc2b563c9b759d16147f33fe5c64bf0af3640cb4ae543e49615ae17b22e01"
+  license "Apache-2.0"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/rain"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "644515b7352c40464da6e763e1cbc5eee1d830f2818fcf0a4d0bef6c39db8020"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args, "cmd/rain/main.go"
+    bash_completion.install "docs/bash_completion.sh"
+    zsh_completion.install "docs/zsh_completion.sh"
+  end
+
+  def caveats
+    <<~EOS
+      Deploying CloudFormation stacks with rain requires the AWS CLI to be installed.
+      All other functionality works without the AWS CLI.
+    EOS
+  end
+
+  test do
+    (testpath/"test.template").write <<~EOS
+      Resources:
+        Bucket:
+          Type: AWS::S3::Bucket
+    EOS
+    assert_equal "test.template: formatted OK", shell_output("#{bin}/rain fmt -v test.template").strip
+  end
+end
