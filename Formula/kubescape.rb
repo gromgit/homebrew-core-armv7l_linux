0e1c5ea85aa09225065166893548286bf39c774d
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 15:39:28 +0800

    kubescape: update 2.0.157 bottle.

---
+++ Formula/kubescape.rb
@@ -0,0 +1,38 @@
+class Kubescape < Formula
+  desc "Kubernetes testing according to Hardening Guidance by NSA and CISA"
+  homepage "https://github.com/armosec/kubescape"
+  url "https://github.com/armosec/kubescape/archive/v2.0.157.tar.gz"
+  sha256 "7e69b637fcc9110ce7f8d677485f329647ed51f42fcc6538af46cbb45ef60feb"
+  license "Apache-2.0"
+  head "https://github.com/armosec/kubescape.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/kubescape"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "2587b4ee80da1604a4a1e08036862133be356b42447a9f6bd9fca4a1999f096f"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/armosec/kubescape/v2/core/cautils.BuildNumber=v#{version}
+    ]
+
+    system "go", "build", *std_go_args(ldflags: ldflags)
+
+    output = Utils.safe_popen_read(bin/"kubescape", "completion", "bash")
+    (bash_completion/"kubescape").write output
+    output = Utils.safe_popen_read(bin/"kubescape", "completion", "zsh")
+    (zsh_completion/"_kubescape").write output
+    output = Utils.safe_popen_read(bin/"kubescape", "completion", "fish")
+    (fish_completion/"kubescape.fish").write output
+  end
+
+  test do
+    manifest = "https://raw.githubusercontent.com/GoogleCloudPlatform/microservices-demo/b8fe8900ca1da10c85c9a203d9832b2ee33cc85f/release/kubernetes-manifests.yaml"
+    assert_match "FAILED RESOURCES", shell_output("#{bin}/kubescape scan framework nsa #{manifest}")
+
+    assert_match version.to_s, shell_output("#{bin}/kubescape version")
+  end
+end
