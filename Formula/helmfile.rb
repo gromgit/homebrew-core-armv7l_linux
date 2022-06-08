f70f6563d7af320d1bc15191d5afa0ac24985bef
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 10:52:04 +0800

    helmfile: update 0.144.0 bottle.

---
+++ Formula/helmfile.rb
@@ -0,0 +1,41 @@
+class Helmfile < Formula
+  desc "Deploy Kubernetes Helm Charts"
+  homepage "https://github.com/roboll/helmfile"
+  url "https://github.com/roboll/helmfile/archive/v0.144.0.tar.gz"
+  sha256 "fc767d10ec21ca464caaefd309f410d96685a985090c237907a22bd983112c62"
+  license "MIT"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/helmfile"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "cf239fcb3153485487bb806f74a1cf0fc3b5ea87ab448e9d428b4f182ee7a092"
+  end
+
+  depends_on "go" => :build
+  depends_on "helm"
+
+  def install
+    system "go", "build", "-ldflags", "-X github.com/roboll/helmfile/pkg/app/version.Version=v#{version}",
+             "-o", bin/"helmfile", "-v", "github.com/roboll/helmfile"
+  end
+
+  test do
+    (testpath/"helmfile.yaml").write <<-EOS
+    repositories:
+    - name: stable
+      url: https://charts.helm.sh/stable
+
+    releases:
+    - name: vault                            # name of this release
+      namespace: vault                       # target namespace
+      createNamespace: true                  # helm 3.2+ automatically create release namespace (default true)
+      labels:                                # Arbitrary key value pairs for filtering releases
+        foo: bar
+      chart: stable/vault                    # the chart being installed to create this release, referenced by `repository/chart` syntax
+      version: ~1.24.1                       # the semver of the chart. range constraint is supported
+    EOS
+    system Formula["helm"].opt_bin/"helm", "create", "foo"
+    output = "Adding repo stable https://charts.helm.sh/stable"
+    assert_match output, shell_output("#{bin}/helmfile -f helmfile.yaml repos 2>&1")
+    assert_match version.to_s, shell_output("#{bin}/helmfile -v")
+  end
+end
