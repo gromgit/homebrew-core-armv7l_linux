b127002c768e68482f9731d3f7ff87d512983102
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 21:21:01 +0800

    okteto: update 2.3.2 bottle.

---
+++ Formula/okteto.rb
@@ -0,0 +1,38 @@
+class Okteto < Formula
+  desc "Build better apps by developing and testing code directly in Kubernetes"
+  homepage "https://okteto.com"
+  url "https://github.com/okteto/okteto/archive/2.3.2.tar.gz"
+  sha256 "36b9f91d42ad082b1280421c0fa137f4ff6afc7a99ab646d92e73beb29c8881f"
+  license "Apache-2.0"
+  head "https://github.com/okteto/okteto.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/okteto"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "d6b4b87b9fd48d3fd9e9de87d2bdb3f359ecfcff627ee7af6aead86124988260"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X github.com/okteto/okteto/pkg/config.VersionString=#{version}"
+    tags = "osusergo netgo static_build"
+    system "go", "build", *std_go_args(ldflags: ldflags), "-tags", tags
+
+    bash_output = Utils.safe_popen_read(bin/"okteto", "completion", "bash")
+    (bash_completion/"okteto").write bash_output
+    zsh_output = Utils.safe_popen_read(bin/"okteto", "completion", "zsh")
+    (zsh_completion/"_okteto").write zsh_output
+    fish_output = Utils.safe_popen_read(bin/"okteto", "completion", "fish")
+    (fish_completion/"okteto.fish").write fish_output
+  end
+
+  test do
+    assert_match "okteto version #{version}", shell_output("#{bin}/okteto version")
+
+    assert_match "Please run 'okteto context' to select one context",
+      shell_output(bin/"okteto init --context test 2>&1", 1)
+
+    assert_match "No contexts are available.",
+      shell_output(bin/"okteto context list 2>&1", 1)
+  end
+end
