74b4a1a1c39be99f3fcc813beebdd631c9d8e7f4
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 23:44:47 +0800

    prestd: update 1.0.14 bottle.

---
+++ Formula/prestd.rb
@@ -0,0 +1,45 @@
+class Prestd < Formula
+  desc "Simplify and accelerate development on any Postgres application, existing or new"
+  homepage "https://github.com/prest/prest"
+  url "https://github.com/prest/prest/archive/v1.0.14.tar.gz"
+  sha256 "65716a72f7dca448e0b4c7082dd8c7dee57c29320eae5c44b2f38fab0d77dae7"
+  license "MIT"
+  head "https://github.com/prest/prest.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/prestd"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "86ab9c2c47f8de45dda2a14320dc154a645f87c271b68d760fb93c423bf73d21"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args, "-ldflags",
+      "-s -w -X github.com/prest/prest/helpers.PrestVersionNumber=#{version}",
+      "./cmd/prestd"
+  end
+
+  test do
+    (testpath/"prest.toml").write <<~EOS
+      [jwt]
+      default = false
+
+      [pg]
+      host = "127.0.0.1"
+      user = "prest"
+      pass = "prest"
+      port = 5432
+      database = "prest"
+    EOS
+
+    output = shell_output("prestd migrate up --path .", 255)
+    assert_match "connect: connection refused", output
+
+    assert_match version.to_s, shell_output("prestd version")
+  end
+end
