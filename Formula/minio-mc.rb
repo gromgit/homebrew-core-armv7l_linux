d4e6caa94666f1b7e16260891327c14a727bdb39
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 19:08:34 +0800

    minio-mc: update 20220509040826 bottle.

---
+++ Formula/minio-mc.rb
@@ -0,0 +1,48 @@
+class MinioMc < Formula
+  desc "Replacement for ls, cp and other commands for object storage"
+  homepage "https://github.com/minio/mc"
+  url "https://github.com/minio/mc.git",
+      tag:      "RELEASE.2022-05-09T04-08-26Z",
+      revision: "b5a0640899f8f8653bcacd19791c92ca22066ba3"
+  version "20220509040826"
+  license "AGPL-3.0-or-later"
+  head "https://github.com/minio/mc.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(%r{href=.*?/tag/(?:RELEASE[._-]?)?([\d\-TZ]+)["' >]}i)
+    strategy :github_latest do |page, regex|
+      page.scan(regex).map { |match| match&.first&.gsub(/\D/, "") }
+    end
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/minio-mc"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "a7787956e32b873630832e00765491c2e09568ff16a2db6e48d4f404a7d53a7a"
+  end
+
+  depends_on "go" => :build
+
+  conflicts_with "midnight-commander", because: "both install an `mc` binary"
+
+  def install
+    if build.head?
+      system "go", "build", "-trimpath", "-o", bin/"mc"
+    else
+      minio_release = `git tag --points-at HEAD`.chomp
+      minio_version = minio_release.gsub(/RELEASE\./, "").chomp.gsub(/T(\d+)-(\d+)-(\d+)Z/, 'T\1:\2:\3Z')
+      proj = "github.com/minio/mc"
+
+      system "go", "build", "-trimpath", "-o", bin/"mc", "-ldflags", <<~EOS
+        -X #{proj}/cmd.Version=#{minio_version}
+        -X #{proj}/cmd.ReleaseTag=#{minio_release}
+        -X #{proj}/cmd.CommitID=#{Utils.git_head}
+      EOS
+    end
+  end
+
+  test do
+    system bin/"mc", "mb", testpath/"test"
+    assert_predicate testpath/"test", :exist?
+  end
+end
