f31c4becbd7ae67c07037ca19aae75aa6fa08e8f
Adrian Ho <the.gromgit@gmail.com>
2022-06-09 00:22:11 +0800

    rack: update 1.2 bottle.

---
+++ Formula/rack.rb
@@ -0,0 +1,42 @@
+class Rack < Formula
+  desc "CLI for Rackspace"
+  homepage "https://github.com/rackspace/rack"
+  url "https://github.com/rackspace/rack.git",
+      tag:      "1.2",
+      revision: "09c14b061f4a115c8f1ff07ae6be96d9b11e08df"
+  license "Apache-2.0"
+  head "https://github.com/rackspace/rack.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/rack"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "8301e32098ad99abfd2b6160b0de116f4cc7bcb5bd1619ac5f421800185c0e85"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    ENV["TRAVIS_TAG"] = version
+    ENV["GO111MODULE"] = "auto"
+
+    rackpath = buildpath/"src/github.com/rackspace/rack"
+    rackpath.install Dir["{*,.??*}"]
+
+    cd rackpath do
+      # This is a slightly grim hack to handle the weird logic around
+      # deciding whether to add a = or not on the ldflags, as mandated
+      # by Go 1.7+.
+      # https://github.com/rackspace/rack/issues/446
+      inreplace "script/build", "go1.5", Utils.safe_popen_read("go", "version")[/go1\.\d/]
+
+      ln_s "internal", "vendor"
+      system "script/build", "rack"
+      bin.install "rack"
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    system "#{bin}/rack"
+  end
+end
