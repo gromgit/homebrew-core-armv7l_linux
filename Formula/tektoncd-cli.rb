1115bb28332b06c2585af6725f0ed5f6e3308a9a
Adrian Ho <the.gromgit@gmail.com>
2022-06-09 05:04:15 +0800

    tektoncd-cli: update 0.23.1 bottle.

---
+++ Formula/tektoncd-cli.rb
@@ -0,0 +1,37 @@
+class TektoncdCli < Formula
+  desc "CLI for interacting with TektonCD"
+  homepage "https://github.com/tektoncd/cli"
+  url "https://github.com/tektoncd/cli/archive/v0.23.1.tar.gz"
+  sha256 "49ea8c907c10514e219b3536fad481c537c09b8fa264eb0c0f3c4ece61bcabc5"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/tektoncd-cli"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "2a963587eef2d3459a64a43843d15ca976207e4df36e07b1e8e4996eed8645ad"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "make", "bin/tkn"
+    bin.install "bin/tkn" => "tkn"
+
+    output = Utils.safe_popen_read(bin/"tkn", "completion", "bash")
+    (bash_completion/"tkn").write output
+    output = Utils.safe_popen_read(bin/"tkn", "completion", "zsh")
+    (zsh_completion/"_tkn").write output
+    output = Utils.safe_popen_read(bin/"tkn", "completion", "fish")
+    (fish_completion/"tkn.fish").write output
+  end
+
+  test do
+    cmd = "#{bin}/tkn pipelinerun describe homebrew-formula"
+    io = IO.popen(cmd, err: [:child, :out])
+    assert_match "Error: Couldn't get kubeConfiguration namespace", io.read
+  end
+end
