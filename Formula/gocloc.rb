5fbd760a45fe8f845f0220e09b6894577db831a3
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 08:43:33 +0800

    gocloc: update 0.4.2 bottle.

---
+++ Formula/gocloc.rb
@@ -0,0 +1,32 @@
+class Gocloc < Formula
+  desc "Little fast LoC counter"
+  homepage "https://github.com/hhatto/gocloc"
+  url "https://github.com/hhatto/gocloc/archive/v0.4.2.tar.gz"
+  sha256 "4b3c092b405d9bd50b49d1aee1c3fa284445812b3fcfae95989a0dd2b75a25c0"
+  license "MIT"
+  head "https://github.com/hhatto/gocloc.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/gocloc"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "0c48b15608db331ac5c67f259773f388c5f1c10cfc5b3519f8dd768e2bda06df"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args, "./cmd/gocloc"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      int main(void) {
+        return 0;
+      }
+    EOS
+
+    assert_equal "{\"languages\":[{\"name\":\"C\",\"files\":1,\"code\":4,\"comment\":0," \
+                 "\"blank\":0}],\"total\":{\"files\":1,\"code\":4,\"comment\":0,\"blank\":0}}",
+                 shell_output("#{bin}/gocloc --output-type=json .")
+  end
+end
