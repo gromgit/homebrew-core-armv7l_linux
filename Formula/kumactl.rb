85a2c7c05920b4c7e40588825b0f87b5b1c33571
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 16:05:06 +0800

    kumactl: update 1.6.0 bottle.

---
+++ Formula/kumactl.rb
@@ -0,0 +1,48 @@
+class Kumactl < Formula
+  desc "Kuma control plane command-line utility"
+  homepage "https://kuma.io/"
+  url "https://github.com/kumahq/kuma/archive/1.6.0.tar.gz"
+  sha256 "5dfd6c561a0b3e190868df00f4ec4aa93a9ba159cabe982b98b20c5c0c5df6aa"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/kumactl"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "45c66d6eda1e78a4ecc185cdab481259fdb089311317f13479f3a39c5957ed9c"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/kumahq/kuma/pkg/version.version=#{version}
+      -X github.com/kumahq/kuma/pkg/version.gitTag=#{version}
+      -X github.com/kumahq/kuma/pkg/version.buildDate=#{time.strftime("%F")}
+    ]
+
+    system "go", "build", *std_go_args(ldflags: ldflags), "./app/kumactl"
+
+    output = Utils.safe_popen_read("#{bin}/kumactl", "completion", "bash")
+    (bash_completion/"kumactl").write output
+
+    output = Utils.safe_popen_read("#{bin}/kumactl", "completion", "zsh")
+    (zsh_completion/"_kumactl").write output
+
+    output = Utils.safe_popen_read("#{bin}/kumactl", "completion", "fish")
+    (fish_completion/"kumactl.fish").write output
+  end
+
+  test do
+    assert_match "Management tool for Kuma.", shell_output("#{bin}/kumactl")
+    assert_match version.to_s, shell_output("#{bin}/kumactl version 2>&1")
+
+    touch testpath/"config.yml"
+    assert_match "Error: no resource(s) passed to apply",
+    shell_output("#{bin}/kumactl apply -f config.yml 2>&1", 1)
+  end
+end
