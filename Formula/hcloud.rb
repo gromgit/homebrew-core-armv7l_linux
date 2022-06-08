35bfab9ca3f28aae0c864f99c2a5996a21e85192
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 10:41:21 +0800

    hcloud: update 1.29.5 bottle.

---
+++ Formula/hcloud.rb
@@ -0,0 +1,41 @@
+class Hcloud < Formula
+  desc "Command-line interface for Hetzner Cloud"
+  homepage "https://github.com/hetznercloud/cli"
+  url "https://github.com/hetznercloud/cli/archive/v1.29.5.tar.gz"
+  sha256 "8a8a0a8ee86eaae329090657f236c8f736c5c67209fedf5047977f8ef8c2afb8"
+  license "MIT"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/hcloud"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "e4077621548ac018b2ad3f3105b8ebed823ed7090e88669157e24f7c70366097"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X github.com/hetznercloud/cli/internal/version.Version=v#{version}"
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/hcloud"
+
+    output = Utils.safe_popen_read(bin/"hcloud", "completion", "bash")
+    (bash_completion/"hcloud").write output
+    output = Utils.safe_popen_read(bin/"hcloud", "completion", "zsh")
+    (zsh_completion/"_hcloud").write output
+    output = Utils.safe_popen_read(bin/"hcloud", "completion", "fish")
+    (fish_completion/"hcloud.fish").write output
+  end
+
+  test do
+    config_path = testpath/".config/hcloud/cli.toml"
+    ENV["HCLOUD_CONFIG"] = config_path
+    assert_match "", shell_output("#{bin}/hcloud context active")
+    config_path.write <<~EOS
+      active_context = "test"
+      [[contexts]]
+      name = "test"
+      token = "foobar"
+    EOS
+    assert_match "test", shell_output("#{bin}/hcloud context list")
+    assert_match "test", shell_output("#{bin}/hcloud context active")
+    assert_match "hcloud v#{version}", shell_output("#{bin}/hcloud version")
+  end
+end
