a18bad05c017aef7b3aece947bb45f4597291c54
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 22:33:16 +0800

    packer: update 1.8.1 bottle.

---
+++ Formula/packer.rb
@@ -0,0 +1,53 @@
+class Packer < Formula
+  desc "Tool for creating identical machine images for multiple platforms"
+  homepage "https://packer.io"
+  url "https://github.com/hashicorp/packer/archive/v1.8.1.tar.gz"
+  sha256 "2a264119f7bdeeb82e79e0c9a02e4fa3d9bdf3e984c47e0c89ca2856eecb3b88"
+  license "MPL-2.0"
+  head "https://github.com/hashicorp/packer.git", branch: "main"
+
+  livecheck do
+    url "https://releases.hashicorp.com/packer/"
+    regex(%r{href=.*?v?(\d+(?:\.\d+)+)/?["' >]}i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/packer"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "5cc267d7d62fbd38403768a2d4402e55fda0d5e95f631186e83cd9a52a4f42cb"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+
+    # Allow packer to find plugins in Homebrew prefix
+    bin.env_script_all_files libexec/"bin", PACKER_PLUGIN_PATH: "$PACKER_PLUGIN_PATH:#{HOMEBREW_PREFIX/"bin"}"
+
+    zsh_completion.install "contrib/zsh-completion/_packer"
+  end
+
+  test do
+    minimal = testpath/"minimal.json"
+    minimal.write <<~EOS
+      {
+        "builders": [{
+          "type": "amazon-ebs",
+          "region": "us-east-1",
+          "source_ami": "ami-59a4a230",
+          "instance_type": "m3.medium",
+          "ssh_username": "ubuntu",
+          "ami_name": "homebrew packer test  {{timestamp}}"
+        }],
+        "provisioners": [{
+          "type": "shell",
+          "inline": [
+            "sleep 30",
+            "sudo apt-get update"
+          ]
+        }]
+      }
+    EOS
+    system "#{bin}/packer", "validate", "-syntax-only", minimal
+  end
+end
