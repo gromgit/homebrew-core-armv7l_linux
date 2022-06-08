fbb7a75b2200cc4d421d0996a0b56af1526ea842
Adrian Ho <the.gromgit@gmail.com>
2022-06-09 04:31:01 +0800

    syncthing: update 1.20.1 bottle.

---
+++ Formula/syncthing.rb
@@ -0,0 +1,43 @@
+class Syncthing < Formula
+  desc "Open source continuous file synchronization application"
+  homepage "https://syncthing.net/"
+  url "https://github.com/syncthing/syncthing/archive/v1.20.1.tar.gz"
+  sha256 "4b355eda64b9f52d53b34ab99a1ff73dc3d5ad5e43086223e7fc7f4d05a58a05"
+  license "MPL-2.0"
+  head "https://github.com/syncthing/syncthing.git", branch: "main"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/syncthing"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "c887713217d0ef78c07bfb36e2d7d6e503b670b516af23e6960df21542eb7b94"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    build_version = build.head? ? "v0.0.0-#{version}" : "v#{version}"
+    system "go", "run", "build.go", "--version", build_version, "--no-upgrade", "tar"
+    bin.install "syncthing"
+
+    man1.install Dir["man/*.1"]
+    man5.install Dir["man/*.5"]
+    man7.install Dir["man/*.7"]
+  end
+
+  service do
+    run [opt_bin/"syncthing", "-no-browser", "-no-restart"]
+    keep_alive true
+    log_path var/"log/syncthing.log"
+    error_log_path var/"log/syncthing.log"
+  end
+
+  test do
+    build_version = build.head? ? "v0.0.0-#{version}" : "v#{version}"
+    assert_match "syncthing #{build_version} ", shell_output("#{bin}/syncthing --version")
+    system bin/"syncthing", "-generate", "./"
+  end
+end
