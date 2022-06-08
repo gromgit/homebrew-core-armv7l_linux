8fe3592d1c9b7d6883c576f6f6bbdf151ad23131
Adrian Ho <the.gromgit@gmail.com>
2022-06-09 03:48:25 +0800

    staticcheck: update 2022.1.2 bottle.

---
+++ Formula/staticcheck.rb
@@ -0,0 +1,35 @@
+class Staticcheck < Formula
+  desc "State of the art linter for the Go programming language"
+  homepage "https://staticcheck.io/"
+  url "https://github.com/dominikh/go-tools/archive/2022.1.2.tar.gz"
+  sha256 "807a3b3e5375c3b3b591567dc49e1e1f014498681bb89d6508ef2cc3a76307ab"
+  license "MIT"
+  head "https://github.com/dominikh/go-tools.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/staticcheck"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "13a529b32f4a3fd8300ef47780ee0a5aac8e405c6fc4ff1006e82e609ebcbfc1"
+  end
+
+  depends_on "go"
+
+  def install
+    system "go", "build", *std_go_args, "./cmd/staticcheck"
+  end
+
+  test do
+    (testpath/"test.go").write <<~EOS
+      package main
+
+      import "fmt"
+
+      func main() {
+        var x uint
+        x = 1
+        fmt.Println(x)
+      }
+    EOS
+    json_output = JSON.parse(shell_output("#{bin}/staticcheck -f json test.go", 1))
+    assert_equal json_output["code"], "S1021"
+  end
+end
