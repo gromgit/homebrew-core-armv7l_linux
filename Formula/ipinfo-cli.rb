448ccd62d1db4f3b977136fbd8ee13bd9a536370
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 11:57:00 +0800

    ipinfo-cli: update 2.8.1 bottle.

---
+++ Formula/ipinfo-cli.rb
@@ -0,0 +1,31 @@
+class IpinfoCli < Formula
+  desc "Official CLI for the IPinfo IP Address API"
+  homepage "https://ipinfo.io/"
+  url "https://github.com/ipinfo/cli/archive/ipinfo-2.8.1.tar.gz"
+  sha256 "efc690a7a532db51395de9b744651e09c9db0fe8d7d8f1285409cddb476620d3"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    regex(/^ipinfo[._-]v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/ipinfo-cli"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "372c03fbc8f3fd07f2892e2bbe2563e757dcc1d64c38f7e0ce77a066538fc523"
+  end
+
+  depends_on "go" => :build
+
+  conflicts_with "ipinfo", because: "ipinfo and ipinfo-cli install the same binaries"
+
+  def install
+    system "./ipinfo/build.sh"
+    bin.install "build/ipinfo"
+  end
+
+  test do
+    assert_equal version.to_s, shell_output("#{bin}/ipinfo version").chomp
+    assert_equal "1.1.1.0\n1.1.1.1\n1.1.1.2\n1.1.1.3\n", `#{bin}/ipinfo prips 1.1.1.1/30`
+  end
+end
