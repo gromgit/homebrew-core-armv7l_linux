f6aeaf35bb4f5daf96e8b5e365e016791d3ddb16
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 19:54:49 +0800

    nebula: update 1.5.2_1 bottle.

---
+++ Formula/nebula.rb
@@ -0,0 +1,43 @@
+class Nebula < Formula
+  desc "Scalable overlay networking tool for connecting computers anywhere"
+  homepage "https://github.com/slackhq/nebula"
+  url "https://github.com/slackhq/nebula/archive/v1.5.2.tar.gz"
+  sha256 "391ac38161561690a65c0fa5ad65a2efb2d187323cc8ee84caa95fa24cb6c36a"
+  license "MIT"
+  revision 1
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/nebula"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "fbb4a18d87e21ebde9ca5236b9cd26b60c70044b6dd748248d05da2afda55593"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ENV["BUILD_NUMBER"] = version
+    system "make", "service"
+    bin.install "./nebula"
+    bin.install "./nebula-cert"
+    prefix.install_metafiles
+  end
+
+  plist_options startup: true
+  service do
+    run [opt_bin/"nebula", "-config", etc/"nebula/config.yml"]
+    keep_alive true
+    log_path var/"log/nebula.log"
+    error_log_path var/"log/nebula.log"
+  end
+
+  test do
+    system "#{bin}/nebula-cert", "ca", "-name", "testorg"
+    system "#{bin}/nebula-cert", "sign", "-name", "host", "-ip", "192.168.100.1/24"
+    (testpath/"config.yml").write <<~EOS
+      pki:
+        ca: #{testpath}/ca.crt
+        cert: #{testpath}/host.crt
+        key: #{testpath}/host.key
+    EOS
+    system "#{bin}/nebula", "-test", "-config", "config.yml"
+  end
+end
