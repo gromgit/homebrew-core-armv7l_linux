cbf84ce348ff599977a16765c765be7bc3f44d5a
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 19:51:31 +0800

    nats-streaming-server: update 0.24.6 bottle.

---
+++ Formula/nats-streaming-server.rb
@@ -0,0 +1,46 @@
+class NatsStreamingServer < Formula
+  desc "Lightweight cloud messaging system"
+  homepage "https://nats.io"
+  url "https://github.com/nats-io/nats-streaming-server/archive/v0.24.6.tar.gz"
+  sha256 "750e091f3473688a1b0c251513bf9beb7a12e3e1359d541b53381bd91e09b9be"
+  license "Apache-2.0"
+  head "https://github.com/nats-io/nats-streaming-server.git", branch: "main"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/nats-streaming-server"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "c4d349e85f3ebe4de68f46e1b8b09bd69feb113d190a67e8dd54a81ce2f0e97f"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-ldflags", "-s -w", "-trimpath", "-o", bin/"nats-streaming-server"
+    prefix.install_metafiles
+  end
+
+  service do
+    run opt_bin/"nats-streaming-server"
+  end
+
+  test do
+    port = free_port
+    http_port = free_port
+    pid = fork do
+      exec "#{bin}/nats-streaming-server",
+           "--port=#{port}",
+           "--http_port=#{http_port}",
+           "--pid=#{testpath}/pid",
+           "--log=#{testpath}/log"
+    end
+    sleep 3
+
+    begin
+      assert_match "uptime", shell_output("curl localhost:#{http_port}/varz")
+      assert_predicate testpath/"log", :exist?
+      assert_match version.to_s, File.read(testpath/"log")
+    ensure
+      Process.kill "SIGINT", pid
+      Process.wait pid
+    end
+  end
+end
