96967d365ba03ce9ab7c53f83639412521c253c8
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 21:27:40 +0800

    opa: update 0.41.0 bottle.

---
+++ Formula/opa.rb
@@ -0,0 +1,37 @@
+class Opa < Formula
+  desc "Open source, general-purpose policy engine"
+  homepage "https://www.openpolicyagent.org"
+  url "https://github.com/open-policy-agent/opa/archive/v0.41.0.tar.gz"
+  sha256 "51771d3b0f5d84f6c35948b2253b499041719f683989cf72a596ff8ff23eebeb"
+  license "Apache-2.0"
+  head "https://github.com/open-policy-agent/opa.git", branch: "main"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/opa"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "11c9c48267a0a24a2815748ed96f88f80fed12bb97f2661fc480e639dea3abe4"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args,
+              "-ldflags", "-X github.com/open-policy-agent/opa/version.Version=#{version}"
+    system "./build/gen-man.sh", "man1"
+    man.install "man1"
+
+    bash_output = Utils.safe_popen_read(bin/"opa", "completion", "bash")
+    (bash_completion/"opa").write bash_output
+
+    zsh_output = Utils.safe_popen_read(bin/"opa", "completion", "zsh")
+    (zsh_completion/"_opa").write zsh_output
+
+    fish_output = Utils.safe_popen_read(bin/"opa", "completion", "fish")
+    (fish_completion/"opa.fish").write fish_output
+  end
+
+  test do
+    output = shell_output("#{bin}/opa eval -f pretty '[x, 2] = [1, y]' 2>&1")
+    assert_equal "+---+---+\n| x | y |\n+---+---+\n| 1 | 2 |\n+---+---+\n", output
+    assert_match "Version: #{version}", shell_output("#{bin}/opa version 2>&1")
+  end
+end
