2b85cba228607afa4203017118dbdd3c6483c75e
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 08:19:56 +0800

    go-critic: update 0.6.3 bottle.

---
+++ Formula/go-critic.rb
@@ -0,0 +1,44 @@
+class GoCritic < Formula
+  desc "Opinionated Go source code linter"
+  homepage "https://go-critic.com"
+  url "https://github.com/go-critic/go-critic/archive/refs/tags/v0.6.3.tar.gz"
+  sha256 "74fd0995f401c49206b569f4c11be867da01f627e8979861d67ac8ea60173d7d"
+  license "MIT"
+  head "https://github.com/go-critic/go-critic.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/go-critic"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "6f5173c3c5cc127eeb373b0a0d06d0ebd4812ebaacdb4c77e7e48598cfbcd122"
+  end
+
+  depends_on "go"
+
+  def install
+    ldflags = "-s -w"
+    ldflags += " -X main.Version=v#{version}" unless build.head?
+    system "go", "build", "-trimpath", "-ldflags", ldflags, "-o", bin/"gocritic", "./cmd/gocritic"
+  end
+
+  test do
+    (testpath/"main.go").write <<~EOS
+      package main
+
+      import "fmt"
+
+      func main() {
+        str := "Homebrew"
+        if len(str) <= 0 {
+          fmt.Println("If you're reading this, something is wrong.")
+        }
+      }
+    EOS
+
+    output = shell_output("#{bin}/gocritic check main.go 2>&1", 1)
+    assert_match "sloppyLen: len(str) <= 0 can be len(str) == 0", output
+  end
+end
