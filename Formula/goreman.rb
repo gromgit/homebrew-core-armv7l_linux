cfbb53697d206b8f1796a949fc075a72d33a0d53
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 09:52:32 +0800

    goreman: update 0.3.12 bottle.

---
+++ Formula/goreman.rb
@@ -0,0 +1,31 @@
+class Goreman < Formula
+  desc "Foreman clone written in Go"
+  homepage "https://github.com/mattn/goreman"
+  url "https://github.com/mattn/goreman/archive/v0.3.12.tar.gz"
+  sha256 "2068badbfffbe213df2d901be00fed273766ef1329895589e99d4ee8cfcfa7d2"
+  license "MIT"
+  head "https://github.com/mattn/goreman.git", branch: "master"
+
+  livecheck do
+    url :homepage
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/goreman"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "c5c93c762e685d4fe7156f3d3faaafbfe3707ea9487c550d67d47afe39489cef"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-ldflags", "-s -w", "-trimpath", "-o", bin/"goreman"
+  end
+
+  test do
+    (testpath/"Procfile").write "web: echo 'hello' > goreman-homebrew-test.out"
+    system bin/"goreman", "start"
+    assert_predicate testpath/"goreman-homebrew-test.out", :exist?
+    assert_match "hello", (testpath/"goreman-homebrew-test.out").read
+  end
+end
