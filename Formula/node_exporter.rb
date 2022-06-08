d5ec43213241b7653cc2e50e1d56ab2ef7624623
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 20:12:00 +0800

    node_exporter: update 1.3.1 bottle.

---
+++ Formula/node_exporter.rb
@@ -0,0 +1,60 @@
+class NodeExporter < Formula
+  desc "Prometheus exporter for machine metrics"
+  homepage "https://prometheus.io/"
+  url "https://github.com/prometheus/node_exporter/archive/v1.3.1.tar.gz"
+  sha256 "66856b6b8953e094c46d7dd5aabd32801375cf4d13d9fe388e320cbaeaff573a"
+  license "Apache-2.0"
+  head "https://github.com/prometheus/node_exporter.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/node_exporter"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "449ca22729667441242578f761eed7ab85f687f94ed4dc49e8891cb4a332d4e9"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -X github.com/prometheus/common/version.Version=#{version}
+      -X github.com/prometheus/common/version.BuildUser=Homebrew
+    ]
+    system "go", "build", "-ldflags", ldflags.join(" "), "-trimpath",
+           "-o", bin/"node_exporter"
+    prefix.install_metafiles
+
+    touch etc/"node_exporter.args"
+
+    (bin/"node_exporter_brew_services").write <<~EOS
+      #!/bin/bash
+      exec #{bin}/node_exporter $(<#{etc}/node_exporter.args)
+    EOS
+  end
+
+  def caveats
+    <<~EOS
+      When run from `brew services`, `node_exporter` is run from
+      `node_exporter_brew_services` and uses the flags in:
+        #{etc}/node_exporter.args
+    EOS
+  end
+
+  service do
+    run [opt_bin/"node_exporter_brew_services"]
+    keep_alive false
+    log_path var/"log/node_exporter.log"
+    error_log_path var/"log/node_exporter.err.log"
+  end
+
+  test do
+    assert_match "node_exporter", shell_output("#{bin}/node_exporter --version 2>&1")
+
+    fork { exec bin/"node_exporter" }
+    sleep 2
+    assert_match "# HELP", shell_output("curl -s localhost:9100/metrics")
+  end
+end
