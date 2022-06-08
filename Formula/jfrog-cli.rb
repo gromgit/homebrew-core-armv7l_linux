b865b3f5031b69e64441eb5f46bdfd4f2e9b75dd
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 12:08:46 +0800

    jfrog-cli: update 2.18.2 bottle.

---
+++ Formula/jfrog-cli.rb
@@ -0,0 +1,40 @@
+class JfrogCli < Formula
+  desc "Command-line interface for JFrog products"
+  homepage "https://www.jfrog.com/confluence/display/CLI/JFrog+CLI"
+  url "https://github.com/jfrog/jfrog-cli/archive/refs/tags/v2.18.2.tar.gz"
+  sha256 "f03faf282899cdb7773e7f4ce9921f67040078f592b8e4c275ac6d4917d32eb4"
+  license "Apache-2.0"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/jfrog-cli"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "566367ac40718362e371a6b20833d7131a647289f82ac71549398f4e996b7050"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w -extldflags '-static'", output: bin/"jf")
+    bin.install_symlink "jf" => "jfrog"
+
+    # Install bash completion
+    output = Utils.safe_popen_read(bin/"jf", "completion", "bash")
+    (bash_completion/"jf").write output
+
+    # Install zsh completion
+    output = Utils.safe_popen_read(bin/"jf", "completion", "zsh")
+    (zsh_completion/"_jf").write output
+
+    # Install fish completion
+    output = Utils.safe_popen_read(bin/"jf", "completion", "fish")
+    (fish_completion/"jf.fish").write output
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/jf -v")
+    assert_match version.to_s, shell_output("#{bin}/jfrog -v")
+    with_env(JFROG_CLI_REPORT_USAGE: "false", CI: "true") do
+      assert_match "build name must be provided in order to generate build-info",
+        shell_output("#{bin}/jf rt bp --dry-run --url=http://127.0.0.1 2>&1", 1)
+    end
+  end
+end
