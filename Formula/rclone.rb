3ff7eebc42dc9d76ae81103fcaabc6814d251e58
Adrian Ho <the.gromgit@gmail.com>
2022-06-09 00:47:24 +0800

    rclone: update 1.58.1 bottle.

---
+++ Formula/rclone.rb
@@ -0,0 +1,40 @@
+class Rclone < Formula
+  desc "Rsync for cloud storage"
+  homepage "https://rclone.org/"
+  url "https://github.com/rclone/rclone/archive/v1.58.1.tar.gz"
+  sha256 "b1fe94642547d63ce52cdc49a06696e8b478a04ca100ab4ab1c92ff7157177a9"
+  license "MIT"
+  head "https://github.com/rclone/rclone.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/rclone"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "f1443edfa9e8ee7088942e650d17cf2e3aec7cb5acbe7e814b8676ea649fac46"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    args = *std_go_args(ldflags: "-s -w -X github.com/rclone/rclone/fs.Version=v#{version}")
+    args += ["-tags", "brew"] if OS.mac?
+    system "go", "build", *args
+    man1.install "rclone.1"
+    system bin/"rclone", "genautocomplete", "bash", "rclone.bash"
+    system bin/"rclone", "genautocomplete", "zsh", "_rclone"
+    system bin/"rclone", "genautocomplete", "fish", "rclone.fish"
+    bash_completion.install "rclone.bash" => "rclone"
+    zsh_completion.install "_rclone"
+    fish_completion.install "rclone.fish"
+  end
+
+  def caveats
+    <<~EOS
+      Homebrew's installation does not include the `mount` subcommand on MacOS.
+    EOS
+  end
+
+  test do
+    (testpath/"file1.txt").write "Test!"
+    system bin/"rclone", "copy", testpath/"file1.txt", testpath/"dist"
+    assert_match File.read(testpath/"file1.txt"), File.read(testpath/"dist/file1.txt")
+  end
+end
