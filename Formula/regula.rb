627e5fa4fa72b1306e4e84f80f733647d15bacf6
Adrian Ho <the.gromgit@gmail.com>
2022-06-09 00:54:56 +0800

    regula: update 2.6.1 bottle.

---
+++ Formula/regula.rb
@@ -0,0 +1,52 @@
+class Regula < Formula
+  desc "Checks infrastructure as code templates using Open Policy Agent/Rego"
+  homepage "https://regula.dev/"
+  url "https://github.com/fugue/regula.git",
+      tag:      "v2.6.1",
+      revision: "0d40e66c4bec929779b8bb5cde5f22ed1e522ede"
+  license "Apache-2.0"
+  head "https://github.com/fugue/regula.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/regula"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "57203b34f6be0910e310055a71dca9167add4f1f646fa0575510bd0942a371fb"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/fugue/regula/v2/pkg/version.Version=#{version}
+      -X github.com/fugue/regula/v2/pkg/version.GitCommit=#{Utils.git_short_head}
+    ].join(" ")
+
+    system "go", "build", *std_go_args(ldflags: ldflags)
+
+    bash_output = Utils.safe_popen_read(bin/"regula", "completion", "bash")
+    (bash_completion/"regula").write bash_output
+    zsh_output = Utils.safe_popen_read(bin/"regula", "completion", "zsh")
+    (zsh_completion/"_regula").write zsh_output
+    fish_output = Utils.safe_popen_read(bin/"regula", "completion", "fish")
+    (fish_completion/"regula.fish").write fish_output
+  end
+
+  test do
+    (testpath/"infra/test.tf").write <<~EOS
+      resource "aws_s3_bucket" "foo-bucket" {
+        region        = "us-east-1"
+        bucket        = "test"
+        acl           = "public-read"
+        force_destroy = true
+
+        versioning {
+          enabled = true
+        }
+      }
+    EOS
+
+    assert_match "Found 10 problems", shell_output(bin/"regula run infra", 1)
+
+    assert_match version.to_s, shell_output(bin/"regula version")
+  end
+end
