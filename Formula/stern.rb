a6bfe7167704038fcedc4c7bb52f2d8da73672d7
Adrian Ho <the.gromgit@gmail.com>
2022-06-09 04:17:43 +0800

    stern: update 1.21.0 bottle.

---
+++ Formula/stern.rb
@@ -0,0 +1,33 @@
+class Stern < Formula
+  desc "Tail multiple Kubernetes pods & their containers"
+  homepage "https://github.com/stern/stern"
+  url "https://github.com/stern/stern/archive/v1.21.0.tar.gz"
+  sha256 "0ccf1375ee3c20508c37de288a46faa6b0e4dffb3a3749f4b699a30f95e861be"
+  license "Apache-2.0"
+  head "https://github.com/stern/stern.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/stern"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "4eb7679042a591225583d7fc2f4feb38b0220e814d7e49960ec6bab87939d05f"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/stern/stern/cmd.version=#{version}")
+
+    # Install shell completion
+    output = Utils.safe_popen_read("#{bin}/stern", "--completion=bash")
+    (bash_completion/"stern").write output
+
+    output = Utils.safe_popen_read("#{bin}/stern", "--completion=zsh")
+    (zsh_completion/"_stern").write output
+
+    output = Utils.safe_popen_read("#{bin}/stern", "--completion=fish")
+    (fish_completion/"stern.fish").write output
+  end
+
+  test do
+    assert_match "version: #{version}", shell_output("#{bin}/stern --version")
+  end
+end
