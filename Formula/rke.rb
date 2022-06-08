9fc4ac65ed2cc82caaf05405a465524d23f9f26b
Adrian Ho <the.gromgit@gmail.com>
2022-06-09 01:06:48 +0800

    rke: update 1.3.11 bottle.

---
+++ Formula/rke.rb
@@ -0,0 +1,36 @@
+class Rke < Formula
+  desc "Rancher Kubernetes Engine, a Kubernetes installer that works everywhere"
+  homepage "https://rancher.com/docs/rke/latest/en/"
+  url "https://github.com/rancher/rke/archive/v1.3.11.tar.gz"
+  sha256 "1522e4964f0c68a00cc1632c5e7608802d37e0508d29517ce8401d3a848ca14a"
+  license "Apache-2.0"
+
+  # It's necessary to check releases instead of tags here (to avoid upstream
+  # tag issues) but we can't use the `GithubLatest` strategy because upstream
+  # creates releases for more than one minor version (i.e., the "latest" release
+  # isn't the newest version at times). We normally avoid checking the releases
+  # page because pagination can cause problems but this is our only choice.
+  livecheck do
+    url "https://github.com/rancher/rke/releases?q=prerelease%3Afalse"
+    regex(%r{href=["']?[^"' >]*?/tag/v?(\d+(?:\.\d+)+)["' >]}i)
+    strategy :page_match
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/rke"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "7c188cf317372e205011b191e744aa3aa34d508746e394742b0597d22d46beca"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-ldflags",
+            "-w -X main.VERSION=v#{version}",
+            "-o", bin/"rke"
+  end
+
+  test do
+    system bin/"rke", "config", "-e"
+    assert_predicate testpath/"cluster.yml", :exist?
+  end
+end
