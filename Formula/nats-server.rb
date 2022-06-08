ceb2ea562981e60e29388ba975dc3efd646c2054
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 19:47:45 +0800

    nats-server: update 2.8.4 bottle.

---
+++ Formula/nats-server.rb
@@ -0,0 +1,39 @@
+class NatsServer < Formula
+  desc "Lightweight cloud messaging system"
+  homepage "https://nats.io"
+  url "https://github.com/nats-io/nats-server/archive/v2.8.4.tar.gz"
+  sha256 "172c5d04c3867adcb6b2322d87d7f7029b63e9465fffffcf99d4ca652820635f"
+  license "Apache-2.0"
+  head "https://github.com/nats-io/nats-server.git", branch: "main"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/nats-server"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "0f3e5d2075334c4526d32929b3801ded654948ae50ab19462c52cc5659c5cfcb"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  service do
+    run opt_bin/"nats-server"
+  end
+
+  test do
+    port = free_port
+    http_port = free_port
+    fork do
+      exec bin/"nats-server",
+           "--port=#{port}",
+           "--http_port=#{http_port}",
+           "--pid=#{testpath}/pid",
+           "--log=#{testpath}/log"
+    end
+    sleep 3
+
+    assert_match version.to_s, shell_output("curl localhost:#{http_port}/varz")
+    assert_predicate testpath/"log", :exist?
+  end
+end
