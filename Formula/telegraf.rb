0492018974e906da4787cb2fe664a68f4df8127b
Adrian Ho <the.gromgit@gmail.com>
2022-06-09 06:27:38 +0800

    telegraf: update 1.22.4 bottle.

---
+++ Formula/telegraf.rb
@@ -0,0 +1,44 @@
+class Telegraf < Formula
+  desc "Server-level metric gathering agent for InfluxDB"
+  homepage "https://www.influxdata.com/"
+  url "https://github.com/influxdata/telegraf/archive/v1.22.4.tar.gz"
+  sha256 "828172fd3125eebc9777f3a4b42c5292bc85d6d87e4a5b41a7bb1b3e6fbc7d2a"
+  license "MIT"
+  head "https://github.com/influxdata/telegraf.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/telegraf"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "844d1bfa58fd1e09bfe10e4f65ddd019deb2d32694d73dc6f3c7ce22da32c0c4"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args, "-ldflags", "-X main.version=#{version}", "./cmd/telegraf"
+    etc.install "etc/telegraf.conf" => "telegraf.conf"
+  end
+
+  def post_install
+    # Create directory for additional user configurations
+    (etc/"telegraf.d").mkpath
+  end
+
+  service do
+    run [opt_bin/"telegraf", "-config", etc/"telegraf.conf", "-config-directory", etc/"telegraf.d"]
+    keep_alive true
+    working_dir var
+    log_path var/"log/telegraf.log"
+    error_log_path var/"log/telegraf.log"
+  end
+
+  test do
+    (testpath/"config.toml").write shell_output("#{bin}/telegraf -sample-config")
+    system "#{bin}/telegraf", "-config", testpath/"config.toml", "-test",
+           "-input-filter", "cpu:mem"
+  end
+end
