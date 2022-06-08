8bc08c5b50fb8374ea0e16327ca35ad868aa0ee9
Adrian Ho <the.gromgit@gmail.com>
2022-06-09 04:37:46 +0800

    tailscale: update 1.24.2 bottle.

---
+++ Formula/tailscale.rb
@@ -0,0 +1,40 @@
+class Tailscale < Formula
+  desc "Easiest, most secure way to use WireGuard and 2FA"
+  homepage "https://tailscale.com"
+  url "https://github.com/tailscale/tailscale.git",
+      tag:      "v1.24.2",
+      revision: "dce2409b15837f30885405b8b1d27e7b7fd6bf7a"
+  license "BSD-3-Clause"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/tailscale"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "b0b917de2ce1319f0268fef422694a6a9f054882b363d235faaf984a9974aa5a"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    vars = Utils.safe_popen_read("./build_dist.sh", "shellvars")
+    ldflags = %W[
+      -s -w
+      -X tailscale.com/version.Long=#{vars.match(/VERSION_LONG="(.*)"/)[1]}
+      -X tailscale.com/version.Short=#{vars.match(/VERSION_SHORT="(.*)"/)[1]}
+      -X tailscale.com/version.GitCommit=#{vars.match(/VERSION_GIT_HASH="(.*)-dirty"/)[1]}
+    ].join(" ")
+    system "go", "build", *std_go_args(ldflags: ldflags), "tailscale.com/cmd/tailscale"
+    system "go", "build", *std_go_args(ldflags: ldflags), "-o", bin/"tailscaled", "tailscale.com/cmd/tailscaled"
+  end
+
+  test do
+    version_text = shell_output("#{bin}/tailscale version")
+    assert_match version.to_s, version_text
+    assert_match(/commit: [a-f0-9]{40}/, version_text)
+
+    fork do
+      system bin/"tailscaled", "-tun=userspace-networking", "-socket=#{testpath}/tailscaled.socket"
+    end
+
+    sleep 2
+    assert_match "Logged out.", shell_output("#{bin}/tailscale --socket=#{testpath}/tailscaled.socket status", 1)
+  end
+end
