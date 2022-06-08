8f2c829043dfc8606f690b414d5b8b4f37c393c6
Adrian Ho <the.gromgit@gmail.com>
2022-06-09 02:11:38 +0800

    seqkit: update 2.2.0 bottle.

---
+++ Formula/seqkit.rb
@@ -0,0 +1,31 @@
+class Seqkit < Formula
+  desc "Cross-platform and ultrafast toolkit for FASTA/Q file manipulation in Golang"
+  homepage "https://bioinf.shenwei.me/seqkit"
+  url "https://github.com/shenwei356/seqkit/archive/v2.2.0.tar.gz"
+  sha256 "6e7e292532d78d54ac7aecb934f992c85d04b2dcb2d7a1141808bada0f5b13ba"
+  license "MIT"
+  head "https://github.com/shenwei356/seqkit.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/seqkit"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "c15bb8be37a2d469fea260216cff2c73f7d1b0ec81db87bafcc3444a16621d18"
+  end
+
+  depends_on "go" => :build
+
+  resource "testdata" do
+    url "https://raw.githubusercontent.com/shenwei356/seqkit/e37d70a7e0ca0e53d6dbd576bd70decac32aba64/tests/seqs4amplicon.fa"
+    sha256 "b0f09da63e3c677cc698d5cdff60e2d246368263c22385937169a9a4c321178a"
+  end
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "./seqkit"
+  end
+
+  test do
+    resource("testdata").stage do
+      assert_equal ">seq1\nCCCACTGAAA",
+      shell_output("#{bin}/seqkit amplicon --quiet -F CCC -R TTT seqs4amplicon.fa").strip
+    end
+  end
+end
