d7d772621dfe2ca2313d90a5e737ee7e2f94e738
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 19:21:16 +0800

    mmctl: update 6.7.0 bottle.

---
+++ Formula/mmctl.rb
@@ -0,0 +1,39 @@
+class Mmctl < Formula
+  desc "Remote CLI tool for Mattermost server"
+  homepage "https://github.com/mattermost/mmctl"
+  url "https://github.com/mattermost/mmctl.git",
+      tag:      "v6.7.0",
+      revision: "144fdb067b1a4923b92420a31da99103ecfc45e6"
+  license "Apache-2.0"
+  head "https://github.com/mattermost/mmctl.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/mmctl"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "fba26b287421f720aa4780ef76d87be16e0043525aa61a65af24e631d36d69c7"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X github.com/mattermost/mmctl/commands.BuildHash=#{Utils.git_head}"
+    system "go", "build", *std_go_args(ldflags: ldflags), "-mod=vendor"
+
+    # Install shell completions
+    output = Utils.safe_popen_read(bin/"mmctl", "completion", "bash")
+    (bash_completion/"mmctl").write output
+    output = Utils.safe_popen_read(bin/"mmctl", "completion", "zsh")
+    (zsh_completion/"_mmctl").write output
+  end
+
+  test do
+    output = pipe_output("#{bin}/mmctl help 2>&1")
+    refute_match(/.*No such file or directory.*/, output)
+    refute_match(/.*command not found.*/, output)
+    assert_match(/.*mmctl \[command\].*/, output)
+  end
+end
