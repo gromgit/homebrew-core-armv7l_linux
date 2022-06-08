9e78c35712d8328123719c864d6c58a07c2a066a
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 08:00:59 +0800

    glooctl: update 1.11.13 bottle.

---
+++ Formula/glooctl.rb
@@ -0,0 +1,43 @@
+class Glooctl < Formula
+  desc "Envoy-Powered API Gateway"
+  homepage "https://docs.solo.io/gloo/latest/"
+  # NOTE: Please wait until the newest stable release is finished building and
+  # no longer marked as "Pre-release" before creating a PR for a new version.
+  url "https://github.com/solo-io/gloo.git",
+      tag:      "v1.11.13",
+      revision: "0621612abab3a61e220fa320eee925b1fff11440"
+  license "Apache-2.0"
+  head "https://github.com/solo-io/gloo.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/glooctl"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "d5269cce8f5172f4c6b409bb54fe86541b0e0c4d96672524db37e99bb4014036"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "make", "glooctl", "TAGGED_VERSION=v#{version}"
+    bin.install "_output/glooctl"
+  end
+
+  test do
+    run_output = shell_output("#{bin}/glooctl 2>&1")
+    assert_match "glooctl is the unified CLI for Gloo.", run_output
+
+    version_output = shell_output("#{bin}/glooctl version 2>&1")
+    assert_match "Client: {\"version\":\"#{version}\"}", version_output
+
+    version_output = shell_output("#{bin}/glooctl version 2>&1")
+    assert_match "Server: version undefined", version_output
+
+    # Should error out as it needs access to a Kubernetes cluster to operate correctly
+    status_output = shell_output("#{bin}/glooctl get proxy 2>&1", 1)
+    assert_match "failed to create kube client", status_output
+  end
+end
