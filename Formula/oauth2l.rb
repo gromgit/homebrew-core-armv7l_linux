76d3596105d657c4a87c370a6b1fa5e80be95f66
Adrian Ho <the.gromgit@gmail.com>
2022-06-08 20:45:16 +0800

    oauth2l: update 1.2.2 bottle.

---
+++ Formula/oauth2l.rb
@@ -0,0 +1,27 @@
+class Oauth2l < Formula
+  desc "Simple CLI for interacting with Google oauth tokens"
+  homepage "https://github.com/google/oauth2l"
+  url "https://github.com/google/oauth2l/archive/v1.2.2.tar.gz"
+  sha256 "6bee262a59669be86e578190f4a7ce6f7b18d5082bd647de82c4a11257a91e83"
+  license "Apache-2.0"
+  head "https://github.com/google/oauth2l.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/oauth2l"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "f121b6a5015f24ceda13b3848009504b110ef28e7b58302f38291e251c0e091c"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GO111MODULE"] = "on"
+
+    system "go", "build", "-o", "oauth2l"
+    bin.install "oauth2l"
+  end
+
+  test do
+    assert_match "Invalid Value",
+      shell_output("#{bin}/oauth2l info abcd1234")
+  end
+end
