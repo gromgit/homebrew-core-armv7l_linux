3178098f76a1178fddfe0f0e76d1cc8ae05263a3
Adrian Ho <the.gromgit@gmail.com>
2022-06-11 07:08:20 +0800

    universal-ctags: update p5.9.20220605.0 bottle.

---
+++ Formula/universal-ctags.rb
@@ -0,0 +1,57 @@
+class UniversalCtags < Formula
+  desc "Maintained ctags implementation"
+  homepage "https://github.com/universal-ctags/ctags"
+  url "https://github.com/universal-ctags/ctags/archive/refs/tags/p5.9.20220605.0.tar.gz"
+  version "p5.9.20220605.0"
+  sha256 "72b88dd241cc4c7ff1f052b155b9c21d9136d69d9932e976409979d739f522ce"
+  license "GPL-2.0-only"
+  head "https://github.com/universal-ctags/ctags.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^(p\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/universal-ctags"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "dcc681d2b6b72605cdabc9191815389ee367ddd5ef5c1a22bd1e39d43582e6b3"
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "docutils" => :build
+  depends_on "pkg-config" => :build
+  depends_on "jansson"
+  depends_on "libyaml"
+
+  uses_from_macos "libxml2"
+
+  conflicts_with "ctags", because: "this formula installs the same executable as the ctags formula"
+
+  def install
+    system "./autogen.sh"
+    system "./configure", *std_configure_args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      #include <stdlib.h>
+
+      void func()
+      {
+        printf("Hello World!");
+      }
+
+      int main()
+      {
+        func();
+        return 0;
+      }
+    EOS
+    system bin/"ctags", "-R", "."
+    assert_match(/func.*test\.c/, File.read("tags"))
+  end
+end
