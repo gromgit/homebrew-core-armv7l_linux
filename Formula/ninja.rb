ccf085ac6644f28f4c2609f987fe6784b35cb3e8
Adrian Ho <the.gromgit@gmail.com>
2022-06-10 18:12:55 +0800

    ninja: update 1.11.0 bottle.

---
+++ Formula/ninja.rb
@@ -0,0 +1,52 @@
+class Ninja < Formula
+  desc "Small build system for use with gyp or CMake"
+  homepage "https://ninja-build.org/"
+  url "https://github.com/ninja-build/ninja/archive/v1.11.0.tar.gz"
+  sha256 "3c6ba2e66400fe3f1ae83deb4b235faf3137ec20bd5b08c29bfc368db143e4c6"
+  license "Apache-2.0"
+  head "https://github.com/ninja-build/ninja.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/ninja"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "4315aac98e6a9ef6b8e9b0369a5ec1e34b7d13d2b28303d44a511961f08a03e2"
+  end
+
+  # Ninja only needs Python for some non-core functionality.
+  depends_on "python@3.10" => [:build, :test]
+
+  def install
+    py = Formula["python@3.10"].opt_bin/"python3"
+    system py, "./configure.py", "--bootstrap", "--verbose", "--with-python=python3"
+
+    bin.install "ninja"
+    bash_completion.install "misc/bash-completion" => "ninja-completion.sh"
+    zsh_completion.install "misc/zsh-completion" => "_ninja"
+    doc.install "doc/manual.asciidoc"
+    elisp.install "misc/ninja-mode.el"
+    (share/"vim/vimfiles/syntax").install "misc/ninja.vim"
+  end
+
+  test do
+    ENV.prepend_path "PATH", Formula["python@3.10"].opt_bin
+    (testpath/"build.ninja").write <<~EOS
+      cflags = -Wall
+
+      rule cc
+        command = gcc $cflags -c $in -o $out
+
+      build foo.o: cc foo.c
+    EOS
+    system bin/"ninja", "-t", "targets"
+    port = free_port
+    fork do
+      exec bin/"ninja", "-t", "browse", "--port=#{port}", "--hostname=127.0.0.1", "--no-browser", "foo.o"
+    end
+    sleep 2
+    assert_match "foo.c", shell_output("curl -s http://127.0.0.1:#{port}?foo.o")
+  end
+end
