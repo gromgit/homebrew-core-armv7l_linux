eb0463822ba066f8167296fbd7dd2e059b4edc5f
Adrian Ho <the.gromgit@gmail.com>
2022-06-10 19:40:50 +0800

    libnghttp2: update 1.47.0 bottle.

---
+++ Formula/libnghttp2.rb
@@ -0,0 +1,60 @@
+class Libnghttp2 < Formula
+  desc "HTTP/2 C Library"
+  homepage "https://nghttp2.org/"
+  url "https://github.com/nghttp2/nghttp2/releases/download/v1.47.0/nghttp2-1.47.0.tar.gz"
+  mirror "http://fresh-center.net/linux/www/nghttp2-1.47.0.tar.gz"
+  mirror "http://fresh-center.net/linux/www/legacy/nghttp2-1.47.0.tar.gz"
+  sha256 "62f50f0e9fc479e48b34e1526df8dd2e94136de4c426b7680048181606832b7c"
+  license "MIT"
+
+  livecheck do
+    formula "nghttp2"
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/libnghttp2"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "7d0215b968fa2dc1e552cf1d7d6e18d33269c06d3dc55a6c03005e3275c59dfb"
+  end
+
+  head do
+    url "https://github.com/nghttp2/nghttp2.git", branch: "master"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "pkg-config" => :build
+
+  # These used to live in `nghttp2`.
+  link_overwrite "include/nghttp2"
+  link_overwrite "lib/libnghttp2.a"
+  link_overwrite "lib/libnghttp2.dylib"
+  link_overwrite "lib/libnghttp2.14.dylib"
+  link_overwrite "lib/libnghttp2.so"
+  link_overwrite "lib/libnghttp2.so.14"
+  link_overwrite "lib/pkgconfig/libnghttp2.pc"
+
+  def install
+    system "autoreconf", "-ivf" if build.head?
+    system "./configure", *std_configure_args, "--enable-lib-only"
+    system "make", "-C", "lib"
+    system "make", "-C", "lib", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~'EOS'
+      #include <nghttp2/nghttp2.h>
+      #include <stdio.h>
+
+      int main() {
+        nghttp2_info *info = nghttp2_version(0);
+        printf("%s", info->version_str);
+        return 0;
+      }
+    EOS
+
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lnghttp2", "-o", "test"
+    assert_equal version.to_s, shell_output("./test")
+  end
+end
