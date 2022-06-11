6d129c0257cff35352f43c1629b00059b546ff3f
Adrian Ho <the.gromgit@gmail.com>
2022-06-11 23:10:31 +0800

    clhep: update 2.4.5.2 bottle.

---
+++ Formula/clhep.rb
@@ -0,0 +1,46 @@
+class Clhep < Formula
+  desc "Class Library for High Energy Physics"
+  homepage "https://proj-clhep.web.cern.ch/proj-clhep/"
+  url "https://proj-clhep.web.cern.ch/proj-clhep/dist1/clhep-2.4.5.2.tgz"
+  sha256 "3a87de7c0c41a877212ee3d0d2ac5afe315299ae278ce7858b2ec7249ef2d911"
+  license "GPL-3.0-only"
+  head "https://gitlab.cern.ch/CLHEP/CLHEP.git", branch: "develop"
+
+  livecheck do
+    url :homepage
+    regex(%r{atest release.*?<b>v?(\d+(?:\.\d+)+)</b>}im)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/clhep"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "6efb81bcc800d86ad649979c344cf223b1f2e8c4cf92d22f0f69c6b854ad6dfd"
+  end
+
+  depends_on "cmake" => :build
+
+  def install
+    (buildpath/"CLHEP").install buildpath.children if build.head?
+    system "cmake", "-S", "CLHEP", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <iostream>
+      #include <Vector/ThreeVector.h>
+
+      int main() {
+        CLHEP::Hep3Vector aVec(1, 2, 3);
+        std::cout << "r: " << aVec.mag();
+        std::cout << " phi: " << aVec.phi();
+        std::cout << " cos(theta): " << aVec.cosTheta() << std::endl;
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "-std=c++11", "-L#{lib}", "-lCLHEP", "-I#{include}/CLHEP",
+           testpath/"test.cpp", "-o", "test"
+    assert_equal "r: 3.74166 phi: 1.10715 cos(theta): 0.801784",
+                 shell_output("./test").chomp
+  end
+end
