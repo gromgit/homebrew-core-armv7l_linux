f1bcbc9a1e726695ef65349af1a1de81c45a7039
Adrian Ho <the.gromgit@gmail.com>
2022-06-11 22:48:41 +0800

    libmd: update 1.0.4 bottle.

---
+++ Formula/libmd.rb
@@ -0,0 +1,53 @@
+class Libmd < Formula
+  desc "BSD Message Digest library"
+  homepage "https://www.hadrons.org/software/libmd/"
+  url "https://libbsd.freedesktop.org/releases/libmd-1.0.4.tar.xz"
+  sha256 "f51c921042e34beddeded4b75557656559cf5b1f2448033b4c1eec11c07e530f"
+  license "BSD-3-Clause"
+
+  livecheck do
+    url "https://libbsd.freedesktop.org/releases/"
+    regex(/href=.*?libmd[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/libmd"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "7f982482515ff60498c7fabde49356d5c0b6ac60a375bfeaaa0ad7fb8fc1998b"
+  end
+
+  depends_on :linux
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdlib.h>
+      #include <stdio.h>
+      #include <string.h>
+      #include <md5.h>
+
+      int main() {
+          MD5_CTX ctx;
+          uint8_t results[MD5_DIGEST_LENGTH];
+          char *buf;
+          buf = "abc";
+          int n;
+          n = strlen(buf);
+          MD5Init(&ctx);
+          MD5Update(&ctx, (uint8_t *)buf, n);
+          MD5Final(results, &ctx);
+          for (n = 0; n < MD5_DIGEST_LENGTH; n++)
+              printf("%02x", results[n]);
+          putchar('\\n');
+          return EXIT_SUCCESS;
+      }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lmd", "-o", "test"
+    assert_equal "900150983cd24fb0d6963f7d28e17f72", shell_output("./test").chomp
+  end
+end
