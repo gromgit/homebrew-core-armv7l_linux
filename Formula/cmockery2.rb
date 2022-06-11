68b5a71bdaba9da03640389a1d0528aa7e9d74ce
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 06:38:24 +0800

    cmockery2: update 1.3.9 bottle.

---
+++ Formula/cmockery2.rb
@@ -0,0 +1,36 @@
+class Cmockery2 < Formula
+  desc "Reviving cmockery unit test framework from Google"
+  homepage "https://github.com/lpabon/cmockery2"
+  url "https://github.com/lpabon/cmockery2/archive/v1.3.9.tar.gz"
+  sha256 "c38054768712351102024afdff037143b4392e1e313bdabb9380cab554f9dbf2"
+  license "Apache-2.0"
+  head "https://github.com/lpabon/cmockery2.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/cmockery2"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "34319561522ea9fe29fc87c93b78db2f65b3d3239f5b19d4bff1cf91b19db94d"
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+
+  def install
+    system "./autogen.sh"
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+    (share+"example").install "src/example/calculator.c"
+  end
+
+  test do
+    system ENV.cc, share+"example/calculator.c", "-L#{lib}", "-lcmockery", "-o", "calculator"
+    system "./calculator"
+  end
+end
