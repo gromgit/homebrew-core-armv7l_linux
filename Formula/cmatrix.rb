9f40d5819547def2f6ab50b79e81253d33d241b0
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 06:16:36 +0800

    cmatrix: update 2.0 bottle.

---
+++ Formula/cmatrix.rb
@@ -0,0 +1,28 @@
+class Cmatrix < Formula
+  desc "Console Matrix"
+  homepage "https://github.com/abishekvashok/cmatrix/"
+  url "https://github.com/abishekvashok/cmatrix/archive/v2.0.tar.gz"
+  sha256 "ad93ba39acd383696ab6a9ebbed1259ecf2d3cf9f49d6b97038c66f80749e99a"
+  license "GPL-3.0-or-later"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/cmatrix"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "7cf78c084e3a236f0f0ad4bac9cc8aaded58eed935caf31a8aa6e2053d819442"
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+
+  uses_from_macos "ncurses"
+
+  def install
+    system "autoreconf", "-i"
+    system "./configure", "--prefix=#{prefix}", "--mandir=#{man}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/cmatrix", "-V"
+  end
+end
