ce6385c8c99c77a121274b2ec95780ba42745a8e
Adrian Ho <the.gromgit@gmail.com>
2022-06-11 15:35:23 +0800

    bibtexconv: update 1.3.2 bottle.

---
+++ Formula/bibtexconv.rb
@@ -0,0 +1,35 @@
+class Bibtexconv < Formula
+  desc "BibTeX file converter"
+  homepage "https://www.uni-due.de/~be0001/bibtexconv/"
+  url "https://github.com/dreibh/bibtexconv/archive/bibtexconv-1.3.2.tar.gz"
+  sha256 "6eb1c82a8287ae749ac129d48c241c558881385a792dcc800a30809caf2a2109"
+  license "GPL-3.0-or-later"
+  head "https://github.com/dreibh/bibtexconv.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/bibtexconv"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "2e7648c9dc8bc5b68ba55f7d580737d40c33dec50443f2e0a11d317ddc6aede9"
+  end
+
+  depends_on "bison" => :build
+  depends_on "cmake" => :build
+  depends_on "openssl@1.1"
+
+  uses_from_macos "flex" => :build
+  uses_from_macos "curl"
+
+  def install
+    system "cmake", *std_cmake_args,
+                    "-DCRYPTO_LIBRARY=#{Formula["openssl@1.1"].opt_lib}/#{shared_library("libcrypto")}"
+    system "make", "install"
+  end
+
+  test do
+    cp "#{opt_share}/doc/bibtexconv/examples/ExampleReferences.bib", testpath
+
+    system bin/"bibtexconv", "#{testpath}/ExampleReferences.bib",
+                             "-export-to-bibtex=UpdatedReferences.bib",
+                             "-check-urls", "-only-check-new-urls",
+                             "-non-interactive"
+  end
+end
