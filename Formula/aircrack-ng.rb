7182419ad682c802c8aa9839ec01adae5328bed0
Adrian Ho <the.gromgit@gmail.com>
2022-06-11 09:05:34 +0800

    aircrack-ng: update 1.7 bottle.

---
+++ Formula/aircrack-ng.rb
@@ -0,0 +1,82 @@
+class AircrackNg < Formula
+  desc "Next-generation aircrack with lots of new features"
+  homepage "https://aircrack-ng.org/"
+  url "https://download.aircrack-ng.org/aircrack-ng-1.7.tar.gz"
+  sha256 "05a704e3c8f7792a17315080a21214a4448fd2452c1b0dd5226a3a55f90b58c3"
+  license all_of: ["GPL-2.0-or-later", "BSD-3-Clause", "OpenSSL"]
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?aircrack-ng[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/aircrack-ng"
+    sha256 armv7l_linux: "2812e5a523ba2c4f14c4bb7abc28367dce92a72cabc88791240f480cdc097fa1"
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "openssl@1.1"
+  depends_on "pcre"
+  depends_on "sqlite"
+
+  uses_from_macos "libpcap"
+
+  # Remove root requirement from OUI update script. See:
+  # https://github.com/Homebrew/homebrew/pull/12755
+  patch :DATA
+
+  def install
+    system "./autogen.sh", "--disable-silent-rules",
+                           "--disable-dependency-tracking",
+                           "--prefix=#{prefix}",
+                           "--with-experimental"
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      Run `airodump-ng-oui-update` install or update the Airodump-ng OUI file.
+    EOS
+  end
+
+  test do
+    assert_match "usage: aircrack-ng", shell_output("#{bin}/aircrack-ng --help")
+    assert_match "Logical CPUs", shell_output("#{bin}/aircrack-ng -u")
+    expected_simd = Hardware::CPU.arm? ? "neon" : "sse2"
+    assert_match expected_simd, shell_output("#{bin}/aircrack-ng --simd-list")
+  end
+end
+
+__END__
+--- a/scripts/airodump-ng-oui-update
++++ b/scripts/airodump-ng-oui-update
+@@ -20,25 +20,6 @@ fi
+
+ AIRODUMP_NG_OUI="${OUI_PATH}/airodump-ng-oui.txt"
+ OUI_IEEE="${OUI_PATH}/oui.txt"
+-USERID=""
+-
+-
+-# Make sure the user is root
+-if [ x"`which id 2> /dev/null`" != "x" ]
+-then
+-	USERID="`id -u 2> /dev/null`"
+-fi
+-
+-if [ x$USERID = "x" -a x$(id -ru) != "x" ]
+-then
+-	USERID=$(id -ru)
+-fi
+-
+-if [ x$USERID != "x" -a x$USERID != "x0" ]
+-then
+-	echo Run it as root ; exit ;
+-fi
+-
+ 
+ if [ ! -d "${OUI_PATH}" ]; then
+ 	mkdir -p ${OUI_PATH}
