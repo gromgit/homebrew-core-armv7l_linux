3b97c61eaec47e9e8ca508e563944fac4c4e11b0
Adrian Ho <the.gromgit@gmail.com>
2022-06-11 17:16:52 +0800

    blahtexml: update 0.9_1 bottle.

---
+++ Formula/blahtexml.rb
@@ -0,0 +1,48 @@
+class Blahtexml < Formula
+  desc "Converts equations into Math ML"
+  homepage "http://gva.noekeon.org/blahtexml/"
+  url "http://gva.noekeon.org/blahtexml/blahtexml-0.9-src.tar.gz"
+  sha256 "c5145b02bdf03cd95b7b136de63286819e696639824961d7408bec4591bc3737"
+  license "BSD-3-Clause"
+  revision 1
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/blahtexml"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "a22e0fbf577f33783920c3cec9e64940ba6bf7b95806ae7aebcdde1b50dd8f91"
+  end
+
+  depends_on "xerces-c"
+
+  # Add missing unistd.h includes, taken from MacPorts
+  patch :p0 do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/0632225f/blahtexml/patch-mainPng.cpp.diff"
+    sha256 "7d4bce5630881099b71beedbbc09b64c61849513b4ac00197b349aab2eba1687"
+  end
+
+  patch :p0 do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/0632225f/blahtexml/patch-main.cpp.diff"
+    sha256 "d696d10931f2c2ded1cef50842b78887dba36679fbb2e0abc373e7b6405b8468"
+  end
+
+  def install
+    ENV.cxx11
+    if OS.mac?
+      system "make", "blahtex-mac"
+      system "make", "blahtexml-mac"
+    else
+      # Parallel make has a race condition between mkdir and file write.
+      # Fatal error: can't create bin-blahtex/main.o: No such file or directory
+      ENV.deparallelize
+      system "make", "blahtex-linux"
+      system "make", "blahtexml-linux"
+    end
+    bin.install "blahtex"
+    bin.install "blahtexml"
+  end
+
+  test do
+    input = '\sqrt{x^2+\alpha}'
+    output = pipe_output("#{bin}/blahtex --mathml", input)
+    assert_match "<msqrt><msup><mi>x</mi><mn>2</mn></msup><mo ", output
+  end
+end
