10dec50b3ee555091b8db0ba5eb7744c6d160490
Adrian Ho <the.gromgit@gmail.com>
2022-06-11 14:28:15 +0800

    htslib: update 1.15.1 bottle.

---
+++ Formula/htslib.rb
@@ -0,0 +1,42 @@
+class Htslib < Formula
+  desc "C library for high-throughput sequencing data formats"
+  homepage "https://www.htslib.org/"
+  url "https://github.com/samtools/htslib/releases/download/1.15.1/htslib-1.15.1.tar.bz2"
+  sha256 "8d7f8bf9658226942eeab70af2a22aca618577eaa8fe2ed9416ee306d5351aa1"
+  license "MIT"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/htslib"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "2870f4e7c02844b8d6e9a36f18530a7e823852c9e45d45392be5b51d4b3db322"
+  end
+
+  depends_on "xz"
+
+  uses_from_macos "bzip2"
+  uses_from_macos "curl"
+  uses_from_macos "zlib"
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--enable-libcurl"
+    system "make", "install"
+  end
+
+  test do
+    sam = testpath/"test.sam"
+    sam.write <<~EOS
+      @SQ	SN:chr1	LN:500
+      r1	0	chr1	100	0	4M	*	0	0	ATGC	ABCD
+      r2	0	chr1	200	0	4M	*	0	0	AATT	EFGH
+    EOS
+    assert_match "SAM", shell_output("#{bin}/htsfile #{sam}")
+    system "#{bin}/bgzip -c #{sam} > sam.gz"
+    assert_predicate testpath/"sam.gz", :exist?
+    system "#{bin}/tabix", "-p", "sam", "sam.gz"
+    assert_predicate testpath/"sam.gz.tbi", :exist?
+  end
+end
