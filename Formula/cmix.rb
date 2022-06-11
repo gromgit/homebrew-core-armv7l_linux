33f633c28eabbaf77a1d4c956ae8b9b57d58ebb5
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 06:27:48 +0800

    cmix: update 18.0.0 bottle.

--- Formula/cmix.rb
+++ Formula/cmix.rb
@@ -7,13 +7,9 @@ class Cmix < Formula
   license "GPL-3.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, monterey:     "ad293329e67ffa22e3e159643c1324b587096c27daf0f0b77feb3633f3adf0c3"
-    sha256 cellar: :any_skip_relocation, big_sur:      "3ab169d97ef3e781f7fab12bcf99a74a584462fa154e59d85f96449aa525077c"
-    sha256 cellar: :any_skip_relocation, catalina:     "9dbbd3e8367f799405fbb237d68fe46e968bda502a5779f3be1c467b56e394b8"
-    sha256 cellar: :any_skip_relocation, mojave:       "448fa06555b59d6a0541d1e36ff9eac14e05775fd2ef119e860a305368b800ec"
-    sha256 cellar: :any_skip_relocation, high_sierra:  "6e1bc1de5f3c36e6fcda7874b8fbd18938aedbdbce94039763302f9643964a0a"
-    sha256 cellar: :any_skip_relocation, sierra:       "3dc97bda2656e2b2ffccb50915f9a981513fff5a8f90af2a1c5521afe52568d0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "ced902c9d32516366f0135dfa397ede6068c5c6434db5db16b1fcd7cb0a6a069"
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/cmix"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "c3bf0711fc46cdf8ae5baa2a6b4e3283b78caf5006c44471c66dc2cea02d5df9"
   end
 
   def install
