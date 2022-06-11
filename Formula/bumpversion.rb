0a6fd70efb1fd3bd24217584be72021c3dfb81f9
Adrian Ho <the.gromgit@gmail.com>
2022-06-11 19:02:48 +0800

    bumpversion: update 1.0.1_1 bottle.

---
+++ Formula/bumpversion.rb
@@ -0,0 +1,42 @@
+class Bumpversion < Formula
+  include Language::Python::Virtualenv
+
+  desc "Increase version numbers with SemVer terms"
+  homepage "https://pypi.python.org/pypi/bumpversion"
+  # maintained fork for the project
+  # Ongoing maintenance discussion for the project, https://github.com/c4urself/bump2version/issues/86
+  url "https://files.pythonhosted.org/packages/29/2a/688aca6eeebfe8941235be53f4da780c6edee05dbbea5d7abaa3aab6fad2/bump2version-1.0.1.tar.gz"
+  sha256 "762cb2bfad61f4ec8e2bdf452c7c267416f8c70dd9ecb1653fd0bbb01fa936e6"
+  license "MIT"
+  revision 1
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/bumpversion"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "47c761381b1f5eebe43e06c7ddef35c7815fa67a7deae1f6d6650a65b933cc66"
+  end
+
+  depends_on "python@3.10"
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    ENV["COLUMNS"] = "80"
+    command = if OS.mac?
+      "script -q /dev/null #{bin}/bumpversion --help"
+    else
+      "script -q /dev/null -c \"#{bin}/bumpversion --help\""
+    end
+    assert_includes shell_output(command), "bumpversion: v#{version}"
+
+    version_file = testpath/"VERSION"
+    version_file.write "0.0.0"
+    system bin/"bumpversion", "--current-version", "0.0.0", "minor", version_file
+    assert_match "0.1.0", version_file.read
+    system bin/"bumpversion", "--current-version", "0.1.0", "patch", version_file
+    assert_match "0.1.1", version_file.read
+    system bin/"bumpversion", "--current-version", "0.1.1", "major", version_file
+    assert_match "1.0.0", version_file.read
+  end
+end
