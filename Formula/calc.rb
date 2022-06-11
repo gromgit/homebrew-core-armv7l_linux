d12ce0c66d27e0d3b51a7e5c0009e0b558a283da
Adrian Ho <the.gromgit@gmail.com>
2022-06-11 19:27:45 +0800

    calc: update 2.14.1.0 bottle.

---
+++ Formula/calc.rb
@@ -0,0 +1,44 @@
+class Calc < Formula
+  desc "Arbitrary precision calculator"
+  homepage "http://www.isthe.com/chongo/tech/comp/calc/"
+  url "https://downloads.sourceforge.net/project/calc/calc/2.14.1.0/calc-2.14.1.0.tar.bz2"
+  sha256 "0b5616652e31ee1b54585dcc8512d02180a12f8addc09c4049d3d08edb54af40"
+  license "LGPL-2.1-or-later"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/calc"
+    sha256 armv7l_linux: "ea2f23d6c1fd4fb4bbb6bbbb3b0731afb15142330ee9cfc2b8590d6f2c4fc870"
+  end
+
+  depends_on "readline"
+
+  on_linux do
+    depends_on "util-linux" # for `col`
+  end
+
+  def install
+    ENV.deparallelize
+
+    ENV["EXTRA_CFLAGS"] = ENV.cflags
+    ENV["EXTRA_LDFLAGS"] = ENV.ldflags
+
+    args = [
+      "BINDIR=#{bin}",
+      "LIBDIR=#{lib}",
+      "MANDIR=#{man1}",
+      "CALC_INCDIR=#{include}/calc",
+      "CALC_SHAREDIR=#{pkgshare}",
+      "USE_READLINE=-DUSE_READLINE",
+      "READLINE_LIB=-L#{Formula["readline"].opt_lib} -lreadline",
+      "READLINE_EXTRAS=-lhistory -lncurses",
+    ]
+    args << "INCDIR=#{MacOS.sdk_path}/usr/include" if OS.mac?
+    system "make", "install", *args
+
+    libexec.install "#{bin}/cscript"
+  end
+
+  test do
+    assert_equal "11", shell_output("#{bin}/calc 0xA + 1").strip
+  end
+end
