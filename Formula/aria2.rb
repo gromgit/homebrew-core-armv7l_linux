efc7966d93e695a95938adf021bdc2c2ebdd367c
Adrian Ho <the.gromgit@gmail.com>
2022-06-11 11:03:42 +0800

    aria2: update 1.36.0_1 bottle.

---
+++ Formula/aria2.rb
@@ -0,0 +1,56 @@
+class Aria2 < Formula
+  desc "Download with resuming and segmented downloading"
+  homepage "https://aria2.github.io/"
+  url "https://github.com/aria2/aria2/releases/download/release-1.36.0/aria2-1.36.0.tar.xz"
+  sha256 "58d1e7608c12404f0229a3d9a4953d0d00c18040504498b483305bcb3de907a5"
+  license "GPL-2.0-or-later"
+  revision 1
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/aria2"
+    sha256 armv7l_linux: "1d606c89d34a7cb609591177c8a2af211e47b4203316ef5f10066df917535b39"
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "gettext"
+  depends_on "libssh2"
+  depends_on "sqlite"
+
+  uses_from_macos "libxml2"
+  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "openssl@1.1"
+  end
+
+  def install
+    ENV.cxx11
+
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --with-libssh2
+      --without-gnutls
+      --without-libgmp
+      --without-libnettle
+      --without-libgcrypt
+    ]
+    if OS.mac?
+      args << "--with-appletls"
+      args << "--without-openssl"
+    else
+      args << "--without-appletls"
+      args << "--with-openssl"
+    end
+
+    system "./configure", *args
+    system "make", "install"
+
+    bash_completion.install "doc/bash_completion/aria2c"
+  end
+
+  test do
+    system "#{bin}/aria2c", "https://brew.sh/"
+    assert_predicate testpath/"index.html", :exist?, "Failed to create index.html!"
+  end
+end
