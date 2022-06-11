958100f42ef86635e515c32a36f6445bd0ab1522
Adrian Ho <the.gromgit@gmail.com>
2022-06-11 13:41:32 +0800

    bagit: update 1.8.1_1 bottle.

---
+++ Formula/bagit.rb
@@ -0,0 +1,38 @@
+class Bagit < Formula
+  include Language::Python::Virtualenv
+
+  desc "Library for creation, manipulation, and validation of bags"
+  homepage "https://libraryofcongress.github.io/bagit-python/"
+  url "https://files.pythonhosted.org/packages/e5/99/927b704237a1286f1022ea02a2fdfd82d5567cfbca97a4c343e2de7e37c4/bagit-1.8.1.tar.gz"
+  sha256 "37df1330d2e8640c8dee8ab6d0073ac701f0614d25f5252f9e05263409cee60c"
+  license "CC0-1.0"
+  revision 1
+  version_scheme 1
+  head "https://github.com/LibraryOfCongress/bagit-python.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(%r{href=.*?/project/bagit/v?(\d+(?:\.\d+)+)/?["' >]}i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/bagit"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "501ae7f024c571fb9f86c8f6975ecbdf8824ff73dfcebfa45a8913a33332fccf"
+  end
+
+  depends_on "python@3.10"
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    system "#{bin}/bagit.py", "--source-organization", "Library of Congress", testpath.to_s
+    assert_predicate testpath/"bag-info.txt", :exist?
+    assert_predicate testpath/"bagit.txt", :exist?
+    assert_match "Bag-Software-Agent: bagit.py", File.read("bag-info.txt")
+    assert_match "BagIt-Version: 0.97", File.read("bagit.txt")
+
+    assert_match version.to_s, shell_output("#{bin}/bagit.py --version")
+  end
+end
