08e177c98cc5b3cf44ffcd9983d3e8e4eb608bdb
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 05:49:59 +0800

    cloc: update 1.92 bottle.

--- Formula/cloc.rb
+++ Formula/cloc.rb
@@ -7,12 +7,9 @@ class Cloc < Formula
   head "https://github.com/AlDanial/cloc.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0efc12ede2a34d0f3861223327a7e1ea559e401e567969e50d84d825cc3c90b9"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "05ea934d7a6ce536bbb928c9cd9a5d9ba27bc09109da8edf5ce39c39fdc71d82"
-    sha256 cellar: :any_skip_relocation, monterey:       "3e7b13dcfa8cfa88ac066471984c7966768f104085fd3384ee4948f481a1108d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "79233c01f28f7f76b4e763c2f4301297502b47b77e8f39fea10f18576d12cc62"
-    sha256 cellar: :any_skip_relocation, catalina:       "c66fde2a289083b2e121817f17f5b75769f638a4b90c0b49a3e0fc89cec9e655"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "879b082292c2ca231e4146d94867f121006494ae5331a6aee8b0f531d63918a3"
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/cloc"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "2815cf86c0333f1e9696b20138161590ee05762cfd2a084cb96ca16dc81d73cc"
   end
 
   uses_from_macos "perl"
