beb1fc2325478bb31220c2124722899b79feb25c
Adrian Ho <the.gromgit@gmail.com>
2022-06-11 12:25:14 +0800

    austin: update 3.3.0 bottle.

---
+++ Formula/austin.rb
@@ -0,0 +1,29 @@
+class Austin < Formula
+  desc "Python frame stack sampler for CPython"
+  homepage "https://github.com/P403n1x87/austin"
+  url "https://github.com/P403n1x87/austin/archive/v3.3.0.tar.gz"
+  sha256 "a0dcfee0dffecb00b85a84f3c7befff7e61fd4b504228c1e6ce7bc5af9790506"
+  license "GPL-3.0-or-later"
+  head "https://github.com/P403n1x87/austin.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/austin"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "c0d2c17a11b35790d152fa3ed0edc9f695b599f694dd3dc82766c9d2d2c6044b"
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "python@3.10" => :test
+
+  def install
+    system "autoreconf", "--install"
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+    man1.install "src/austin.1"
+  end
+
+  test do
+    shell_output(bin/"austin #{Formula["python@3.10"].opt_bin}/python3 -c \"from time import sleep; sleep(1)\"", 37)
+  end
+end
