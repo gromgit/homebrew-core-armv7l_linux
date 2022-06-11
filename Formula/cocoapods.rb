4963503d4d01c873bd385aa1a8cdcec554bdff8c
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 06:51:12 +0800

    cocoapods: update 1.11.3 bottle.

---
+++ Formula/cocoapods.rb
@@ -0,0 +1,35 @@
+class Cocoapods < Formula
+  desc "Dependency manager for Cocoa projects"
+  homepage "https://cocoapods.org/"
+  url "https://github.com/CocoaPods/CocoaPods/archive/1.11.3.tar.gz"
+  sha256 "91d31754611520529b101ee57a059c5caadcd7ddb3c5b3b1065edc0ef5c43372"
+  license "MIT"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/cocoapods"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "b01e6ad97173a43e91c8d800c067c864c16ff2f8f8175956e6c6891373dd3b70"
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "ruby" if Hardware::CPU.arm?
+
+  uses_from_macos "libffi", since: :catalina
+  uses_from_macos "ruby", since: :catalina
+
+  def install
+    if MacOS.version >= :mojave && MacOS::CLT.installed?
+      ENV["SDKROOT"] = ENV["HOMEBREW_SDKROOT"] = MacOS::CLT.sdk_path(MacOS.version)
+    end
+
+    ENV["GEM_HOME"] = libexec
+    system "gem", "build", "cocoapods.gemspec"
+    system "gem", "install", "cocoapods-#{version}.gem"
+    # Other executables don't work currently.
+    bin.install libexec/"bin/pod", libexec/"bin/xcodeproj"
+    bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
+  end
+
+  test do
+    system "#{bin}/pod", "list"
+  end
+end
