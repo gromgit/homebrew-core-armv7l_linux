2bab35c3e90f5fef9953ac9c6bd01324f1e17436
Adrian Ho <the.gromgit@gmail.com>
2022-06-11 20:20:48 +0800

    yamale: update 4.0.4 bottle.

---
+++ Formula/yamale.rb
@@ -0,0 +1,56 @@
+class Yamale < Formula
+  include Language::Python::Virtualenv
+
+  desc "Schema and validator for YAML"
+  homepage "https://github.com/23andMe/Yamale"
+  url "https://files.pythonhosted.org/packages/0c/93/3002a45542579cdd626a011f39bbe19ddcc1fbe0541081824c39ef216147/yamale-4.0.4.tar.gz"
+  sha256 "e524caf71cbbbd15aa295e8bdda01688ac4b5edaf38dd60851ddff6baef383ba"
+  license "MIT"
+  head "https://github.com/23andMe/Yamale.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/yamale"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "d50816cb5a9018e222dece587e52a2afe461d437d244ed4396b78130326f619b"
+  end
+
+  depends_on "libyaml"
+  depends_on "python@3.10"
+
+  resource "PyYAML" do
+    url "https://files.pythonhosted.org/packages/36/2b/61d51a2c4f25ef062ae3f74576b01638bebad5e045f747ff12643df63844/PyYAML-6.0.tar.gz"
+    sha256 "68fb519c14306fec9720a2a5b45bc9f0c8d1b9c72adf45c37baedfcd949c35a2"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    (testpath/"schema.yaml").write <<~EOS
+      string: str()
+      number: num(required=False)
+      datetime: timestamp(min='2010-01-01 0:0:0')
+    EOS
+    (testpath/"data1.yaml").write <<~EOS
+      string: bo is awesome
+      datetime: 2011-01-01 00:00:00
+    EOS
+    (testpath/"some_data.yaml").write <<~EOS
+      string: one
+      number: 3
+      datetime: 2015-01-01 00:00:00
+    EOS
+    output = shell_output("#{bin}/yamale -s schema.yaml data1.yaml")
+    assert_match "Validation success!", output
+
+    output = shell_output("#{bin}/yamale -s schema.yaml some_data.yaml")
+    assert_match "Validation success!", output
+
+    (testpath/"good.yaml").write <<~EOS
+      ---
+      foo: bar
+    EOS
+    output = shell_output("#{bin}/yamale -s schema.yaml schema.yaml", 1)
+    assert_match "Validation failed!", output
+  end
+end
