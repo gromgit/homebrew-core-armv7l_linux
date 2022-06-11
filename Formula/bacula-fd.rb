1f299cdf61e10dbddba55c4039a8df6b8f5d4660
Adrian Ho <the.gromgit@gmail.com>
2022-06-11 13:37:18 +0800

    bacula-fd: update 11.0.6 bottle.

---
+++ Formula/bacula-fd.rb
@@ -0,0 +1,65 @@
+class BaculaFd < Formula
+  desc "Network backup solution"
+  homepage "https://www.bacula.org/"
+  url "https://downloads.sourceforge.net/project/bacula/bacula/11.0.6/bacula-11.0.6.tar.gz"
+  sha256 "0195a08bcd4f578ae4a9ce0d91f7f86731c634d56b810534722d721b2a9eecb7"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/bacula-fd"
+    sha256 armv7l_linux: "f6474950e7ec3cde7fabf4be5613bbbfa6115f26f40b550b1725cf8677347e4c"
+  end
+
+  depends_on "openssl@1.1"
+  depends_on "readline"
+
+  uses_from_macos "zlib"
+
+  conflicts_with "bareos-client",
+    because: "both install a `bconsole` executable"
+
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-pre-0.4.2.418-big_sur.diff"
+    sha256 "83af02f2aa2b746bb7225872cab29a253264be49db0ecebb12f841562d9a2923"
+  end
+
+  def install
+    # CoreFoundation is also used alongside IOKit
+    inreplace "configure", '"-framework IOKit"',
+                           '"-framework IOKit -framework CoreFoundation"'
+
+    # * sets --disable-conio in order to force the use of readline
+    #   (conio support not tested)
+    # * working directory in /var/lib/bacula, reasonable place that
+    #   matches Debian's location.
+    system "./configure", "--prefix=#{prefix}",
+                          "--sbindir=#{bin}",
+                          "--with-working-dir=#{var}/lib/bacula",
+                          "--with-pid-dir=#{var}/run",
+                          "--with-logdir=#{var}/log/bacula",
+                          "--enable-client-only",
+                          "--disable-conio",
+                          "--with-readline=#{Formula["readline"].opt_prefix}"
+
+    system "make"
+    system "make", "install"
+
+    # Avoid references to the Homebrew shims directory
+    inreplace prefix/"etc/bacula_config", "#{Superenv.shims_path}/", ""
+
+    (var/"lib/bacula").mkpath
+  end
+
+  def post_install
+    (var/"run").mkpath
+  end
+
+  plist_options startup: true
+  service do
+    run [opt_bin/"bacula-fd", "-f"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/bacula-fd -? 2>&1", 1)
+  end
+end
