a43a5425bf5694f90dd52a1e74edbc943bf20426
Adrian Ho <the.gromgit@gmail.com>
2022-06-11 15:22:21 +0800

    librdkafka: update 1.8.2 bottle.

---
+++ Formula/librdkafka.rb
@@ -0,0 +1,46 @@
+class Librdkafka < Formula
+  desc "Apache Kafka C/C++ library"
+  homepage "https://github.com/edenhill/librdkafka"
+  url "https://github.com/edenhill/librdkafka/archive/refs/tags/v1.8.2.tar.gz"
+  sha256 "6a747d293a7a4613bd2897e28e8791476fbe1ae7361f2530a876e0fd483482a6"
+  license "BSD-2-Clause"
+  head "https://github.com/edenhill/librdkafka.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/librdkafka"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "abcabb1c07141fe18305cc435ae6a9dc186277a50fd4c46183f034cc0a89b779"
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "python@3.10" => :build
+  depends_on "lz4"
+  depends_on "lzlib"
+  depends_on "openssl@1.1"
+  depends_on "zstd"
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <librdkafka/rdkafka.h>
+
+      int main (int argc, char **argv)
+      {
+        int partition = RD_KAFKA_PARTITION_UA; /* random */
+        int version = rd_kafka_version();
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-lrdkafka", "-lz", "-lpthread", "-o", "test"
+    system "./test"
+  end
+end
