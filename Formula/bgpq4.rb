f047f9168d19e9d957fa89a20091d2f7c15b0460
Adrian Ho <the.gromgit@gmail.com>
2022-06-11 15:14:58 +0800

    bgpq4: update 1.4 bottle.

---
+++ Formula/bgpq4.rb
@@ -0,0 +1,38 @@
+class Bgpq4 < Formula
+  desc "BGP filtering automation for Cisco, Juniper, BIRD and OpenBGPD routers"
+  homepage "https://github.com/bgp/bgpq4"
+  url "https://github.com/bgp/bgpq4/archive/refs/tags/1.4.tar.gz"
+  sha256 "db4bb0e035e62f00b515529988ad8a552871dcf17ea1d32e0cbf3aae18c2602e"
+  license "BSD-2-Clause"
+  head "https://github.com/bgp/bgpq4.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/bgpq4"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "a58703267a6a8955444d0ae3a00796c5898f467cbc25a4e8cd1d45d17de45a58"
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+
+  def install
+    system "./bootstrap"
+    system "./configure", "--prefix=#{prefix}", "--mandir=#{man}"
+    system "make", "install"
+  end
+
+  test do
+    output = <<~EOS
+      no ip prefix-list NN
+      ! generated prefix-list NN is empty
+      ip prefix-list NN deny 0.0.0.0/0
+    EOS
+
+    assert_match output, shell_output("#{bin}/bgpq4 AS-ANY")
+  end
+end
