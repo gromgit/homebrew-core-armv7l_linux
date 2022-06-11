1db474ba02ca3f49a2ee3daa50c8f7e07887f1dc
Adrian Ho <the.gromgit@gmail.com>
2022-06-11 20:36:21 +0800

    choose: update 0.1.0_4 bottle.

---
+++ Formula/choose.rb
@@ -0,0 +1,44 @@
+class Choose < Formula
+  desc "Make choices on the command-line"
+  homepage "https://github.com/geier/choose"
+  url "https://github.com/geier/choose/archive/v0.1.0.tar.gz"
+  sha256 "d09a679920480e66bff36c76dd4d33e8ad739a53eace505d01051c114a829633"
+  license "MIT"
+  revision 4
+  head "https://github.com/geier/choose.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/choose"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "9cf822e70a8275a179afdbfb328ed47e7aa25cd8a0d534caa82ae5632dc96271"
+  end
+
+  depends_on "python@3.10"
+
+  conflicts_with "choose-gui", because: "both install a `choose` binary"
+  conflicts_with "choose-rust", because: "both install a `choose` binary"
+
+  resource "urwid" do
+    url "https://files.pythonhosted.org/packages/45/dd/d57924f77b0914f8a61c81222647888fbb583f89168a376ffeb5613b02a6/urwid-2.1.0.tar.gz"
+    sha256 "0896f36060beb6bf3801cb554303fef336a79661401797551ba106d23ab4cd86"
+  end
+
+  def install
+    xy = Language::Python.major_minor_version "python3"
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
+
+    resource("urwid").stage do
+      system "python3", *Language::Python.setup_install_args(libexec)
+    end
+
+    bin.install "choose"
+
+    bin.env_script_all_files(libexec/"bin", PYTHONPATH: ENV["PYTHONPATH"])
+  end
+
+  test do
+    # There isn't really a better test than that the executable exists
+    # and is executable because you can't run it without producing an
+    # interactive selection ui.
+    assert_predicate bin/"choose", :executable?
+  end
+end
