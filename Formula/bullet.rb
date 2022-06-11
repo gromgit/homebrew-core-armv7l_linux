ecebaabc6b7700261319fc671bec4b53e6ac1fc6
Adrian Ho <the.gromgit@gmail.com>
2022-06-11 18:57:57 +0800

    bullet: update 3.24 bottle.

---
+++ Formula/bullet.rb
@@ -0,0 +1,92 @@
+class Bullet < Formula
+  desc "Physics SDK"
+  homepage "https://bulletphysics.org/"
+  url "https://github.com/bulletphysics/bullet3/archive/3.24.tar.gz"
+  sha256 "6b1e987d6f8156fa8a6468652f4eaad17b3e11252c9870359e5bca693e35780b"
+  license "Zlib"
+  head "https://github.com/bulletphysics/bullet3.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/bullet"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "35e7ad8612f4b994cc758dd0f64ce3f103d1970a18a22f052486737595bebee0"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "python@3.10" => :build
+
+  def install
+    # C++11 for nullptr usage in examples. Can remove when fixed upstream.
+    # Issue ref: https://github.com/bulletphysics/bullet3/pull/4243
+    ENV.cxx11 if OS.linux?
+
+    common_args = %w[
+      -DBT_USE_EGL=ON
+      -DBUILD_UNIT_TESTS=OFF
+      -DINSTALL_EXTRA_LIBS=ON
+    ]
+
+    double_args = std_cmake_args + %W[
+      -DCMAKE_INSTALL_RPATH=#{opt_lib}/bullet/double
+      -DUSE_DOUBLE_PRECISION=ON
+      -DBUILD_SHARED_LIBS=ON
+    ]
+
+    mkdir "builddbl" do
+      system "cmake", "..", *double_args, *common_args
+      system "make", "install"
+    end
+    dbllibs = lib.children
+    (lib/"bullet/double").install dbllibs
+
+    args = std_cmake_args + %W[
+      -DBUILD_PYBULLET_NUMPY=ON
+      -DCMAKE_INSTALL_RPATH=#{opt_lib}
+    ]
+
+    mkdir "build" do
+      system "cmake", "..", *args, *common_args, "-DBUILD_SHARED_LIBS=OFF", "-DBUILD_PYBULLET=OFF"
+      system "make", "install"
+
+      system "make", "clean"
+
+      system "cmake", "..", *args, *common_args, "-DBUILD_SHARED_LIBS=ON", "-DBUILD_PYBULLET=ON"
+      system "make", "install"
+    end
+
+    # Install single-precision library symlinks into `lib/"bullet/single"` for consistency
+    lib.each_child do |f|
+      next if f == lib/"bullet"
+
+      (lib/"bullet/single").install_symlink f
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include "LinearMath/btPolarDecomposition.h"
+      int main() {
+        btMatrix3x3 I = btMatrix3x3::getIdentity();
+        btMatrix3x3 u, h;
+        polarDecompose(I, u, h);
+        return 0;
+      }
+    EOS
+
+    cxx_lib = if OS.mac?
+      "-lc++"
+    else
+      "-lstdc++"
+    end
+
+    # Test single-precision library
+    system ENV.cc, "test.cpp", "-I#{include}/bullet", "-L#{lib}",
+                   "-lLinearMath", cxx_lib, "-o", "test"
+    system "./test"
+
+    # Test double-precision library
+    system ENV.cc, "test.cpp", "-I#{include}/bullet", "-L#{lib}/bullet/double",
+                   "-lLinearMath", cxx_lib, "-o", "test"
+    system "./test"
+  end
+end
