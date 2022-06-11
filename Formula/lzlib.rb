554a5f58c6fded6296cf5d04ba68a2e8e84ffa08
Adrian Ho <the.gromgit@gmail.com>
2022-06-11 15:17:14 +0800

    lzlib: update 1.13 bottle.

---
+++ Formula/lzlib.rb
@@ -0,0 +1,41 @@
+class Lzlib < Formula
+  desc "Data compression library"
+  homepage "https://www.nongnu.org/lzip/lzlib.html"
+  url "https://download.savannah.gnu.org/releases/lzip/lzlib/lzlib-1.13.tar.gz"
+  mirror "https://download-mirror.savannah.gnu.org/releases/lzip/lzlib/lzlib-1.13.tar.gz"
+  sha256 "a1ab58f3148ba4b2674e938438166042137a9275bed747306641acfddc9ffb80"
+  license "BSD-2-Clause"
+
+  livecheck do
+    url "https://download.savannah.gnu.org/releases/lzip/lzlib/"
+    regex(/href=.*?lzlib[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/lzlib"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "6b4ce55cf7002155dc33f7638097300016bfc4a706187951d72560184de57d0c"
+  end
+
+  def install
+    system "./configure", "--prefix=#{prefix}",
+                          "CC=#{ENV.cc}",
+                          "CFLAGS=#{ENV.cflags}"
+    system "make"
+    system "make", "check"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      #include <stdint.h>
+      #include "lzlib.h"
+      int main (void) {
+        printf ("%s", LZ_version());
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-I#{include}", "-llz",
+                   "-o", "test"
+    assert_equal version.to_s, shell_output("./test")
+  end
+end
