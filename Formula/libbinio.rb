2dbf4b1c59f1c01f32ce5a4d1c3875706ac3b806
Adrian Ho <the.gromgit@gmail.com>
2022-06-11 08:17:08 +0800

    libbinio: update 1.5 bottle.

---
+++ Formula/libbinio.rb
@@ -0,0 +1,45 @@
+class Libbinio < Formula
+  desc "Binary I/O stream class library"
+  homepage "https://adplug.github.io/libbinio/"
+  url "https://github.com/adplug/libbinio/releases/download/libbinio-1.5/libbinio-1.5.tar.bz2"
+  sha256 "398b2468e7838d2274d1f62dbc112e7e043433812f7ae63ef29f5cb31dc6defd"
+  license "LGPL-2.1-or-later"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/libbinio"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "677a7a34068412a4916e7bf108481b3f76684ea02d55b12263e48e495a10b588"
+  end
+
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
+    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
+  end
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}", "--infodir=#{info}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      // test
+      // do not change the line above!
+      #include <libbinio/binfile.h>
+      #include <string.h>
+
+      int main(void)
+      {
+        binifstream     file("test.cpp");
+        char            string[256];
+
+        file.readString(string, 256, '\\n');
+
+        return strcmp (string, "// test");
+      }
+    EOS
+    system ENV.cxx, "test.cpp", "-I#{include}", "-L#{lib}", "-lbinio", "-o", "test"
+    system "./test"
+  end
+end
