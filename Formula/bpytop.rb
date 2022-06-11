54e6dc2dd3d614f7c6e8ea963b448e330b815eeb
Adrian Ho <the.gromgit@gmail.com>
2022-06-11 17:54:54 +0800

    bpytop: update 1.0.68 bottle.

---
+++ Formula/bpytop.rb
@@ -0,0 +1,61 @@
+class Bpytop < Formula
+  include Language::Python::Virtualenv
+  include Language::Python::Shebang
+
+  desc "Linux/OSX/FreeBSD resource monitor"
+  homepage "https://github.com/aristocratos/bpytop"
+  url "https://github.com/aristocratos/bpytop/archive/v1.0.68.tar.gz"
+  sha256 "3a936f8899efb66246e82bbcab33249bf94aabcefbe410e56f045a1ce3c9949f"
+  license "Apache-2.0"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/bpytop"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "34161558939ff424b38439f947a8c5b63bd3db2ec8d4bc3e01c7692c65e5a306"
+  end
+
+  depends_on "python@3.10"
+  on_macos do
+    depends_on "osx-cpu-temp"
+  end
+
+  resource "psutil" do
+    url "https://files.pythonhosted.org/packages/e1/b0/7276de53321c12981717490516b7e612364f2cb372ee8901bd4a66a000d7/psutil-5.8.0.tar.gz"
+    sha256 "0c9ccb99ab76025f2f0bbecf341d4656e9c1351db8cc8a03ccd62e318ab4b5c6"
+  end
+
+  def install
+    venv = virtualenv_create(libexec, "python3")
+    venv.pip_install resources
+    system "make", "install", "PREFIX=#{prefix}"
+    pkgshare.install "themes"
+
+    # Replace shebang with virtualenv python
+    rw_info = python_shebang_rewrite_info("#{libexec}/bin/python")
+    rewrite_shebang rw_info, bin/"bpytop"
+  end
+
+  test do
+    config = (testpath/".config/bpytop")
+    mkdir config/"themes"
+    (config/"bpytop.conf").write <<~EOS
+      #? Config file for bpytop v. #{version}
+
+      update_ms=2000
+      log_level=DEBUG
+    EOS
+
+    require "pty"
+    require "io/console"
+
+    r, w, pid = PTY.spawn("#{bin}/bpytop")
+    r.winsize = [80, 130]
+    sleep 5
+    w.write "\cC"
+
+    log = (config/"error.log").read
+    assert_match "bpytop version #{version} started with pid #{pid}", log
+    refute_match(/ERROR:/, log)
+  ensure
+    Process.kill("TERM", pid)
+  end
+end
