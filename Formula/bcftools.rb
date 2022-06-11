8863e4823bbbff4e2e3fdac4841ccf3915771753
Adrian Ho <the.gromgit@gmail.com>
2022-06-11 14:32:38 +0800

    bcftools: update 1.15.1 bottle.

---
+++ Formula/bcftools.rb
@@ -0,0 +1,36 @@
+class Bcftools < Formula
+  desc "Tools for BCF/VCF files and variant calling from samtools"
+  homepage "https://www.htslib.org/"
+  url "https://github.com/samtools/bcftools/releases/download/1.15.1/bcftools-1.15.1.tar.bz2"
+  sha256 "f21f9564873eb27ccf22d13b91a64acb8fbbfe4f9e4c37933a54b9a95857f2d7"
+  # The bcftools source code is MIT/Expat-licensed, but when it is configured
+  # with --enable-libgsl the resulting executable is GPL-licensed.
+  license "GPL-3.0-or-later"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/bcftools"
+    sha256 armv7l_linux: "8898eb480601bb5134b97ab1798d20f937323003d4e3405da071367a09f6fa51"
+  end
+
+  depends_on "gsl"
+  depends_on "htslib"
+
+  def install
+    system "./configure", "--prefix=#{prefix}",
+                          "--with-htslib=#{Formula["htslib"].opt_prefix}",
+                          "--enable-libgsl"
+    system "make", "install"
+    pkgshare.install "test/query.vcf"
+  end
+
+  test do
+    output = shell_output("#{bin}/bcftools stats #{pkgshare}/query.vcf")
+    assert_match "number of SNPs:\t3", output
+    assert_match "fixploidy", shell_output("#{bin}/bcftools plugin -l")
+  end
+end
