59f5c6f851f14b6ae764465a7abd086dcdc96e0d
Adrian Ho <the.gromgit@gmail.com>
2022-06-11 09:48:04 +0800

    aldo: update 0.7.7 bottle.

---
+++ Formula/aldo.rb
@@ -0,0 +1,35 @@
+class Aldo < Formula
+  desc "Morse code learning tool released under GPL"
+  homepage "https://www.nongnu.org/aldo/"
+  url "https://savannah.nongnu.org/download/aldo/aldo-0.7.7.tar.bz2"
+  sha256 "f1b8849d09267fff3c1f5122097d90fec261291f51b1e075f37fad8f1b7d9f92"
+  license "GPL-3.0"
+
+  livecheck do
+    url "https://download.savannah.gnu.org/releases/aldo/"
+    regex(/href=.*?aldo[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/aldo"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "692534a81cbb327af20bd14f82a49675a431670c762bd3559a4fa61dd0d67215"
+  end
+
+  depends_on "libao"
+
+  # Reported upstream:
+  # https://savannah.nongnu.org/bugs/index.php?42127
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/85fa66a9/aldo/0.7.7.patch"
+    sha256 "3b6c6cc067fc690b5af4042a2326cee2b74071966e9e2cd71fab061fde6c4a5d"
+  end
+
+  def install
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    assert_match "Aldo #{version} Main Menu", pipe_output("#{bin}/aldo", "6")
+  end
+end
