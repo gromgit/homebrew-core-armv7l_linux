7cbe60efc02521ba81e7be5a49a254b73722232e
Adrian Ho <the.gromgit@gmail.com>
2022-06-11 20:03:40 +0800

    cglm: update 0.8.5 bottle.

---
+++ Formula/cglm.rb
@@ -0,0 +1,44 @@
+class Cglm < Formula
+  desc "Optimized OpenGL/Graphics Math (glm) for C"
+  homepage "https://github.com/recp/cglm"
+  url "https://github.com/recp/cglm/archive/v0.8.5.tar.gz"
+  sha256 "baefa21342d228a83c90708459a745d5aa9b0ebb381555eea42db1f37fdf7a5a"
+  license "MIT"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/cglm"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "26a7f9b3ed76541a3178763eb2cf456e677e1047421f41fafe81e7117a9aec2d"
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+
+  def install
+    system "autoreconf", "-fiv"
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <cglm/cglm.h>
+      #include <assert.h>
+
+      int main() {
+        vec3 x = {1.0f, 0.0f, 0.0f},
+             y = {0.0f, 1.0f, 0.0f},
+             z = {0.0f, 0.0f, 1.0f};
+        vec3 r;
+
+        glm_cross(x, y, r);
+        assert(glm_vec3_eqv_eps(r, z));
+        return 0;
+      }
+    EOS
+    system ENV.cc, "-I#{include}", testpath/"test.c", "-o", "test"
+    system "./test"
+  end
+end
