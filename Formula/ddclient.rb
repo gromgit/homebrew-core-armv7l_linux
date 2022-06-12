1c1e16f9135845025dcb2b547d908860bce3d1e2
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 12:58:44 +0800

    ddclient: update 3.9.1 bottle.

---
+++ Formula/ddclient.rb
@@ -0,0 +1,112 @@
+class Ddclient < Formula
+  desc "Update dynamic DNS entries"
+  homepage "https://ddclient.net/"
+  url "https://github.com/ddclient/ddclient/archive/v3.9.1.tar.gz"
+  sha256 "e4969e15cc491fc52bdcd649d4c2b0e4b1bf0c9f9dba23471c634871acc52470"
+  license "GPL-2.0"
+  head "https://github.com/wimpunk/ddclient.git", branch: "develop"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/ddclient"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "449615086d9430b608e2d72cd58cb870fa715de12948694134e24045c9c21afb"
+  end
+
+  uses_from_macos "perl"
+
+  on_linux do
+    resource "NetAddr::IP" do
+      url "https://cpan.metacpan.org/authors/id/M/MI/MIKER/NetAddr-IP-4.079.tar.gz"
+      sha256 "ec5a82dfb7028bcd28bb3d569f95d87dd4166cc19867f2184ed3a59f6d6ca0e7"
+    end
+  end
+
+  resource "Data::Validate::IP" do
+    url "https://cpan.metacpan.org/authors/id/D/DR/DROLSKY/Data-Validate-IP-0.27.tar.gz"
+    sha256 "e1aa92235dcb9c6fd9b6c8cda184d1af73537cc77f4f83a0f88207a8bfbfb7d6"
+  end
+
+  def install
+    ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
+
+    resources.each do |r|
+      r.stage do
+        system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}"
+        system "make"
+        system "make", "install"
+      end
+    end
+
+    # Adjust default paths in script
+    inreplace "ddclient" do |s|
+      s.gsub! "/etc/ddclient", "#{etc}/ddclient"
+      s.gsub! "/var/cache/ddclient", "#{var}/run/ddclient"
+    end
+
+    sbin.install "ddclient"
+    sbin.env_script_all_files(libexec/"sbin", PERL5LIB: ENV["PERL5LIB"])
+
+    # Install sample files
+    inreplace "sample-ddclient-wrapper.sh",
+      "/etc/ddclient", "#{etc}/ddclient"
+
+    inreplace "sample-etc_cron.d_ddclient",
+      "/usr/sbin/ddclient", "#{sbin}/ddclient"
+
+    inreplace "sample-etc_ddclient.conf",
+      "/var/run/ddclient.pid", "#{var}/run/ddclient/pid"
+
+    doc.install %w[
+      sample-ddclient-wrapper.sh
+      sample-etc_cron.d_ddclient
+      sample-etc_ddclient.conf
+    ]
+  end
+
+  def post_install
+    (etc/"ddclient").mkpath
+    (var/"run/ddclient").mkpath
+  end
+
+  def caveats
+    <<~EOS
+      For ddclient to work, you will need to create a configuration file
+      in #{etc}/ddclient. A sample configuration can be found in
+      #{opt_share}/doc/ddclient.
+
+      Note: don't enable daemon mode in the configuration file; see
+      additional information below.
+
+      The next reboot of the system will automatically start ddclient.
+
+      You can adjust the execution interval by changing the value of
+      StartInterval (in seconds) in /Library/LaunchDaemons/#{plist_path.basename}.
+    EOS
+  end
+
+  plist_options startup: true
+
+  service do
+    run [opt_sbin/"ddclient", "-file", etc/"ddclient/ddclient.conf"]
+    run_type :interval
+    interval 300
+    working_dir etc/"ddclient"
+  end
+
+  test do
+    begin
+      pid = fork do
+        exec sbin/"ddclient", "-file", doc/"sample-etc_ddclient.conf", "-debug", "-verbose", "-noquiet"
+      end
+      sleep 1
+    ensure
+      Process.kill "TERM", pid
+      Process.wait
+    end
+    $CHILD_STATUS.success?
+  end
+end
