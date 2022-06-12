b529f6ae12e2fec53ae3d96ff62d941480cfae25
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 16:06:15 +0800

    dos2unix: update 7.4.2 bottle.

---
+++ Formula/dos2unix.rb
@@ -0,0 +1,53 @@
+class Dos2unix < Formula
+  desc "Convert text between DOS, UNIX, and Mac formats"
+  homepage "https://waterlan.home.xs4all.nl/dos2unix.html"
+  url "https://waterlan.home.xs4all.nl/dos2unix/dos2unix-7.4.2.tar.gz"
+  mirror "https://fossies.org/linux/misc/dos2unix-7.4.2.tar.gz"
+  sha256 "6035c58df6ea2832e868b599dfa0d60ad41ca3ecc8aa27822c4b7a9789d3ae01"
+  license "BSD-2-Clause"
+
+  livecheck do
+    url "https://waterlan.home.xs4all.nl/dos2unix/"
+    regex(/href=.*?dos2unix[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/dos2unix"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "4eee1d5752d137beba85b7339617bc551920de04b55e87c21f6b87a28f2e91d8"
+  end
+
+  def install
+    args = %W[
+      prefix=#{prefix}
+      CC=#{ENV.cc}
+      CPP=#{ENV.cc}
+      CFLAGS=#{ENV.cflags}
+      ENABLE_NLS=
+      install
+    ]
+
+    system "make", *args
+  end
+
+  test do
+    # write a file with lf
+    path = testpath/"test.txt"
+    path.write "foo\nbar\n"
+
+    # unix2mac: convert lf to cr
+    system "#{bin}/unix2mac", path
+    assert_equal "foo\rbar\r", path.read
+
+    # mac2unix: convert cr to lf
+    system "#{bin}/mac2unix", path
+    assert_equal "foo\nbar\n", path.read
+
+    # unix2dos: convert lf to cr+lf
+    system "#{bin}/unix2dos", path
+    assert_equal "foo\r\nbar\r\n", path.read
+
+    # dos2unix: convert cr+lf to lf
+    system "#{bin}/dos2unix", path
+    assert_equal "foo\nbar\n", path.read
+  end
+end
