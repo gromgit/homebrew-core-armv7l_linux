d453dcf2c8d2809cf8b63fb4b6962dfb2be3e270
Adrian Ho <the.gromgit@gmail.com>
2022-06-13 03:36:12 +0800

    foma: update 0.9.18 bottle.

---
+++ Formula/foma.rb
@@ -0,0 +1,46 @@
+class Foma < Formula
+  desc "Finite-state compiler and C library"
+  homepage "https://code.google.com/p/foma/"
+  url "https://bitbucket.org/mhulden/foma/downloads/foma-0.9.18.tar.gz"
+  sha256 "cb380f43e86fc7b3d4e43186db3e7cff8f2417e18ea69cc991e466a3907d8cbd"
+  license "GPL-2.0-only"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/foma"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "0e2838da2a892a966c097ea794695f73c9b8350fba7930aad199e6997afce7ac"
+  end
+
+  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "readline"
+  end
+
+  conflicts_with "freeling", because: "freeling ships its own copy of foma"
+
+  def install
+    system "make"
+    system "make", "install", "prefix=#{prefix}"
+  end
+
+  test do
+    # Source: https://code.google.com/p/foma/wiki/ExampleScripts
+    (testpath/"toysyllabify.script").write <<~EOS
+      define V [a|e|i|o|u];
+      define Gli [w|y];
+      define Liq [r|l];
+      define Nas [m|n];
+      define Obs [p|t|k|b|d|g|f|v|s|z];
+
+      define Onset (Obs) (Nas) (Liq) (Gli); # Each element is optional.
+      define Coda  Onset.r;                 # Is mirror image of onset.
+
+      define Syllable Onset V Coda;
+      regex Syllable @> ... "." || _ Syllable;
+
+      apply down> abrakadabra
+    EOS
+
+    system "#{bin}/foma", "-f", "toysyllabify.script"
+  end
+end
