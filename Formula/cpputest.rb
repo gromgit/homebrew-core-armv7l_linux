2a0954c6dfd78bd8cbed67770620e5ff338efe9a
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 09:49:20 +0800

    cpputest: update 4.0 bottle.

---
+++ Formula/cpputest.rb
@@ -0,0 +1,43 @@
+class Cpputest < Formula
+  desc "C /C++ based unit xUnit test framework"
+  homepage "https://www.cpputest.org/"
+  url "https://github.com/cpputest/cpputest/releases/download/v4.0/cpputest-4.0.tar.gz"
+  sha256 "21c692105db15299b5529af81a11a7ad80397f92c122bd7bf1e4a4b0e85654f7"
+  license "BSD-3-Clause"
+  head "https://github.com/cpputest/cpputest.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/cpputest"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "5c9b10ae4740e6541f55bf9b838177d951cca20f22ad1f2854674887561fd5a2"
+  end
+
+  depends_on "cmake" => :build
+
+  def install
+    cd "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include "CppUTest/CommandLineTestRunner.h"
+
+      TEST_GROUP(HomebrewTest)
+      {
+      };
+
+      TEST(HomebrewTest, passing)
+      {
+        CHECK(true);
+      }
+      int main(int ac, char** av)
+      {
+        return CommandLineTestRunner::RunAllTests(ac, av);
+      }
+    EOS
+    system ENV.cxx, "test.cpp", "-L#{lib}", "-lCppUTest", "-o", "test"
+    assert_match "OK (1 tests", shell_output("./test")
+  end
+end
