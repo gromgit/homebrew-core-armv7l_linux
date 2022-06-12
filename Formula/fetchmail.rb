82bdb34ba198b70ae4626bf576e5616395590b2e
Adrian Ho <the.gromgit@gmail.com>
2022-06-13 01:14:02 +0800

    fetchmail: update 6.4.30 bottle.

---
+++ Formula/fetchmail.rb
@@ -0,0 +1,36 @@
+class Fetchmail < Formula
+  desc "Client for fetching mail from POP, IMAP, ETRN or ODMR-capable servers"
+  homepage "https://www.fetchmail.info/"
+  url "https://downloads.sourceforge.net/project/fetchmail/branch_6.4/fetchmail-6.4.30.tar.xz"
+  sha256 "35824c20aa1d8d97e3f1611fef49b9f7f68396f7cbc4d3a807fed27ae9ea445a"
+  license all_of: [
+    "LGPL-2.1-or-later",
+    "ISC",
+    "BSD-3-Clause",
+    :public_domain,
+    "GPL-2.0-or-later" => { with: "openvpn-openssl-exception" },
+  ]
+
+  livecheck do
+    url :stable
+    regex(%r{url=.*?/branch_\d+(?:\.\d+)*?/fetchmail[._-]v?(\d+(?:\.\d+)+)\.t}i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/fetchmail"
+    sha256 armv7l_linux: "75538669c7b1b121e1e50b768eb22911c55f7b13cad5bca182e6e8177aaf8432"
+  end
+
+  depends_on "openssl@1.1"
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--with-ssl=#{Formula["openssl@1.1"].opt_prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/fetchmail", "--version"
+  end
+end
