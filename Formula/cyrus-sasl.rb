7d3c10258771a9794362c62bea4862d894851d3c
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 11:55:58 +0800

    cyrus-sasl: update 2.1.28_1 bottle.

---
+++ Formula/cyrus-sasl.rb
@@ -0,0 +1,48 @@
+class CyrusSasl < Formula
+  desc "Simple Authentication and Security Layer"
+  homepage "https://www.cyrusimap.org/sasl/"
+  url "https://github.com/cyrusimap/cyrus-sasl/releases/download/cyrus-sasl-2.1.28/cyrus-sasl-2.1.28.tar.gz"
+  sha256 "7ccfc6abd01ed67c1a0924b353e526f1b766b21f42d4562ee635a8ebfc5bb38c"
+  license "BSD-3-Clause-Attribution"
+  revision 1
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/cyrus-sasl"
+    sha256 armv7l_linux: "54dc2e0d4efcef7dbea39648452b6e2fc8f1802b7f284288513236fe9427f230"
+  end
+
+  keg_only :provided_by_macos
+
+  depends_on "krb5"
+  depends_on "openssl@1.1"
+
+  uses_from_macos "libxcrypt"
+
+  def install
+    system "./configure",
+      "--disable-macos-framework",
+      "--disable-dependency-tracking",
+      "--disable-silent-rules",
+      "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <sasl/saslutil.h>
+      #include <assert.h>
+      #include <stdio.h>
+      int main(void) {
+        char buf[123] = "\\0";
+        unsigned len = 0;
+        int ret = sasl_encode64("Hello, world!", 13, buf, sizeof buf, &len);
+        assert(ret == SASL_OK);
+        printf("%u %s", len, buf);
+        return 0;
+      }
+    EOS
+
+    system ENV.cxx, "-o", "test", "test.cpp", "-I#{include}", "-L#{lib}", "-lsasl2"
+    assert_equal "20 SGVsbG8sIHdvcmxkIQ==", shell_output("./test")
+  end
+end
