939921d8454b384f81d83ab6b10646afdd15ef88
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 14:25:31 +0800

    diction: update 1.11 bottle.

---
+++ Formula/diction.rb
@@ -0,0 +1,26 @@
+class Diction < Formula
+  desc "GNU diction and style"
+  homepage "https://www.gnu.org/software/diction/"
+  url "https://ftp.gnu.org/gnu/diction/diction-1.11.tar.gz"
+  mirror "https://ftpmirror.gnu.org/diction/diction-1.11.tar.gz"
+  sha256 "35c2f1bf8ddf0d5fa9f737ffc8e55230736e5d850ff40b57fdf5ef1d7aa024f6"
+  license "GPL-3.0"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/diction"
+    sha256 armv7l_linux: "7c4410292c0ab60d99aac3cd06ee6c1cdd15d5b456bd7f834bb1b273098e9971"
+  end
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    file = "test.txt"
+    (testpath/file).write "The quick brown fox jumps over the lazy dog."
+    assert_match(/^.*35 characters.*9 words.*$/m, shell_output("#{bin}/style #{file}"))
+    assert_match "No phrases in 1 sentence found.", shell_output("#{bin}/diction #{file}")
+  end
+end
