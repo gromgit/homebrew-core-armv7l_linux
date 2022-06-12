8f235d0d3e002a60ea375ce1dcc6c6ced633a66d
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 16:15:18 +0800

    dotbot: update 1.19.0_1 bottle.

---
+++ Formula/dotbot.rb
@@ -0,0 +1,39 @@
+class Dotbot < Formula
+  include Language::Python::Virtualenv
+
+  desc "Tool that bootstraps your dotfiles"
+  homepage "https://github.com/anishathalye/dotbot"
+  url "https://files.pythonhosted.org/packages/d3/67/733dbf0b444d41af473238537d5ef7bd5906870f35a69ef4f7dc64e74519/dotbot-1.19.0.tar.gz"
+  sha256 "29f4a461462a5ff3b1e9929849458e88d827a45d764f582c633237edd373f0af"
+  license "MIT"
+  revision 1
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/dotbot"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "151a08a52d3d88dee31277276ed64bf41eecfec480b95fda5d8999229a858db2"
+  end
+
+  depends_on "python@3.10"
+
+  resource "PyYAML" do
+    url "https://files.pythonhosted.org/packages/a0/a4/d63f2d7597e1a4b55aa3b4d6c5b029991d3b824b5bd331af8d4ab1ed687d/PyYAML-5.4.1.tar.gz"
+    sha256 "607774cbba28732bfa802b54baa7484215f530991055bb562efbed5b2f20a45e"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    (testpath/"install.conf.yaml").write <<~EOS
+      - create:
+        - brew
+        - .brew/test
+    EOS
+
+    output = shell_output("#{bin}/dotbot -c #{testpath}/install.conf.yaml")
+    assert_match "All tasks executed successfully", output
+    assert_predicate testpath/"brew", :exist?
+    assert_predicate testpath/".brew/test", :exist?
+  end
+end
