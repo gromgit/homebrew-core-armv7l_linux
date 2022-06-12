5b5cd48d27c9a2f8e115a20ce668f791bac989d9
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 20:59:20 +0800

    libexif: update 0.6.24 bottle.

---
+++ Formula/libexif.rb
@@ -0,0 +1,48 @@
+class Libexif < Formula
+  desc "EXIF parsing library"
+  homepage "https://libexif.github.io/"
+  url "https://github.com/libexif/libexif/releases/download/v0.6.24/libexif-0.6.24.tar.bz2"
+  sha256 "d47564c433b733d83b6704c70477e0a4067811d184ec565258ac563d8223f6ae"
+  license "LGPL-2.1"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/libexif"
+    sha256 armv7l_linux: "8dedb07aba5ea036d69b05096d23a08bc3ad8b071716f1e06714c548accf1a7f"
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "gettext"
+
+  def install
+    system "autoreconf", "-ivf"
+    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      #include <libexif/exif-loader.h>
+
+      int main(int argc, char **argv) {
+        ExifLoader *loader = exif_loader_new();
+        ExifData *data;
+        if (loader) {
+          exif_loader_write_file(loader, argv[1]);
+          data = exif_loader_get_data(loader);
+          printf(data ? "Exif data loaded" : "No Exif data");
+        }
+      }
+    EOS
+    flags = %W[
+      -I#{include}
+      -L#{lib}
+      -lexif
+    ]
+    system ENV.cc, "test.c", "-o", "test", *flags
+    test_image = test_fixtures("test.jpg")
+    assert_equal "No Exif data", shell_output("./test #{test_image}")
+  end
+end
