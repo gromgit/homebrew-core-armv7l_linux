0aa9039e28535efb56b72662a259ac94a74411b7
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 20:30:07 +0800

    elinks: update 0.11.7_3 bottle.

---
+++ Formula/elinks.rb
@@ -0,0 +1,62 @@
+class Elinks < Formula
+  desc "Text mode web browser"
+  homepage "http://elinks.or.cz/"
+  url "http://elinks.or.cz/download/elinks-0.11.7.tar.bz2"
+  sha256 "456db6f704c591b1298b0cd80105f459ff8a1fc07a0ec1156a36c4da6f898979"
+  revision 3
+
+  livecheck do
+    url "http://elinks.or.cz/download/"
+    regex(/href=.*?elinks[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/elinks"
+    sha256 armv7l_linux: "53941d400798ec24e7f7ac447b37a1963b136a8d3eb7f161c4281c900ceddaa6"
+  end
+
+  head do
+    url "http://elinks.cz/elinks.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "openssl@1.1"
+
+  uses_from_macos "zlib"
+
+  # Two patches for compatibility with OpenSSL 1.1, from FreeBSD:
+  # https://www.freshports.org/www/elinks/
+  patch :p0 do
+    url "https://svnweb.freebsd.org/ports/head/www/elinks/files/patch-src_network_ssl_socket.c?revision=485945&view=co"
+    sha256 "a4f199f6ce48989743d585b80a47bc6e0ff7a4fa8113d120e2732a3ffa4f58cc"
+  end
+
+  patch :p0 do
+    url "https://svnweb.freebsd.org/ports/head/www/elinks/files/patch-src_network_ssl_ssl.c?revision=494026&view=co"
+    sha256 "45c140d5db26fc0d98f4d715f5f355e56c12f8009a8dd9bf20b05812a886c348"
+  end
+
+  def install
+    ENV.deparallelize
+    ENV.delete("LD")
+    system "./autogen.sh" if build.head?
+    system "./configure", "--prefix=#{prefix}",
+                          "--without-spidermonkey",
+                          "--enable-256-colors"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.html").write <<~EOS
+      <!DOCTYPE html>
+      <title>elinks test</title>
+      Hello world!
+      <ol><li>one</li><li>two</li></ol>
+    EOS
+    assert_match(/^\s*Hello world!\n+ *1. one\n *2. two\s*$/,
+                 shell_output("#{bin}/elinks test.html"))
+  end
+end
