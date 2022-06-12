fe34cdd384c5a7aa26f5f3caadac13ed0c561875
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 09:41:55 +0800

    cpptest: update 2.0.0 bottle.

---
+++ Formula/cpptest.rb
@@ -0,0 +1,49 @@
+class Cpptest < Formula
+  desc "Unit testing framework handling automated tests in C++"
+  homepage "https://cpptest.sourceforge.io/"
+  url "https://downloads.sourceforge.net/project/cpptest/cpptest/cpptest-2.0.0/cpptest-2.0.0.tar.bz2"
+  mirror "https://github.com/cpptest/cpptest/releases/download/2.0.0/cpptest-2.0.0.tar.bz2"
+  sha256 "7c258936a407bcd1635a9b7719fbdcd6c6e044b5d32f53bbf6fbf6f205e5e429"
+  license "LGPL-2.1-or-later"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/cpptest"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "5005e35df6c5e075522478d5bae048c66004fd771be573649084b76035452a2a"
+  end
+
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
+    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
+  end
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <assert.h>
+      #include <cpptest.h>
+
+      class TestCase: public Test::Suite
+      {
+      public:
+        TestCase() { TEST_ADD(TestCase::test); }
+        void test() { TEST_ASSERT(1 + 1 == 2); }
+      };
+
+      int main()
+      {
+        TestCase ts;
+        Test::TextOutput output(Test::TextOutput::Verbose);
+        assert(ts.run(output));
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "test.cpp", "-std=c++11", "-L#{lib}", "-lcpptest", "-o", "test"
+    system "./test"
+  end
+end
