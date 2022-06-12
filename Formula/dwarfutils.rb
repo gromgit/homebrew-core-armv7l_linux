b4fb7085336f6190c64a932f66c02198b0d45c49
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 18:42:39 +0800

    dwarfutils: update 0.4.0 bottle.

---
+++ Formula/dwarfutils.rb
@@ -0,0 +1,66 @@
+class Dwarfutils < Formula
+  desc "Dump and produce DWARF debug information in ELF objects"
+  homepage "https://www.prevanders.net/dwarf.html"
+  url "https://www.prevanders.net/libdwarf-0.4.0.tar.xz"
+  sha256 "2cb065faa323b02f836a858ce62d2b9efa11ecd0f6e8b1133fdab5ae9318e198"
+  license all_of: ["BSD-2-Clause", "LGPL-2.1-or-later", "GPL-2.0-or-later"]
+  version_scheme 1
+
+  livecheck do
+    url :homepage
+    regex(%r{href=(?:["']?|.*?/)libdwarf[._-]v?(\d+(?:\.\d+)+)\.t}i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/dwarfutils"
+    sha256 armv7l_linux: "53dc1ac6618bb8bf29c541ee8c80ac7463404f027a25fbd1066beccf76ac8c57"
+  end
+
+  head do
+    url "https://github.com/davea42/libdwarf-code.git", branch: "master"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "pkg-config" => :build
+
+  uses_from_macos "zlib"
+
+  def install
+    system "sh", "autogen.sh" if build.head?
+    system "./configure", *std_configure_args, "--enable-shared"
+    system "make", "install"
+  end
+
+  test do
+    system bin/"dwarfdump", "-V"
+
+    (testpath/"test.c").write <<~EOS
+      #include <dwarf.h>
+      #include <libdwarf.h>
+      #include <stdio.h>
+      #include <string.h>
+
+      int main(void) {
+        const char *out = NULL;
+        int res = dwarf_get_children_name(0, &out);
+
+        if (res != DW_DLV_OK) {
+          printf("Getting name failed\\n");
+          return 1;
+        }
+
+        if (strcmp(out, "DW_children_no") != 0) {
+          printf("Name did not match: %s\\n", out);
+          return 1;
+        }
+
+        return 0;
+      }
+    EOS
+    system ENV.cc, "-I#{include}/libdwarf-0", "test.c", "-L#{lib}", "-ldwarf", "-o", "test"
+    system "./test"
+  end
+end
