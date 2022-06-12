cf0e0ac7ca15e032a88daac035a0e73cea591e8f
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 12:03:38 +0800

    cython: update 0.29.30 bottle.

---
+++ Formula/cython.rb
@@ -0,0 +1,46 @@
+class Cython < Formula
+  desc "Compiler for writing C extensions for the Python language"
+  homepage "https://cython.org/"
+  url "https://files.pythonhosted.org/packages/d4/ad/7ce0cccd68824ac9623daf4e973c587aa7e2d23418cd028f8860c80651f5/Cython-0.29.30.tar.gz"
+  sha256 "2235b62da8fe6fa8b99422c8e583f2fb95e143867d337b5c75e4b9a1a865f9e3"
+  license "Apache-2.0"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/cython"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "0bf32f9559eaccf0440cb6da5c2db43f761d7804ecf379e9f6023ab070a5777e"
+  end
+
+  keg_only <<~EOS
+    this formula is mainly used internally by other formulae.
+    Users are advised to use `pip` to install cython
+  EOS
+
+  depends_on "python@3.9"
+
+  def install
+    xy = Language::Python.major_minor_version Formula["python@3.9"].opt_bin/"python3"
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
+    system Formula["python@3.9"].opt_bin/"python3", *Language::Python.setup_install_args(libexec)
+
+    bin.install Dir[libexec/"bin/*"]
+    bin.env_script_all_files(libexec/"bin", PYTHONPATH: ENV["PYTHONPATH"])
+  end
+
+  test do
+    xy = Language::Python.major_minor_version Formula["python@3.9"].opt_bin/"python3"
+    ENV.prepend_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
+
+    phrase = "You are using Homebrew"
+    (testpath/"package_manager.pyx").write "print '#{phrase}'"
+    (testpath/"setup.py").write <<~EOS
+      from distutils.core import setup
+      from Cython.Build import cythonize
+
+      setup(
+        ext_modules = cythonize("package_manager.pyx")
+      )
+    EOS
+    system Formula["python@3.9"].opt_bin/"python3", "setup.py", "build_ext", "--inplace"
+    assert_match phrase, shell_output("#{Formula["python@3.9"].opt_bin}/python3 -c 'import package_manager'")
+  end
+end
