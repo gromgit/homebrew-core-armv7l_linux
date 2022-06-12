9efcb9680c033e5e3766cea81482e4155b05fd81
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 14:32:45 +0800

    diffutils: update 3.8 bottle.

---
+++ Formula/diffutils.rb
@@ -0,0 +1,24 @@
+class Diffutils < Formula
+  desc "File comparison utilities"
+  homepage "https://www.gnu.org/s/diffutils/"
+  url "https://ftp.gnu.org/gnu/diffutils/diffutils-3.8.tar.xz"
+  mirror "https://ftpmirror.gnu.org/diffutils/diffutils-3.8.tar.xz"
+  sha256 "a6bdd7d1b31266d11c4f4de6c1b748d4607ab0231af5188fc2533d0ae2438fec"
+  license "GPL-3.0"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/diffutils"
+    sha256 armv7l_linux: "be3b23ecc621717dd2ec46ef8258daf67c2ac8b9a926eb4c22dc156b1b2f4375"
+  end
+
+  def install
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"a").write "foo"
+    (testpath/"b").write "foo"
+    system bin/"diff", "a", "b"
+  end
+end
