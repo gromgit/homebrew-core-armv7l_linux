4ae00c8bc6a6da475f20330bdd7711ca62e9f5b7
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 14:50:05 +0800

    distcc: update 3.4_1 bottle.

---
+++ Formula/distcc.rb
@@ -0,0 +1,59 @@
+class Distcc < Formula
+  desc "Distributed compiler client and server"
+  homepage "https://github.com/distcc/distcc/"
+  url "https://github.com/distcc/distcc/releases/download/v3.4/distcc-3.4.tar.gz"
+  sha256 "2b99edda9dad9dbf283933a02eace6de7423fe5650daa4a728c950e5cd37bd7d"
+  license "GPL-2.0-or-later"
+  revision 1
+  head "https://github.com/distcc/distcc.git", branch: "master"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/distcc"
+    sha256 armv7l_linux: "257a8c89e3c5e1a09292b640e49da7f9e55da345ba00646fc008662b3f276758"
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "python@3.10"
+
+  resource "libiberty" do
+    url "https://ftp.debian.org/debian/pool/main/libi/libiberty/libiberty_20210106.orig.tar.xz"
+    sha256 "9df153d69914c0f5a9145e0abbb248e72feebab6777c712a30f1c3b8c19047d4"
+  end
+
+  def install
+    # While libiberty recommends that packages vendor libiberty into their own source,
+    # distcc wants to have a package manager-installed version.
+    # Rather than make a package for a floating package like this, let's just
+    # make it a resource.
+    buildpath.install resource("libiberty")
+    cd "libiberty" do
+      system "./configure"
+      system "make"
+    end
+    ENV.append "LDFLAGS", "-L#{buildpath}/libiberty"
+    ENV.append_to_cflags "-I#{buildpath}/include"
+
+    # Make sure python stuff is put into the Cellar.
+    # --root triggers a bug and installs into HOMEBREW_PREFIX/lib/python2.7/site-packages instead of the Cellar.
+    inreplace "Makefile.in", '--root="$$DESTDIR"', ""
+    system "./autogen.sh"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  service do
+    run [opt_bin/"distcc", "--allow=192.168.0.1/24"]
+    keep_alive true
+    working_dir opt_prefix
+  end
+
+  test do
+    system "#{bin}/distcc", "--version"
+  end
+end
