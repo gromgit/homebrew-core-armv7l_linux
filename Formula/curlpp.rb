bfcd2488bdafb053506d3f3e99717ddb17d16432
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 11:39:07 +0800

    curlpp: update 0.8.1_1 bottle.

---
+++ Formula/curlpp.rb
@@ -0,0 +1,53 @@
+class Curlpp < Formula
+  desc "C++ wrapper for libcURL"
+  homepage "https://www.curlpp.org/"
+  url "https://github.com/jpbarrette/curlpp/archive/v0.8.1.tar.gz"
+  sha256 "97e3819bdcffc3e4047b6ac57ca14e04af85380bd93afe314bee9dd5c7f46a0a"
+  license "MIT"
+  revision 1
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/curlpp"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "60042b1d5cac0542023de185a3b68fe2a0a3c8724ec1d4fbd9638509b3bbf389"
+  end
+
+  depends_on "cmake" => :build
+
+  uses_from_macos "curl"
+
+  def install
+    ENV.cxx11
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+    inreplace bin/"curlpp-config", Superenv.shims_path/ENV.cc, ENV.cc
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <curlpp/cURLpp.hpp>
+      #include <curlpp/Easy.hpp>
+      #include <curlpp/Options.hpp>
+      #include <curlpp/Exception.hpp>
+
+      int main() {
+        try {
+          curlpp::Cleanup myCleanup;
+          curlpp::Easy myHandle;
+          myHandle.setOpt(new curlpp::options::Url("https://google.com"));
+          myHandle.perform();
+        } catch (curlpp::RuntimeError & e) {
+          std::cout << e.what() << std::endl;
+          return -1;
+        } catch (curlpp::LogicError & e) {
+          std::cout << e.what() << std::endl;
+          return -1;
+        }
+
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "-std=c++11", "test.cpp", "-o", "test", "-I#{include}",
+                    "-L#{lib}", "-lcurlpp", "-lcurl"
+    system "./test"
+  end
+end
