7b7fd129629fbd49f3e60b1ca1e4517ab37795ea
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 13:02:17 +0800

    lzip: update 1.23 bottle.

---
+++ Formula/lzip.rb
@@ -0,0 +1,40 @@
+class Lzip < Formula
+  desc "LZMA-based compression program similar to gzip or bzip2"
+  homepage "https://www.nongnu.org/lzip/"
+  url "https://download-mirror.savannah.gnu.org/releases/lzip/lzip-1.23.tar.gz"
+  sha256 "4792c047ddf15ef29d55ba8e68a1a21e0cb7692d87ecdf7204419864582f280d"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url "https://download.savannah.gnu.org/releases/lzip/"
+    regex(/href=.*?lzip[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/lzip"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "2cb448c6f53eb4ae5133225cc5614180433bc8a0e994ff680a030e51f1fe4428"
+  end
+
+  def install
+    system "./configure", "--prefix=#{prefix}",
+                          "CXX=#{ENV.cxx}",
+                          "CXXFLAGS=#{ENV.cflags}"
+    system "make", "check"
+    ENV.deparallelize
+    system "make", "install"
+  end
+
+  test do
+    path = testpath/"data.txt"
+    original_contents = "." * 1000
+    path.write original_contents
+
+    # compress: data.txt -> data.txt.lz
+    system "#{bin}/lzip", path
+    refute_predicate path, :exist?
+
+    # decompress: data.txt.lz -> data.txt
+    system "#{bin}/lzip", "-d", "#{path}.lz"
+    assert_equal original_contents, path.read
+  end
+end
