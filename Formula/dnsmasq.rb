68fee200868db2ddb54b2a844d9fca18c836281d
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 15:08:08 +0800

    dnsmasq: update 2.86 bottle.

---
+++ Formula/dnsmasq.rb
@@ -0,0 +1,68 @@
+class Dnsmasq < Formula
+  desc "Lightweight DNS forwarder and DHCP server"
+  homepage "https://thekelleys.org.uk/dnsmasq/doc.html"
+  url "https://thekelleys.org.uk/dnsmasq/dnsmasq-2.86.tar.gz"
+  sha256 "ef15f608a83ee2b1d1d2c1f11d089a7e0ac401ffb0991de73fc01ce5f290e512"
+  license any_of: ["GPL-2.0-only", "GPL-3.0-only"]
+
+  livecheck do
+    url "https://thekelleys.org.uk/dnsmasq/"
+    regex(/href=.*?dnsmasq[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/dnsmasq"
+    sha256 armv7l_linux: "dbc2b6bc9d1289514885c0bbe6826b4d4aeeb83d076b491bac3c1df3e6d25573"
+  end
+
+  depends_on "pkg-config" => :build
+
+  def install
+    ENV.deparallelize
+
+    # Fix etc location
+    inreplace %w[dnsmasq.conf.example src/config.h man/dnsmasq.8
+                 man/es/dnsmasq.8 man/fr/dnsmasq.8].each do |s|
+      s.gsub! "/var/lib/misc/dnsmasq.leases",
+              var/"lib/misc/dnsmasq/dnsmasq.leases", false
+      s.gsub! "/etc/dnsmasq.conf", etc/"dnsmasq.conf", false
+      s.gsub! "/var/run/dnsmasq.pid", var/"run/dnsmasq/dnsmasq.pid", false
+      s.gsub! "/etc/dnsmasq.d", etc/"dnsmasq.d", false
+      s.gsub! "/etc/ppp/resolv.conf", etc/"dnsmasq.d/ppp/resolv.conf", false
+      s.gsub! "/etc/dhcpc/resolv.conf", etc/"dnsmasq.d/dhcpc/resolv.conf", false
+      s.gsub! "/usr/sbin/dnsmasq", HOMEBREW_PREFIX/"sbin/dnsmasq", false
+    end
+
+    # Fix compilation on newer macOS versions.
+    ENV.append_to_cflags "-D__APPLE_USE_RFC_3542"
+
+    inreplace "Makefile" do |s|
+      s.change_make_var! "CFLAGS", ENV.cflags
+      s.change_make_var! "LDFLAGS", ENV.ldflags
+    end
+
+    system "make", "install", "PREFIX=#{prefix}"
+
+    etc.install "dnsmasq.conf.example" => "dnsmasq.conf"
+  end
+
+  def post_install
+    (var/"lib/misc/dnsmasq").mkpath
+    (var/"run/dnsmasq").mkpath
+    (etc/"dnsmasq.d/ppp").mkpath
+    (etc/"dnsmasq.d/dhcpc").mkpath
+    touch etc/"dnsmasq.d/ppp/.keepme"
+    touch etc/"dnsmasq.d/dhcpc/.keepme"
+  end
+
+  plist_options startup: true
+
+  service do
+    run [opt_sbin/"dnsmasq", "--keep-in-foreground", "-C", etc/"dnsmasq.conf", "-7", etc/"dnsmasq.d,*.conf"]
+    keep_alive true
+  end
+
+  test do
+    system "#{sbin}/dnsmasq", "--test"
+  end
+end
