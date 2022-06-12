de832725897f9ecb52624a98b8631a54bda27202
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 22:00:58 +0800

    exiftool: update 12.30 bottle.

---
+++ Formula/exiftool.rb
@@ -0,0 +1,45 @@
+class Exiftool < Formula
+  desc "Perl lib for reading and writing EXIF metadata"
+  homepage "https://exiftool.org"
+  # Ensure release is tagged production before submitting.
+  # https://exiftool.org/history.html
+  url "https://cpan.metacpan.org/authors/id/E/EX/EXIFTOOL/Image-ExifTool-12.30.tar.gz"
+  mirror "https://exiftool.org/Image-ExifTool-12.30.tar.gz"
+  sha256 "3be7cda70b471df589c75a4adbb71bae62e633022b0ba62585f3bcd91b35544f"
+  license any_of: ["Artistic-1.0-Perl", "GPL-1.0-or-later"]
+
+  livecheck do
+    url "https://exiftool.org/history.html"
+    regex(/production release is.*?href=.*?Image[._-]ExifTool[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/exiftool"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "b6d408ef0f465f15b8165594a28be11114f97f992d5921a4eb5732855101fe91"
+  end
+
+  uses_from_macos "perl"
+
+  def install
+    # Enable large file support
+    # https://exiftool.org/forum/index.php?topic=3916.msg18182#msg18182
+    inreplace "lib/Image/ExifTool.pm", "LargeFileSupport => undef", "LargeFileSupport => 1"
+
+    # replace the hard-coded path to the lib directory
+    inreplace "exiftool", "$1/lib", libexec/"lib"
+
+    system "perl", "Makefile.PL"
+    system "make", "all"
+    libexec.install "lib"
+    bin.install "exiftool"
+    doc.install Dir["html/*"]
+    man1.install "blib/man1/exiftool.1"
+    man3.install Dir["blib/man3/*"]
+  end
+
+  test do
+    test_image = test_fixtures("test.jpg")
+    assert_match %r{MIME Type\s+: image/jpeg},
+                 shell_output("#{bin}/exiftool #{test_image}")
+  end
+end
