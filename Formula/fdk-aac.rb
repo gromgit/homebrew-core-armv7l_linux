15277895eb0761534ea5facf3d170eee236bdce3
Adrian Ho <the.gromgit@gmail.com>
2022-06-13 01:03:10 +0800

    fdk-aac: update 2.0.2 bottle.

---
+++ Formula/fdk-aac.rb
@@ -0,0 +1,33 @@
+class FdkAac < Formula
+  desc "Standalone library of the Fraunhofer FDK AAC code from Android"
+  homepage "https://sourceforge.net/projects/opencore-amr/"
+  url "https://downloads.sourceforge.net/project/opencore-amr/fdk-aac/fdk-aac-2.0.2.tar.gz"
+  sha256 "c9e8630cf9d433f3cead74906a1520d2223f89bcd3fa9254861017440b8eb22f"
+  license "Apache-2.0"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/fdk-aac"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "a12f6c755593136948b89baef65a5e1caec98427ae7b9120c180787561d9798c"
+  end
+
+  head do
+    url "https://git.code.sf.net/p/opencore-amr/fdk-aac.git", branch: "master"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  def install
+    system "./autogen.sh" if build.head?
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--enable-example"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/aac-enc", test_fixtures("test.wav"), "test.aac"
+    assert_predicate testpath/"test.aac", :exist?
+  end
+end
