805a045617f1f0f42666aadba983aba7d177fcb7
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 12:53:30 +0800

    dcm2niix: update 1.0.20211006 bottle.

---
+++ Formula/dcm2niix.rb
@@ -0,0 +1,38 @@
+class Dcm2niix < Formula
+  desc "DICOM to NIfTI converter"
+  homepage "https://www.nitrc.org/plugins/mwiki/index.php/dcm2nii:MainPage"
+  url "https://github.com/rordenlab/dcm2niix/archive/v1.0.20211006.tar.gz"
+  sha256 "44b737d0101483de17ac1273f2d2c6a4d572a7b76ea040d69aa34d5e484144b9"
+  license "BSD-3-Clause"
+  version_scheme 1
+  head "https://github.com/rordenlab/dcm2niix.git", branch: "master"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/dcm2niix"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "5ef014f59301243ae02c947b47ffa2f151444754f4569ffb00b815c5cb263b47"
+  end
+
+  depends_on "cmake" => :build
+
+  resource "sample.dcm" do
+    url "https://raw.githubusercontent.com/dangom/sample-dicom/master/MR000000.dcm"
+    sha256 "4efd3edd2f5eeec2f655865c7aed9bc552308eb2bc681f5dd311b480f26f3567"
+  end
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    resource("sample.dcm").stage testpath
+    system "#{bin}/dcm2niix", "-f", "%d_%e", "-z", "n", "-b", "y", testpath
+    assert_predicate testpath/"localizer_1.nii", :exist?
+    assert_predicate testpath/"localizer_1.json", :exist?
+  end
+end
