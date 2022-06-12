5c666efb1f99ab442422c9271b33e4f219975f7e
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 20:06:30 +0800

    ebook-tools: update 0.2.2_3 bottle.

---
+++ Formula/ebook-tools.rb
@@ -0,0 +1,29 @@
+class EbookTools < Formula
+  desc "Access and convert several ebook formats"
+  homepage "https://sourceforge.net/projects/ebook-tools/"
+  url "https://downloads.sourceforge.net/project/ebook-tools/ebook-tools/0.2.2/ebook-tools-0.2.2.tar.gz"
+  sha256 "cbc35996e911144fa62925366ad6a6212d6af2588f1e39075954973bbee627ae"
+  license "MIT"
+  revision 3
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/ebook-tools"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "97f3684bd967849e8d9ab054488a505e1af74294a9454cf0a4710a9de5fc1a56"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "libzip"
+
+  uses_from_macos "libxml2"
+
+  def install
+    system "cmake", ".",
+                    "-DLIBZIP_INCLUDE_DIR=#{Formula["libzip"].lib}/libzip/include",
+                    *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/einfo", "-help"
+  end
+end
