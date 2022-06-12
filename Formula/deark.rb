be430243c5a25ba7976c1583f30b88feb6442787
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 13:07:34 +0800

    deark: update 1.6.2 bottle.

---
+++ Formula/deark.rb
@@ -0,0 +1,33 @@
+class Deark < Formula
+  desc "File conversion utility for older formats"
+  homepage "https://entropymine.com/deark/"
+  url "https://entropymine.com/deark/releases/deark-1.6.2.tar.gz"
+  sha256 "199f5cd6c65cd23af9323bc464f0d33f53d22686135695b4aa4ddf2ec43534ae"
+  license "MIT"
+
+  livecheck do
+    url "https://entropymine.com/deark/releases/"
+    regex(/href=.*?deark[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/deark"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "0a820bd51a30b21cad20a1974800178ae43b2a59e91fd66fef2c04df4ab591b6"
+  end
+
+  def install
+    system "make"
+    bin.install "deark"
+  end
+
+  test do
+    require "base64"
+
+    (testpath/"test.gz").write ::Base64.decode64 <<~EOS
+      H4sICKU51VoAA3Rlc3QudHh0APNIzcnJ11HwyM9NTSpKLVfkAgBuKJNJEQAAAA==
+    EOS
+    system "#{bin}/deark", "test.gz"
+    file = (testpath/"output.000.test.txt").readlines.first
+    assert_match "Hello, Homebrew!", file
+  end
+end
