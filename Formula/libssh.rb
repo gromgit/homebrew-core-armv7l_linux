8aa533519d7417a59e0ef0c5ffda8c6e0e940bfa
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 22:04:56 +0800

    libssh: update 0.9.6 bottle.

---
+++ Formula/libssh.rb
@@ -0,0 +1,46 @@
+class Libssh < Formula
+  desc "C library SSHv1/SSHv2 client and server protocols"
+  homepage "https://www.libssh.org/"
+  url "https://www.libssh.org/files/0.9/libssh-0.9.6.tar.xz"
+  sha256 "86bcf885bd9b80466fe0e05453c58b877df61afa8ba947a58c356d7f0fab829b"
+  license "LGPL-2.1-or-later"
+  head "https://git.libssh.org/projects/libssh.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/libssh"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "040c5c82b926f83f6d5cf792a74501ac381bd1c7c8e366d163746dfb379b6b62"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "openssl@1.1"
+
+  uses_from_macos "zlib"
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", "-DBUILD_STATIC_LIB=ON",
+                            "-DWITH_SYMBOL_VERSIONING=OFF",
+                            *std_cmake_args
+      system "make", "install"
+      lib.install "src/libssh.a"
+    end
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <libssh/libssh.h>
+      #include <stdlib.h>
+      int main()
+      {
+        ssh_session my_ssh_session = ssh_new();
+        if (my_ssh_session == NULL)
+          exit(-1);
+        ssh_free(my_ssh_session);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "-I#{include}", testpath/"test.c",
+           "-L#{lib}", "-lssh", "-o", testpath/"test"
+    system "./test"
+  end
+end
