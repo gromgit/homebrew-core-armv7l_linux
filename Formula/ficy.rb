5f5cab661cf41219b32630ded1c5f9fe37dc2f40
Adrian Ho <the.gromgit@gmail.com>
2022-06-13 01:23:48 +0800

    ficy: update 1.0.21 bottle.

---
+++ Formula/ficy.rb
@@ -0,0 +1,28 @@
+class Ficy < Formula
+  desc "Icecast/Shoutcast stream grabber suite"
+  homepage "https://www.thregr.org/~wavexx/software/fIcy/"
+  url "https://www.thregr.org/~wavexx/software/fIcy/releases/fIcy-1.0.21.tar.gz"
+  sha256 "8564b16d3a52fa6dc286b02bfcc19e4acdc148c30f1750ca144e2ea47c84fd81"
+  license "LGPL-2.1-only"
+  head "https://gitlab.com/wavexx/fIcy.git", branch: "master"
+
+  livecheck do
+    url :homepage
+    regex(%r{href=.*?releases/fIcy[._-]v?(\d+(?:\.\d+)+)\.t}i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/ficy"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "6ec02b3d409c9b1284ce1f9af95dbf7a543940eb554242e4bc3178e4f8dabbcd"
+  end
+
+  def install
+    system "make"
+    bin.install "fIcy", "fPls", "fResync"
+  end
+
+  test do
+    cp test_fixtures("test.mp3"), testpath
+    system "#{bin}/fResync", "-n", "1", "test.mp3"
+  end
+end
