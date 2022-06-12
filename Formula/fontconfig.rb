778feb2873a72ae4df4848265aabab35151a413c
Adrian Ho <the.gromgit@gmail.com>
2022-06-13 03:56:30 +0800

    fontconfig: update 2.14.0 bottle.

---
+++ Formula/fontconfig.rb
@@ -0,0 +1,71 @@
+class Fontconfig < Formula
+  desc "XML-based font configuration API for X Windows"
+  homepage "https://wiki.freedesktop.org/www/Software/fontconfig/"
+  url "https://www.freedesktop.org/software/fontconfig/release/fontconfig-2.14.0.tar.xz"
+  sha256 "dcbeb84c9c74bbfdb133d535fe1c7bedc9f2221a8daf3914b984c44c520e9bac"
+  license "MIT"
+
+  livecheck do
+    url :stable
+    regex(/href=.*?fontconfig[._-]v?(\d+\.\d+\.(?:\d|[0-8]\d+))\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/fontconfig"
+    sha256 armv7l_linux: "3f31894c09ce324e7d509faba0cdba590b93583fdf428ff0284fe2fd637fd39a"
+  end
+
+  head do
+    url "https://gitlab.freedesktop.org/fontconfig/fontconfig.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "gettext" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "freetype"
+
+  uses_from_macos "gperf" => :build
+  uses_from_macos "python" => :build, since: :catalina
+  uses_from_macos "bzip2"
+  uses_from_macos "expat"
+
+  on_linux do
+    depends_on "gettext" => :build
+    depends_on "json-c" => :build
+    depends_on "util-linux"
+  end
+
+  def install
+    font_dirs = %w[
+      /System/Library/Fonts
+      /Library/Fonts
+      ~/Library/Fonts
+    ]
+
+    font_dirs << Dir["/System/Library/Assets{,V2}/com_apple_MobileAsset_Font*"].max if MacOS.version >= :sierra
+
+    system "autoreconf", "-iv" if build.head?
+    ENV["UUID_CFLAGS"] = "-I#{Formula["util-linux"].include}" if OS.linux?
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--disable-docs",
+                          "--enable-static",
+                          "--with-add-fonts=#{font_dirs.join(",")}",
+                          "--prefix=#{prefix}",
+                          "--localstatedir=#{var}",
+                          "--sysconfdir=#{etc}"
+    system "make", "install", "RUN_FC_CACHE_TEST=false"
+  end
+
+  def post_install
+    ohai "Regenerating font cache, this may take a while"
+    system "#{bin}/fc-cache", "-frv"
+  end
+
+  test do
+    system "#{bin}/fc-list"
+  end
+end
