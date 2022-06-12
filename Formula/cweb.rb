8a0dbebad706641d80a2975356ab932956b2a0a4
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 11:49:07 +0800

    cweb: update 4.7 bottle.

---
+++ Formula/cweb.rb
@@ -0,0 +1,54 @@
+class Cweb < Formula
+  desc "Literate documentation system for C, C++, and Java"
+  homepage "https://cs.stanford.edu/~knuth/cweb.html"
+  url "https://github.com/ascherer/cweb/archive/cweb-4.7.tar.gz"
+  sha256 "818c1a2966744548935f67e899ffa41605056fb06492d959af9fa276353f0dbf"
+  # See disucssions in this thread, https://github.com/ascherer/cweb/issues/29
+  license :cannot_represent
+
+  livecheck do
+    url :stable
+    regex(/^cweb[._-]v?(\d+(?:\.\d+)+[a-z]*?)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/cweb"
+    sha256 armv7l_linux: "298786a29ab601013a835b61d5998cff94155695d991b8a6a8dd8c27916c354a"
+  end
+
+  conflicts_with "texlive", because: "both install `cweb` binaries"
+
+  def install
+    ENV.deparallelize
+
+    macrosdir = share/"texmf/tex/generic"
+    cwebinputs = lib/"cweb"
+
+    # make install doesn't use `mkdir -p` so this is needed
+    [bin, man1, macrosdir, elisp, cwebinputs].each(&:mkpath)
+
+    system "make", "install",
+      "DESTDIR=#{bin}/",
+      "MANDIR=#{man1}",
+      "MANEXT=1",
+      "MACROSDIR=#{macrosdir}",
+      "EMACSDIR=#{elisp}",
+      "CWEBINPUTS=#{cwebinputs}"
+  end
+
+  test do
+    (testpath/"test.w").write <<~EOS
+      @* Hello World
+      This is a minimal program written in CWEB.
+
+      @c
+      #include <stdio.h>
+      void main() {
+          printf("Hello world!");
+      }
+    EOS
+    system bin/"ctangle", "test.w"
+    system ENV.cc, "test.c", "-o", "hello"
+    assert_equal "Hello world!", pipe_output("./hello")
+  end
+end
