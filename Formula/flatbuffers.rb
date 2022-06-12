4b572d4e925fdbc9b814e3a78e2831417d32f0e5
Adrian Ho <the.gromgit@gmail.com>
2022-06-13 01:58:58 +0800

    flatbuffers: update 2.0.6 bottle.

---
+++ Formula/flatbuffers.rb
@@ -0,0 +1,79 @@
+class Flatbuffers < Formula
+  desc "Serialization library for C++, supporting Java, C#, and Go"
+  homepage "https://google.github.io/flatbuffers"
+  url "https://github.com/google/flatbuffers/archive/v2.0.6.tar.gz"
+  sha256 "e2dc24985a85b278dd06313481a9ca051d048f9474e0f199e372fea3ea4248c9"
+  license "Apache-2.0"
+  head "https://github.com/google/flatbuffers.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/flatbuffers"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "fef98fd4187940915fdd61f847d2c2194f6b2c701794e170561a897864d158c8"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "python@3.10" => :build
+
+  conflicts_with "osrm-backend", because: "both install flatbuffers headers"
+
+  def install
+    system "cmake", "-G", "Unix Makefiles", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    def testfbs
+      <<~EOS
+        // example IDL file
+
+        namespace MyGame.Sample;
+
+        enum Color:byte { Red = 0, Green, Blue = 2 }
+
+        union Any { Monster }  // add more elements..
+
+          struct Vec3 {
+            x:float;
+            y:float;
+            z:float;
+          }
+
+          table Monster {
+            pos:Vec3;
+            mana:short = 150;
+            hp:short = 100;
+            name:string;
+            friendly:bool = false (deprecated);
+            inventory:[ubyte];
+            color:Color = Blue;
+          }
+
+        root_type Monster;
+
+      EOS
+    end
+    (testpath/"test.fbs").write(testfbs)
+
+    def testjson
+      <<~EOS
+        {
+          pos: {
+            x: 1,
+            y: 2,
+            z: 3
+          },
+          hp: 80,
+          name: "MyMonster"
+        }
+      EOS
+    end
+    (testpath/"test.json").write(testjson)
+
+    system bin/"flatc", "-c", "-b", "test.fbs", "test.json"
+  end
+end
