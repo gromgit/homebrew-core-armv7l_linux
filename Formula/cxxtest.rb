a69c7a3be023661f82d3a604960c0dfb64babe66
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 11:51:49 +0800

    cxxtest: update 4.4_3 bottle.

---
+++ Formula/cxxtest.rb
@@ -0,0 +1,45 @@
+class Cxxtest < Formula
+  include Language::Python::Virtualenv
+
+  desc "C++ unit testing framework similar to JUnit, CppUnit and xUnit"
+  homepage "https://cxxtest.com/"
+  url "https://github.com/CxxTest/cxxtest/releases/download/4.4/cxxtest-4.4.tar.gz"
+  mirror "https://deb.debian.org/debian/pool/main/c/cxxtest/cxxtest_4.4.orig.tar.gz"
+  sha256 "1c154fef91c65dbf1cd4519af7ade70a61d85a923b6e0c0b007dc7f4895cf7d8"
+  license "LGPL-3.0"
+  revision 3
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/cxxtest"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "921dfdb2724ad69b3394838793b83b4f4aa5aa957a36c24c467439aa00d666d7"
+  end
+
+  depends_on "python@3.10"
+
+  def install
+    venv = virtualenv_create(libexec, Formula["python@3.10"].opt_bin/"python3")
+    venv.pip_install_and_link buildpath/"python"
+
+    include.install "cxxtest"
+    doc.install Dir["doc/*"]
+  end
+
+  test do
+    testfile = testpath/"MyTestSuite1.h"
+    testfile.write <<~EOS
+      #include <cxxtest/TestSuite.h>
+
+      class MyTestSuite1 : public CxxTest::TestSuite {
+      public:
+          void testAddition(void) {
+              TS_ASSERT(1 + 1 > 1);
+              TS_ASSERT_EQUALS(1 + 1, 2);
+          }
+      };
+    EOS
+
+    system bin/"cxxtestgen", "--error-printer", "-o", testpath/"runner.cpp", testfile
+    system ENV.cxx, "-o", testpath/"runner", testpath/"runner.cpp"
+    system testpath/"runner"
+  end
+end
