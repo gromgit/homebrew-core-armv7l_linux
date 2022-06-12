72db50b89ca47a61ae27c14b9db1cf33a1ca3490
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 15:42:02 +0800

    doc8: update 0.11.2 bottle.

---
+++ Formula/doc8.rb
@@ -0,0 +1,55 @@
+class Doc8 < Formula
+  include Language::Python::Virtualenv
+
+  desc "Style checker for Sphinx documentation"
+  homepage "https://github.com/PyCQA/doc8"
+  url "https://files.pythonhosted.org/packages/e0/38/af1889fbcfae4b66eb936f0f43ceaa315b19b8d4ae1123e1bcab0d9d0738/doc8-0.11.2.tar.gz"
+  sha256 "c35a231f88f15c204659154ed3d499fa4d402d7e63d41cba7b54cf5e646123ab"
+  license "Apache-2.0"
+  head "https://github.com/PyCQA/doc8.git", branch: "main"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/doc8"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "cdcdf6f8fca87cb7142b4fa48cf6700d92db0a495ef05342e940bda25e7de33c"
+  end
+
+  depends_on "python@3.10"
+
+  resource "docutils" do
+    url "https://files.pythonhosted.org/packages/57/b1/b880503681ea1b64df05106fc7e3c4e3801736cf63deffc6fa7fc5404cf5/docutils-0.18.1.tar.gz"
+    sha256 "679987caf361a7539d76e584cbeddc311e3aee937877c87346f31debc63e9d06"
+  end
+
+  resource "pbr" do
+    url "https://files.pythonhosted.org/packages/96/9f/f4bc832eeb4ae723b86372277da56a5643b0ad472a95314e8f516a571bb0/pbr-5.9.0.tar.gz"
+    sha256 "e8dca2f4b43560edef58813969f52a56cef023146cbb8931626db80e6c1c4308"
+  end
+
+  resource "Pygments" do
+    url "https://files.pythonhosted.org/packages/59/0f/eb10576eb73b5857bc22610cdfc59e424ced4004fe7132c8f2af2cc168d3/Pygments-2.12.0.tar.gz"
+    sha256 "5eb116118f9612ff1ee89ac96437bb6b49e8f04d8a13b514ba26f620208e26eb"
+  end
+
+  resource "restructuredtext-lint" do
+    url "https://files.pythonhosted.org/packages/48/9c/6d8035cafa2d2d314f34e6cd9313a299de095b26e96f1c7312878f988eec/restructuredtext_lint-1.4.0.tar.gz"
+    sha256 "1b235c0c922341ab6c530390892eb9e92f90b9b75046063e047cacfb0f050c45"
+  end
+
+  resource "stevedore" do
+    url "https://files.pythonhosted.org/packages/67/73/cd693fde78c3b2397d49ad2c6cdb082eb0b6a606188876d61f53bae16293/stevedore-3.5.0.tar.gz"
+    sha256 "f40253887d8712eaa2bb0ea3830374416736dc8ec0e22f5a65092c1174c44335"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    (testpath/"broken.rst").write <<~EOS
+      Heading
+      ------
+    EOS
+    output = pipe_output("#{bin}/doc8 broken.rst 2>&1")
+    assert_match "D000 Title underline too short.", output
+  end
+end
