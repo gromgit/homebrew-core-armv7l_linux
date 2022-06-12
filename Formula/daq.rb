b6a624b1e1723ea6dc48b61ab73b6186c86d9306
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 12:23:20 +0800

    daq: update 3.0.8 bottle.

---
+++ Formula/daq.rb
@@ -0,0 +1,53 @@
+class Daq < Formula
+  desc "Network intrusion prevention and detection system"
+  homepage "https://www.snort.org/"
+  url "https://github.com/snort3/libdaq/archive/v3.0.8.tar.gz"
+  mirror "https://fossies.org/linux/misc/libdaq-3.0.8.tar.gz"
+  sha256 "e80cd94f539881388d35a00b8703dffcb6a0f8138b4dc38d0ba951747ca16f3e"
+  license "GPL-2.0-only"
+  head "https://github.com/snort3/libdaq.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/daq"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "df47da70fd97c739cca5925cdff9a944e226dbd5321212e18838acbd2a7d741d"
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+
+  uses_from_macos "libpcap"
+
+  def install
+    system "./bootstrap"
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <assert.h>
+      #include <stdio.h>
+      #include <daq.h>
+      #include <daq_module_api.h>
+
+      extern const DAQ_ModuleAPI_t pcap_daq_module_data;
+      static DAQ_Module_h static_modules[] = { &pcap_daq_module_data, NULL };
+
+      int main()
+      {
+        int rval = daq_load_static_modules(static_modules);
+        assert(rval == 1);
+        DAQ_Module_h module = daq_modules_first();
+        assert(module != NULL);
+        printf("[%s] - Type: 0x%x", daq_module_get_name(module), daq_module_get_type(module));
+        module = daq_modules_next();
+        assert(module == NULL);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-ldaq", "-ldaq_static_pcap", "-lpcap", "-lpthread", "-o", "test"
+    assert_match "[pcap] - Type: 0xb", shell_output("./test")
+  end
+end
