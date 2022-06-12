07a1ae05e198558478ee394f59d471fd34b0d8be
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 11:25:07 +0800

    cubelib: update 4.7 bottle.

---
+++ Formula/cubelib.rb
@@ -0,0 +1,56 @@
+class Cubelib < Formula
+  desc "Cube, is a performance report explorer for Scalasca and Score-P"
+  homepage "https://scalasca.org/software/cube-4.x/download.html"
+  url "https://apps.fz-juelich.de/scalasca/releases/cube/4.7/dist/cubelib-4.7.tar.gz", using: :homebrew_curl
+  sha256 "e44352c80a25a49b0fa0748792ccc9f1be31300a96c32de982b92477a8740938"
+  license "BSD-3-Clause"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?cubelib[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/cubelib"
+    sha256 armv7l_linux: "b57ae753ff87051e78e13c904eb422983d7ec2aa919cae38d3d26d7f0bb89638"
+  end
+
+  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "pkg-config" => :build
+  end
+
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
+    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
+    directory "build-frontend"
+  end
+
+  def install
+    ENV.deparallelize
+
+    args = %w[--disable-silent-rules]
+    if ENV.compiler == :clang
+      args << "--with-nocross-compiler-suite=clang"
+      args << "CXXFLAGS=-stdlib=libc++"
+      args << "LDFLAGS=-stdlib=libc++"
+    end
+
+    system "./configure", *std_configure_args, *args
+    system "make"
+    system "make", "install"
+
+    inreplace pkgshare/"cubelib.summary", "#{Superenv.shims_path}/", ""
+  end
+
+  test do
+    cp_r "#{share}/doc/cubelib/example/", testpath
+    chdir "#{testpath}/example" do
+      # build and run tests
+      system "make", "-f", "Makefile.frontend", "all"
+      system "make", "-f", "Makefile.frontend", "run"
+    end
+  end
+end
