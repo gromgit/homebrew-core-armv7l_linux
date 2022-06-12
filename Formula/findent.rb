f36d26764287de2a5c36284b302753521368d9db
Adrian Ho <the.gromgit@gmail.com>
2022-06-13 01:31:10 +0800

    findent: update 4.2.0 bottle.

---
+++ Formula/findent.rb
@@ -0,0 +1,35 @@
+class Findent < Formula
+  desc "Indent and beautify Fortran sources and generate dependency information"
+  homepage "https://www.ratrabbit.nl/ratrabbit/findent/index.html"
+  url "https://downloads.sourceforge.net/project/findent/findent-4.2.0.tar.gz"
+  sha256 "6e6411eecfe6586d991ed7dbeeb47859fcfe3655615619912703da6da6bdc3c2"
+  license "BSD-3-Clause"
+
+  livecheck do
+    url :stable
+    regex(%r{url=.*?/findent[._-]v?(\d+(?:\.\d+)+)\.(?:t|zip)}i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/findent"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "d26f9697002e5ec80b3bc48eaf3b8c1c907b562ffb98cd52e0f4f174a280e568"
+  end
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+    (pkgshare/"test").install %w[test/progfree.f.in test/progfree.f.try.f.ref]
+  end
+
+  test do
+    cp_r pkgshare/"test/progfree.f.in", testpath
+    cp_r pkgshare/"test/progfree.f.try.f.ref", testpath
+    flags = File.open(testpath/"progfree.f.in", &:readline).sub(/ *! */, "").chomp
+    system "#{bin}/findent #{flags} < progfree.f.in > progfree.f.out.f90"
+    assert_predicate testpath/"progfree.f.out.f90", :exist?
+    assert compare_file(testpath/"progfree.f.try.f.ref", testpath/"progfree.f.out.f90")
+  end
+end
