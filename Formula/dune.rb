e8044b42850e27ffc6194a3d20f802a6e6eecbca
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 18:13:00 +0800

    dune: update 3.2.0 bottle.

--- Formula/dune.rb
+++ Formula/dune.rb
@@ -7,12 +7,9 @@ class Dune < Formula
   head "https://github.com/ocaml/dune.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a092d71e2a557640c5eb2536626fcce8bd3266ec62b63b7154c9831045709c58"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1b7d11b76354ab68c5bbd3163f4ba52c26071ad742cc4cf8ffb9f958a5c53589"
-    sha256 cellar: :any_skip_relocation, monterey:       "bda67220b7c34bc6b30c99d9b3cf6054f28c23111243e58af96e050300c31526"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ebcbb0e30e5764f16dc659c8dfe939186c9cf7ead5229cc47c498d8533625907"
-    sha256 cellar: :any_skip_relocation, catalina:       "c2d9d4af81f9599631fc7060641ddc18e039b2887e6eee7f4893d7ab3031bda7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "071d66e8406dfba0d2e48ce4a8f8402ddbf9d71df5f8246dee5e0865effc2442"
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/dune"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "2d86c2ed5e9df27b13602b437494e10de0bdb26bdbfed60fb48132de08bca8ee"
   end
 
   depends_on "ocaml" => [:build, :test]
