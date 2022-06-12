f275b5d47b8437c855243f8f10b326b768e270e0
Adrian Ho <the.gromgit@gmail.com>
2022-06-13 03:13:07 +0800

    fluent-bit: update 1.9.3 bottle.

---
+++ Formula/fluent-bit.rb
@@ -0,0 +1,49 @@
+class FluentBit < Formula
+  desc "Fast and Lightweight Logs and Metrics processor"
+  homepage "https://github.com/fluent/fluent-bit"
+  url "https://github.com/fluent/fluent-bit/archive/v1.9.3.tar.gz"
+  sha256 "26ed6279aced8f122664184d6a999eb3c2372a19aeb1a7ddad9704d23b44b6be"
+  license "Apache-2.0"
+  head "https://github.com/fluent/fluent-bit.git", branch: "master"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/fluent-bit"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "3760e03dae392b35d849de704e19fc1084472678db37a63205e37f1fd9a0294c"
+  end
+
+  depends_on "bison" => :build
+  depends_on "cmake" => :build
+  depends_on "flex" => :build
+
+  on_linux do
+    depends_on "openssl@1.1"
+  end
+
+  def install
+    # Prevent fluent-bit to install files into global init system
+    #
+    # For more information see https://github.com/fluent/fluent-bit/issues/3393
+    inreplace "src/CMakeLists.txt", "if(IS_DIRECTORY /lib/systemd/system)", "if(False)"
+    inreplace "src/CMakeLists.txt", "elseif(IS_DIRECTORY /usr/share/upstart)", "elif(False)"
+
+    chdir "build" do
+      # Per https://luajit.org/install.html: If MACOSX_DEPLOYMENT_TARGET
+      # is not set then it's forced to 10.4, which breaks compile on Mojave.
+      # fluent-bit builds against a vendored Luajit.
+      ENV["MACOSX_DEPLOYMENT_TARGET"] = MacOS.version
+
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    output = shell_output("#{bin}/fluent-bit -V").chomp
+    assert_match "Fluent Bit v#{version}", output
+  end
+end
