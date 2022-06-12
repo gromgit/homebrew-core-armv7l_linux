03478b3eeed5ee4317cf2a2f8a2cf10f12349c85
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 20:53:32 +0800

    envv: update 1.7 bottle.

---
+++ Formula/envv.rb
@@ -0,0 +1,34 @@
+class Envv < Formula
+  desc "Shell-independent handling of environment variables"
+  homepage "https://github.com/jakewendt/envv#readme"
+  url "https://github.com/jakewendt/envv/archive/v1.7.tar.gz"
+  sha256 "1db05b46904e0cc4d777edf3ea14665f6157ade0567359e28663b5b00f6fa59a"
+  license "MIT"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/envv"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "8a85bc45da2edde9e95b6efec62ec3db751342859a397c71564e67933b5a964b"
+  end
+
+  def install
+    system "make"
+
+    bin.install "envv"
+    man1.install "envv.1"
+  end
+
+  test do
+    ENV["mylist"] = "A:B:C"
+    assert_equal "mylist=A:C; export mylist", shell_output("#{bin}/envv del mylist B").strip
+    assert_equal "mylist=B:C; export mylist", shell_output("#{bin}/envv del mylist A").strip
+    assert_equal "mylist=A:B; export mylist", shell_output("#{bin}/envv del mylist C").strip
+
+    assert_equal "", shell_output("#{bin}/envv add mylist B").strip
+    assert_equal "mylist=B:A:C; export mylist", shell_output("#{bin}/envv add mylist B 1").strip
+    assert_equal "mylist=A:C:B; export mylist", shell_output("#{bin}/envv add mylist B 99").strip
+
+    assert_equal "mylist=A:B:C:D; export mylist", shell_output("#{bin}/envv add mylist D").strip
+    assert_equal "mylist=D:A:B:C; export mylist", shell_output("#{bin}/envv add mylist D 1").strip
+    assert_equal "mylist=A:B:D:C; export mylist", shell_output("#{bin}/envv add mylist D 3").strip
+  end
+end
