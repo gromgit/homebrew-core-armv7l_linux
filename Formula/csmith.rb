06c9ed9fee9fa7a78d10826892e6cc7af09d86e4
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 10:37:57 +0800

    csmith: update 2.3.0 bottle.

---
+++ Formula/csmith.rb
@@ -0,0 +1,46 @@
+class Csmith < Formula
+  desc "Generates random C programs conforming to the C99 standard"
+  homepage "https://embed.cs.utah.edu/csmith/"
+  url "https://embed.cs.utah.edu/csmith/csmith-2.3.0.tar.gz"
+  sha256 "f247cc0aede5f8a0746271b40a5092b5b5a2d034e5e8f7a836c879dde3fb65d5"
+  license "BSD-2-Clause"
+  head "https://github.com/csmith-project/csmith.git", branch: "master"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?csmith[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/csmith"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "322657551fb89561fde3d527874416a5abbe5f3dfa1ec222f2a8178b63915d8e"
+  end
+
+  uses_from_macos "m4" => :build
+
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
+    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
+  end
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+    mv "#{bin}/compiler_test.in", share
+    (include/"csmith-#{version}/runtime").install Dir["runtime/*.h"]
+  end
+
+  def caveats
+    <<~EOS
+      It is recommended that you set the environment variable 'CSMITH_PATH' to
+        #{include}/csmith-#{version}
+    EOS
+  end
+
+  test do
+    system "#{bin}/csmith", "-o", "test.c"
+  end
+end
