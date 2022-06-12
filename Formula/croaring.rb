781c2c993403e56c89f633c9a6fafc39f1ac7c8c
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 10:26:50 +0800

    croaring: update 0.5.0 bottle.

---
+++ Formula/croaring.rb
@@ -0,0 +1,40 @@
+class Croaring < Formula
+  desc "Roaring bitmaps in C (and C++)"
+  homepage "https://roaringbitmap.org"
+  url "https://github.com/RoaringBitmap/CRoaring/archive/v0.5.0.tar.gz"
+  sha256 "edab1b1a464e5a361ff622dc833170b2f33729c161aee4c2e53a324ac62ef78f"
+  license "Apache-2.0"
+  head "https://github.com/RoaringBitmap/CRoaring.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/croaring"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "6592097459848762c690de810becf74fa8fb7ca85f69be64473a0c2575f8cdbe"
+  end
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", ".", *std_cmake_args, "-DENABLE_ROARING_TESTS=OFF"
+    system "make", "install"
+    system "make", "clean"
+    system "cmake", ".", *std_cmake_args, "-DROARING_BUILD_STATIC=ON"
+    system "make"
+    lib.install "src/libroaring.a"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      #include <roaring/roaring.h>
+      int main() {
+          roaring_bitmap_t *r1 = roaring_bitmap_create();
+          for (uint32_t i = 100; i < 1000; i++) roaring_bitmap_add(r1, i);
+          printf("cardinality = %d\\n", (int) roaring_bitmap_get_cardinality(r1));
+          roaring_bitmap_free(r1);
+          return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lroaring", "-o", "test"
+    assert_equal "cardinality = 900\n", shell_output("./test")
+  end
+end
