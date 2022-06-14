dd1c05322e79e7919067fe3c1374efe3f6765519
Adrian Ho <the.gromgit@gmail.com>
2022-06-15 03:06:55 +0800

    kimwitu++: update 2.3.13 bottle.

---
+++ Formula/kimwitu++.rb
@@ -0,0 +1,26 @@
+class Kimwituxx < Formula
+  desc "Tool for processing trees (i.e. terms)"
+  homepage "https://www2.informatik.hu-berlin.de/sam/kimwitu++/"
+  url "https://download.savannah.gnu.org/releases/kimwitu-pp/kimwitu++-2.3.13.tar.gz"
+  sha256 "3f6d9fbb35cc4760849b18553d06bc790466ca8b07884ed1a1bdccc3a9792a73"
+
+  livecheck do
+    url "https://download.savannah.gnu.org/releases/kimwitu-pp/"
+    regex(/href=.*?kimwitu\+\+[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/kimwitu++"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "59ba9c35676665d8ed52968cd74b1af0a361dad33402ee972e1557949d81ee7f"
+  end
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    bin.mkpath
+    man1.mkpath
+    system "make", "install"
+  end
+end
