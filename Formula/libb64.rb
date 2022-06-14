b210b73c2e69734ebfc7332309da494230c4c078
Adrian Ho <the.gromgit@gmail.com>
2022-06-15 06:44:39 +0800

    libb64: update 1.2.1 bottle.

---
+++ Formula/libb64.rb
@@ -0,0 +1,36 @@
+class Libb64 < Formula
+  desc "Base64 encoding/decoding library"
+  homepage "https://libb64.sourceforge.io/"
+  url "https://downloads.sourceforge.net/project/libb64/libb64/libb64/libb64-1.2.1.zip"
+  sha256 "20106f0ba95cfd9c35a13c71206643e3fb3e46512df3e2efb2fdbf87116314b2"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/libb64"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "d7b69f75af12b66535033e8e25e8a466f6b94e03dcd4dd887132fcc3b52c7ca9"
+  end
+
+  def install
+    system "make", "all_src"
+    include.install "include/b64"
+    lib.install "src/libb64.a"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <string.h>
+      #include <b64/cencode.h>
+      int main()
+      {
+        base64_encodestate B64STATE;
+        base64_init_encodestate(&B64STATE);
+        char buf[32];
+        int c = base64_encode_block("\x01\x02\x03\x04", 4, buf, &B64STATE);
+        c += base64_encode_blockend(buf+c, &B64STATE);
+        if (memcmp(buf,"AQIDBA==",8)) return(-1);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lb64", "-o", "test"
+    system "./test"
+  end
+end
