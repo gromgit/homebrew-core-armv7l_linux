b1f8fadc1bd49b56bc933a498f4da7e2ad0dc096
Adrian Ho <the.gromgit@gmail.com>
2022-06-14 18:27:58 +0800

    httrack: update 3.49.2_1 bottle.

---
+++ Formula/httrack.rb
@@ -0,0 +1,43 @@
+class Httrack < Formula
+  desc "Website copier/offline browser"
+  homepage "https://www.httrack.com/"
+  # Always use mirror.httrack.com when you link to a new version of HTTrack, as
+  # link to download.httrack.com will break on next HTTrack update.
+  url "https://mirror.httrack.com/historical/httrack-3.49.2.tar.gz"
+  sha256 "3477a0e5568e241c63c9899accbfcdb6aadef2812fcce0173688567b4c7d4025"
+  revision 1
+
+  livecheck do
+    url "https://mirror.httrack.com/historical/"
+    regex(/href=.*?httrack[._-]v?(\d+(?:\.\d+)+)\./i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/httrack"
+    sha256 armv7l_linux: "74254877563437e8a486f83ecc3f4518e7a36b880aa651ee1a689d12eabecb56"
+  end
+
+  depends_on "openssl@1.1"
+
+  uses_from_macos "zlib"
+
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-pre-0.4.2.418-big_sur.diff"
+    sha256 "83af02f2aa2b746bb7225872cab29a253264be49db0ecebb12f841562d9a2923"
+  end
+
+  def install
+    ENV.deparallelize
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make", "install"
+    # Don't need Gnome integration
+    rm_rf Dir["#{share}/{applications,pixmaps}"]
+  end
+
+  test do
+    download = "https://raw.githubusercontent.com/Homebrew/homebrew/65c59dedea31/.yardopts"
+    system bin/"httrack", download, "-O", testpath
+    assert_predicate testpath/"raw.githubusercontent.com", :exist?
+  end
+end
