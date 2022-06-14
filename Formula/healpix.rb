ccdb040d74f0d7999cfbcc21711d2510ee67d200
Adrian Ho <the.gromgit@gmail.com>
2022-06-14 15:11:38 +0800

    healpix: update 3.81 bottle.

---
+++ Formula/healpix.rb
@@ -0,0 +1,64 @@
+class Healpix < Formula
+  desc "Hierarchical Equal Area isoLatitude Pixelization of a sphere"
+  homepage "https://healpix.jpl.nasa.gov"
+  url "https://downloads.sourceforge.net/project/healpix/Healpix_3.81/Healpix_3.81_2022Jan25.tar.gz"
+  version "3.81"
+  sha256 "82d92bb21626371f9d280e59e82ad0f47d9ae62c70d86ccd1026e0310f193551"
+  license "GPL-2.0-or-later"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/healpix"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "27292cde3bc2ef5135dfa802bbe0637167b6193138382062060ef7172cc7ac2c"
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "cfitsio"
+
+  def install
+    configure_args = %w[
+      --disable-dependency-tracking
+      --disable-silent-rules
+    ]
+
+    cd "src/C/autotools" do
+      system "autoreconf", "--install"
+      system "./configure", "--prefix=#{prefix}", *configure_args
+      system "make", "install"
+    end
+
+    cd "src/common_libraries/libsharp" do
+      system "./configure", "--prefix=#{prefix}", *configure_args
+      system "make", "install"
+    end
+
+    cd "src/cxx" do
+      ENV["SHARP_CFLAGS"] = "-I#{include}"
+      ENV["SHARP_LIBS"] = "-L#{lib} -lsharp"
+      system "./configure", "--prefix=#{prefix}", *configure_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cxx").write <<-EOS
+      #include <math.h>
+      #include <stdio.h>
+      #include "chealpix.h"
+      int main(void) {
+        long nside, npix, pp, ns1;
+        nside = 1024;
+        for (pp = 0; pp < 14; pp++) {
+          nside = pow(2, pp);
+          npix = nside2npix(nside);
+          ns1  = npix2nside(npix);
+        }
+      };
+    EOS
+
+    system ENV.cxx, "-o", "test", "test.cxx", "-L#{lib}", "-lchealpix"
+    system "./test"
+  end
+end
