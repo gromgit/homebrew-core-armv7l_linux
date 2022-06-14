baf8d7a3d2ce06c3a02c06075b6ae5f313766c6e
Adrian Ho <the.gromgit@gmail.com>
2022-06-15 06:36:39 +0800

    libgpg-error: update 1.45 bottle.

---
+++ Formula/libgpg-error.rb
@@ -0,0 +1,32 @@
+class LibgpgError < Formula
+  desc "Common error values for all GnuPG components"
+  homepage "https://www.gnupg.org/related_software/libgpg-error/"
+  url "https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.45.tar.bz2"
+  sha256 "570f8ee4fb4bff7b7495cff920c275002aea2147e9a1d220c068213267f80a26"
+  license "LGPL-2.1-or-later"
+
+  livecheck do
+    url "https://gnupg.org/ftp/gcrypt/libgpg-error/"
+    regex(/href=.*?libgpg-error[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/libgpg-error"
+    sha256 armv7l_linux: "6cf5a41751a8ae6f175202465142ed79f6c9ef2849e5846f39ae925aa252f81b"
+  end
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}",
+                          "--enable-static"
+    system "make", "install"
+
+    # avoid triggering mandatory rebuilds of software that hard-codes this path
+    inreplace bin/"gpg-error-config", prefix, opt_prefix
+  end
+
+  test do
+    system "#{bin}/gpg-error-config", "--libs"
+  end
+end
