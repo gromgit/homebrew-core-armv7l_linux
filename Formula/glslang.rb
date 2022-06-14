70057420e24d9ff8b320ab782026a6b43aaa1a75
Adrian Ho <the.gromgit@gmail.com>
2022-06-14 10:27:01 +0800

    glslang: update 11.10.0 bottle.

---
+++ Formula/glslang.rb
@@ -0,0 +1,48 @@
+class Glslang < Formula
+  desc "OpenGL and OpenGL ES reference compiler for shading languages"
+  homepage "https://www.khronos.org/opengles/sdk/tools/Reference-Compiler/"
+  url "https://github.com/KhronosGroup/glslang/archive/11.10.0.tar.gz"
+  sha256 "8ffc19c435232d09299dd2c91e247292b3508c1b826a3497c60682e4bbf2d602"
+  license all_of: ["BSD-3-Clause", "GPL-3.0-or-later", "MIT", "Apache-2.0"]
+  head "https://github.com/KhronosGroup/glslang.git"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/glslang"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "ed037deed1009044dd5437ea335243480eef7ac397b3bb379c4be766dd14604d"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "python@3.10" => :build
+
+  def install
+    args = %w[
+      -DBUILD_EXTERNAL=OFF
+      -DENABLE_CTEST=OFF
+    ]
+
+    system "cmake", ".", *std_cmake_args, *args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.frag").write <<~EOS
+      #version 110
+      void main() {
+        gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+      }
+    EOS
+    (testpath/"test.vert").write <<~EOS
+      #version 110
+      void main() {
+          gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+      }
+    EOS
+    system "#{bin}/glslangValidator", "-i", testpath/"test.vert", testpath/"test.frag"
+  end
+end
