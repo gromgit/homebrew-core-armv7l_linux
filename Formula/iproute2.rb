4c506a3c3581ae50fe710cf436c4a6879f35fd20
Adrian Ho <the.gromgit@gmail.com>
2022-06-14 21:46:24 +0800

    iproute2: update 5.18.0 bottle.

---
+++ Formula/iproute2.rb
@@ -0,0 +1,40 @@
+class Iproute2 < Formula
+  desc "Linux routing utilities"
+  homepage "https://wiki.linuxfoundation.org/networking/iproute2"
+  url "https://mirrors.edge.kernel.org/pub/linux/utils/net/iproute2/iproute2-5.18.0.tar.xz"
+  sha256 "5ba3d464d51c8c283550d507ffac3d10f7aec587b7c66b0ccb6950643646389e"
+  license "GPL-2.0-only"
+  head "https://git.kernel.org/pub/scm/network/iproute2/iproute2.git", branch: "main"
+
+  livecheck do
+    url "https://mirrors.edge.kernel.org/pub/linux/utils/net/iproute2/"
+    regex(/href=.*?iproute2[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/iproute2"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "69f8f55fb172aaa8cd0255375694f02956f64a21b366e4925810fb1c5dcaa60c"
+  end
+
+  depends_on "bison" => :build
+  depends_on "flex" => :build
+  depends_on :linux
+
+  def install
+    system "make"
+    system "make", "install",
+           "PREFIX=#{prefix}",
+           "SBINDIR=#{sbin}",
+           "CONFDIR=#{etc}/iproute2",
+           "NETNS_RUN_DIR=#{var}/run/netns",
+           "NETNS_ETC_DIR=#{etc}/netns",
+           "ARPDDIR=#{var}/lib/arpd",
+           "KERNEL_INCLUDE=#{include}",
+           "DBM_INCLUDE=#{include}"
+  end
+
+  test do
+    output = shell_output("#{sbin}/ip addr").strip
+    assert_match "link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00", output
+  end
+end
