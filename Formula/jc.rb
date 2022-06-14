f08a95c69cdeacf5fdd093b43aaec73f4ecd64c5
Adrian Ho <the.gromgit@gmail.com>
2022-06-14 23:52:53 +0800

    jc: update 1.20.0 bottle.

---
+++ Formula/jc.rb
@@ -0,0 +1,46 @@
+class Jc < Formula
+  include Language::Python::Virtualenv
+
+  desc "Serializes the output of command-line tools to structured JSON output"
+  homepage "https://github.com/kellyjonbrazil/jc"
+  url "https://files.pythonhosted.org/packages/cf/b9/2961fd37643c73029d3be84e3e92e14d39d6d224f1405ed866b302ecb5ee/jc-1.20.0.tar.gz"
+  sha256 "2ed37b60ce1ca35253739ac90e67be034a60d2c3f2f845726b040ec465cc90f4"
+  license "MIT"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/jc"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "31a5a95a5ef711665ad6424afb6ef498433178eefa787191547e2cdb93cfd481"
+  end
+
+  depends_on "python@3.10"
+
+  resource "Pygments" do
+    url "https://files.pythonhosted.org/packages/59/0f/eb10576eb73b5857bc22610cdfc59e424ced4004fe7132c8f2af2cc168d3/Pygments-2.12.0.tar.gz"
+    sha256 "5eb116118f9612ff1ee89ac96437bb6b49e8f04d8a13b514ba26f620208e26eb"
+  end
+
+  resource "ruamel.yaml" do
+    url "https://files.pythonhosted.org/packages/46/a9/6ed24832095b692a8cecc323230ce2ec3480015fbfa4b79941bd41b23a3c/ruamel.yaml-0.17.21.tar.gz"
+    sha256 "8b7ce697a2f212752a35c1ac414471dc16c424c9573be4926b56ff3f5d23b7af"
+  end
+
+  resource "ruamel.yaml.clib" do
+    url "https://files.pythonhosted.org/packages/8b/25/08e5ad2431a028d0723ca5540b3af6a32f58f25e83c6dda4d0fcef7288a3/ruamel.yaml.clib-0.2.6.tar.gz"
+    sha256 "4ff604ce439abb20794f05613c374759ce10e3595d1867764dd1ae675b85acbd"
+  end
+
+  resource "xmltodict" do
+    url "https://files.pythonhosted.org/packages/39/0d/40df5be1e684bbaecdb9d1e0e40d5d482465de6b00cbb92b84ee5d243c7f/xmltodict-0.13.0.tar.gz"
+    sha256 "341595a488e3e01a85a9d8911d8912fd922ede5fecc4dce437eb4b6c8d037e56"
+  end
+
+  def install
+    virtualenv_install_with_resources
+    man1.install "man/jc.1"
+  end
+
+  test do
+    assert_equal "[{\"header1\":\"data1\",\"header2\":\"data2\"}]\n", \
+                  pipe_output("#{bin}/jc --csv", "header1, header2\n data1, data2")
+  end
+end
