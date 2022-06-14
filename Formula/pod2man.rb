0b78ec149c9ab18b416c5d59301c348aa5c634ed
Adrian Ho <the.gromgit@gmail.com>
2022-06-14 20:10:55 +0800

    pod2man: update 4.14_3 bottle.

---
+++ Formula/pod2man.rb
@@ -0,0 +1,46 @@
+class Pod2man < Formula
+  desc "Perl documentation generator"
+  homepage "https://www.eyrie.org/~eagle/software/podlators/"
+  url "https://archives.eyrie.org/software/perl/podlators-4.14.tar.xz"
+  sha256 "e504c3d9772b538d7ea31ce2c5e7a562d64a5b7f7c26277b1d7a0de1f6acfdf4"
+  revision 3
+
+  livecheck do
+    url "https://archives.eyrie.org/software/perl/"
+    regex(/href=.*?podlators[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/pod2man"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "cd859b5271789a78b0bcde4178e92f29d03981b7fee3abf9b8334631b448e251"
+  end
+
+  keg_only "perl ships with pod2man"
+
+  resource "Pod::Simple" do
+    url "https://cpan.metacpan.org/authors/id/K/KH/KHW/Pod-Simple-3.42.tar.gz"
+    sha256 "a9fceb2e0318e3786525e6bf205e3e143f0cf3622740819cab5f058e657e8ac5"
+  end
+
+  def install
+    resource("Pod::Simple").stage do
+      system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}"
+      system "make"
+      system "make", "install"
+    end
+    ENV.prepend_path "PERL5LIB", libexec/"lib/perl5"
+
+    system "perl", "Makefile.PL", "INSTALL_BASE=#{prefix}",
+                   "INSTALLSITEMAN1DIR=#{man1}", "INSTALLSITEMAN3DIR=#{man3}"
+    system "make"
+    system "make", "install"
+    bin.env_script_all_files libexec/"bin", PERL5LIB: "#{lib}/perl5:#{libexec}/lib/perl5"
+  end
+
+  test do
+    (testpath/"test.pod").write "=head2 Test heading\n"
+    manpage = shell_output("#{bin}/pod2man #{testpath}/test.pod")
+    assert_match '.SS "Test heading"', manpage
+    assert_match "Pod::Man #{version}", manpage
+  end
+end
