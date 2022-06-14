a7585ffe7393cf49b773a97eb4f6396b12e1ef94
Adrian Ho <the.gromgit@gmail.com>
2022-06-15 01:34:00 +0800

    jsonnet: update 0.18.0 bottle.

---
+++ Formula/jsonnet.rb
@@ -0,0 +1,53 @@
+class Jsonnet < Formula
+  desc "Domain specific configuration language for defining JSON data"
+  homepage "https://jsonnet.org/"
+  url "https://github.com/google/jsonnet/archive/v0.18.0.tar.gz"
+  sha256 "85c240c4740f0c788c4d49f9c9c0942f5a2d1c2ae58b2c71068107bc80a3ced4"
+  license "Apache-2.0"
+  head "https://github.com/google/jsonnet.git", branch: "master"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/jsonnet"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "f7250df628cdcef1aec7abb75404b3c214ef43f2bdfd2c709b72e219d562a714"
+  end
+
+  conflicts_with "go-jsonnet", because: "both install binaries with the same name"
+
+  def install
+    ENV.cxx11
+    system "make"
+    bin.install "jsonnet"
+    bin.install "jsonnetfmt"
+  end
+
+  test do
+    (testpath/"example.jsonnet").write <<~EOS
+      {
+        person1: {
+          name: "Alice",
+          welcome: "Hello " + self.name + "!",
+        },
+        person2: self.person1 { name: "Bob" },
+      }
+    EOS
+
+    expected_output = {
+      "person1" => {
+        "name"    => "Alice",
+        "welcome" => "Hello Alice!",
+      },
+      "person2" => {
+        "name"    => "Bob",
+        "welcome" => "Hello Bob!",
+      },
+    }
+
+    output = shell_output("#{bin}/jsonnet #{testpath}/example.jsonnet")
+    assert_equal expected_output, JSON.parse(output)
+  end
+end
