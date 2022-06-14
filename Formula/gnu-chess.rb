bafa848abe63be6347027798bb1da329f396a6ce
Adrian Ho <the.gromgit@gmail.com>
2022-06-14 10:45:09 +0800

    gnu-chess: update 6.2.9 bottle.

---
+++ Formula/gnu-chess.rb
@@ -0,0 +1,63 @@
+class GnuChess < Formula
+  desc "Chess-playing program"
+  homepage "https://www.gnu.org/software/chess/"
+  url "https://ftp.gnu.org/gnu/chess/gnuchess-6.2.9.tar.gz"
+  mirror "https://ftpmirror.gnu.org/chess/gnuchess-6.2.9.tar.gz"
+  sha256 "ddfcc20bdd756900a9ab6c42c7daf90a2893bf7f19ce347420ce36baebc41890"
+  license "GPL-3.0-or-later"
+
+  livecheck do
+    url :stable
+    regex(/href=.*?gnuchess[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/gnu-chess"
+    sha256 armv7l_linux: "7f925524e7cacf9820807b8628e451ff54afb33787319f098b5ce7f717aece26"
+  end
+
+  head do
+    url "https://svn.savannah.gnu.org/svn/chess/trunk"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "help2man" => :build
+    depends_on "gettext"
+  end
+
+  depends_on "readline"
+
+  resource "book" do
+    url "https://ftp.gnu.org/gnu/chess/book_1.02.pgn.gz"
+    sha256 "deac77edb061a59249a19deb03da349cae051e52527a6cb5af808d9398d32d44"
+  end
+
+  def install
+    #  Fix "install-sh: Permission denied" issue
+    chmod "+x", "install-sh"
+
+    if build.head?
+      system "autoreconf", "--install"
+      chmod 0755, "install-sh"
+    end
+
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+
+    resource("book").stage do
+      doc.install "book_1.02.pgn"
+    end
+  end
+
+  def caveats
+    <<~EOS
+      This formula also downloads the additional opening book.  The
+      opening book is a PGN file located in #{doc} that can be added
+      using gnuchess commands.
+    EOS
+  end
+
+  test do
+    assert_equal "GNU Chess #{version}", shell_output("#{bin}/gnuchess --version").chomp
+  end
+end
