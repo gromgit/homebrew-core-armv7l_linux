752ba6c2de7e7ffbc510c7f94f45eb9f77b18f00
Adrian Ho <the.gromgit@gmail.com>
2022-06-14 19:09:58 +0800

    icoutils: update 0.32.3 bottle.

---
+++ Formula/icoutils.rb
@@ -0,0 +1,30 @@
+class Icoutils < Formula
+  desc "Create and extract MS Windows icons and cursors"
+  homepage "https://www.nongnu.org/icoutils/"
+  url "https://savannah.nongnu.org/download/icoutils/icoutils-0.32.3.tar.bz2"
+  sha256 "17abe02d043a253b68b47e3af69c9fc755b895db68fdc8811786125df564c6e0"
+  license "GPL-3.0"
+
+  livecheck do
+    url "https://download.savannah.gnu.org/releases/icoutils/"
+    regex(/href=.*?icoutils[._-](\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/icoutils"
+    sha256 armv7l_linux: "fbbd4771d1ee77f775d85e657974f02d768aff232b4bb0627d32918600fbef60"
+  end
+
+  depends_on "libpng"
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-rpath",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system bin/"icotool", "-l", test_fixtures("test.ico")
+  end
+end
