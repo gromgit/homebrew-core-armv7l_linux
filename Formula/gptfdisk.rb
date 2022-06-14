38277b6b11899c67a9bbcc1f7d227f9d6af00202
Adrian Ho <the.gromgit@gmail.com>
2022-06-14 12:45:23 +0800

    gptfdisk: update 1.0.9 bottle.

---
+++ Formula/gptfdisk.rb
@@ -0,0 +1,50 @@
+class Gptfdisk < Formula
+  desc "Text-mode partitioning tools"
+  homepage "https://www.rodsbooks.com/gdisk/"
+  url "https://downloads.sourceforge.net/project/gptfdisk/gptfdisk/1.0.9/gptfdisk-1.0.9.tar.gz"
+  sha256 "dafead2693faeb8e8b97832b23407f6ed5b3219bc1784f482dd855774e2d50c2"
+  license "GPL-2.0-or-later"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/gptfdisk"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "c13544c7ed1ddf8f1b14e24e43fd245f434e9345c029403988120bccd4cd0a8a"
+  end
+
+  depends_on "popt"
+
+  uses_from_macos "ncurses"
+
+  on_linux do
+    depends_on "util-linux"
+  end
+
+  def install
+    if OS.mac?
+      inreplace "Makefile.mac" do |s|
+        s.gsub! "/usr/local/Cellar/ncurses/6.2/lib/libncurses.dylib", "-L/usr/lib -lncurses"
+        s.gsub! "-L/usr/local/lib $(LDLIBS) -lpopt", "-L#{Formula["popt"].opt_lib} $(LDLIBS) -lpopt"
+      end
+
+      system "make", "-f", "Makefile.mac"
+    else
+      %w[ncurses popt util-linux].each do |dep|
+        ENV.append_to_cflags "-I#{Formula[dep].opt_include}"
+        ENV.append "LDFLAGS", "-L#{Formula[dep].opt_lib}"
+      end
+
+      system "make", "-f", "Makefile"
+    end
+
+    %w[cgdisk fixparts gdisk sgdisk].each do |program|
+      bin.install program
+      man8.install "#{program}.8"
+    end
+  end
+
+  test do
+    system "dd", "if=/dev/zero", "of=test.dmg", "bs=1024k", "count=1"
+    assert_match "completed successfully", shell_output("#{bin}/sgdisk -o test.dmg")
+    assert_match "GUID", shell_output("#{bin}/sgdisk -p test.dmg")
+    assert_match "Found valid GPT with protective MBR", shell_output("#{bin}/gdisk -l test.dmg")
+  end
+end
