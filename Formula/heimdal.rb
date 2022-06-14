a3b3fe80dcb92aa9a351dadf13f096ec9a0e9773
Adrian Ho <the.gromgit@gmail.com>
2022-06-14 15:36:40 +0800

    heimdal: update 7.7.0_3 bottle.

---
+++ Formula/heimdal.rb
@@ -0,0 +1,80 @@
+class Heimdal < Formula
+  desc "Free Kerberos 5 implementation"
+  homepage "https://www.h5l.org"
+  url "https://github.com/heimdal/heimdal/releases/download/heimdal-7.7.0/heimdal-7.7.0.tar.gz"
+  sha256 "f02d3314d634cc55eb9cf04a1eae0d96b293e45a1f837de9d894e800161b7d1b"
+  license "BSD-3-Clause"
+  revision 3
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+    regex(%r{href=.*?/tag/heimdal[._-]v?(\d+(?:\.\d+)+)["' >]}i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/heimdal"
+    sha256 armv7l_linux: "1a913fee01d8a8021c449f6d5361ea00a1936d21e979f3126e96d75aaf198f4d"
+  end
+
+  keg_only "conflicts with Kerberos"
+
+  depends_on "bison" => :build
+  depends_on "berkeley-db"
+  depends_on "flex"
+  depends_on "lmdb"
+  depends_on "openldap"
+  depends_on "openssl@1.1"
+
+  uses_from_macos "libxcrypt"
+  uses_from_macos "perl"
+
+  resource "JSON" do
+    url "https://cpan.metacpan.org/authors/id/I/IS/ISHIGAKI/JSON-4.02.tar.gz"
+    sha256 "444a88755a89ffa2a5424ab4ed1d11dca61808ebef57e81243424619a9e8627c"
+  end
+
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
+    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
+  end
+
+  def install
+    ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
+
+    resource("JSON").stage do
+      system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}"
+      system "make"
+      system "make", "install"
+    end
+
+    ENV.append "LDFLAGS", "-L#{Formula["berkeley-db"].opt_lib}"
+    ENV.append "LDFLAGS", "-L#{Formula["lmdb"].opt_lib}"
+    ENV.append "CFLAGS", "-I#{Formula["lmdb"].opt_include}"
+
+    args = %W[
+      --disable-debug
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --without-x
+      --enable-static=no
+      --enable-pthread-support
+      --disable-afs-support
+      --disable-ndbm-db
+      --disable-heimdal-documentation
+      --with-openldap=#{Formula["openldap"].opt_prefix}
+      --with-openssl=#{Formula["openssl@1.1"].opt_prefix}
+      --with-hcrypto-default-backend=ossl
+      --with-berkeley-db
+      --with-berkeley-db-include=#{Formula["berkeley-db"].opt_include}
+    ]
+
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    assert_match "-L#{lib}", shell_output("#{bin}/krb5-config --libs")
+  end
+end
