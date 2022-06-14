1d2611b8c3478d7ff82e1dd123fc9315f952635b
Adrian Ho <the.gromgit@gmail.com>
2022-06-14 11:17:09 +0800

    gnu-units: update 2.21 bottle.

---
+++ Formula/gnu-units.rb
@@ -0,0 +1,53 @@
+class GnuUnits < Formula
+  desc "GNU unit conversion tool"
+  homepage "https://www.gnu.org/software/units/"
+  url "https://ftp.gnu.org/gnu/units/units-2.21.tar.gz"
+  mirror "https://ftpmirror.gnu.org/units/units-2.21.tar.gz"
+  sha256 "6c3e80a9f980589fd962a5852a2674642257db1c5fd5b27c4d9e664f3486cbaf"
+  license "GPL-3.0-or-later"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/gnu-units"
+    sha256 armv7l_linux: "b80a3092d5f925523bfcd4cc4ad74f0ebe84d61d3d2a4083ec7452c83c385bb2"
+  end
+
+  depends_on "readline"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --with-installed-readline
+    ]
+
+    args << "--program-prefix=g" if OS.mac?
+    system "./configure", *args
+    system "make", "install"
+
+    if OS.mac?
+      (libexec/"gnubin").install_symlink bin/"gunits" => "units"
+      (libexec/"gnubin").install_symlink bin/"gunits_cur" => "units_cur"
+      (libexec/"gnuman/man1").install_symlink man1/"gunits.1" => "units.1"
+    end
+    libexec.install_symlink "gnuman" => "man"
+  end
+
+  def caveats
+    on_macos do
+      <<~EOS
+        All commands have been installed with the prefix "g".
+        If you need to use these commands with their normal names, you
+        can add a "gnubin" directory to your PATH from your bashrc like:
+          PATH="#{opt_libexec}/gnubin:$PATH"
+      EOS
+    end
+  end
+
+  test do
+    if OS.mac?
+      assert_equal "* 18288", shell_output("#{bin}/gunits '600 feet' 'cm' -1").strip
+      assert_equal "* 18288", shell_output("#{opt_libexec}/gnubin/units '600 feet' 'cm' -1").strip
+    else
+      assert_equal "* 18288", shell_output("#{bin}/units '600 feet' 'cm' -1").strip
+    end
+  end
+end
