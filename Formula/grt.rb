a2ffa9b2057b77106b68c6df6970041ff34e2aff
Adrian Ho <the.gromgit@gmail.com>
2022-06-14 13:34:24 +0800

    grt: update 0.2.4 bottle.

---
+++ Formula/grt.rb
@@ -0,0 +1,34 @@
+class Grt < Formula
+  desc "Gesture Recognition Toolkit for real-time machine learning"
+  homepage "https://nickgillian.com/grt/"
+  url "https://github.com/nickgillian/grt/archive/v0.2.4.tar.gz"
+  sha256 "55bcabe7a58916461dc4341758eff2a45bd5b236c263dfe6e58c176c1a7e1ac4"
+  license "MIT"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/grt"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "407cfce1e64c0155c36c37f690e1ae54e5f2d24199ce728cbd7dd3ee4b83c6d7"
+  end
+
+  depends_on "cmake" => :build
+
+  def install
+    cd "build"
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <GRT/GRT.h>
+      int main() {
+        GRT::GestureRecognitionPipeline pipeline;
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "test.cpp", "-std=c++11", "-I#{include}", "-L#{lib}", "-lgrt", "-o", "test"
+    system "./test"
+  end
+end
