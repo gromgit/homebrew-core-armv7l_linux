9ff4f1150977a4c6ad8ed55dcad05753f9972ec3
Adrian Ho <the.gromgit@gmail.com>
2022-06-14 22:14:01 +0800

    ircii: update 20210314 bottle.

---
+++ Formula/ircii.rb
@@ -0,0 +1,47 @@
+class Ircii < Formula
+  desc "IRC and ICB client"
+  homepage "http://www.eterna.com.au/ircii/"
+  url "https://ircii.warped.com/ircii-20210314.tar.bz2"
+  mirror "https://deb.debian.org/debian/pool/main/i/ircii/ircii_20210314.orig.tar.bz2"
+  sha256 "866f2b847daed3d70859f208f7cb0f20b58c0933b2159f7ff92a68c518d393a9"
+  license all_of: [
+    "BSD-3-Clause",
+    "BSD-2-Clause",
+    "GPL-2.0-or-later",
+    "MIT",
+    :public_domain,
+  ]
+
+  livecheck do
+    url "https://ircii.warped.com/"
+    regex(/href=.*?ircii[._-]v?(\d{6,8})\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/ircii"
+    sha256 armv7l_linux: "5e64f809fd1251cfbe801d6541eee447965e76935d390598d9c281c6bb293d0d"
+  end
+
+  depends_on "openssl@1.1"
+
+  uses_from_macos "ncurses"
+
+  def install
+    ENV.append "LIBS", "-liconv" if OS.mac?
+    system "./configure", "--prefix=#{prefix}",
+                          "--with-default-server=irc.libera.chat",
+                          "--enable-ipv6"
+    system "make"
+    ENV.deparallelize
+    system "make", "install"
+  end
+
+  test do
+    IO.popen("#{bin}/irc -d", "r+") do |pipe|
+      assert_match "Connecting to port 6667 of server irc.libera.chat", pipe.gets
+      pipe.puts "/quit"
+      pipe.close_write
+      pipe.close
+    end
+  end
+end
