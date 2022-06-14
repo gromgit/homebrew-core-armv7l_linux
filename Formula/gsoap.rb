e76f260f8f7ff0409123421981df94d8880b1984
Adrian Ho <the.gromgit@gmail.com>
2022-06-14 13:46:17 +0800

    gsoap: update 2.8.122 bottle.

---
+++ Formula/gsoap.rb
@@ -0,0 +1,36 @@
+class Gsoap < Formula
+  desc "SOAP stub and skeleton compiler for C and C++"
+  homepage "https://www.genivia.com/products.html"
+  url "https://downloads.sourceforge.net/project/gsoap2/gsoap_2.8.122.zip"
+  sha256 "3eb8486c986b44071bb5f19ef7b990791f819bb267c0bf640e95bd991070fad8"
+  license any_of: ["GPL-2.0-or-later", "gSOAP-1.3b"]
+
+  livecheck do
+    url :stable
+    regex(%r{url=.*?/gsoap[._-]v?(\d+(?:\.\d+)+)\.zip}i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/gsoap"
+    sha256 armv7l_linux: "07cb69c29716db39ca0774c3fdce95cd460e14c332ce80550d4c2b2585fa9338"
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "openssl@1.1"
+
+  uses_from_macos "bison"
+  uses_from_macos "flex"
+  uses_from_macos "zlib"
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/wsdl2h", "-o", "calc.h", "https://www.genivia.com/calc.wsdl"
+    system "#{bin}/soapcpp2", "calc.h"
+    assert_predicate testpath/"calc.add.req.xml", :exist?
+  end
+end
