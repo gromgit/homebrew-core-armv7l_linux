3ed93e4223a127da794fd03b22fb412c7eff9400
Adrian Ho <the.gromgit@gmail.com>
2022-06-14 18:00:32 +0800

    htpdate: update 1.3.4 bottle.

---
+++ Formula/htpdate.rb
@@ -0,0 +1,29 @@
+class Htpdate < Formula
+  desc "Synchronize time with remote web servers"
+  homepage "https://www.vervest.org/htp/"
+  url "https://www.vervest.org/htp/archive/c/htpdate-1.3.4.tar.gz"
+  sha256 "744f9200cfd3b008a5516c5eb6da727af532255a329126a7b8f49a5623985642"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url "https://www.vervest.org/htp/?download"
+    regex(/href=.*?htpdate[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/htpdate"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "c92c2824574d48589165915e64fdfec0e8cef97bf4ca1cb1be83723d2f154f30"
+  end
+
+  def install
+    system "make", "prefix=#{prefix}",
+                   "STRIP=/usr/bin/strip",
+                   "CC=#{ENV.cc}",
+                   "CFLAGS=#{ENV.cflags}",
+                   "install"
+  end
+
+  test do
+    system "#{sbin}/htpdate", "-q", "-d", "-u", ENV["USER"], "example.org"
+  end
+end
