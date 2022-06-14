a100105b566637013276359faf48c9a03ce964e0
Adrian Ho <the.gromgit@gmail.com>
2022-06-14 14:29:52 +0800

    haruhi-dl: update 2021.8.1_1 bottle.

---
+++ Formula/haruhi-dl.rb
@@ -0,0 +1,35 @@
+class HaruhiDl < Formula
+  include Language::Python::Virtualenv
+
+  desc "Fork of youtube-dl, focused on bringing a fast, steady stream of updates"
+  homepage "https://git.sakamoto.pl/laudom/haruhi-dl"
+  url "https://files.pythonhosted.org/packages/24/f2/a2d22274cfa8f09c849495e8a5106cf72365091b58d55a45c2c91d9f79b9/haruhi_dl-2021.8.1.tar.gz"
+  sha256 "069dc4a5f82f98861a291c7edd8bb1ca01eb74602dd36220343a75cb7bb617a8"
+  license "LGPL-3.0-or-later"
+  revision 1
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/haruhi-dl"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "fa0baa4ea6d37673509ce85e18feef4341662ad0e60dc3316ba45d0b94058e65"
+  end
+
+  deprecate! date: "2022-01-15", because: :deprecated_upstream
+
+  depends_on "python@3.10"
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    # History of homebrew-core (video)
+    haruhi_output = shell_output("#{bin}/haruhi-dl --simulate https://www.youtube.com/watch?v=pOtd1cbOP7k")
+
+    expected_output = <<~EOS
+      [youtube] pOtd1cbOP7k: Downloading webpage
+      [youtube] pOtd1cbOP7k: Downloading MPD manifest
+    EOS
+
+    assert_equal expected_output, haruhi_output
+  end
+end
