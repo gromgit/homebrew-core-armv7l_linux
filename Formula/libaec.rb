06dff7e59463cd474ec9bd640195ba4fabded8d4
Adrian Ho <the.gromgit@gmail.com>
2022-06-15 06:25:20 +0800

    libaec: update 1.0.6 bottle.

---
+++ Formula/libaec.rb
@@ -0,0 +1,65 @@
+class Libaec < Formula
+  desc "Adaptive Entropy Coding implementing Golomb-Rice algorithm"
+  homepage "https://gitlab.dkrz.de/k202009/libaec"
+  url "https://gitlab.dkrz.de/k202009/libaec/-/archive/v1.0.6/libaec-v1.0.6.tar.bz2"
+  sha256 "31fb65b31e835e1a0f3b682d64920957b6e4407ee5bbf42ca49549438795a288"
+  license "BSD-2-Clause"
+  head "https://gitlab.dkrz.de/k202009/libaec.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/libaec"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "a33f1f54264dae3bf3290326039653c8efaa49206d1d1caf99b2428bad332caf"
+  end
+
+  depends_on "cmake" => :build
+
+  # These may have been linked by `szip` before keg_only change
+  link_overwrite "include/szlib.h"
+  link_overwrite "lib/libsz.a"
+  link_overwrite "lib/libsz.dylib"
+  link_overwrite "lib/libsz.2.dylib"
+  link_overwrite "lib/libsz.so"
+  link_overwrite "lib/libsz.so.2"
+
+  def install
+    mkdir "build" do
+      # We run `make test` for libraries
+      system "cmake", "..", *std_cmake_args, "-DBUILD_TESTING=ON"
+      system "make", "install"
+      system "make", "test"
+    end
+  end
+
+  test do
+    system bin/"aec", "-v"
+    (testpath/"test.cpp").write <<~EOS
+      #include <cassert>
+      #include <cstddef>
+      #include <cstdlib>
+      #include <libaec.h>
+      int main() {
+        unsigned char * data = (unsigned char *) calloc(1024, sizeof(unsigned char));
+        unsigned char * compressed = (unsigned char *) calloc(1024, sizeof(unsigned char));
+        for(int i = 0; i < 1024; i++) { data[i] = (unsigned char)(i); }
+        struct aec_stream strm;
+        strm.bits_per_sample = 16;
+        strm.block_size      = 64;
+        strm.rsi             = 129;
+        strm.flags           = AEC_DATA_PREPROCESS | AEC_DATA_MSB;
+        strm.next_in         = data;
+        strm.avail_in        = 1024;
+        strm.next_out        = compressed;
+        strm.avail_out       = 1024;
+        assert(aec_encode_init(&strm) == 0);
+        assert(aec_encode(&strm, AEC_FLUSH) == 0);
+        assert(strm.total_out > 0);
+        assert(aec_encode_end(&strm) == 0);
+        free(data);
+        free(compressed);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.cpp", "-I#{include}", "-L#{lib}", "-laec", "-o", "test"
+    system "./test"
+  end
+end
