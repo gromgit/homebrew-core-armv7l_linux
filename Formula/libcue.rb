fcea8a7ae5751a97328cce5069b8ba39c4257638
Adrian Ho <the.gromgit@gmail.com>
2022-06-15 07:35:49 +0800

    libcue: update 2.2.1 bottle.

---
+++ Formula/libcue.rb
@@ -0,0 +1,32 @@
+class Libcue < Formula
+  desc "Cue sheet parser library for C"
+  homepage "https://github.com/lipnitsk/libcue"
+  url "https://github.com/lipnitsk/libcue/archive/v2.2.1.tar.gz"
+  sha256 "f27bc3ebb2e892cd9d32a7bee6d84576a60f955f29f748b9b487b173712f1200"
+  license "GPL-2.0"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/libcue"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "48791bcd237e23f2b642bc5b4e1e04f9f126439f5d9445eba780767059d46eb7"
+  end
+
+  depends_on "cmake" => :build
+
+  uses_from_macos "bison" => :build
+  uses_from_macos "flex" => :build
+
+  def install
+    system "cmake", ".", "-DBUILD_SHARED_LIBS=ON", *std_cmake_args
+    system "make", "install"
+    (pkgshare/"tests").install Dir["t/*"]
+  end
+
+  test do
+    cp_r (pkgshare/"tests").children, testpath
+    Dir["*.c"].each do |f|
+      system ENV.cc, f, "-o", "test", "-L#{lib}", "-lcue", "-I#{include}"
+      system "./test"
+      rm "test"
+    end
+  end
+end
