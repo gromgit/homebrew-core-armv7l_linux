6a4f20b0551414376135c8e135b057c978b7ffd5
Adrian Ho <the.gromgit@gmail.com>
2022-06-14 11:20:50 +0800

    gnu-which: update 2.21 bottle.

---
+++ Formula/gnu-which.rb
@@ -0,0 +1,53 @@
+class GnuWhich < Formula
+  desc "GNU implementation of which utility"
+  # Previous homepage is dead. Have linked to the GNU Projects page for now.
+  homepage "https://savannah.gnu.org/projects/which/"
+  url "https://ftp.gnu.org/gnu/which/which-2.21.tar.gz"
+  mirror "https://ftpmirror.gnu.org/which/which-2.21.tar.gz"
+  sha256 "f4a245b94124b377d8b49646bf421f9155d36aa7614b6ebf83705d3ffc76eaad"
+  license "GPL-3.0-or-later"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/gnu-which"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "943aa01632ec4f69958741b29547fa04b557e0c0e30e52292e6b341a26de71ad"
+  end
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --disable-dependency-tracking
+    ]
+
+    args << "--program-prefix=g" if OS.mac?
+    system "./configure", *args
+    system "make", "install"
+
+    if OS.mac?
+      (libexec/"gnubin").install_symlink bin/"gwhich" => "which"
+      (libexec/"gnuman/man1").install_symlink man1/"gwhich.1" => "which.1"
+    end
+
+    libexec.install_symlink "gnuman" => "man"
+  end
+
+  def caveats
+    on_macos do
+      <<~EOS
+        GNU "which" has been installed as "gwhich".
+        If you need to use it as "which", you can add a "gnubin" directory
+        to your PATH from your bashrc like:
+
+            PATH="#{opt_libexec}/gnubin:$PATH"
+      EOS
+    end
+  end
+
+  test do
+    if OS.mac?
+      system "#{bin}/gwhich", "gcc"
+      system "#{opt_libexec}/gnubin/which", "gcc"
+    else
+      system "#{bin}/which", "gcc"
+    end
+  end
+end
