d7afe495bef23f95fa3af28a74bd0e14c93b6d23
Adrian Ho <the.gromgit@gmail.com>
2022-06-14 19:22:19 +0800

    id3tool: update 1.2a bottle.

---
+++ Formula/id3tool.rb
@@ -0,0 +1,34 @@
+class Id3tool < Formula
+  desc "ID3 editing tool"
+  homepage "http://nekohako.xware.cx/id3tool/"
+  url "http://nekohako.xware.cx/id3tool/id3tool-1.2a.tar.gz"
+  sha256 "7908d66c5aabe2a53ae8019e8234f4231485d80be4b2fe72c9d04013cff1caec"
+  license "BSD-3-Clause"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?id3tool[._-]v?(\d+(?:\.\d+)+[a-z]?)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/id3tool"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "94255fa1899fcf0001909c72170a5b1d85867dcf91bc3d94a58cebddd2a35e1f"
+  end
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--mandir=#{man}"
+    system "make", "install"
+  end
+
+  test do
+    mp3 = "#{testpath}/test.mp3"
+    cp test_fixtures("test.mp3"), mp3
+
+    system "#{bin}/id3tool", "-t", "Homebrew", mp3
+    assert_match(/Song Title:\s+Homebrew/,
+                 shell_output("#{bin}/id3tool #{mp3}").chomp)
+  end
+end
