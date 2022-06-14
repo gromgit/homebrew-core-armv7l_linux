fdf031e4f9d523ffc2ad8e55b6fc4d1c0be11d2f
Adrian Ho <the.gromgit@gmail.com>
2022-06-14 14:11:03 +0800

    gzrt: update 0.8 bottle.

---
+++ Formula/gzrt.rb
@@ -0,0 +1,53 @@
+class Gzrt < Formula
+  desc "Gzip recovery toolkit"
+  homepage "https://www.urbanophile.com/arenn/coding/gzrt/gzrt.html"
+  url "https://www.urbanophile.com/arenn/coding/gzrt/gzrt-0.8.tar.gz"
+  sha256 "b0b7dc53dadd8309ad9f43d6d6be7ac502c68ef854f1f9a15bd7f543e4571fee"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?gzrt[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/gzrt"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "341421dd3fc348d445980738ed8c4f7539aeed8bac4d7fce25a55b995911bc49"
+  end
+
+  uses_from_macos "zlib"
+
+  def install
+    system "make"
+    bin.install "gzrecover"
+    man1.install "gzrecover.1"
+  end
+
+  test do
+    filename = "data.txt"
+    fixed_filename = "#{filename}.recovered"
+    path = testpath/filename
+    fixed_path = testpath/fixed_filename
+
+    original_contents = "." * 1000
+    path.write original_contents
+
+    # Compress data into archive
+    gzip path
+    refute_predicate path, :exist?
+
+    # Corrupt the archive to test the recovery process
+    File.open("#{path}.gz", "r+b") do |file|
+      file.seek(11)
+      data = file.read(1).unpack1("C*")
+      data = ~data
+      file.write([data].pack("C*"))
+    end
+
+    # Verify that file corruption is detected and attempt to recover
+    system bin/"gzrecover", "-v", "#{path}.gz"
+
+    # Verify that recovered data is reasonably close - unlike lziprecover,
+    # this process is not perfect, even for small errors
+    assert_match original_contents, fixed_path.read
+  end
+end
