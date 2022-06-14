25dde563040bc35e7951dd17663b4cdff4bf9234
Adrian Ho <the.gromgit@gmail.com>
2022-06-14 14:33:24 +0800

    hashcash: update 1.22 bottle.

---
+++ Formula/hashcash.rb
@@ -0,0 +1,28 @@
+class Hashcash < Formula
+  desc "Proof-of-work algorithm to counter denial-of-service (DoS) attacks"
+  homepage "http://hashcash.org"
+  url "http://hashcash.org/source/hashcash-1.22.tgz"
+  sha256 "0192f12d41ce4848e60384398c5ff83579b55710601c7bffe6c88bc56b547896"
+
+  livecheck do
+    url "http://hashcash.org/source/"
+    regex(/href=.*?hashcash[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/hashcash"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "efba02b251f40580e97cdb2315c155ad790f95020de8c5b0c0301ce299017ab8"
+  end
+
+  def install
+    system "make", "install",
+                   "PACKAGER=HOMEBREW",
+                   "INSTALL_PATH=#{bin}",
+                   "MAN_INSTALL_PATH=#{man1}",
+                   "DOC_INSTALL_PATH=#{doc}"
+  end
+
+  test do
+    system "#{bin}/hashcash", "-mb10", "test@example.com"
+  end
+end
