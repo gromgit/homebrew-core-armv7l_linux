4132096485be5719e2789508c3257811b59e2120
Adrian Ho <the.gromgit@gmail.com>
2022-06-14 22:06:25 +0800

    ircd-hybrid: update 8.2.39 bottle.

---
+++ Formula/ircd-hybrid.rb
@@ -0,0 +1,55 @@
+class IrcdHybrid < Formula
+  desc "High-performance secure IRC server"
+  homepage "https://www.ircd-hybrid.org/"
+  url "https://downloads.sourceforge.net/project/ircd-hybrid/ircd-hybrid/ircd-hybrid-8.2.39/ircd-hybrid-8.2.39.tgz"
+  sha256 "035d271f6b0dd451157f80146d189bc1c9b84cc9ba1b7ad06fd72ee5108e6e4d"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url :stable
+    regex(%r{url=.*?/ircd-hybrid[._-]v?(\d+(?:\.\d+)+)\.t}i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/ircd-hybrid"
+    sha256 armv7l_linux: "f1e13750d7f98c9cef5f1008b8edab0898aab4d38668ff6afe6050277fd3a185"
+  end
+
+  depends_on "openssl@1.1"
+
+  conflicts_with "expect", because: "both install an `mkpasswd` binary"
+  conflicts_with "ircd-irc2", because: "both install an `ircd` binary"
+
+  # ircd-hybrid needs the .la files
+  skip_clean :la
+
+  def install
+    ENV.deparallelize # build system trips over itself
+
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--localstatedir=#{var}",
+                          "--sysconfdir=#{etc}",
+                          "--enable-openssl=#{Formula["openssl@1.1"].opt_prefix}"
+    system "make", "install"
+    etc.install "doc/reference.conf" => "ircd.conf"
+  end
+
+  def caveats
+    <<~EOS
+      You'll more than likely need to edit the default settings in the config file:
+        #{etc}/ircd.conf
+    EOS
+  end
+
+  service do
+    run opt_bin/"ircd"
+    keep_alive false
+    working_dir HOMEBREW_PREFIX
+    error_log_path var/"ircd.log"
+  end
+
+  test do
+    system "#{bin}/ircd", "-version"
+  end
+end
