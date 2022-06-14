de054da3b753b38af2772a0a6a569ba2ef72c1e5
Adrian Ho <the.gromgit@gmail.com>
2022-06-14 20:21:05 +0800

    imath: update 3.1.5 bottle.

---
+++ Formula/imath.rb
@@ -0,0 +1,45 @@
+class Imath < Formula
+  desc "Library of 2D and 3D vector, matrix, and math operations"
+  homepage "https://www.openexr.com/"
+  url "https://github.com/AcademySoftwareFoundation/Imath/archive/refs/tags/v3.1.5.tar.gz"
+  sha256 "1e9c7c94797cf7b7e61908aed1f80a331088cc7d8873318f70376e4aed5f25fb"
+  license "BSD-3-Clause"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/imath"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "3f8356b43dab8804ae403230edb76abb7577231910142c701fa8448cf28a40ae"
+  end
+
+  depends_on "cmake" => :build
+
+  # These used to be provided by `ilmbase`
+  link_overwrite "lib/libImath.dylib"
+  link_overwrite "lib/libImath.so"
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~'EOS'
+      #include <ImathRoots.h>
+      #include <algorithm>
+      #include <iostream>
+
+      int main(int argc, char *argv[])
+      {
+        double x[2] = {0.0, 0.0};
+        int n = IMATH_NAMESPACE::solveQuadratic(1.0, 3.0, 2.0, x);
+
+        if (x[0] > x[1])
+          std::swap(x[0], x[1]);
+
+        std::cout << n << ", " << x[0] << ", " << x[1] << "\n";
+      }
+    EOS
+    system ENV.cxx, "-std=c++11", "-I#{include}/Imath", "-o", testpath/"test", "test.cpp"
+    assert_equal "2, -2, -1\n", shell_output("./test")
+  end
+end
