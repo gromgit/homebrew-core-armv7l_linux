c9aa13465b71a1554c83d77f4039338308a66423
Adrian Ho <the.gromgit@gmail.com>
2022-06-14 10:58:18 +0800

    gnu-sed: update 4.8 bottle.

---
+++ Formula/gnu-sed.rb
@@ -0,0 +1,62 @@
+class GnuSed < Formula
+  desc "GNU implementation of the famous stream editor"
+  homepage "https://www.gnu.org/software/sed/"
+  url "https://ftp.gnu.org/gnu/sed/sed-4.8.tar.xz"
+  mirror "https://ftpmirror.gnu.org/sed/sed-4.8.tar.xz"
+  sha256 "f79b0cfea71b37a8eeec8490db6c5f7ae7719c35587f21edb0617f370eeff633"
+  license "GPL-3.0-or-later"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/gnu-sed"
+    sha256 armv7l_linux: "d160117ab430c228c8fd630357c65e2bb238e8353b897a6bb5da3f5532e930bc"
+  end
+
+  conflicts_with "ssed", because: "both install share/info/sed.info"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --disable-dependency-tracking
+    ]
+
+    args << if OS.mac?
+      "--program-prefix=g"
+    else
+      "--without-selinux"
+    end
+    system "./configure", *args
+    system "make", "install"
+
+    if OS.mac?
+      (libexec/"gnubin").install_symlink bin/"gsed" =>"sed"
+      (libexec/"gnuman/man1").install_symlink man1/"gsed.1" => "sed.1"
+    end
+
+    libexec.install_symlink "gnuman" => "man"
+  end
+
+  def caveats
+    on_macos do
+      <<~EOS
+        GNU "sed" has been installed as "gsed".
+        If you need to use it as "sed", you can add a "gnubin" directory
+        to your PATH from your bashrc like:
+
+            PATH="#{opt_libexec}/gnubin:$PATH"
+      EOS
+    end
+  end
+
+  test do
+    (testpath/"test.txt").write "Hello world!"
+    if OS.mac?
+      system "#{bin}/gsed", "-i", "s/world/World/g", "test.txt"
+      assert_match "Hello World!", File.read("test.txt")
+
+      system "#{opt_libexec}/gnubin/sed", "-i", "s/world/World/g", "test.txt"
+    else
+      system "#{bin}/sed", "-i", "s/world/World/g", "test.txt"
+    end
+    assert_match "Hello World!", File.read("test.txt")
+  end
+end
