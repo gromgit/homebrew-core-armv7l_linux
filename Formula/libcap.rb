848cd1b5e3e504f06bbd56a4ca7a1addeec71009
Adrian Ho <the.gromgit@gmail.com>
2022-06-15 07:03:33 +0800

    libcap: update 2.64 bottle.

---
+++ Formula/libcap.rb
@@ -0,0 +1,27 @@
+class Libcap < Formula
+  desc "User-space interfaces to POSIX 1003.1e capabilities"
+  homepage "https://sites.google.com/site/fullycapable/"
+  url "https://mirrors.edge.kernel.org/pub/linux/libs/security/linux-privs/libcap2/libcap-2.64.tar.xz"
+  sha256 "c8465e1f0b068d5fc06199231135ccac7adb56d662b1de93589252e8cd071e13"
+  license all_of: ["BSD-3-Clause", "GPL-2.0-or-later"]
+
+  livecheck do
+    url "https://mirrors.edge.kernel.org/pub/linux/libs/security/linux-privs/libcap2/"
+    regex(/href=.*?libcap[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/libcap"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "c815109fc914671fbf6bc6326380afdf6618deb1a00828c5af4b580caddf3374"
+  end
+
+  depends_on :linux
+
+  def install
+    system "make", "install", "prefix=#{prefix}", "lib=lib", "RAISE_SETFCAP=no"
+  end
+
+  test do
+    assert_match "usage", shell_output("#{sbin}/getcap 2>&1", 1)
+  end
+end
