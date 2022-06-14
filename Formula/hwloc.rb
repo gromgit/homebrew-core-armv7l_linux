0cab4033229e6c608398588ef4e5b7272f18f717
Adrian Ho <the.gromgit@gmail.com>
2022-06-14 18:40:15 +0800

    hwloc: update 2.7.1 bottle.

--- Formula/hwloc.rb
+++ Formula/hwloc.rb
@@ -11,12 +11,9 @@ class Hwloc < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "79d74dd3f15558150715098a5a639d9b08cbdf297e867bc863207bcd95ce8d0d"
-    sha256 cellar: :any,                 arm64_big_sur:  "ab3450c99e27c6282dfb38a6cc62c4298555768877eb7fd0756a8a70f5269b00"
-    sha256 cellar: :any,                 monterey:       "418855a04fc56557912cef120f882a23983fd7afd31a04078db3976b11b42788"
-    sha256 cellar: :any,                 big_sur:        "3f0ebbc902aece206e2025bbf6970a3136d291bd9d8857907049e30ab8ffdf30"
-    sha256 cellar: :any,                 catalina:       "c2706f7a282d00f815777df735d985c8ca50960c64c267d40308c85421eff331"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2b5628c3a9c15eb78961338d90e76892643995535c96a0cc7b18eb4a5aabda42"
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/hwloc"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "14969674da05a33963e68c790eb84278ce683e90fbe981f06fb52c69b7d65da0"
   end
 
   head do
