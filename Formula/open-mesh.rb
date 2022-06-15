8417cb38667d8c277351e1eaf93b03e10e8f197e
Adrian Ho <the.gromgit@gmail.com>
2022-06-16 02:42:09 +0800

    open-mesh: update 9.0 bottle.

---
+++ Formula/open-mesh.rb
@@ -0,0 +1,79 @@
+class OpenMesh < Formula
+  desc "Generic data structure to represent and manipulate polygonal meshes"
+  homepage "https://openmesh.org/"
+  url "https://www.openmesh.org/media/Releases/9.0/OpenMesh-9.0.tar.bz2"
+  sha256 "69311a75b6060993b07fef005b328ea62178c13fbb0c44773874137231510218"
+  license "BSD-3-Clause"
+  head "https://www.graphics.rwth-aachen.de:9000/OpenMesh/OpenMesh.git", branch: "master"
+
+  livecheck do
+    url "https://www.openmesh.org/download/"
+    regex(/href=.*?OpenMesh[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/open-mesh"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "d168403b710e7e40dd2ff5c779dc6d25364aa976e437c1575659f38675289e8d"
+  end
+
+  depends_on "cmake" => :build
+
+  def install
+    ENV.cxx11
+
+    mkdir "build" do
+      system "cmake", "..", "-DBUILD_APPS=OFF", *std_cmake_args, "-DCMAKE_INSTALL_RPATH=#{rpath}"
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <iostream>
+      #include <OpenMesh/Core/IO/MeshIO.hh>
+      #include <OpenMesh/Core/Mesh/PolyMesh_ArrayKernelT.hh>
+      typedef OpenMesh::PolyMesh_ArrayKernelT<>  MyMesh;
+      int main()
+      {
+          MyMesh mesh;
+          MyMesh::VertexHandle vhandle[4];
+          vhandle[0] = mesh.add_vertex(MyMesh::Point(-1, -1,  1));
+          vhandle[1] = mesh.add_vertex(MyMesh::Point( 1, -1,  1));
+          vhandle[2] = mesh.add_vertex(MyMesh::Point( 1,  1,  1));
+          vhandle[3] = mesh.add_vertex(MyMesh::Point(-1,  1,  1));
+          std::vector<MyMesh::VertexHandle>  face_vhandles;
+          face_vhandles.clear();
+          face_vhandles.push_back(vhandle[0]);
+          face_vhandles.push_back(vhandle[1]);
+          face_vhandles.push_back(vhandle[2]);
+          face_vhandles.push_back(vhandle[3]);
+          mesh.add_face(face_vhandles);
+          try
+          {
+          if ( !OpenMesh::IO::write_mesh(mesh, "triangle.off") )
+          {
+              std::cerr << "Cannot write mesh to file 'triangle.off'" << std::endl;
+              return 1;
+          }
+          }
+          catch( std::exception& x )
+          {
+          std::cerr << x.what() << std::endl;
+          return 1;
+          }
+          return 0;
+      }
+
+    EOS
+    flags = %W[
+      -I#{include}
+      -L#{lib}
+      -lOpenMeshCore
+      -lOpenMeshTools
+      --std=c++11
+      -Wl,-rpath,#{lib}
+    ]
+    system ENV.cxx, "test.cpp", "-o", "test", *flags
+    system "./test"
+  end
+end
