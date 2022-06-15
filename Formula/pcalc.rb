c6103ebe17cb24f28da0ba7690a16f3f89d292db
Adrian Ho <the.gromgit@gmail.com>
2022-06-16 04:33:26 +0800

    pcalc: update 2.2 bottle.

---
+++ Formula/pcalc.rb
@@ -0,0 +1,24 @@
+class Pcalc < Formula
+  desc "Calculator for those working with multiple bases, sizes, and close to the bits"
+  homepage "https://github.com/alt-romes/programmer-calculator"
+  url "https://github.com/alt-romes/programmer-calculator/archive/v2.2.tar.gz"
+  sha256 "d76c1d641cdb7d0b68dd30d4ef96d6ccf16cad886b01b4464bdbf3a2fa976172"
+  license "GPL-3.0-or-later"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/pcalc"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "9bcd5d111bea4d583d463729788a4cd5c922a5c41dd2bac6d817e44d9fc212b5"
+  end
+
+  uses_from_macos "ncurses"
+
+  def install
+    system "make"
+    bin.install "pcalc"
+  end
+
+  test do
+    assert_equal "Decimal: 0, Hex: 0x0, Operation:  \nDecimal: 3, Hex: 0x3, Operation:",
+      shell_output("echo \"0x1+0b1+1\nquit\" | #{bin}/pcalc -n").strip
+  end
+end
