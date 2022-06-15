dd2ce54a7688d37a339a93f2d8da31c0c9ceb005
Adrian Ho <the.gromgit@gmail.com>
2022-06-15 22:57:49 +0800

    newlisp: update 10.7.5 bottle.

---
+++ Formula/newlisp.rb
@@ -0,0 +1,45 @@
+class Newlisp < Formula
+  desc "Lisp-like, general-purpose scripting language"
+  homepage "http://www.newlisp.org/"
+  url "http://www.newlisp.org/downloads/newlisp-10.7.5.tgz"
+  sha256 "dc2d0ff651c2b275bc4af3af8ba59851a6fb6e1eaddc20ae75fb60b1e90126ec"
+
+  livecheck do
+    url "http://www.newlisp.org/index.cgi?Downloads"
+    regex(/href=.*?newlisp[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/newlisp"
+    sha256 armv7l_linux: "6cabe6a68ba29fdacadf5f24f34f950c4db1e5983c2e64095ba90e82402a3bca"
+  end
+
+  depends_on "readline"
+
+  def install
+    # Required to use our configuration
+    ENV.append_to_cflags "-DNEWCONFIG -c"
+
+    system "./configure-alt", "--prefix=#{prefix}", "--mandir=#{man}"
+    system "make"
+    system "make", "check"
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      If you have brew in a custom prefix, the included examples
+      will need to be be pointed to your newlisp executable.
+    EOS
+  end
+
+  test do
+    path = testpath/"test.lsp"
+    path.write <<~EOS
+      (println "hello")
+      (exit 0)
+    EOS
+
+    assert_equal "hello\n", shell_output("#{bin}/newlisp #{path}")
+  end
+end
