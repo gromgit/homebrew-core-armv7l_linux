88beca18968985d0e8962f4af89025daeef40227
Adrian Ho <the.gromgit@gmail.com>
2022-06-16 01:54:38 +0800

    odt2txt: update 0.5 bottle.

---
+++ Formula/odt2txt.rb
@@ -0,0 +1,30 @@
+class Odt2txt < Formula
+  desc "Convert OpenDocument files to plain text"
+  homepage "https://github.com/dstosberg/odt2txt/"
+  url "https://github.com/dstosberg/odt2txt/archive/v0.5.tar.gz"
+  sha256 "23a889109ca9087a719c638758f14cc3b867a5dcf30a6c90bf6a0985073556dd"
+  license "GPL-2.0-only"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/odt2txt"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "8975cd16442d799a958eec3f091b16530449696328b99d217d58039b77a9b75c"
+  end
+
+  uses_from_macos "zlib"
+
+  resource "sample" do
+    url "https://github.com/Turbo87/odt2txt/raw/samples/samples/sample-1.odt"
+    sha256 "78a5b17613376e50a66501ec92260d03d9d8106a9d98128f1efb5c07c8bfa0b2"
+  end
+
+  def install
+    system "make", "install", "DESTDIR=#{prefix}"
+  end
+
+  test do
+    resources.each do |r|
+      r.fetch
+      system "#{bin}/odt2txt", r.cached_download
+    end
+  end
+end
