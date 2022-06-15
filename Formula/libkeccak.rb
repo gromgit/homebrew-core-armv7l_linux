637d2eeb2135532aabe65fbeba5459cbd4224b6a
Adrian Ho <the.gromgit@gmail.com>
2022-06-15 09:34:21 +0800

    libkeccak: update 1.3.1.2 bottle.

---
+++ Formula/libkeccak.rb
@@ -0,0 +1,26 @@
+class Libkeccak < Formula
+  desc "Keccak-family hashing library"
+  homepage "https://github.com/maandree/libkeccak"
+  url "https://github.com/maandree/libkeccak/archive/1.3.1.2.tar.gz"
+  sha256 "c17df59e038f9f1b0f09aa79944ba572f5c4efcbfe8bc6bc7aae1b40f035abe9"
+  license "ISC"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/libkeccak"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "213203c7cb688808fb0c8acfe99ff1ddde303fc8e811bf3662c681cc9da51e7a"
+  end
+
+  def install
+    args = ["PREFIX=#{prefix}"]
+    args << "OSCONFIGFILE=macos.mk" if OS.mac?
+
+    system "make", "install", *args
+    pkgshare.install %w[.testfile test.c]
+  end
+
+  test do
+    cp_r pkgshare/".testfile", testpath
+    system ENV.cc, pkgshare/"test.c", "-std=c99", "-O3", "-I#{include}", "-L#{lib}", "-lkeccak", "-o", "test"
+    system "./test"
+  end
+end
