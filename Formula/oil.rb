a3ab0273c73cd981bb7fbb511d1a37d91ff4cb39
Adrian Ho <the.gromgit@gmail.com>
2022-06-16 01:59:50 +0800

    oil: update 0.10.1 bottle.

---
+++ Formula/oil.rb
@@ -0,0 +1,36 @@
+class Oil < Formula
+  desc "Bash-compatible Unix shell with more consistent syntax and semantics"
+  homepage "https://www.oilshell.org/"
+  url "https://www.oilshell.org/download/oil-0.10.1.tar.gz"
+  sha256 "c55c34a52f13eb93d4888612d95dcb451a19e474d6acd75197be055fe020593c"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://www.oilshell.org/releases.html"
+    regex(/href=.*?oil[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/oil"
+    sha256 armv7l_linux: "e57808bd3170b90b649deb5de12411d07757c6182d51ec2644a906a20e5b6ac8"
+  end
+
+  depends_on "readline"
+
+  conflicts_with "omake", because: "both install 'osh' binaries"
+
+  def install
+    system "./configure", "--prefix=#{prefix}",
+                          "--with-readline=#{Formula["readline"].opt_prefix}"
+    system "make"
+    system "./install"
+  end
+
+  test do
+    system "#{bin}/osh", "-c", "shopt -q parse_backticks"
+    assert_equal testpath.to_s, shell_output("#{bin}/osh -c 'echo `pwd -P`'").strip
+
+    system "#{bin}/oil", "-c", "shopt -u parse_equals"
+    assert_equal "bar", shell_output("#{bin}/oil -c 'var foo = \"bar\"; write $foo'").strip
+  end
+end
