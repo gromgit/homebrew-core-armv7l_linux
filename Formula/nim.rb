5e398b85cbe120352ad3d55bc360fda18698f9f1
Adrian Ho <the.gromgit@gmail.com>
2022-06-15 23:43:43 +0800

    nim: update 1.6.6 bottle.

---
+++ Formula/nim.rb
@@ -0,0 +1,67 @@
+class Nim < Formula
+  desc "Statically typed compiled systems programming language"
+  homepage "https://nim-lang.org/"
+  url "https://nim-lang.org/download/nim-1.6.6.tar.xz"
+  sha256 "67b111ce6f3861503b9fcc1cae59fc34d0122566d3ecfef3a064a2174121a452"
+  license "MIT"
+  head "https://github.com/nim-lang/Nim.git", branch: "devel"
+
+  livecheck do
+    url "https://nim-lang.org/install.html"
+    regex(/href=.*?nim[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/nim"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "ccbcbfcf2cfbb5e8d63cdf3db851186cfd64fe1e61e6ef28efbd4f7b87fc907f"
+  end
+
+  depends_on "help2man" => :build
+
+  def install
+    if build.head?
+      # this will clone https://github.com/nim-lang/csources_v1
+      # at some hardcoded revision
+      system "/bin/sh", "build_all.sh"
+      # Build a new version of the compiler with readline bindings
+      system "./koch", "boot", "-d:release", "-d:useLinenoise"
+    else
+      system "/bin/sh", "build.sh"
+      system "bin/nim", "c", "-d:release", "koch"
+      system "./koch", "boot", "-d:release", "-d:useLinenoise"
+      system "./koch", "tools"
+    end
+
+    system "./koch", "geninstall"
+    system "/bin/sh", "install.sh", prefix
+
+    system "help2man", "bin/nim", "-o", "nim.1", "-N"
+    man1.install "nim.1"
+
+    target = prefix/"nim/bin"
+    bin.install_symlink target/"nim"
+    tools = %w[nimble nimgrep nimpretty nimsuggest]
+    tools.each do |t|
+      system "help2man", buildpath/"bin"/t, "-o", "#{t}.1", "-N"
+      man1.install "#{t}.1"
+      target.install buildpath/"bin"/t
+      bin.install_symlink target/t
+    end
+  end
+
+  test do
+    (testpath/"hello.nim").write <<~EOS
+      echo("hello")
+    EOS
+    assert_equal "hello", shell_output("#{bin}/nim compile --verbosity:0 --run #{testpath}/hello.nim").chomp
+
+    (testpath/"hello.nimble").write <<~EOS
+      version = "0.1.0"
+      author = "Author Name"
+      description = "A test nimble package"
+      license = "MIT"
+      requires "nim >= 0.15.0"
+    EOS
+    assert_equal "name: \"hello\"\n", shell_output("#{bin}/nimble dump").lines.first
+  end
+end
