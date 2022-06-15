2ccc81252b73902ad2fa495748899f8a77990bd3
Adrian Ho <the.gromgit@gmail.com>
2022-06-16 00:46:21 +0800

    nspr: update 4.34 bottle.

---
+++ Formula/nspr.rb
@@ -0,0 +1,48 @@
+class Nspr < Formula
+  desc "Platform-neutral API for system-level and libc-like functions"
+  homepage "https://hg.mozilla.org/projects/nspr"
+  url "https://archive.mozilla.org/pub/nspr/releases/v4.34/src/nspr-4.34.tar.gz"
+  sha256 "beef011cd15d8f40794984d17014366513cec5719bf1a78f5e8a3e3a1cebf99c"
+  license "MPL-2.0"
+
+  livecheck do
+    url "https://ftp.mozilla.org/pub/nspr/releases/"
+    regex(%r{href=.*?v?(\d+(?:\.\d+)+)/?["' >]}i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/nspr"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "cd2c692c67a73b007e82d2fbd874ffc2fa44d7ed4aac330f81097e584a6580c0"
+  end
+
+  def install
+    ENV.deparallelize
+    cd "nspr" do
+      args = %W[
+        --disable-debug
+        --prefix=#{prefix}
+        --enable-strip
+        --with-pthreads
+        --enable-ipv6
+        --enable-macos-target=#{MacOS.version}
+        --enable-64bit
+      ]
+      system "./configure", *args
+
+      if OS.mac?
+        # Remove the broken (for anyone but Firefox) install_name
+        inreplace "config/autoconf.mk", "-install_name @executable_path/$@ ", "-install_name #{lib}/$@ "
+      end
+
+      system "make"
+      system "make", "install"
+
+      (bin/"compile-et.pl").unlink
+      (bin/"prerr.properties").unlink
+    end
+  end
+
+  test do
+    system "#{bin}/nspr-config", "--version"
+  end
+end
