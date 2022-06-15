a93ea2a7235ab0325af67f3e4309a3c8ff365ad3
Adrian Ho <the.gromgit@gmail.com>
2022-06-15 11:47:00 +0800

    libsodium: update 1.0.18_1 bottle.

---
+++ Formula/libsodium.rb
@@ -0,0 +1,50 @@
+class Libsodium < Formula
+  desc "NaCl networking and cryptography library"
+  homepage "https://libsodium.org/"
+  url "https://download.libsodium.org/libsodium/releases/libsodium-1.0.18.tar.gz"
+  sha256 "6f504490b342a4f8a4c4a02fc9b866cbef8622d5df4e5452b46be121e46636c1"
+  license "ISC"
+  revision 1
+
+  livecheck do
+    url "https://download.libsodium.org/libsodium/releases/"
+    regex(/href=.*?libsodium[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/libsodium"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "241a8b5c8a5d24cbbec0d060e47c89387355d15f016826b324007ee6f3c98b59"
+  end
+
+  head do
+    url "https://github.com/jedisct1/libsodium.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  def install
+    system "./autogen.sh" if build.head?
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make", "check"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <assert.h>
+      #include <sodium.h>
+
+      int main()
+      {
+        assert(sodium_init() != -1);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}",
+                   "-lsodium", "-o", "test"
+    system "./test"
+  end
+end
