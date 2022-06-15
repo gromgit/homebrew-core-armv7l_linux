ade3756a0d16327d73437421a2b9db767b1cf58c
Adrian Ho <the.gromgit@gmail.com>
2022-06-15 14:24:32 +0800

    lwtools: update 4.19 bottle.

---
+++ Formula/lwtools.rb
@@ -0,0 +1,44 @@
+class Lwtools < Formula
+  desc "Cross-development tools for Motorola 6809 and Hitachi 6309"
+  homepage "http://www.lwtools.ca/"
+  url "http://www.lwtools.ca/releases/lwtools/lwtools-4.19.tar.gz"
+  sha256 "427fec1571c876541895111536f3ccbd9243dd3b3d613f1a9e4b183d031ff681"
+  license "GPL-3.0-only"
+
+  livecheck do
+    url "http://www.lwtools.ca/releases/lwtools/"
+    regex(/href=.*?lwtools[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/lwtools"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "733a1179079b2310020cab409bde78bc8402189b6d209ac8954e7a1275696359"
+  end
+
+  def install
+    system "make"
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    # lwasm
+    (testpath/"foo.asm").write "  SECTION foo\n  stb $1234,x\n"
+    system "#{bin}/lwasm", "--obj", "--output=foo.obj", "foo.asm"
+
+    # lwlink
+    system "#{bin}/lwlink", "--format=raw", "--output=foo.bin", "foo.obj"
+    code = File.open("foo.bin", "rb") { |f| f.read.unpack("C*") }
+    assert_equal [0xe7, 0x89, 0x12, 0x34], code
+
+    # lwobjdump
+    dump = `#{bin}/lwobjdump foo.obj`
+    assert_equal 0, $CHILD_STATUS.exitstatus
+    assert dump.start_with?("SECTION foo")
+
+    # lwar
+    system "#{bin}/lwar", "--create", "foo.lwa", "foo.obj"
+    list = `#{bin}/lwar --list foo.lwa`
+    assert_equal 0, $CHILD_STATUS.exitstatus
+    assert list.start_with?("foo.obj")
+  end
+end
