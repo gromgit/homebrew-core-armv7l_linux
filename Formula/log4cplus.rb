924b82eaa2966b619f00b68d7970ffcba7ffc07f
Adrian Ho <the.gromgit@gmail.com>
2022-06-15 13:55:55 +0800

    log4cplus: update 2.0.7 bottle.

---
+++ Formula/log4cplus.rb
@@ -0,0 +1,49 @@
+class Log4cplus < Formula
+  desc "Logging Framework for C++"
+  homepage "https://sourceforge.net/p/log4cplus/wiki/Home/"
+  url "https://downloads.sourceforge.net/project/log4cplus/log4cplus-stable/2.0.7/log4cplus-2.0.7.tar.xz"
+  sha256 "8f74a0a5920ba044b24e2ebeb0f1e5e36d85d5c23ed48d9fe328882b16130db8"
+  license all_of: ["Apache-2.0", "BSD-2-Clause"]
+
+  livecheck do
+    url :stable
+    regex(/url=.*?log4cplus-stable.*?log4cplus[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/log4cplus"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "221cc1ca240e107edd1720733628e685318f188eb86affbecbaf30d90936b6d8"
+  end
+
+  def install
+    ENV.cxx11
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    # https://github.com/log4cplus/log4cplus/blob/65e4c3/docs/examples.md
+    (testpath/"test.cpp").write <<~EOS
+      #include <log4cplus/logger.h>
+      #include <log4cplus/loggingmacros.h>
+      #include <log4cplus/configurator.h>
+      #include <log4cplus/initializer.h>
+
+      int main()
+      {
+        log4cplus::Initializer initializer;
+        log4cplus::BasicConfigurator config;
+        config.configure();
+
+        log4cplus::Logger logger = log4cplus::Logger::getInstance(
+          LOG4CPLUS_TEXT("main"));
+        LOG4CPLUS_WARN(logger, LOG4CPLUS_TEXT("Hello, World!"));
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "-std=c++11", "-I#{include}", "-L#{lib}",
+                    "test.cpp", "-o", "test", "-llog4cplus"
+    assert_match "Hello, World!", shell_output("./test")
+  end
+end
