d82200cf15bc8efb59372796c28cbe9485a5d8b2
Adrian Ho <the.gromgit@gmail.com>
2022-06-15 09:09:26 +0800

    libhdhomerun: update 20220303 bottle.

---
+++ Formula/libhdhomerun.rb
@@ -0,0 +1,31 @@
+class Libhdhomerun < Formula
+  desc "C library for controlling SiliconDust HDHomeRun TV tuners"
+  homepage "https://www.silicondust.com/support/linux/"
+  url "https://download.silicondust.com/hdhomerun/libhdhomerun_20220303.tgz"
+  sha256 "1e54ffefc2d4893911501da31e662b9d063e6c18afe2cb5c6653325277a54a97"
+  license "LGPL-2.1-or-later"
+
+  livecheck do
+    url "https://download.silicondust.com/hdhomerun/libhdhomerun.tgz"
+    regex(/libhdhomerun[._-]v?(\d+(?:\.\d+)*)\.t/i)
+    strategy :header_match
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/libhdhomerun"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "cd1458c4fc42b96ab91a7cefcd2aa018d051b7120811d5df012f25c7ff5779eb"
+  end
+
+  def install
+    system "make"
+    bin.install "hdhomerun_config"
+    lib.install shared_library("libhdhomerun")
+    include.install Dir["hdhomerun*.h"]
+  end
+
+  test do
+    # Devices may be found or not found, with differing return codes
+    discover = pipe_output("#{bin}/hdhomerun_config discover")
+    assert_match(/no devices found|hdhomerun device|found at/, discover)
+  end
+end
