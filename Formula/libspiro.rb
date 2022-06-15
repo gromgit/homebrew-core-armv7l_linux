532c0d02c53ee9a262901fd219db3dc681443fcf
Adrian Ho <the.gromgit@gmail.com>
2022-06-15 11:58:42 +0800

    libspiro: update 20200505 bottle.

---
+++ Formula/libspiro.rb
@@ -0,0 +1,58 @@
+class Libspiro < Formula
+  desc "Library to simplify the drawing of curves"
+  homepage "https://github.com/fontforge/libspiro"
+  url "https://github.com/fontforge/libspiro/releases/download/20200505/libspiro-dist-20200505.tar.gz"
+  sha256 "06c69a1e8dcbcabcf009fd96fd90b1a244d0257246e376c2c4d57c4ea4af0e49"
+  license "GPL-3.0"
+  version_scheme 1
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/libspiro"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "32766c5e347594ff0ed43ac8ddedf24e8e25f175ecb12c2bbe6741f266b3e928"
+  end
+
+  head do
+    url "https://github.com/fontforge/libspiro.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  def install
+    if build.head?
+      system "autoreconf", "-i"
+      system "automake"
+    end
+
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <spiroentrypoints.h>
+      #include <bezctx.h>
+
+      void moveto(bezctx *bc, double x, double y, int open) {}
+      void lineto(bezctx *bc, double x, double y) {}
+      void quadto(bezctx *bc, double x1, double y1, double x2, double y2) {}
+      void curveto(bezctx *bc, double x1, double y1, double x2, double y2, double x3, double t3) {}
+      void markknot(bezctx *bc, int knot) {}
+
+      int main() {
+        int done;
+        bezctx bc = {moveto, lineto, quadto, curveto, markknot};
+        spiro_cp path[] = {
+          {-100, 0, SPIRO_G4}, {0, 100, SPIRO_G4},
+          {100, 0, SPIRO_G4}, {0, -100, SPIRO_G4}
+        };
+
+        SpiroCPsToBezier1(path, sizeof(path)/sizeof(spiro_cp), 1, &bc, &done);
+        return done == 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lspiro", "-o", "test"
+    system "./test"
+  end
+end
