b09cbb0ee76765c039aa1a5a4eae567a2fa5395a
Adrian Ho <the.gromgit@gmail.com>
2022-06-15 20:07:39 +0800

    mpgtx: update 1.3.1 bottle.

---
+++ Formula/mpgtx.rb
@@ -0,0 +1,31 @@
+class Mpgtx < Formula
+  desc "Toolbox to manipulate MPEG files"
+  homepage "https://mpgtx.sourceforge.io/"
+  url "https://downloads.sourceforge.net/project/mpgtx/mpgtx/1.3.1/mpgtx-1.3.1.tar.gz"
+  sha256 "8815e73e98b862f12ba1ef5eaaf49407cf211c1f668c5ee325bf04af27f8e377"
+  license "GPL-2.0"
+
+  livecheck do
+    url :stable
+    regex(%r{url=.*?/mpgtx[._-]v?(\d+(?:\.\d+)+(?:-\d+)?)(?:-src)?\.t}i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/mpgtx"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "817c562f16b646c59833a6bac1f04ffe900a559b8f0b138ba6ef368c771ada17"
+  end
+
+  def install
+    system "./configure", "--parachute",
+                          "--prefix=#{prefix}",
+                          "--manprefix=#{man}"
+    # Unset LFLAGS, "-s" causes the linker to crash
+    system "make", "LFLAGS="
+    # Override BSD incompatible cp flags set in makefile
+    system "make", "install", "cpflags=RP"
+  end
+
+  test do
+    system "#{bin}/mpgtx", "--version"
+  end
+end
