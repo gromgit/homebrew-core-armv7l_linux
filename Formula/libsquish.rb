eba500248a194b16850fdf531af26168dc04438f
Adrian Ho <the.gromgit@gmail.com>
2022-06-15 12:06:01 +0800

    libsquish: update 1.15_1 bottle.

---
+++ Formula/libsquish.rb
@@ -0,0 +1,42 @@
+class Libsquish < Formula
+  desc "Library for compressing images with the DXT standard"
+  homepage "https://sourceforge.net/projects/libsquish/"
+  url "https://downloads.sourceforge.net/project/libsquish/libsquish-1.15.tgz"
+  sha256 "628796eeba608866183a61d080d46967c9dda6723bc0a3ec52324c85d2147269"
+  license "MIT"
+  revision 1
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/libsquish"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "02ed32bc6dcd28fc46853cccbb6ea3f09a84f95bca53b297a3d424e4b269c66c"
+  end
+
+  depends_on "cmake" => :build
+
+  def install
+    # Static and shared libraries have to be built using separate calls to cmake.
+    args = []
+    args << "-DBUILD_SQUISH_WITH_SSE2=OFF" if Hardware::CPU.arm?
+    system "cmake", "-S", ".", "-B", "build_static", *std_cmake_args, *args
+    system "cmake", "--build", "build_static"
+    lib.install "build_static/libsquish.a"
+
+    args << "-DBUILD_SHARED_LIBS=ON"
+    system "cmake", "-S", ".", "-B", "build_shared", *std_cmake_args, *args
+    system "cmake", "--build", "build_shared"
+    system "cmake", "--install", "build_shared"
+  end
+
+  test do
+    (testpath/"test.cc").write <<~EOS
+      #include <stdio.h>
+      #include <squish.h>
+      int main(void) {
+        printf("%d", GetStorageRequirements(640, 480, squish::kDxt1));
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "-o", "test", "test.cc", "-L#{lib}", "-lsquish"
+    assert_equal "153600", shell_output("./test")
+  end
+end
