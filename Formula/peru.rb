3f103a981c7223f3b5e5c4cf695db25ae3cb16e6
Adrian Ho <the.gromgit@gmail.com>
2022-06-16 05:31:29 +0800

    peru: update 1.3.1 bottle.

---
+++ Formula/peru.rb
@@ -0,0 +1,48 @@
+class Peru < Formula
+  include Language::Python::Virtualenv
+
+  desc "Dependency retriever for version control and archives"
+  homepage "https://github.com/buildinspace/peru"
+  url "https://files.pythonhosted.org/packages/8e/c7/c451e70443c0b82440384d51f4b9517b921d4fe44172d63dc10a09da114f/peru-1.3.1.tar.gz"
+  sha256 "31cbcc3b1c0663866fcfb2065cb7ac26fda1843a3e5638f260cc0d78b3372f39"
+  license "MIT"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/peru"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "edad1e32ef4a0fd480c0e0b560e3a40bfdd7eaf656d655e4ddb9fe8b63eb226f"
+  end
+
+  depends_on "libyaml"
+  depends_on "python@3.10"
+
+  resource "docopt" do
+    url "https://files.pythonhosted.org/packages/a2/55/8f8cab2afd404cf578136ef2cc5dfb50baa1761b68c9da1fb1e4eed343c9/docopt-0.6.2.tar.gz"
+    sha256 "49b3a825280bd66b3aa83585ef59c4a8c82f2c8a522dbe754a8bc8d08c85c491"
+  end
+
+  resource "PyYAML" do
+    url "https://files.pythonhosted.org/packages/36/2b/61d51a2c4f25ef062ae3f74576b01638bebad5e045f747ff12643df63844/PyYAML-6.0.tar.gz"
+    sha256 "68fb519c14306fec9720a2a5b45bc9f0c8d1b9c72adf45c37baedfcd949c35a2"
+  end
+
+  def install
+    # Fix plugins (executed like an executable) looking for Python outside the virtualenv
+    Dir["peru/resources/plugins/**/*.py"].each do |f|
+      inreplace f, "#! /usr/bin/env python3", "#!#{libexec}/bin/python3.10"
+    end
+
+    virtualenv_install_with_resources
+  end
+
+  test do
+    (testpath/"peru.yaml").write <<~EOS
+      imports:
+        peru: peru
+      git module peru:
+        url: https://github.com/buildinspace/peru.git
+    EOS
+    system "#{bin}/peru", "sync"
+    assert_predicate testpath/".peru", :exist?
+    assert_predicate testpath/"peru", :exist?
+  end
+end
