942068cbf8ad98ebf67f2458e731895e1f42b686
Adrian Ho <the.gromgit@gmail.com>
2022-06-15 09:17:21 +0800

    libid3tag: update 0.16.1 bottle.

---
+++ Formula/libid3tag.rb
@@ -0,0 +1,43 @@
+class Libid3tag < Formula
+  desc "ID3 tag manipulation library"
+  homepage "https://www.underbit.com/products/mad/"
+  url "https://github.com/tenacityteam/libid3tag/archive/refs/tags/0.16.1.tar.gz"
+  sha256 "185a6cec84644cf1aade8397dcf76753bcb3bd85ec2111a9e1079214ed85bef0"
+  license "GPL-2.0-only"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/libid3tag"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "1b1749549436bba0501f0477e67689f7f86cb98e825641aa29becc3ad353ad99"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :test
+
+  uses_from_macos "gperf"
+  uses_from_macos "zlib"
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <id3tag.h>
+
+      int main(int n, char** c) {
+        struct id3_file *fp = id3_file_open("#{test_fixtures("test.mp3")}", ID3_FILE_MODE_READONLY);
+        struct id3_tag *tag = id3_file_tag(fp);
+        struct id3_frame *frame = id3_tag_findframe(tag, ID3_FRAME_TITLE, 0);
+        id3_file_close(fp);
+
+        return 0;
+      }
+    EOS
+
+    pkg_config_cflags = shell_output("pkg-config --cflags --libs id3tag").chomp.split
+    system ENV.cc, "test.c", *pkg_config_cflags, "-o", "test"
+    system "./test"
+  end
+end
