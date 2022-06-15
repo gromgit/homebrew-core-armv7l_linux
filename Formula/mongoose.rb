7e7de5537d8257e56923830920f9003fe09b3f2d
Adrian Ho <the.gromgit@gmail.com>
2022-06-15 17:54:35 +0800

    mongoose: update 7.7 bottle.

---
+++ Formula/mongoose.rb
@@ -0,0 +1,58 @@
+class Mongoose < Formula
+  desc "Web server build on top of Libmongoose embedded library"
+  homepage "https://github.com/cesanta/mongoose"
+  url "https://github.com/cesanta/mongoose/archive/7.7.tar.gz"
+  sha256 "4e5733dae31c3a81156af63ca9aa3a6b9b736547f21f23c3ab2f8e3f1ecc16c0"
+  license "GPL-2.0-only"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/mongoose"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "9fefdc84045b555f66afb655f3831f2dba5dc4abc9e98a51c991636a2b62c852"
+  end
+
+  depends_on "openssl@1.1"
+
+  conflicts_with "suite-sparse", because: "suite-sparse vendors libmongoose.dylib"
+
+  def install
+    # No Makefile but is an expectation upstream of binary creation
+    # https://github.com/cesanta/mongoose/issues/326
+    cd "examples/http-server" do
+      system "make", "mongoose_mac", "PROG=mongoose_mac"
+      bin.install "mongoose_mac" => "mongoose"
+    end
+
+    system ENV.cc, "-dynamiclib", "mongoose.c", "-o", "libmongoose.dylib" if OS.mac?
+    if OS.linux?
+      system ENV.cc, "-fPIC", "-c", "mongoose.c"
+      system ENV.cc, "-shared", "-Wl,-soname,libmongoose.so", "-o", "libmongoose.so", "mongoose.o", "-lc", "-lpthread"
+    end
+    lib.install shared_library("libmongoose")
+    include.install "mongoose.h"
+    pkgshare.install "examples"
+    doc.install Dir["docs/*"]
+  end
+
+  test do
+    (testpath/"hello.html").write <<~EOS
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>Homebrew</title>
+        </head>
+        <body>
+          <p>Hi!</p>
+        </body>
+      </html>
+    EOS
+
+    begin
+      pid = fork { exec "#{bin}/mongoose" }
+      sleep 2
+      assert_match "Hi!", shell_output("curl http://localhost:8000/hello.html")
+    ensure
+      Process.kill("SIGINT", pid)
+      Process.wait(pid)
+    end
+  end
+end
