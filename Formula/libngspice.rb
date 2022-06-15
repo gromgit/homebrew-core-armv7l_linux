2e4e38cd056b50bad0d9b6cfde75121261afec42
Adrian Ho <the.gromgit@gmail.com>
2022-06-15 10:29:36 +0800

    libngspice: update 37 bottle.

---
+++ Formula/libngspice.rb
@@ -0,0 +1,68 @@
+class Libngspice < Formula
+  desc "Spice circuit simulator as shared library"
+  homepage "https://ngspice.sourceforge.io/"
+
+  stable do
+    url "https://downloads.sourceforge.net/project/ngspice/ng-spice-rework/37/ngspice-37.tar.gz"
+    sha256 "9beea6741a36a36a70f3152a36c82b728ee124c59a495312796376b30c8becbe"
+
+    # Fix -flat_namespace being used on Big Sur and later.
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
+      sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
+    end
+  end
+
+  livecheck do
+    formula "ngspice"
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/libngspice"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "59ebd434064a557577a9031a85e7e3a81d3470af1ae23b94b113374725880f16"
+  end
+
+  head do
+    url "https://git.code.sf.net/p/ngspice/ngspice.git", branch: "master"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "bison" => :build
+    depends_on "flex" => :build
+    depends_on "libtool" => :build
+  end
+
+  def install
+    system "./autogen.sh" if build.head?
+
+    args = %W[
+      --disable-debug
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --with-ngshared
+      --enable-cider
+      --enable-xspice
+    ]
+
+    system "./configure", *args
+    system "make", "install"
+
+    # remove script files
+    rm_rf Dir[share/"ngspice/scripts"]
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <cstdlib>
+      #include <ngspice/sharedspice.h>
+      int ng_exit(int status, bool immediate, bool quitexit, int ident, void *userdata) {
+        return status;
+      }
+      int main() {
+        return ngSpice_Init(NULL, NULL, ng_exit, NULL, NULL, NULL, NULL);
+      }
+    EOS
+    system ENV.cc, "test.cpp", "-I#{include}", "-L#{lib}", "-lngspice", "-o", "test"
+    system "./test"
+  end
+end
