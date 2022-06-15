42455aab051cc00c34762852d6ffcc85852edba1
Adrian Ho <the.gromgit@gmail.com>
2022-06-16 00:33:45 +0800

    norm: update 1.5.9 bottle.

---
+++ Formula/norm.rb
@@ -0,0 +1,48 @@
+class Norm < Formula
+  desc "NACK-Oriented Reliable Multicast"
+  homepage "https://www.nrl.navy.mil/itd/ncs/products/norm"
+  url "https://github.com/USNavalResearchLaboratory/norm/releases/download/v1.5.9/src-norm-1.5.9.tgz"
+  sha256 "ef6d7bbb7b278584e057acefe3bc764d30122e83fa41d41d8211e39f25b6e3fa"
+  license "BSD-2-Clause"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/norm"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "4825d7e85a0d731a89b0d22634b6e9daeba6b47aa57f87c64ec2a96a3d572fc8"
+  end
+
+  depends_on "python@3.10" => :build
+
+  # Fix warning: 'visibility' attribute ignored [-Wignored-attributes]
+  # Remove in the next release
+  #
+  # Ref https://github.com/USNavalResearchLaboratory/norm/pull/27
+  patch do
+    url "https://github.com/USNavalResearchLaboratory/norm/commit/476b8bb7eba5a9ad02e094de4dce05a06584f5a0.patch?full_index=1"
+    sha256 "08f7cc7002dc1afe6834ec60d4fea5c591f88902d1e76c8c32854a732072ea56"
+  end
+
+  def install
+    system "python3", "./waf", "configure", "--prefix=#{prefix}"
+    system "python3", "./waf", "install"
+
+    include.install "include/normApi.h"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <assert.h>
+      #include <normApi.h>
+
+      int main()
+      {
+        NormInstanceHandle i;
+        i = NormCreateInstance(false);
+        assert(i != NORM_INSTANCE_INVALID);
+        NormDestroyInstance(i);
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "test.c", "-L#{lib}", "-lnorm", "-o", "test"
+    system "./test"
+  end
+end
