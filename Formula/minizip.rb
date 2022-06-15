a958b2c290b03c9594d6aa826dc0f5c0098ad703
Adrian Ho <the.gromgit@gmail.com>
2022-06-15 16:41:00 +0800

    minizip: update 1.2.12 bottle.

---
+++ Formula/minizip.rb
@@ -0,0 +1,59 @@
+class Minizip < Formula
+  desc "C library for zip/unzip via zLib"
+  homepage "https://www.winimage.com/zLibDll/minizip.html"
+  url "https://zlib.net/zlib-1.2.12.tar.gz"
+  mirror "https://downloads.sourceforge.net/project/libpng/zlib/1.2.12/zlib-1.2.12.tar.gz"
+  sha256 "91844808532e5ce316b3c010929493c0244f3d37593afd6de04f71821d5136d9"
+  license "Zlib"
+
+  livecheck do
+    formula "zlib"
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/minizip"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "3dfdfec49534613284f825f03bd97c22de46ce777bf8e5c4b7e931b3fcf02bcd"
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+
+  uses_from_macos "zlib"
+
+  conflicts_with "minizip-ng",
+    because: "both install a `libminizip.a` library"
+
+  # Patch for configure issue
+  # https://github.com/madler/zlib/pull/607
+  patch do
+    url "https://github.com/madler/zlib/commit/05796d3d8d5546cf1b4dfe2cd72ab746afae505d.patch?full_index=1"
+    sha256 "68573842f1619bb8de1fa92071e38e6e51b8df71371e139e4e96be19dd7e9694"
+  end
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+
+    cd "contrib/minizip" do
+      if OS.mac?
+        # edits to statically link to libz.a
+        inreplace "Makefile.am" do |s|
+          s.sub! "-L$(zlib_top_builddir)", "$(zlib_top_builddir)/libz.a"
+          s.sub! "-version-info 1:0:0 -lz", "-version-info 1:0:0"
+          s.sub! "libminizip.la -lz", "libminizip.la"
+        end
+      end
+      system "autoreconf", "-fi"
+      system "./configure", "--prefix=#{prefix}"
+      system "make", "install"
+    end
+  end
+
+  def caveats
+    <<~EOS
+      Minizip headers installed in 'minizip' subdirectory, since they conflict
+      with the venerable 'unzip' library.
+    EOS
+  end
+end
