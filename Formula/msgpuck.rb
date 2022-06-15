14d70d38c5464bf28ce2d66b48e0c9dcbf73848e
Adrian Ho <the.gromgit@gmail.com>
2022-06-15 20:45:05 +0800

    msgpuck: update 2.0 bottle.

---
+++ Formula/msgpuck.rb
@@ -0,0 +1,57 @@
+class Msgpuck < Formula
+  desc "Simple and efficient MsgPack binary serialization library"
+  homepage "https://rtsisyk.github.io/msgpuck/"
+  url "https://github.com/rtsisyk/msgpuck/archive/2.0.tar.gz"
+  sha256 "01e6aa55d4d52a5b19f7ce9a9845506d9ab3f5abcf844a75e880b8378150a63d"
+  license "BSD-2-Clause"
+  head "https://github.com/rtsisyk/msgpuck.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/msgpuck"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "51aca367d69450941361c75d7e5e5a3bbcbbe0ec1f82e8b606380bb7aa56c0ec"
+  end
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~'EOS'
+      /* Encode and decode an array */
+      #include <assert.h>
+      #include <msgpuck.h>
+
+      int main() {
+        const char *str = "hello world";
+
+        char buf[1024];
+        char *w = buf;
+        const char *pos = buf;
+
+        w = mp_encode_array(w, 4);
+        w = mp_encode_uint(w, 10);
+        w = mp_encode_str(w, str, strlen(str));
+        w = mp_encode_bool(w, true);
+        w = mp_encode_double(w, 3.1415);
+
+        assert(mp_typeof(*pos) == MP_ARRAY );
+        mp_decode_array(&pos);
+        assert(mp_typeof(*pos) == MP_UINT  );
+        mp_next(&pos);
+        assert(mp_typeof(*pos) == MP_STR   );
+        mp_next(&pos);
+        assert(mp_typeof(*pos) == MP_BOOL  );
+        mp_next(&pos);
+        assert(mp_typeof(*pos) == MP_DOUBLE);
+        mp_next(&pos);
+
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lmsgpuck", "-o", "test"
+    system "#{testpath}/test"
+  end
+end
