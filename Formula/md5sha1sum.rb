ea6ce160203672a157c24f23f1ac8d9141e069b2
Adrian Ho <the.gromgit@gmail.com>
2022-06-15 16:04:42 +0800

    md5sha1sum: update 0.9.5_1 bottle.

---
+++ Formula/md5sha1sum.rb
@@ -0,0 +1,43 @@
+class Md5sha1sum < Formula
+  desc "Hash utilities"
+  homepage "http://microbrew.org/tools/md5sha1sum/"
+  url "http://microbrew.org/tools/md5sha1sum/md5sha1sum-0.9.5.tar.gz"
+  mirror "https://mirrorservice.org/sites/distfiles.macports.org/md5sha1sum/md5sha1sum-0.9.5.tar.gz"
+  sha256 "2fe6b4846cb3e343ed4e361d1fd98fdca6e6bf88e0bba5b767b0fdc5b299f37b"
+  revision 1
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?md5sha1sum[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/md5sha1sum"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "399f54257eb4a52f08919b0a7f9fe0ceb62cdbd35f48ee03de715898a6064132"
+  end
+
+  depends_on "openssl@1.1"
+
+  conflicts_with "coreutils", because: "both install `md5sum` and `sha1sum` binaries"
+
+  def install
+    openssl = Formula["openssl@1.1"]
+    ENV["SSLINCPATH"] = openssl.opt_include
+    ENV["SSLLIBPATH"] = openssl.opt_lib
+
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+
+    bin.install "md5sum"
+    bin.install_symlink bin/"md5sum" => "sha1sum"
+    bin.install_symlink bin/"md5sum" => "ripemd160sum"
+  end
+
+  test do
+    (testpath/"file.txt").write("This is a test file with a known checksum")
+    (testpath/"file.txt.sha1").write <<~EOS
+      52623d47c33ad3fac30c4ca4775ca760b893b963  file.txt
+    EOS
+    system "#{bin}/sha1sum", "--check", "file.txt.sha1"
+  end
+end
