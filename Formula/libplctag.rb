da2e11b058aaaf11eff9c97bfad9444b907cf3bd
Adrian Ho <the.gromgit@gmail.com>
2022-06-15 11:06:00 +0800

    libplctag: update 2.5.0 bottle.

---
+++ Formula/libplctag.rb
@@ -0,0 +1,40 @@
+class Libplctag < Formula
+  desc "Portable and simple API for accessing AB PLC data over Ethernet"
+  homepage "https://github.com/libplctag/libplctag"
+  url "https://github.com/libplctag/libplctag/archive/v2.5.0.tar.gz"
+  sha256 "4a29097d7066769b059e0f3881ea23e68d7650ceb2aab2a6b278449823cba631"
+  license any_of: ["LGPL-2.0-or-later", "MPL-2.0"]
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/libplctag"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "7ae8bfd32d33cd9bf19707b62e59b65ac53f59524e5777ee57476c09385694cf"
+  end
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdlib.h>
+      #include <libplctag.h>
+
+      int main(int argc, char **argv) {
+        int32_t tag = plc_tag_create("protocol=ab_eip&gateway=192.168.1.42&path=1,0&cpu=LGX&elem_size=4&elem_count=10&name=myDINTArray", 1);
+        if (!tag) abort();
+        plc_tag_destroy(tag);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-lplctag", "-o", "test"
+    system "./test"
+  end
+end
