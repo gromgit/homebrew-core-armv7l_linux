072e4c7b6d45a48549a3bfb6d87a2c67119fc5dd
Adrian Ho <the.gromgit@gmail.com>
2022-06-16 05:38:54 +0800

    pgbadger: update 11.8 bottle.

---
+++ Formula/pgbadger.rb
@@ -0,0 +1,56 @@
+class Pgbadger < Formula
+  desc "Log analyzer for PostgreSQL"
+  homepage "https://pgbadger.darold.net/"
+  url "https://github.com/darold/pgbadger/archive/v11.8.tar.gz"
+  sha256 "ddf4714ac058e0170359af43c22addcc0872ae17ba6f15c4e3c5a71be3b68291"
+  license "PostgreSQL"
+  head "https://github.com/darold/pgbadger.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/pgbadger"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "bba682e6c639223569ae9fbc529b368693df69657574dfbace4d62cd536a2d77"
+  end
+
+  def install
+    system "perl", "Makefile.PL", "DESTDIR=#{buildpath}"
+    system "make"
+    system "make", "install"
+
+    man_dir = if OS.mac?
+      "share/man/man1"
+    else
+      "man/man1"
+    end
+    bin.install "usr/local/bin/pgbadger"
+    man1.install "usr/local/#{man_dir}/pgbadger.1p"
+  end
+
+  def caveats
+    <<~EOS
+      You must configure your PostgreSQL server before using pgBadger.
+      Edit postgresql.conf (in #{var}/postgres if you use Homebrew's
+      PostgreSQL), set the following parameters, and restart PostgreSQL:
+
+        log_destination = 'stderr'
+        log_line_prefix = '%t [%p]: [%l-1] user=%u,db=%d '
+        log_statement = 'none'
+        log_duration = off
+        log_min_duration_statement = 0
+        log_checkpoints = on
+        log_connections = on
+        log_disconnections = on
+        log_lock_waits = on
+        log_temp_files = 0
+        lc_messages = 'C'
+    EOS
+  end
+
+  test do
+    (testpath/"server.log").write <<~EOS
+      LOG:  autovacuum launcher started
+      LOG:  database system is ready to accept connections
+    EOS
+    system bin/"pgbadger", "-f", "syslog", "server.log"
+    assert_predicate testpath/"out.html", :exist?
+  end
+end
