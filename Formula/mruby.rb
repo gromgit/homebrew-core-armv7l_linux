98bae68615cac93abdaa931136698fb9c1d4c510
Adrian Ho <the.gromgit@gmail.com>
2022-06-15 20:29:13 +0800

    mruby: update 3.1.0 bottle.

---
+++ Formula/mruby.rb
@@ -0,0 +1,41 @@
+class Mruby < Formula
+  desc "Lightweight implementation of the Ruby language"
+  homepage "https://mruby.org/"
+  url "https://github.com/mruby/mruby/archive/3.1.0.tar.gz"
+  sha256 "64ce0a967028a1a913d3dfc8d3f33b295332ab73be6f68e96d0f675f18c79ca8"
+  license "MIT"
+  head "https://github.com/mruby/mruby.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/mruby"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "f854ff603341ec788a6096b317a32d34afe269ed175975d8676b323806c90848"
+  end
+
+  depends_on "bison" => :build
+  uses_from_macos "ruby" => :build
+
+  on_linux do
+    depends_on "readline"
+  end
+
+  def install
+    cp "build_config/default.rb", buildpath/"homebrew.rb"
+    inreplace buildpath/"homebrew.rb",
+      "conf.gembox 'default'",
+      "conf.gembox 'full-core'"
+    ENV["MRUBY_CONFIG"] = buildpath/"homebrew.rb"
+
+    system "make"
+
+    cd "build/host/" do
+      lib.install Dir["lib/*.a"]
+      prefix.install %w[bin mrbgems mrblib]
+    end
+
+    prefix.install "include"
+  end
+
+  test do
+    system "#{bin}/mruby", "-e", "true"
+  end
+end
