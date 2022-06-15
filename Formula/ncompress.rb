ac949be92737de4bae62ff2769a0e62830d5786e
Adrian Ho <the.gromgit@gmail.com>
2022-06-15 21:58:36 +0800

    ncompress: update 5.0 bottle.

---
+++ Formula/ncompress.rb
@@ -0,0 +1,30 @@
+class Ncompress < Formula
+  desc "Fast, simple LZW file compressor"
+  homepage "https://github.com/vapier/ncompress"
+  url "https://github.com/vapier/ncompress/archive/v5.0.tar.gz"
+  sha256 "96ec931d06ab827fccad377839bfb91955274568392ddecf809e443443aead46"
+  license "Unlicense"
+  head "https://github.com/vapier/ncompress.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/ncompress"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "464a0bbfafa4420b42a5f29b02c2efbc650f9ea3babc515d2e4a49acef787ca3"
+  end
+
+  keg_only :provided_by_macos
+
+  def install
+    system "make", "install", "BINDIR=#{bin}", "MANDIR=#{man1}"
+  end
+
+  test do
+    Pathname.new("hello").write "Hello, world!"
+    system "#{bin}/compress", "-f", "hello"
+    assert_match "Hello, world!", shell_output("#{bin}/compress -cd hello.Z")
+  end
+end
