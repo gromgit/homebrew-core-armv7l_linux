c1c889004b9aa700059bcfd3b599e3696fe2a726
Adrian Ho <the.gromgit@gmail.com>
2022-06-16 04:46:02 +0800

    pcapplusplus: update 22.05 bottle.

---
+++ Formula/pcapplusplus.rb
@@ -0,0 +1,51 @@
+class Pcapplusplus < Formula
+  desc "C++ network sniffing, packet parsing and crafting framework"
+  homepage "https://pcapplusplus.github.io"
+  url "https://github.com/seladb/PcapPlusPlus/archive/v22.05.tar.gz"
+  sha256 "5f299c4503bf5d3c29f82b8d876a19be7dea29c2aadcb52f2f3b394846c21da9"
+  license "Unlicense"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/pcapplusplus"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "3dae0bfd7c48112e9f681ea873eaeb6d7a0880538fa8e17335a32f58ff4ab4ac"
+  end
+
+  uses_from_macos "libpcap"
+
+  def install
+    os = OS.mac? ? "mac_os_x" : OS.kernel_name.downcase
+    system "./configure-#{os}.sh", "--install-dir", prefix
+
+    # library requires to run 'make all' and
+    # 'make install' in two separate commands.
+    system "make", "all"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include "stdlib.h"
+      #include "PcapLiveDeviceList.h"
+      int main() {
+        const std::vector<pcpp::PcapLiveDevice*>& devList =
+          pcpp::PcapLiveDeviceList::getInstance().getPcapLiveDevicesList();
+        if (devList.size() > 0) {
+          if (devList[0]->getName() == "")
+            return 1;
+          return 0;
+        }
+        return 0;
+      }
+    EOS
+
+    (testpath/"Makefile").write <<~EOS
+      include #{etc}/PcapPlusPlus.mk
+      all:
+      \tg++ $(PCAPPP_BUILD_FLAGS) $(PCAPPP_INCLUDES) -c -o test.o test.cpp
+      \tg++ -L#{lib} -o test test.o $(PCAPPP_LIBS)
+    EOS
+
+    system "make", "all"
+    system "./test"
+  end
+end
