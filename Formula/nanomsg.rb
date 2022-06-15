ac55b43837b3098d5ef8ae9c5dabbb6bc1daea03
Adrian Ho <the.gromgit@gmail.com>
2022-06-15 21:45:36 +0800

    nanomsg: update 1.2 bottle.

---
+++ Formula/nanomsg.rb
@@ -0,0 +1,38 @@
+class Nanomsg < Formula
+  desc "Socket library in C"
+  homepage "https://nanomsg.org/"
+  url "https://github.com/nanomsg/nanomsg/archive/1.2.tar.gz"
+  sha256 "6ef7282e833df6a364f3617692ef21e59d5c4878acea4f2d7d36e21c8858de67"
+  license "MIT"
+  head "https://github.com/nanomsg/nanomsg.git", branch: "master"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/nanomsg"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "5fa18d302817c02fa99381a2c16eba11b16dff2d9bb8afcf1b227204ef3c4abd"
+  end
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", *std_cmake_args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    bind = "tcp://127.0.0.1:#{free_port}"
+
+    fork do
+      exec "#{bin}/nanocat --rep --bind #{bind} --format ascii --data home"
+    end
+    sleep 2
+
+    output = shell_output("#{bin}/nanocat --req --connect #{bind} --format ascii --data brew")
+    assert_match "home", output
+  end
+end
