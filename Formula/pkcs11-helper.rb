84219b31d4f6e817095bf2ab29582009895c40f7
Adrian Ho <the.gromgit@gmail.com>
2022-06-16 07:12:18 +0800

    pkcs11-helper: update 1.29.0 bottle.

---
+++ Formula/pkcs11-helper.rb
@@ -0,0 +1,53 @@
+class Pkcs11Helper < Formula
+  desc "Library to simplify the interaction with PKCS#11"
+  homepage "https://github.com/OpenSC/OpenSC/wiki/pkcs11-helper"
+  url "https://github.com/OpenSC/pkcs11-helper/releases/download/pkcs11-helper-1.29.0/pkcs11-helper-1.29.0.tar.bz2"
+  sha256 "996846a3c8395e03d8c0515111dc84d82e6e3648d44ba28cb2dbbbca2d4db7d6"
+  license any_of: ["BSD-3-Clause", "GPL-2.0-or-later"]
+  head "https://github.com/OpenSC/pkcs11-helper.git", branch: "master"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+    regex(%r{href=.*?/tag/pkcs11-helper[._-]v?(\d+(?:\.\d+)+)["' >]}i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/pkcs11-helper"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "dcd662bb8aecb82fe2f8280cde0352f868c8ea5f348e6381f5ef353b65cd2b4f"
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "openssl@1.1"
+
+  def install
+    args = %W[
+      --disable-debug
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+    ]
+
+    system "autoreconf", "--verbose", "--install", "--force"
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      #include <stdlib.h>
+      #include <pkcs11-helper-1.0/pkcs11h-core.h>
+
+      int main() {
+        printf("Version: %08x", pkcs11h_getVersion ());
+        return 0;
+      }
+    EOS
+    system ENV.cc, testpath/"test.c", "-I#{include}", "-L#{lib}",
+                   "-lpkcs11-helper", "-o", "test"
+    system "./test"
+  end
+end
