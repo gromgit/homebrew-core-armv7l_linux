0c04dd4dac9ee63b5b67c715ff852e509df8ef4d
Adrian Ho <the.gromgit@gmail.com>
2022-06-15 15:12:15 +0800

    mariadb-connector-c: update 3.3.1 bottle.

---
+++ Formula/mariadb-connector-c.rb
@@ -0,0 +1,46 @@
+class MariadbConnectorC < Formula
+  desc "MariaDB database connector for C applications"
+  homepage "https://mariadb.org/download/?tab=connector&prod=connector-c"
+  url "https://downloads.mariadb.com/Connectors/c/connector-c-3.3.1/mariadb-connector-c-3.3.1-src.tar.gz"
+  mirror "https://fossies.org/linux/misc/mariadb-connector-c-3.3.1-src.tar.gz/"
+  sha256 "29993f4ae4c975662724978792d1a503b9ee760fbb194d321a754253cbe60aad"
+  license "LGPL-2.1-or-later"
+  head "https://github.com/mariadb-corporation/mariadb-connector-c.git", branch: "3.3"
+
+  # https://mariadb.org/download/ sometimes lists an older version as newest,
+  # so we check the JSON data used to populate the mariadb.com downloads page
+  # (which lists GA releases).
+  livecheck do
+    url "https://mariadb.com/downloads_data.json"
+    regex(/href=.*?mariadb-connector-c[._-]v?(\d+(?:\.\d+)+)-src\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/mariadb-connector-c"
+    sha256 armv7l_linux: "76719044f755223ca16834e6e386faa17a6ba927057f4ad072d06a21d887c347"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "openssl@1.1"
+
+  uses_from_macos "curl"
+  uses_from_macos "zlib"
+
+  conflicts_with "mariadb", because: "both install `mariadb_config`"
+
+  def install
+    args = std_cmake_args
+    args << "-DWITH_OPENSSL=On"
+    args << "-DWITH_EXTERNAL_ZLIB=On"
+    args << "-DOPENSSL_INCLUDE_DIR=#{Formula["openssl@1.1"].opt_include}"
+    args << "-DINSTALL_MANDIR=#{share}"
+    args << "-DCOMPILATION_COMMENT=Homebrew"
+
+    system "cmake", ".", *args
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/mariadb_config", "--cflags"
+  end
+end
