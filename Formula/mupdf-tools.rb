e392616cc044adba3336a269910459ee3a816c85
Adrian Ho <the.gromgit@gmail.com>
2022-06-15 21:04:57 +0800

    mupdf-tools: update 1.19.1 bottle.

---
+++ Formula/mupdf-tools.rb
@@ -0,0 +1,38 @@
+class MupdfTools < Formula
+  desc "Lightweight PDF and XPS viewer"
+  homepage "https://mupdf.com/"
+  url "https://mupdf.com/downloads/archive/mupdf-1.19.1-source.tar.xz"
+  sha256 "b5eac663fe74f33c430eda342f655cf41fa73d71610f0884768a856a82e3803e"
+  license "AGPL-3.0-or-later"
+  head "https://git.ghostscript.com/mupdf.git", branch: "master"
+
+  livecheck do
+    formula "mupdf"
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/mupdf-tools"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "d63f21607793240c2505bed65d46d70bc05710c400e83ab40c8cfefd11bd1017"
+  end
+
+  conflicts_with "mupdf",
+    because: "mupdf and mupdf-tools install the same binaries"
+
+  def install
+    system "make", "install",
+           "build=release",
+           "verbose=yes",
+           "HAVE_X11=no",
+           "HAVE_GLUT=no",
+           "CC=#{ENV.cc}",
+           "prefix=#{prefix}"
+
+    # Symlink `mutool` as `mudraw` (a popular shortcut for `mutool draw`).
+    bin.install_symlink bin/"mutool" => "mudraw"
+    man1.install_symlink man1/"mutool.1" => "mudraw.1"
+  end
+
+  test do
+    assert_match "Homebrew test", shell_output("#{bin}/mudraw -F txt #{test_fixtures("test.pdf")}")
+  end
+end
