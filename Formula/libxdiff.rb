bc5baca97419fcff3bdfd12ebfdca2cb36f7387d
Adrian Ho <the.gromgit@gmail.com>
2022-06-15 13:19:44 +0800

    libxdiff: update 0.23 bottle.

---
+++ Formula/libxdiff.rb
@@ -0,0 +1,32 @@
+class Libxdiff < Formula
+  desc "Implements diff functions for binary and text files"
+  homepage "http://www.xmailserver.org/xdiff-lib.html"
+  url "http://www.xmailserver.org/libxdiff-0.23.tar.gz"
+  sha256 "e9af96174e83c02b13d452a4827bdf47cb579eafd580953a8cd2c98900309124"
+  license "LGPL-2.1"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?libxdiff[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/libxdiff"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "e8f290b63504fd3717ebb9282d83a1a7307b4884f85f4b10ff53418e850bbae3"
+  end
+
+  on_macos do
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  def install
+    system "autoreconf", "--force", "--verbose", "--install" if OS.mac?
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--mandir=#{man}"
+    system "make", "install"
+  end
+end
