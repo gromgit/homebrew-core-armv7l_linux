1c3dff6108e9a666b9f86437612b828f68f677d2
Adrian Ho <the.gromgit@gmail.com>
2022-06-16 09:14:09 +0800

    proftpd: update 1.3.7d bottle.

---
+++ Formula/proftpd.rb
@@ -0,0 +1,50 @@
+class Proftpd < Formula
+  desc "Highly configurable GPL-licensed FTP server software"
+  homepage "http://www.proftpd.org/"
+  url "https://github.com/proftpd/proftpd/archive/v1.3.7d.tar.gz"
+  mirror "https://fossies.org/linux/misc/proftpd-1.3.7d.tar.gz"
+  mirror "https://ftp.osuosl.org/pub/blfs/conglomeration/proftpd/proftpd-1.3.7d.tar.gz"
+  version "1.3.7d"
+  sha256 "b231536e2978116801d06278e805b18e5240568d2bc921693ac7147652e267e4"
+  license "GPL-2.0-or-later"
+
+  # Proftpd uses an incrementing letter after the numeric version for
+  # maintenance releases. Versions like `1.2.3a` and `1.2.3b` are not alpha and
+  # beta respectively. Prerelease versions use a format like `1.2.3rc1`.
+  livecheck do
+    url :stable
+    strategy :github_latest
+    regex(%r{href=.*?/tag/v?(\d+(?:\.\d+)+[a-z]?)["' >]}i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/proftpd"
+    sha256 armv7l_linux: "867ef5858cd05ae1a866c98cfb7956acf0ce4280938fb2d7c30963f447b13037"
+  end
+
+  def install
+    # fixes unknown group 'nogroup'
+    # http://www.proftpd.org/docs/faq/linked/faq-ch4.html#AEN434
+    inreplace "sample-configurations/basic.conf", "nogroup", "nobody"
+
+    system "./configure", "--prefix=#{prefix}",
+                          "--sysconfdir=#{etc}",
+                          "--localstatedir=#{var}"
+    ENV.deparallelize
+    install_user = ENV["USER"]
+    install_group = `groups`.split[0]
+    system "make", "INSTALL_USER=#{install_user}", "INSTALL_GROUP=#{install_group}", "install"
+  end
+
+  service do
+    run [opt_sbin/"proftpd"]
+    keep_alive false
+    working_dir HOMEBREW_PREFIX
+    log_path "/dev/null"
+    error_log_path "/dev/null"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{opt_sbin}/proftpd -v")
+  end
+end
