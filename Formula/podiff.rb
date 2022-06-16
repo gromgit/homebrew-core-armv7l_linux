b178839fa973b4c27c5f0d9c6d42420d81cd9534
Adrian Ho <the.gromgit@gmail.com>
2022-06-16 08:15:03 +0800

    podiff: update 1.3 bottle.

---
+++ Formula/podiff.rb
@@ -0,0 +1,43 @@
+class Podiff < Formula
+  desc "Compare textual information in two PO files"
+  homepage "https://puszcza.gnu.org.ua/software/podiff/"
+  url "https://download.gnu.org.ua/pub/release/podiff/podiff-1.3.tar.gz"
+  sha256 "edfa62c7e1a45ec7e94609e41ed93589717a20b1eb8bb06d52134f2bab034050"
+  license "GPL-3.0-or-later"
+
+  livecheck do
+    url "https://download.gnu.org.ua/pub/release/podiff/"
+    regex(/href=.*?podiff[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/podiff"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "b954fc0e7d03972a49f54a90039940617cbc5d86eb6face87709893904fe4e24"
+  end
+
+  def install
+    system "make"
+    bin.install "podiff"
+    man1.install "podiff.1"
+  end
+
+  def caveats
+    <<~EOS
+      To use with git, add this to your .git/config or global git config file:
+
+        [diff "podiff"]
+        command = #{HOMEBREW_PREFIX}/bin/podiff -D-u
+
+      Then add the following line to the .gitattributes file in
+      the directory with your PO files:
+
+        *.po diff=podiff
+
+      See `man podiff` for more information.
+    EOS
+  end
+
+  test do
+    system "#{bin}/podiff", "-v"
+  end
+end
