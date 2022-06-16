3c421a44aa34436d078a3e45204d5fc48a276f7b
Adrian Ho <the.gromgit@gmail.com>
2022-06-16 08:30:50 +0800

    polyml: update 5.9 bottle.

---
+++ Formula/polyml.rb
@@ -0,0 +1,37 @@
+class Polyml < Formula
+  desc "Standard ML implementation"
+  homepage "https://www.polyml.org/"
+  url "https://github.com/polyml/polyml/archive/v5.9.tar.gz"
+  sha256 "5aa452a49f2ac0278668772af4ea0b9bf30c93457e60ff7f264c5aec2023c83e"
+  license "LGPL-2.1-or-later"
+  head "https://github.com/polyml/polyml.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/polyml"
+    sha256 armv7l_linux: "fe212883a487e21a7223776d821789cddae48609d2ddecd5be8e4e83b5eac69d"
+  end
+
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
+    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
+  end
+
+  def install
+    system "./configure", "--disable-dependency-tracking", "--disable-debug",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"hello.ml").write <<~EOS
+      let
+        fun concatWithSpace(a,b) = a ^ " " ^ b
+      in
+        TextIO.print(concatWithSpace("Hello", "World"))
+      end
+    EOS
+    assert_match "Hello World", shell_output("#{bin}/poly --script hello.ml")
+  end
+end
