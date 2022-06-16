9b0b395da491fd1dfbb2b460add2101abc73d54c
Adrian Ho <the.gromgit@gmail.com>
2022-06-16 10:54:53 +0800

    pyinstaller: update 5.1 bottle.

---
+++ Formula/pyinstaller.rb
@@ -0,0 +1,52 @@
+class Pyinstaller < Formula
+  include Language::Python::Virtualenv
+
+  desc "Bundle a Python application and all its dependencies"
+  homepage "https://pyinstaller.org/"
+  url "https://files.pythonhosted.org/packages/c9/41/fbb2f0e6e1934a47a99295d67ba178477f8809ae939c96608c711596f478/pyinstaller-5.1.tar.gz"
+  sha256 "9596c70c860cbce19537354db95b180351959b4cd14a70db6ab1d1432668c313"
+  license "GPL-2.0-or-later"
+  head "https://github.com/pyinstaller/pyinstaller.git", branch: "develop"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/pyinstaller"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "2eec1511fc7b79193f78ab7c4373c1667fabf7a6cf2aaca287d3fedbf7a8b1bc"
+  end
+
+  depends_on "python@3.10"
+
+  resource "altgraph" do
+    url "https://files.pythonhosted.org/packages/a9/f1/62830c4915178dbc6948687916603f1cd37c2c299634e4a8ee0efc9977e7/altgraph-0.17.2.tar.gz"
+    sha256 "ebf2269361b47d97b3b88e696439f6e4cbc607c17c51feb1754f90fb79839158"
+  end
+
+  resource "macholib" do
+    url "https://files.pythonhosted.org/packages/16/1b/85fd523a1d5507e9a5b63e25365e0a26410d5b6ee89082426e6ffff30792/macholib-1.16.tar.gz"
+    sha256 "001bf281279b986a66d7821790d734e61150d52f40c080899df8fefae056e9f7"
+  end
+
+  resource "pyinstaller-hooks-contrib" do
+    url "https://files.pythonhosted.org/packages/b3/8c/c390f589d257717dbb6725147dc3dff89e832c432ac59aa8c1c74030746f/pyinstaller-hooks-contrib-2022.5.tar.gz"
+    sha256 "90a05207ceea2f8c166f12c3add46e24c0ed6a78234e5f99320f8683d56e0dec"
+  end
+
+  def install
+    cd "bootloader" do
+      system "python3", "./waf", "all", "--no-universal2", "STRIP=/usr/bin/strip"
+    end
+    virtualenv_install_with_resources
+  end
+
+  test do
+    (testpath/"easy_install.py").write <<~EOS
+      """Run the EasyInstall command"""
+
+      if __name__ == '__main__':
+          from setuptools.command.easy_install import main
+          main()
+    EOS
+    system bin/"pyinstaller", "-F", "--distpath=#{testpath}/dist", "--workpath=#{testpath}/build",
+                              "#{testpath}/easy_install.py"
+    assert_predicate testpath/"dist/easy_install", :exist?
+  end
+end
