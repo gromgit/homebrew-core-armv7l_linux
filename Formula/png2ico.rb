b8d34caaadad09793d6da31245782021a71a14b0
Adrian Ho <the.gromgit@gmail.com>
2022-06-16 08:05:22 +0800

    png2ico: update 2002-12-08_1 bottle.

---
+++ Formula/png2ico.rb
@@ -0,0 +1,50 @@
+class Png2ico < Formula
+  desc "PNG to icon converter"
+  homepage "https://www.winterdrache.de/freeware/png2ico/"
+  url "https://www.winterdrache.de/freeware/png2ico/data/png2ico-src-2002-12-08.tar.gz"
+  sha256 "d6bc2b8f9dacfb8010e5f5654aaba56476df18d88e344ea1a32523bb5843b68e"
+  license "GPL-2.0-only"
+  revision 1
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?png2ico-src[._-]v?(\d+(?:[.-]\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/png2ico"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "f156b294b203bbbdf9241390a48e437ef996496d3dbbce135b5cdc681660964b"
+  end
+
+  depends_on "libpng"
+
+  # Fix build with recent clang
+  patch :DATA
+
+  def install
+    inreplace "Makefile", "g++", "$(CXX)"
+    system "make", "CPPFLAGS=#{ENV.cxxflags} #{ENV.cppflags} #{ENV.ldflags}"
+    bin.install "png2ico"
+    man1.install "doc/png2ico.1"
+  end
+
+  test do
+    system "#{bin}/png2ico", "out.ico", test_fixtures("test.png")
+    assert_predicate testpath/"out.ico", :exist?
+  end
+end
+
+__END__
+diff --git a/png2ico.cpp b/png2ico.cpp
+index 8fb87e4..9dedb97 100644
+--- a/png2ico.cpp
++++ b/png2ico.cpp
+@@ -34,6 +34,8 @@ Notes about transparent and inverted pixels:
+ #include <cstdio>
+ #include <vector>
+ #include <climits>
++#include <cstdlib>
++#include <cstring>
+
+ #if __GNUC__ > 2
+ #include <ext/hash_map>
