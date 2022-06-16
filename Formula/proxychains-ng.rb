c5e4c08ea4c866c6c512d1bcd58337c18197ed6d
Adrian Ho <the.gromgit@gmail.com>
2022-06-16 09:33:04 +0800

    proxychains-ng: update 4.16 bottle.

---
+++ Formula/proxychains-ng.rb
@@ -0,0 +1,24 @@
+class ProxychainsNg < Formula
+  desc "Hook preloader"
+  homepage "https://sourceforge.net/projects/proxychains-ng/"
+  url "https://github.com/rofl0r/proxychains-ng/archive/v4.16.tar.gz"
+  sha256 "5f66908044cc0c504f4a7e618ae390c9a78d108d3f713d7839e440693f43b5e7"
+  license "GPL-2.0-or-later"
+  head "https://github.com/rofl0r/proxychains-ng.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/proxychains-ng"
+    sha256 armv7l_linux: "7cec173361007137dddbc11e65dafb7762c6dd8623f9676bdda3fa09efb28418"
+  end
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--sysconfdir=#{etc}"
+    system "make"
+    system "make", "install"
+    system "make", "install-config"
+  end
+
+  test do
+    assert_match "config file found", shell_output("#{bin}/proxychains4 test 2>&1", 1)
+  end
+end
