e69c7b1f5f5813cf192b60aeb074279d2b1eaa39
Adrian Ho <the.gromgit@gmail.com>
2022-06-16 09:02:07 +0800

    prips: update 1.2.0 bottle.

---
+++ Formula/prips.rb
@@ -0,0 +1,28 @@
+class Prips < Formula
+  desc "Print the IP addresses in a given range"
+  homepage "https://devel.ringlet.net/sysutils/prips/"
+  url "https://devel.ringlet.net/files/sys/prips/prips-1.2.0.tar.xz"
+  sha256 "de28d8a5a619a30d0b3c8a76f9c09c3529d197f3e74b04c8aa994096ab8349d4"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url :homepage
+    regex(/current version .*?prips.*?v?(\d+(?:\.\d+)+)/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-armv7l_linux/releases/download/prips"
+    sha256 cellar: :any_skip_relocation, armv7l_linux: "cb34fee3ee5265da00591cc5f8850b601ce3ed9e8ac06adff4f1e21d214ee6b6"
+  end
+
+  def install
+    system "make"
+    bin.install "prips"
+    man1.install "prips.1"
+  end
+
+  test do
+    assert_equal "127.0.0.0\n127.0.0.1",
+      shell_output("#{bin}/prips 127.0.0.0/31").strip
+  end
+end
